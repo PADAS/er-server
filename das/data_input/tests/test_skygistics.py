23de5c92499e1927129c1cabd3241c5e162dc3b9
vkirkl <kirkl@vulcan.com>
2015-07-26 14:53:10 -0700

    sketched in skygistics satellite data fetch

---
+++ das/data_input/tests/test_skygistics.py
@@ -0,0 +1,71 @@
+import unittest
+
+from data_input.transformers.skygistics import SkygisticsSatelliteClient, SkygisticsLoginError
+
+
+class TestSkygisticsFetch(unittest.TestCase):
+    def setUp(self):
+        self.client = SkygisticsSatelliteClient()
+
+    def test_attempting_to_get_replay_count_with_no_login_fails(self):
+        with self.assertRaises(SkygisticsLoginError):
+            self.client.get_replay_data_count(None, None, None)
+
+    def test_attempting_to_get_replay_data_with_no_login_fails(self):
+        with self.assertRaises(SkygisticsLoginError):
+            self.client.get_replay_data(None, None, None, None, None)
+
+    def test_login_with_bad_user_fails(self):
+        self.assertFalse(self.client.login('user', 'password'))
+
+    def test_login_with_good_user_succeeds(self):
+        try:
+            import das.local_settings as settings
+            username = settings.SKYGISTICS_TEST['username']
+            password = settings.SKYGISTICS_TEST['password']
+            self.assertTrue(self.client.login(username, password))
+        except ImportError:
+            self.skipTest('Settings unavailable.')
+
+    def test_attempting_to_get_replay_count_with_bad_imei_fails(self):
+        try:
+            import das.local_settings as settings
+            username = settings.SKYGISTICS_TEST['username']
+            password = settings.SKYGISTICS_TEST['password']
+            self.assertTrue(self.client.login(username, password))
+
+            imei = '0000'
+            start_datetime = settings.SKYGISTICS_TEST['start_datetime']
+            end_datetime = settings.SKYGISTICS_TEST['end_datetime']
+            with self.assertRaises(SkygisticsLoginError):
+                self.client.get_replay_data_count(imei, start_datetime, end_datetime)
+        except ImportError:
+            self.skipTest('Settings unavailable.')
+
+    def test_attempting_to_get_replay_count_with_good_imei(self):
+        try:
+            import das.local_settings as settings
+            username = settings.SKYGISTICS_TEST['username']
+            password = settings.SKYGISTICS_TEST['password']
+            self.assertTrue(self.client.login(username, password))
+
+            imei = settings.SKYGISTICS_TEST['imei']
+            start_datetime = settings.SKYGISTICS_TEST['start_datetime']
+            end_datetime = settings.SKYGISTICS_TEST['end_datetime']
+            self.client.get_replay_data_count(imei, start_datetime, end_datetime)
+        except ImportError:
+            self.skipTest('Settings unavailable.')
+
+    def test_attempting_to_get_replay_with_good_imei(self):
+        try:
+            import das.local_settings as settings
+            username = settings.SKYGISTICS_TEST['username']
+            password = settings.SKYGISTICS_TEST['password']
+            self.assertTrue(self.client.login(username, password))
+
+            imei = settings.SKYGISTICS_TEST['imei']
+            start_datetime = settings.SKYGISTICS_TEST['start_datetime']
+            end_datetime = settings.SKYGISTICS_TEST['end_datetime']
+            self.client.get_replay_data(imei, start_datetime, end_datetime, skip=0, limit=100)
+        except ImportError:
+            self.skipTest('Settings unavailable.')
---
+++ das/data_input/transformers/skygistics.py
@@ -0,0 +1,124 @@
+""" fetch and transform Skygistics (AWT) data into DAS input format
+"""
+import xml.etree.ElementTree as etree
+import requests
+
+from  data_input.transformers.utils import dictify
+
+
+class SkygisticsLoginError(Exception):
+    pass
+
+
+class SkygisticsSatelliteClient:
+    def __init__(self):
+        # this is mildly ugly:  skygistics returns '0' for a failed login
+        #   but a session_id for success and session_ids may contain hyphens so the session_id must
+        #   be a "string"
+        self.session_id = '0'
+        self.host = 'http://skyq1.skygistics.com'
+        self.api = '/SkygisticsAPI/SkygisticsAPI.asmx'
+
+    def login(self, username, password):
+        """
+        GET /SkygisticsAPI/SkygisticsAPI.asmx/Login?username=string&password=string
+        sets self.session_id based on LoginResult.  0 for failure
+
+        :param username:
+        :param password:
+        :return: true for successful login, false otherwise
+        """
+        try:
+            # todo:  the username and password are in the clear here ...
+            response = requests.get('{0}{1}/Login'.format(self.host, self.api),
+                                    {
+                                        'username': username,
+                                        'password': password
+                                    })
+            # parse response content for session_id
+            self.session_id = etree.fromstring(response.text).text
+        except requests.ConnectionError as e:
+            # todo:  handle connection error, etc.
+            pass
+        except requests.Timeout as e:
+            # todo:  handle timeout
+            pass
+        return self.session_id != '0'
+
+    def get_replay_data_count(self, imei, start_date, end_date):
+        """
+        GET /SkygisticsAPI/SkygisticsAPI.asmx/GetReplayDataCount?
+            sessionid=string&imei=string&startdate=string&enddate=string
+
+        :param imei:
+        :param start_date:
+        :param end_date:
+        :return:
+        """
+        if self.session_id == '0':
+            raise SkygisticsLoginError('Client does not have a valid session_id.')
+        try:
+            # todo:  the username and password are in the clear here ...
+            response = requests.get('{0}{1}/GetReplayDataCount'.format(self.host, self.api),
+                                    {
+                                        'imei': imei,
+                                        'startdate': start_date,
+                                        'enddate': end_date,
+                                        'sessionid': self.session_id
+                                    })
+            # todo:  sad API, it returns a 500 if any param is bad or missing.
+            #   check status code and do better.
+            if response.status_code != 200:
+                raise SkygisticsLoginError('Non 200 response.')
+
+            # parse response content for session_id
+            replay_data_count = etree.fromstring(response.text).text
+        except requests.ConnectionError as e:
+            # todo:  handle connection error, etc.
+            pass
+        except requests.Timeout as e:
+            # todo:  handle timeout
+            pass
+        # todo:  finish this return
+        return False
+
+    def get_replay_data(self, imei, start_date, end_date, skip, limit):
+        """
+        GET /SkygisticsAPI/SkygisticsAPI.asmx/GetReplayData?
+            sessionid=string&imei=string&startdate=string&enddate=string&skip=int&limit=int
+
+        :param imei:
+        :param start_date:
+        :param end_date:
+        :return:
+        """
+        if self.session_id == '0':
+            raise SkygisticsLoginError('Client does not have a valid session_id.')
+        try:
+            # todo:  the username and password are in the clear here ...
+            response = requests.get('{0}{1}/GetReplayData'.format(self.host, self.api),
+                                    {
+                                        'imei': imei,
+                                        'startdate': start_date,
+                                        'enddate': end_date,
+                                        'sessionid': self.session_id,
+                                        'skip': skip,
+                                        'limit': limit
+                                    })
+            # todo:  sad API, it returns a 500 if any param is bad or missing.
+            #   check status code and do better.
+            if response.status_code != 200:
+                raise SkygisticsLoginError('Non 200 response.')
+
+            # parse response content for session_id
+            replay_data = etree.fromstring(response.text)
+            # note:  Jake's been wondering if the fix time is local rather than UTC ...
+            replay_data_dict = dictify(replay_data)
+        except requests.ConnectionError as e:
+            # todo:  handle connection error, etc.
+            pass
+        except requests.Timeout as e:
+            # todo:  handle timeout
+            pass
+        # todo:  finish this return
+        return False
