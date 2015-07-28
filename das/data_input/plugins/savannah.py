9153c1abf7f90d85f9be135a9a72334c46b79f4b
Chris Doehring <chrisdoehring@gmail.com>
2015-07-27 17:07:26 -0700

    Moving savannah plugin.

---
+++ das/data_input/plugins/savannah.py
@@ -0,0 +1,68 @@
+"""
+ Fetch and transform Savannah data into DAS input format
+"""
+
+import http.client
+# import time, datetime
+from functools import namedtuple
+
+from dateutil.parser import parse
+Fix = namedtuple('Fix', ['collar_id', 'lon', 'lat', 'ts', 'speed', 'heading', 'temperature', 'height'])
+
+class SavannahException(Exception):
+    pass
+
+import copy
+
+class SavannahClient(object):
+
+    def __init__(self):
+
+        # TODO: Move this to provider_settings.
+        self.credentials = {
+            'uid': 'ste', 'pwd': 'ndovu4'
+        }
+        self.host = "41.207.72.20"
+
+    def fetch_observations(self, collar_id, start_time, end_time=None):
+        '''
+        Fetch observations from Savannah data-source for a particular collar.
+        :param collar_id: collar_id from trackingmaster record.
+        :param start_time: unix timestamp for earliest data to fetch.
+        :param end_time: <not used>
+        :return: generator, yielding individual records.
+        '''
+
+        conn = http.client.HTTPConnection(self.host)
+
+        payload = copy.copy(self.credentials)
+        payload.update(dict(unixtime=str(start_time), collar=collar_id))
+
+        payload = ['='.join((k, v)) for k, v in payload.items()]
+        payload = '&'.join(payload)
+
+        headers = { 'accept': "*/*",
+                    'content-type': 'application/x-www-form-urlencoded'
+                    }
+
+        conn.request("POST", "/savannah/get_data.asp", payload, headers)
+
+        res = conn.getresponse()
+        if res.status == http.client.OK:
+            for line in res:
+                yield self.parse_line(line.decode('utf-8').strip())
+
+    @classmethod
+    def parse_line(cls, s):
+        dt = Fix._make(s.split(','))
+        dt = dt._replace(ts=parse(dt.ts))
+        return dt
+
+
+class SavannahTransformer(object):
+
+    def __init__(self):
+        pass
+
+    def transform(self, observation):
+        return observation
