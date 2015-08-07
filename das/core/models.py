6d4a22396d150fd8d23ddd4154a463322a0e913c
kirkl <kirkl@vulcan.com>
2015-08-07 10:52:31 -0700

    basic abstract models for timestamping and auditing

---
+++ das/core/models.py
@@ -0,0 +1,17 @@
+from django.contrib.gis.db import models
+from django.contrib.auth.models import User
+
+
+class TimestampedModel(models.Model):
+    create_datetime = models.DateTimeField(auto_now_add=True)
+    last_modified_datetime = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class AuditableModel(TimestampedModel):
+    user = models.ForeignKey(to=User)
+
+    class Meta:
+        abstract = True
