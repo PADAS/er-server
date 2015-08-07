b4f393cd15d6e2078ef7d52018baab054c825d9c
kirkl <kirkl@vulcan.com>
2015-08-07 13:51:34 -0700

    refactored fetches to use _get_text and proper strftime

--- das/data_input/tests/test_skygistics.py
+++ das/data_input/tests/test_skygistics.py
@@ -1,71 +1,102 @@
-import unittest
+import mock
+import datetime
+from django.test import TestCase
+from django.utils import timezone
 
-from data_input.plugins.skygistics import SkygisticsSatelliteClient, SkygisticsLoginError
+from data_input.plugins.plugin import DasPluginFetchError
+from observations.models import Source
+from data_input.models import PluginConf, PluginConfSource
+from data_input.plugins.skygistics import SkygisticsSatellitePlugin, SkygisticsLoginError
 
 
-class TestSkygisticsFetch(unittest.TestCase):
+class TestSkygisticsFetch(TestCase):
     def setUp(self):
-        self.client = SkygisticsSatelliteClient()
+        target = None
+
+        bad_config = PluginConf.objects.create(plugin_name='test bad plugin',
+                                               configuration={
+                                                   'credentials': {
+                                                       'username': 'bad',
+                                                       'password': 'bad'
+                                                   },
+                                                   'host': 'http://skyq1.skygistics.com',
+                                               })
+        self.bad_plugin = SkygisticsSatellitePlugin(bad_config, target)
+
+        config = PluginConf.objects.create(plugin_name='test good plugin',
+                                           configuration={
+                                               'credentials': {
+                                                   'username': 'awtian',
+                                                   'password': 'kenya'
+                                               },
+                                               'host': 'http://skyq1.skygistics.com',
+                                           })
+        source_1 = Source.objects.create(manufacturer_id='01086046SKY4213')
+        PluginConfSource.objects.create(source=source_1,
+                                        plugin_conf=config)
+        source_2 = Source.objects.create(manufacturer_id='01086046SKY0000')
+        PluginConfSource.objects.create(source=source_2,
+                                        plugin_conf=config)
+        self.plugin = SkygisticsSatellitePlugin(config, target)
 
     def test_attempting_to_get_replay_count_with_no_login_fails(self):
         with self.assertRaises(SkygisticsLoginError):
-            self.client.get_replay_data_count(None, None, None)
+            self.plugin.client._get_replay_data_count(None, None, None)
 
     def test_attempting_to_get_replay_data_with_no_login_fails(self):
         with self.assertRaises(SkygisticsLoginError):
-            self.client.get_replay_data(None, None, None, None, None)
+            self.plugin.client._get_replay_data(None, None, None, None, None)
 
     def test_login_with_bad_user_fails(self):
-        self.assertFalse(self.client.login('user', 'password'))
+        self.assertFalse(self.bad_plugin.client._login())
 
     def test_login_with_good_user_succeeds(self):
-        try:
-            import das_server.test_settings as settings
-            username = settings.SKYGISTICS_TEST['username']
-            password = settings.SKYGISTICS_TEST['password']
-            self.assertTrue(self.client.login(username, password))
-        except ImportError:
-            self.skipTest('Settings unavailable.')
+        self.assertTrue(self.plugin.client._login())
 
     def test_attempting_to_get_replay_count_with_bad_imei_fails(self):
-        try:
-            import das_server.test_settings as settings
-            username = settings.SKYGISTICS_TEST['username']
-            password = settings.SKYGISTICS_TEST['password']
-            self.assertTrue(self.client.login(username, password))
+        self.assertTrue(self.plugin.client._login())
+        imei = '0000'
+        start_datetime = datetime.date(1900, 1, 1)
+        end_datetime = datetime.date(1900, 1, 1)
+        with self.assertRaises(DasPluginFetchError):
+            self.plugin.client._get_replay_data_count(imei, start_datetime, end_datetime)
 
-            imei = '0000'
-            start_datetime = settings.SKYGISTICS_TEST['start_datetime']
-            end_datetime = settings.SKYGISTICS_TEST['end_datetime']
-            with self.assertRaises(SkygisticsLoginError):
-                self.client.get_replay_data_count(imei, start_datetime, end_datetime)
-        except ImportError:
-            self.skipTest('Settings unavailable.')
+    def test_attempting_to_get_replay_count_with_good_imei_and_old_date(self):
+        self.assertTrue(self.plugin.client._login())
 
-    def test_attempting_to_get_replay_count_with_good_imei(self):
-        try:
-            import das_server.test_settings as settings
-            username = settings.SKYGISTICS_TEST['username']
-            password = settings.SKYGISTICS_TEST['password']
-            self.assertTrue(self.client.login(username, password))
+        imei = '01086046SKY4213'
+        start_datetime = datetime.date(1900, 1, 1)
+        end_datetime = datetime.date(1900, 1, 1)
+        self.plugin.client._get_replay_data_count(imei, start_datetime, end_datetime)
 
-            imei = settings.SKYGISTICS_TEST['imei']
-            start_datetime = settings.SKYGISTICS_TEST['start_datetime']
-            end_datetime = settings.SKYGISTICS_TEST['end_datetime']
-            self.client.get_replay_data_count(imei, start_datetime, end_datetime)
-        except ImportError:
-            self.skipTest('Settings unavailable.')
+    def test_attempting_to_get_replay_with_good_imei_and_old_date(self):
+        self.assertTrue(self.plugin.client._login())
 
-    def test_attempting_to_get_replay_with_good_imei(self):
-        try:
-            import das_server.test_settings as settings
-            username = settings.SKYGISTICS_TEST['username']
-            password = settings.SKYGISTICS_TEST['password']
-            self.assertTrue(self.client.login(username, password))
+        imei = '01086046SKY4213'
+        start_datetime = datetime.date(1900, 1, 1)
+        end_datetime = datetime.date(1900, 1, 1)
+        self.plugin.client._get_replay_data(imei,
+                                            start_datetime,
+                                            end_datetime,
+                                            skip=0,
+                                            limit=10)
 
-            imei = settings.SKYGISTICS_TEST['imei']
-            start_datetime = settings.SKYGISTICS_TEST['start_datetime']
-            end_datetime = settings.SKYGISTICS_TEST['end_datetime']
-            replay_data = self.client.get_replay_data(imei, start_datetime, end_datetime, skip=0, limit=100)
-        except ImportError:
-            self.skipTest('Settings unavailable.')
+    def test_attempting_to_get_replay_count_with_good_imei_and_current_date(self):
+        self.assertTrue(self.plugin.client._login())
+
+        imei = '01086046SKY4213'
+        start_datetime = datetime.date(1900, 1, 1)
+        end_datetime = timezone.now()
+        self.plugin.client._get_replay_data_count(imei, start_datetime, end_datetime)
+
+    def test_attempting_to_get_replay_with_good_imei_and_current_date(self):
+        self.assertTrue(self.plugin.client._login())
+
+        imei = '01086046SKY4213'
+        start_datetime = datetime.date(1900, 1, 1)
+        end_datetime = timezone.now()
+        self.plugin.client._get_replay_data(imei,
+                                            start_datetime,
+                                            end_datetime,
+                                            skip=0,
+                                            limit=10)
