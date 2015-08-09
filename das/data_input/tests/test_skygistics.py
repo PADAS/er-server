f1bb577f1807f3ced12575eb571f48d7fdce6b4b
kirkl <kirkl@vulcan.com>
2015-08-08 19:17:08 -0700

    refactored fetches to use _get_text and proper strftime

--- das/data_input/tests/test_skygistics.py
+++ das/data_input/tests/test_skygistics.py
@@ -6,14 +6,22 @@ from django.utils import timezone
 from data_input.plugins.plugin import DasPluginFetchError
 from observations.models import Source
 from data_input.models import PluginConf, PluginConfSource
-from data_input.plugins.skygistics import SkygisticsSatellitePlugin, SkygisticsLoginError
+from data_input.plugins.skygistics import SkygisticsSatellitePlugin, SkygisticsLoginError, \
+    DasPluginConfigurationError
+
+
+class TestSkygisticsInit(TestCase):
+    def test_plugin_init_with_no_config_and_no_target(self):
+        config = None
+        target = None
+        with self.assertRaises(DasPluginConfigurationError):
+            SkygisticsSatellitePlugin(config, target)
 
 
 class TestSkygisticsFetch(TestCase):
     def setUp(self):
         target = None
-
-        bad_config = PluginConf.objects.create(plugin_name='test bad plugin',
+        bad_config = PluginConf.objects.create(plugin_name='test bad sky login',
                                                configuration={
                                                    'credentials': {
                                                        'username': 'bad',
@@ -23,7 +31,7 @@ class TestSkygisticsFetch(TestCase):
                                                })
         self.bad_plugin = SkygisticsSatellitePlugin(bad_config, target)
 
-        config = PluginConf.objects.create(plugin_name='test good plugin',
+        config = PluginConf.objects.create(plugin_name='test good sky login',
                                            configuration={
                                                'credentials': {
                                                    'username': 'awtian',
@@ -31,12 +39,6 @@ class TestSkygisticsFetch(TestCase):
                                                },
                                                'host': 'http://skyq1.skygistics.com',
                                            })
-        source_1 = Source.objects.create(manufacturer_id='01086046SKY4213')
-        PluginConfSource.objects.create(source=source_1,
-                                        plugin_conf=config)
-        source_2 = Source.objects.create(manufacturer_id='01086046SKY0000')
-        PluginConfSource.objects.create(source=source_2,
-                                        plugin_conf=config)
         self.plugin = SkygisticsSatellitePlugin(config, target)
 
     def test_attempting_to_get_replay_count_with_no_login_fails(self):
@@ -54,7 +56,7 @@ class TestSkygisticsFetch(TestCase):
         self.assertTrue(self.plugin.client._login())
 
     def test_attempting_to_get_replay_count_with_bad_imei_fails(self):
-        self.assertTrue(self.plugin.client._login())
+        self.assertTrue(self.plugin.client._login(), 'login failed: no testing.')
         imei = '0000'
         start_datetime = datetime.date(1900, 1, 1)
         end_datetime = datetime.date(1900, 1, 1)
@@ -62,7 +64,7 @@ class TestSkygisticsFetch(TestCase):
             self.plugin.client._get_replay_data_count(imei, start_datetime, end_datetime)
 
     def test_attempting_to_get_replay_count_with_good_imei_and_old_date(self):
-        self.assertTrue(self.plugin.client._login())
+        self.assertTrue(self.plugin.client._login(), 'login failed: no testing.')
 
         imei = '01086046SKY4213'
         start_datetime = datetime.date(1900, 1, 1)
@@ -70,7 +72,7 @@ class TestSkygisticsFetch(TestCase):
         self.plugin.client._get_replay_data_count(imei, start_datetime, end_datetime)
 
     def test_attempting_to_get_replay_with_good_imei_and_old_date(self):
-        self.assertTrue(self.plugin.client._login())
+        self.assertTrue(self.plugin.client._login(), 'login failed: no testing.')
 
         imei = '01086046SKY4213'
         start_datetime = datetime.date(1900, 1, 1)
@@ -82,7 +84,7 @@ class TestSkygisticsFetch(TestCase):
                                             limit=10)
 
     def test_attempting_to_get_replay_count_with_good_imei_and_current_date(self):
-        self.assertTrue(self.plugin.client._login())
+        self.assertTrue(self.plugin.client._login(), 'login failed: no testing.')
 
         imei = '01086046SKY4213'
         start_datetime = datetime.date(1900, 1, 1)
@@ -90,7 +92,7 @@ class TestSkygisticsFetch(TestCase):
         self.plugin.client._get_replay_data_count(imei, start_datetime, end_datetime)
 
     def test_attempting_to_get_replay_with_good_imei_and_current_date(self):
-        self.assertTrue(self.plugin.client._login())
+        self.assertTrue(self.plugin.client._login(), 'login failed: no testing.')
 
         imei = '01086046SKY4213'
         start_datetime = datetime.date(1900, 1, 1)
@@ -100,3 +102,23 @@ class TestSkygisticsFetch(TestCase):
                                             end_datetime,
                                             skip=0,
                                             limit=10)
+
+
+class TestSkygisticsTransform(TestCase):
+    def setUp(self):
+        target = None
+        config = PluginConf.objects.create(plugin_name='test good plugin',
+                                           configuration={
+                                               'credentials': {
+                                                   'username': 'awtian',
+                                                   'password': 'kenya'
+                                               },
+                                               'host': 'http://skyq1.skygistics.com',
+                                           })
+        source_1 = Source.objects.create(manufacturer_id='01086046SKY4213')
+        PluginConfSource.objects.create(source=source_1,
+                                        plugin_conf=config)
+        source_2 = Source.objects.create(manufacturer_id='01086046SKY0000')
+        PluginConfSource.objects.create(source=source_2,
+                                        plugin_conf=config)
+        self.plugin = SkygisticsSatellitePlugin(config, target)
