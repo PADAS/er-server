0ef38ea2618efd57793011962fb2d9f1251f22f0
Chris Jones <chrisj@vulcan.com>
2015-08-11 09:17:11 -0700

    return a list of sources for a subject

--- das/api/views.py
+++ das/api/views.py
@@ -1,46 +1,119 @@
 import logging
 import datetime
+import simplejson as json
 from django.views.generic import View
-from django.http import Http404, JsonResponse
+from django.http import HttpResponse
 import dateutil.parser
-from observations.models import Subject, Observation, SubjectSource
+import pytz
+from observations.models import Subject, Observation, SubjectSource, Source
 from das_server import utils
 
 logger = logging.getLogger(__name__)
 
 
-class SubjectsView(View):
+def default_since():
+    """default value for since
+    last 30 days is the default
+    """
+    return datetime.datetime.now(pytz.utc) - datetime.timedelta(days=30)
+
+
+class ApiJsonResponse(HttpResponse):
+    """
+    Custom data payload for the API
+    returns error, data
+    """
+
+    def __init__(self, data, encoder=utils.ExtendedJSONEncoder, **kwargs):
+        kwargs.setdefault('content_type', 'application/json')
+        result = {'data': data, 'status': {'code': 200, 'message': 'OK'}}
+        result = json.dumps(result, cls=encoder)
+        super(ApiJsonResponse, self).__init__(content=result, **kwargs)
+
+
+class ApiError(HttpResponse):
+    """
+    Our custom error, that returns payload as JSON
+    """
+
+    def __init__(self, status_code, encoder=utils.ExtendedJSONEncoder, **kwargs):
+        kwargs.setdefault('content_type', 'application/json')
+        self.status_code = status_code
+        super(ApiError, self).__init__(**kwargs)
+        result = {'status': {'code': self.status_code, 'message': self.reason_phrase}}
+        self.content = json.dumps(result, cls=encoder)
+
+
+class SourceBaseView(View):
+    fields = ('id', 'source_type', 'manufacturer_id', 'model_name', 'additional',)
+
+
+class SubjectBaseView(View):
     fields = ('id', 'name')
+    subject_id = None
+    _subject = None
+
+    @property
+    def subject(self):
+        if not self._subject:
+            try:
+                _subject = Subject.objects.get(id=self.subject_id)
+            except Subject.DoesNotExist:
+                return ApiError(404)
+        return _subject
+
+
+class SubjectsView(View):
     def get(self, request):
         subjects = Subject.objects.all()
         result = []
         for subject in subjects:
-            result.append({k: getattr(subject, k) for k in self.fields})
-        return JsonResponse(result, encoder=utils.ExtendedJSONEncoder, safe=False)
+            result.append({k: getattr(subject, k) for k in SubjectBaseView.fields})
+        return ApiJsonResponse(result)
 
 
-class SubjectSourceTrackView(View):
-    def get(self, request, id, source_id):
-        try:
-            subject = Subject.objects.get(id=id)
-        except Subject.DoesNotExist:
-            return Http404
+class SubjectView(SubjectBaseView):
+    def get(self, request, subject_id):
+        self.subject_id = subject_id
+        result = {k: getattr(self.subject, k) for k in self.fields}
+        return ApiJsonResponse(result)
 
-        since = request.GET.get('since', None)
-        if since:
+
+class SubjectSourcesView(SubjectBaseView):
+    def get(self, request, subject_id):
+        self.subject_id = subject_id
+
+        result = []
+        s_sources = SubjectSource.objects.get_subject_sources(self.subject)
+        for subject_source in s_sources:
+            source = Source.objects.get(id=subject_source.source_id)
+            source = {k: getattr(source, k) for k in SourceBaseView.fields}
+            source['assigned_range'] = subject_source.assigned_range
+            result.append(source)
+
+        return ApiJsonResponse(result)
+
+
+class SubjectSourceTrackView(SubjectBaseView):
+    def get(self, request, subject_id, source_id):
+        self.subject_id = subject_id
+
+        since = request.GET.get('since', default_since())
+        if isinstance(since, str):
             since = dateutil.parser.parse(since)
+
         until = request.GET.get('until', None)
         if until:
             until = dateutil.parser.parse(until)
 
-        color = subject.additional.get('rgb', None)
+        color = self.subject.additional.get('rgb', None)
         if color:
             color = "#" + "".join(color.split(','))
 
-        sds = SubjectSource.objects.filter(subject_id=id)
+        sds = SubjectSource.objects.get_subject_source(self.subject, source_id)
         if not sds:
-            raise Http404
-        observations = Observation.objects.get_source_range_observations(sds, since, until, timespan)
+            return ApiError(404)
+        observations = Observation.objects.get_source_range_observations(sds, since, until)
         coordinates = []
         times = []
         for ob in observations:
@@ -54,14 +127,14 @@ class SubjectSourceTrackView(View):
             },
             "type": "Feature",
             "properties": {
-                "name": subject.name,
+                "name": self.subject.name,
             },
 
         }
         if color:
             feature['style'] = {
-                "color": color,
-                "iconUrl": "http://107.21.94.89/Images/AnimalIcons/Elephant_Male.png",
+                "color": self.subject.color,
+                "iconUrl": self.subject.image_url,
                 "opacity": 1
             }
         #see https://github.com/mapbox/geojson-coordinate-properties
@@ -69,30 +142,25 @@ class SubjectSourceTrackView(View):
 
         result = utils.empty_geojson_featurecollection()
         result['features'].append(feature)
-        return JsonResponse(result, encoder=utils.ExtendedJSONEncoder)
+        return ApiJsonResponse(result)
 
-class SubjectTracksView(View):
-    def get(self, request, id):
-        try:
-            subject = Subject.objects.get(id=id)
-        except Subject.DoesNotExist:
-            return Http404
 
-        since = request.GET.get('since', None)
-        if since:
+class SubjectTracksView(SubjectBaseView):
+    def get(self, request, subject_id):
+        self.subject_id = subject_id
+
+        since = request.GET.get('since', default_since())
+        if isinstance(since, str):
             since = dateutil.parser.parse(since)
+
         until = request.GET.get('until', None)
         if until:
             until = dateutil.parser.parse(until)
 
-        color = subject.additional.get('rgb', None)
-        if color:
-            color = "#" + "".join(color.split(','))
-
-        sds = SubjectSource.objects.filter(subject_id=id)
+        sds = SubjectSource.objects.filter(subject_id=subject_id)
         if not sds:
-            raise Http404
-        observations = Observation.objects.get_source_range_observations(sds, since, until, timespan)
+            return ApiError(404)
+        observations = Observation.objects.get_source_range_observations(sds, since, until)
         coordinates = []
         times = []
         for ob in observations:
@@ -106,14 +174,15 @@ class SubjectTracksView(View):
             },
             "type": "Feature",
             "properties": {
-                "name": subject.name,
+                "name": self.subject.name,
             },
 
         }
-        if color:
+
+        if hasattr(self.subject, 'color'):
             feature['style'] = {
-                "color": color,
-                "iconUrl": "http://107.21.94.89/Images/AnimalIcons/Elephant_Male.png",
+                "color": self.subject.color,
+                "iconUrl": self.subject.image_url,
                 "opacity": 1
             }
         #see https://github.com/mapbox/geojson-coordinate-properties
@@ -121,7 +190,5 @@ class SubjectTracksView(View):
 
         result = utils.empty_geojson_featurecollection()
         result['features'].append(feature)
-        return JsonResponse(result, encoder=utils.ExtendedJSONEncoder)
 
-
-
+        return ApiJsonResponse(result)
