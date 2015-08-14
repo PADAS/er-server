21176f01fbcd100b6781e54efc23165ee31e1dd9
Chris Jones <chrisj@vulcan.com>
2015-08-14 14:30:09 -0700

    specify path to startup in wsgi.py

--- das/das_server/wsgi.py
+++ das/das_server/wsgi.py
@@ -14,7 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "das_server.settings")
 
 # Run background jobs
-from .startup import run
+from das_server.startup import run
 run()
 
 application = get_wsgi_application()
