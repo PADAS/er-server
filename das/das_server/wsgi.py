a207ece2c3df5130a54826db104d7aba74e42cda
Chris Jones <chrisj@vulcan.com>
2015-08-14 09:48:08 -0700

    have an elasticbeanstalk dev instance described.

--- das/das_server/wsgi.py
+++ das/das_server/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "das.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "das_server.settings")
 
 # Run background jobs
 from .startup import run
