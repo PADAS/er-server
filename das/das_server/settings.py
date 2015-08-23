14785574f3dcb72869623c66f589ffe9342ec533
vkirkl <kirkl@vulcan.com>
2015-08-22 17:24:35 -0700

    setting raster aside for a short while

--- das/das_server/settings.py
+++ das/das_server/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.gis',
     'djgeojson',
-    'raster',
+    # 'raster',
     'observations',
     'api',
     'data_input',
