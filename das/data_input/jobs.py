8fa9ad95e937ad43a48b174ba76ba01ff6b6255f
vkirkl <kirkl@vulcan.com>
2015-09-16 15:06:08 -0700

    add Skygistics to jobs.

--- das/data_input/jobs.py
+++ das/data_input/jobs.py
@@ -2,6 +2,8 @@ from data_input.models import PluginConf
 from data_input.plugins.savanna import SavannaPlugin, SavannaTarget
 from data_input.plugins.firms import FirmsPlugin, FirmsTarget
 from data_input.plugins.inreach import InreachPlugin, InreachTarget
+from data_input.plugins.skygistics import SkygisticsSatellitePlugin, SkygisticsTarget
+
 
 def run_savanna():
     '''
@@ -18,6 +20,7 @@ def run_savanna():
         sp = SavannaPlugin(pc, target=consumer)
         sp.execute()
 
+
 def run_firms():
     print('Running firms job')
     try:
@@ -29,8 +32,9 @@ def run_firms():
         sp = FirmsPlugin(pc, target=consumer)
         sp.execute()
 
+
 def run_inreach():
-    print('Running firms job')
+    print('Running inreach job')
     try:
         pc = PluginConf.objects.get(plugin_name='inreach')
     except Exception:
@@ -40,3 +44,14 @@ def run_inreach():
         sp = InreachPlugin(pc, target=consumer)
         sp.execute()
 
+
+def run_skygistics():
+    print('Running skygistics satellite job')
+    try:
+        pc = PluginConf.objects.get(plugin_name='skygistics')
+    except Exception:
+        raise Exception("Looks like data hasn't been loaded for plugin_conf.")
+
+    with SkygisticsTarget() as consumer:
+        sp = SkygisticsSatellitePlugin(pc, target=consumer)
+        sp.execute()
