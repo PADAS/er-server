217446ef2835720a52e8f9614fecb4e6b978c99d
Chris Jones <chrisj@vulcan.com>
2015-09-23 13:34:59 -0700

    fix static directories for production.

--- das/das_server/settings.py
+++ das/das_server/settings.py
@@ -197,6 +197,8 @@ TIME_ZONE = 'UTC'
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "www", "static")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'api', 'static'),)
 
 CORS_URLS_REGEX = r'^/api/.*$'
 
