322d476e7affe41c687a801676132787377a064d
Chris Doehring <chrisdoehring@gmail.com>
2015-10-01 09:12:43 -0700

    Updating schedule for data_input.

--- das/data_input/startup.py
+++ das/data_input/startup.py
@@ -15,10 +15,10 @@ def add_scheduled_jobs(scheduler):
     '''
     Add jobs to schedule.
     '''
-    scheduler.add_job(run_savanna, id='savanna_import', trigger='cron', minute='*/5', replace_existing=True)
+    scheduler.add_job(run_savanna, id='savanna_import', trigger='cron', minute='*/17', replace_existing=True)
     scheduler.add_job(run_firms, id='firms_import', trigger='cron', minute='*/99', replace_existing=True)
-    scheduler.add_job(run_inreach, id='inreach_import', trigger='cron', minute='*/7', replace_existing=True)
-    scheduler.add_job(heartbeat, id='heartbeat', trigger='cron', minute='*', replace_existing=True)
+    scheduler.add_job(run_inreach, id='inreach_import', trigger='cron', minute='*/13', replace_existing=True)
+    scheduler.add_job(heartbeat, id='heartbeat', trigger='cron', minute='*/10', replace_existing=True)
 
 def start_scheduler():
     '''
