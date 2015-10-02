f4de9966a2d1f0f8a7ac25b5aa88ddb77c8d35b6
Chris Jones <chrisj@vulcan.com>
2015-10-02 10:02:03 -0700

    server index.html

--- das/das_server/urls.py
+++ das/das_server/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 import django.conf.urls
 from django.contrib import admin
 import oauth2_provider.views as oauth2_views
+from das_server import views
 
 urlpatterns = [
     url(r'^api/v1.0/', include('api.urls')),
@@ -25,6 +26,7 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^oauth2/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     url(r'^oauth2/token$', oauth2_views.TokenView.as_view(), name="token"),
+    url(r'^$', views.index),
 ]
 
 # give the api a chance to override and return json
