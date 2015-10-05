d4eda943e2160942eb0ad654b87f46e7bbce58da
Chris Jones <chrisj@vulcan.com>
2015-10-05 15:37:33 -0700

    fix missing curly bracket

--- das/das_server/settings.py
+++ das/das_server/settings.py
@@ -265,6 +265,7 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
         'LOCATION': '/tmp/django_cache',
     }
+}
 
 '''
 Associate a plugin name with a plugin-configuration dict that will override the plugin's configuration in the database.
