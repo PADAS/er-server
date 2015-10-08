1eb6742be4c097ebe055d565fafaf783e8226693
kirkl <kirkl@vulcan.com>
2015-10-08 08:37:20 -0700

    raster urls

--- das/das_server/urls.py
+++ das/das_server/urls.py
@@ -26,6 +26,8 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^oauth2/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     url(r'^oauth2/token$', oauth2_views.TokenView.as_view(), name="token"),
+    # raster tiles
+    url(r'^tms/', include('raster.urls')),
     url(r'^$', views.index),
 ]
 
