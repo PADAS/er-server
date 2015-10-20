1f86d2dd8ed9a24b10499f7f005f16afba9b59b9
Chris Jones <chrisj@vulcan.com>
2015-10-20 14:14:58 -0700

    fix raster description

--- das/mapping/serializers.py
+++ das/mapping/serializers.py
@@ -13,16 +13,17 @@ class RasterLayerSerializer(serializers.ModelSerializer):
 
         rep.update(dict(tilejson='2.1.0',
                         version='1.0.0',
-                        scheme='tms'))
+                        ))
 
+        rep['scheme'] = 'tms'
         rep['legend'] = instance.legend.json
-        rep['maxzoom'] = instance.metadata.max_zoom
-        rep['minzoom'] = 0
+        #rep['maxzoom'] = instance.metadata.max_zoom
+        #rep['minzoom'] = 0
         #left, bottom, right, top
-        rep['bounds'] = [instance.metadata.uperleftx,
-                         instance.metadata.uperlefty- instance.metadata.height,
-                         instance.metadata.uperleftx + instance.metadata.width,
-                         instance.metadata.uperlefty]
+        #rep['bounds'] = [instance.metadata.uperleftx,
+        #                 instance.metadata.uperlefty- instance.metadata.height,
+        #                 instance.metadata.uperleftx + instance.metadata.width,
+        #                 instance.metadata.uperlefty]
 
         request = self.context['request']
         url = '{0}/tms/tiles/{1}'.format(os.path.dirname(os.path.dirname(request._request.path)), str(instance.id))
