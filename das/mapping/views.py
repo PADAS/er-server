939d4045e28cca4e30f73a70dc74f500fd2c6a29
Chris Jones <chrisj@vulcan.com>
2015-10-23 12:08:43 -0700

    added mapbox street map

--- das/mapping/views.py
+++ das/mapping/views.py
@@ -85,12 +85,22 @@ class FeatureSetGeoJsonView(APIView):
         return HttpResponse(feature, content_type='application/json')
 
 
-OSM_GEOJSON = {'name': 'OpenStreetMap',
+OSM_GEOJSON = {'name': 'Open Street Map',
                'tiles': ['http://b.tile.openstreetmap.com/{z}/{x}/{y}.png',],
                'maxZoom': 18,
                'attribution': 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, '
                               '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a> ',
                'id': 'examples.map-20v6611k'}
+
+MOSM_GEOJSON = {'name': 'Mapbox Streets Open Street Map',
+                'tiles': ['http://a.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiamFzb25iIiwiYSI6ImNpZnJhdDYydjd5ZXlzM2txbmF1YXRha2kifQ.aPy_2GpKESLXxXNwOduwLQ',
+                          'http://b.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiamFzb25iIiwiYSI6ImNpZnJhdDYydjd5ZXlzM2txbmF1YXRha2kifQ.aPy_2GpKESLXxXNwOduwLQ'
+                          ],
+                'attribution': '<a href=\"https://www.mapbox.com/about/maps/\" target=\"_blank\">&copy; Mapbox &copy; OpenStreetMap</a> <a class=\"mapbox-improve-map\" href=\"https://www.mapbox.com/map-feedback/\" target=\"_blank\">Improve this map</a>',
+                'id': 'mapbox.streets',
+                'source':'mapbox:///mapbox.mapbox-terrain-v2,mapbox.mapbox-streets-v5'
+                }
+
 
 class MapListJsonView(APIView):
     """
@@ -104,7 +114,7 @@ class MapListJsonView(APIView):
         mbtiles = MBTiles.objects.all()
         mbtiles_s = list(m.tilejson(request._request) for m in mbtiles)
 
-        return Response(list(itertools.chain(rasters_s.data, mbtiles_s, [OSM_GEOJSON,])))
+        return Response(list(itertools.chain(rasters_s.data, mbtiles_s, [OSM_GEOJSON, MOSM_GEOJSON])))
 
 
 #
