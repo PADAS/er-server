a49f24a54635e5b2f26efe9044ffd894ad395410
Chris Jones <chrisj@vulcan.com>
2015-11-04 08:46:31 -0800

    update local_settings_template

--- das/das_server/local_settings_template.py
+++ das/das_server/local_settings_template.py
@@ -4,7 +4,7 @@ call your project be overriding the settings file
  --settings=local_settings
 
 """
-
+import platform
 from .settings import *
 
 SECRET_KEY = ''
@@ -26,23 +26,47 @@ DATABASES = {
         'NAME': 'dasdb',
         'USER': 'postgres',
         'HOST': 'localhost',
+        #'PASSWORD': '',
     }
 }
 
 
-# On Windows, install Shapely, then set the geos library path appropriately
-#GEOS_LIBRARY_PATH = 'C:\python34\Lib\site-packages\shapely\DLLs\geos_c.dll'
+GEOS_LIBRARY_PATH = '/usr/local/lib/libgeos_c.so'
+GDAL_LIBRARY_PATH = '/usr/lib/libgdal.so'
+if platform.system().lower() == 'windows':
+    # On Windows, after pip install GDAL, set the geos library path appropriately
+    GEOS_LIBRARY_PATH = 'C:\projects\das\dasvir\Lib\site-packages\osgeo\geos_c.dll'
+    GDAL_LIBRARY_PATH = 'C:\projects\das\dasvir\Lib\site-packages\osgeo\gdal111.dll'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+DEV = True
+ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
+TIME_ZONE = 'US/Pacific'
+
+STATIC_URL = '/static/'
 
 #add the path to your local copy of the das-web static root dir that contains index.html
-#STATICFILES_DIRS = ('C:\projects\das\das-web-public\public',)
+#STATICFILES_DIRS = STATICFILES_DIRS + (os.path.join(BASE_DIR, 'www'),)
 
 """
 We put test fixtures in a non-conventional place, so build a list of directories here to let Django
 know where to find them.
 Our convention is to include fixtures in <app_name>/tests/fixtures/
 """
-_ = ('%s/tests/fixtures' % x for x in ('observations', 'data_input'))
-FIXTURE_DIRS = list(os.path.join(BASE_DIR, x) for x in _)
+_test_fixtures = ('%s/tests/fixtures' % x for x in ('observations',
+                                                    'data_input',
+                                                    'mapping',
+                                                    'das_server'))
+FIXTURE_DIRS = list(os.path.join(BASE_DIR, x) for x in _test_fixtures)
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
 
 '''
 We're using apscheduler to run scheduled tasks.
