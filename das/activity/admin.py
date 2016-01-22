0aa4cfc1ff56586a1cb4fd577b13cdedad90ed7c
Chris Doehring <chrisdoehring@gmail.com>
2016-01-22 10:41:27 -0800

    Move activity admin to use OSMGeoAdmin.

--- das/activity/admin.py
+++ das/activity/admin.py
@@ -1,21 +1,16 @@
-from django.contrib import admin
-from django.contrib.gis.admin.options import GeoModelAdmin
+from django.contrib.gis import admin
 import activity.models as models
 from django.contrib.staticfiles.templatetags.staticfiles import static
-
 
 class EventAttachmentInline(admin.StackedInline):
     model=models.EventAttachment
 
 @admin.register(models.Event)
-class EventAdmin(GeoModelAdmin):
-    # Relying on static content in mapping app.
+class EventAdmin(admin.OSMGeoAdmin):
     openlayers_url = static('js/openlayers_2.13/OpenLayers.js')
-    # wms_layer = 'MODIS_Terra_CorrectedReflectance_TrueColor'
-    # wms_url = 'http://map1a.vis.earthdata.nasa.gov/wmts-geo/wmts.cgi'
-
     wms_layer = 'terrain,overlay'
     wms_url= 'http://tiles.maps.eox.at/wms/'
+
     list_display = ('created_at', 'event_type', 'name', 'location', 'attributes',)
     readonly_fields = ('id', 'created_at', 'updated_at')
     inlines = [
