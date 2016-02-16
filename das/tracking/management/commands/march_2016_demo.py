3f54992bdc8f635e9f1e1608217e0945e6936aba
Joseph Sheedy <joseph.sheedy@gmail.com>
2016-02-15 17:11:34 -0800

    fixed demo script, refined Observation points

--- das/tracking/management/commands/march_2016_demo.py
+++ das/tracking/management/commands/march_2016_demo.py
@@ -1,16 +1,18 @@
+import subprocess
 from datetime import datetime, timedelta
 
-from django.contrib.gis.geos import Point
+from django.contrib.gis.geos import Point, Polygon, MultiPolygon
 from django.core.management.base import BaseCommand
+from django.db import transaction
 import pytz
 
+from activity.models import Event, EventAttachment
 from analyzers.models import ContainmentAnalyzer, SubjectAnalyzer, \
     GeofenceAnalyzer, ImmobilityAnalyzer, ProximityAnalyzer, SpeedAnalyzer
 from mapping.models import FeatureType, PolygonFeature
 from observations.models import Subject, SubjectSource, Source, Observation
 from tracking.pubsub_registry import notify_new_tracks
 
-from django.contrib.gis.geos import Polygon, MultiPolygon
 
 
 source, subject = None, None
@@ -21,11 +23,22 @@ points = (
     (37.45, 0.225),
     (37.50, 0.225),
     (37.55, 0.225),
-    (37.35, 0.425),
-    (37.55, 0.0),
+    (37.55, 0.230),
+    (37.55, 0.24),
+    (37.55, 0.25),
+    (37.50, 0.26),
+    (37.45, 0.27),
+    (37.42, 0.3),
+    (37.35, 0.3),
+    (37.55, 0.15),
+    (37.50, 0.16),
+    (37.45, 0.17),
+    (37.40, 0.2),
+    (37.35, 0.2),
 )
 
 source_id = '276600ae-06de-4fca-be79-58bb29695f5b'
+subject_id = '276600ae-06de-4fca-be79-58bb29695f5c'
 
 def delete_subject():
     Subject.objects.filter(name="Topsy").delete()
@@ -35,6 +48,11 @@ def delete_source():
 
 def delete_observations():
     Observation.objects.filter(source_id=source_id).delete()
+
+def delete_events():
+    for event_attachment in EventAttachment.objects.filter(target_id=subject_id):
+        event_attachment.event.delete()
+        event_attachment.delete()
 
 def delete_analyzers():
     pass
@@ -53,6 +71,7 @@ def create_actors():
 
     global subject
     subject = Subject.objects.create(
+        id=subject_id,
         name = 'Topsy',
         additional = {'sex': 'Female', 'species': 'Elephant'},
         subject_type='wildlife',
@@ -91,26 +110,26 @@ def create_analyzers():
 
 
 def drive():
-    # create Observation objects for each point
-    # t0 = datetime(2016,3,1)
+    while True:
+        delete_observations()
+        delete_events()
 
+        t0 = datetime.now(tz=pytz.UTC) - timedelta(hours=24*3-1)
 
-    t0 = datetime.now() - timedelta(days=3)
+        for i, point in enumerate(points):
+            dt = timedelta(hours=i)
+            t = t0 + dt
 
-    for i, point in enumerate(points):
-        dt = timedelta(hours=i)
-        t = t0 + dt
-
-        _ = Observation.objects.create(
-            source_id=source.id,
-            location=Point(point),
-            recorded_at=t,
-            additional={}
-        )
-        from django.db import transaction
-        transaction.on_commit(lambda: notify_new_tracks(source.id))
-        transaction.commit()
-        input('press the any key')
+            _ = Observation.objects.create(
+                source_id=source.id,
+                location=Point(point),
+                recorded_at=t,
+                additional={}
+            )
+            transaction.on_commit(lambda: notify_new_tracks(source.id))
+            transaction.commit()
+            # sleep for a second, or continue on enter press
+            subprocess.call('read -t 1', shell=True)
 
 class Command(BaseCommand):
 
@@ -119,11 +138,13 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         delete_analyzers()
         delete_subject_analyzers()
-        delete_observations()
         delete_subject()
         delete_source()
+        delete_observations()
+        delete_events()
 
         create_actors()
         create_analyzers()
 
+        input('setup complete, press enter to start demo')
         drive()
