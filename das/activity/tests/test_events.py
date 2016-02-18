69381487f38cc45a81207b03d816bf7d2793df31
Joseph Sheedy <joseph.sheedy@gmail.com>
2016-02-17 18:40:00 -0800

    fixed event test

--- das/activity/tests/test_events.py
+++ das/activity/tests/test_events.py
@@ -18,7 +18,7 @@ class TestSourcePlugin(TestCase):
             e = Event.objects.create(name='Bogus event', description=fake_long_description,
                                      provenance=Event.INFORMANT,
                                      event_type=Event.ET_LIVESTOCK_THEFT,
-                                     priority='urgent',
+                                     priority=Event.PRI_URGENT,
                                      attributes={},
                                      )
 
