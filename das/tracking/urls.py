1944c908bfee49c951728277b40f9dd4de3a100b
Chris Doehring <chrisdoehring@gmail.com>
2016-02-23 12:41:14 -0800

    Added tracking/messages API.

--- das/tracking/urls.py
+++ das/tracking/urls.py
@@ -2,10 +2,6 @@ from django.conf.urls import url
 from tracking import views
 
 urlpatterns = [
-
-        # url(r'^source/(?P<id>[0-9a-fA-F]{8}-?[0-9a-fA-F]{4}-?4[0-9a-fA-F]{3}-?[89abAB][0-9a-fA-F]{3}-?[0-9a-fA-F]{12})/observations/?$',
-        #     views.SourceObservationsView.as_view()),
-        # url(r'^source/(?P<manufacturer_id>.+)/?$', views.SourceList.as_view()),
-        url(r'^observations/?$', views.observation_list, ),
-        # url(r'^tracks/?$', views.TracksView().as_view())
+    url(r'^observations/?$', views.observation_list, ),
+    url(r'^messages/?$', views.message_list, ),
 ]
