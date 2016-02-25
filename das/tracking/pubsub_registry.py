9094d3a0d21fe072b7828cb93f2a7cc8ec9d901b
Joseph Sheedy <joseph.sheedy@gmail.com>
2016-02-24 16:05:02 -0800

    removed tracking.pubsub_registry debug log callback. no need to duplicate that now that we have sufficient example pubsub registrations

--- das/tracking/pubsub_registry.py
+++ das/tracking/pubsub_registry.py
@@ -1,20 +1,16 @@
 import logging
 
-logger = logging.getLogger(__name__)
 from das_server import pubsub
 
-def tracking_callback(body, message):
-    logger.debug('Tracking received message [{}].'.format(body))
 
+logger = logging.getLogger(__name__)
 
-PUBSUB_SUBSCRIPTIONS = (
-    ('das.#', tracking_callback),
-)
 
 def notify_new_tracks(source_id):
     '''Notify of new observations for the given source.'''
     try:
         pubsub.publish({'source_id': str(source_id)}, 'das.tracking.source.observations.new')
+        return True
+
     except Exception:
         logger.exception('Exception while publishing message.')
-    return True
