773ad54f745009f59cae80e0450d229d5186e24e
Chris Jones <chrisj@vulcan.com>
2016-04-01 09:39:46 -0700

    remove coverage from django jenkins config

--- das/das_server/local_ci.py
+++ das/das_server/local_ci.py
@@ -21,7 +21,6 @@ INSTALLED_APPS = (
 
 JENKINS_TASKS = (
     'django_jenkins.tasks.run_pylint',
-    'django_jenkins.tasks.with_coverage',
     'django_jenkins.tasks.run_pep8',
     'django_jenkins.tasks.run_sloccount'
 )
