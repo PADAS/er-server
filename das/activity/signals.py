22db68b9b67ce559e24588806a6cf19e9cd2ccf6
Chris Jones <chrisj@vulcan.com>
2016-05-24 12:48:04 -0700

    helps to have unique receiver function names

--- das/activity/signals.py
+++ das/activity/signals.py
@@ -20,14 +20,15 @@ def event_post_save(sender, instance, created, **kwargs):
 
 
 @receiver(post_save, sender=EventNote)
-def event_post_save(sender, instance, created, **kwargs):
-    logger.info("saved note for event {}, created={}".format(instance.event.pk, str(created)))
+def event_note_post_save(sender, instance, created, **kwargs):
+    logger.info("saved note for event {}, created={}".format(instance.event_id, str(created)))
     transaction.on_commit(lambda: pubsub.publish(
-        {'event_id': str(instance.event.pk)}, 'das.event.update'))
+        {'event_id': str(instance.event_id)}, 'das.event.update'))
 
 
 @receiver(post_save, sender=EventAttachment)
-def event_post_save(sender, instance, created, **kwargs):
-    logger.info("saved attachment for event {}, created={}".format(instance.event.pk, str(created)))
+def event_attachment_post_save(sender, instance, created, **kwargs):
+    logger.info("saved attachment for event {}, created={}".format(instance.event_id, str(created)))
     transaction.on_commit(lambda: pubsub.publish(
-        {'event_id': str(instance.event.pk)}, 'das.event.update'))
+        {'event_id': str(instance.event_id)}, 'das.event.update'))
+
