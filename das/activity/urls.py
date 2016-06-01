af3d4f4ff7ad0a90602519a7d4ddcddc638a8613
Chris Jones <chrisj@vulcan.com>
2016-06-01 15:48:26 -0700

    don't require trailing slash on EventsView router

--- das/activity/urls.py
+++ das/activity/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from rest_framework import routers
 from . import views
 
-router = routers.SimpleRouter()
+router = routers.SimpleRouter(trailing_slash=False)
 router.register(r'events', views.EventsViewSet)
 
 urlpatterns = [
