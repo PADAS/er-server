0e9cceb3f28424c3cadc85db3aae301d8453722b
Chris Doehring <chrisdoehring@gmail.com>
2016-06-27 13:04:28 -0700

    Fixed migration script error.

--- das/activity/migrations/0023_eventphoto.py
+++ das/activity/migrations/0023_eventphoto.py
@@ -14,7 +14,6 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('activity', '0022_merge'),
     ]
 
     operations = [
