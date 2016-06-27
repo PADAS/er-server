d15c1b017ca3f8c6f685d2ac275b6afa8f829a26
Chris Doehring <chrisdoehring@gmail.com>
2016-06-27 13:12:44 -0700

    Fix migration dependency.

--- das/activity/migrations/0023_eventphoto.py
+++ das/activity/migrations/0023_eventphoto.py
@@ -14,6 +14,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('activity', '0021_choices'),
     ]
 
     operations = [
