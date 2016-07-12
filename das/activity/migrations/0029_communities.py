2ed2da8a585e2b449a1a514508b89f1b2a1fb30c
Chris Jones <chrisj@vulcan.com>
2016-07-12 09:05:31 -0700

    change migration filename after merge with master

--- das/activity/migrations/0029_communities.py
+++ das/activity/migrations/0029_communities.py
@@ -8,7 +8,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('activity', '0027_photorevision'),
+        ('activity', '0028_eventphoto_filename'),
     ]
 
     operations = [
