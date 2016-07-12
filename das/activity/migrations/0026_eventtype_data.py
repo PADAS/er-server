0d4f69a1d37e9bbd27c0a450e46d3017a2c14063
Chris Jones <chrisj@vulcan.com>
2016-07-12 09:54:25 -0700

    force permission creation during a migration so that they can be referenced while loading initial data.

--- das/activity/migrations/0026_eventtype_data.py
+++ das/activity/migrations/0026_eventtype_data.py
@@ -4,8 +4,14 @@ from __future__ import unicode_literals
 
 from django.db import migrations
 from django.core.management import call_command
+from django.contrib.auth.management import create_permissions
+
 
 def populate_eventtype(apps, schema_editor):
+    #initial data references permissions
+    apps.models_module = True
+    create_permissions(apps, verbosity=0)
+    apps.models_module = None
     call_command('loaddata', 'initial_eventtype')
 
 
