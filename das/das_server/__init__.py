7f7ff33f74cc4544cc144a2864e3808e68b8abf5
Chris Jones <chrisj@vulcan.com>
2016-07-22 16:06:06 -0700

    add BUILD_NUMBER to version. Jenkins can then replace it.

--- das/das_server/__init__.py
+++ das/das_server/__init__.py
@@ -1,4 +1,5 @@
-VERSION = (0, 1, 1, 'alpha')
+BUILD_NUMBER = 1
+VERSION = (0, 1, BUILD_NUMBER, 'alpha')
 
 if VERSION[-1] != "final": # pragma: no cover
     __version__ = '.'.join(map(str, VERSION))
