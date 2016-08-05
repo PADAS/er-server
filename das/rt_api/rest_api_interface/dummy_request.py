f2c6d8eea88fca25d05046cc66d707dbb2e08d9f
Shawn Hickey <shawnh@vulcan.com>
2016-08-05 12:02:06 -0700

    Fix bug with authenticating dummy requests from the socket

--- das/rt_api/rest_api_interface/dummy_request.py
+++ das/rt_api/rest_api_interface/dummy_request.py
@@ -3,7 +3,9 @@ from oauthlib.common import Request
 
 class DummyRequest(Request):
     _request = None
-    def __init__(self, uri='/dummy', http_method='POST', body={}, headers={}, encoding='utf-8'):
+    def __init__(self, uri='/dummy', http_method='POST', body={}, headers={}, encoding='utf-8', user = None):
+        Request.__init__(self, uri, http_method, body, headers, encoding)
+
         self.method = http_method
         self.META = headers
         self.POST = body
@@ -12,7 +14,11 @@ class DummyRequest(Request):
         self._request = self
         self.query_params = {}
         self.successful_authenticator = 'dummy_authentication'
-        Request.__init__(self, uri, http_method, body, headers, encoding)
+        self._authenticator = 'dummy_authenticatior'
+        self._user = user
+        self.user = user
+        self._force_auth_user = user
+
 
     def get_full_path(self):
         return self.uri
