408364476d4832c4a5d88da1efa47d8953b7367d
Shawn Hickey <shawnh@vulcan.com>
2016-08-09 12:28:37 -0700

    fix bug where permissions checks would fail for socket users that had the required permissions

--- das/rt_api/rest_api_interface/dummy_request.py
+++ das/rt_api/rest_api_interface/dummy_request.py
@@ -15,7 +15,6 @@ class DummyRequest(Request):
         self.query_params = {}
         self.successful_authenticator = 'dummy_authentication'
         self._authenticator = 'dummy_authenticatior'
-        self._user = user
         self.user = user
         self._force_auth_user = user
 
