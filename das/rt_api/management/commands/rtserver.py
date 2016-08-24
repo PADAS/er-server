81734179dfd2762b07c651808e5ec7399c65ffe2
Chris Jones <chrisj@vulcan.com>
2016-08-24 09:52:06 -0700

    set max_green_threads to 9 so that we don't go over the max 10 redis connection limit in kombu

--- das/rt_api/management/commands/rtserver.py
+++ das/rt_api/management/commands/rtserver.py
@@ -19,7 +19,9 @@ from rt_api.sios import create_rt_socketio
 
 logger = logging.getLogger('rt_api')
 
-MAX_GREEN_THREADS = 20
+#The Kombu redis transport has a max of 10 redis connections
+#keep green threads below this to avoid maxing out redis connections
+MAX_GREEN_THREADS = 9
 
 
 class Command(runserver.Command):
