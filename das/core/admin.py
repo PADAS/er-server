d7c3c3040c280887190f4dac3708e4d739fbb4c0
Shawn Hickey <shawnh@vulcan.com>
2016-09-30 16:34:14 -0700

    Add DynamicChoice model

--- das/core/admin.py
+++ das/core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 # Register your models here.
-from core.models import Choice
+from core.models import Choice, DynamicChoice
 
 
 class HierarchyModelAdmin(admin.ModelAdmin):
@@ -9,6 +9,11 @@ class HierarchyModelAdmin(admin.ModelAdmin):
 
 @admin.register(Choice)
 class ChoiceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(DynamicChoice)
+class DynamicChoiceAdmin(admin.ModelAdmin):
     pass
 
 
