7781d5bafb513ea9908fad4869614eccd63513b3
Chris Jones <chrisj@vulcan.com>
2016-10-04 13:20:13 -0700

    global setting for the event matrix, returned in status

--- das/das_server/views.py
+++ das/das_server/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render_to_response
+from django.conf import settings
 from django.template import RequestContext
 from rest_framework import generics
 from rest_framework.permissions import AllowAny, DjangoObjectPermissions
@@ -13,6 +14,7 @@ def index(request):
 
 class VersionSerializer(rest_framework.serializers.Serializer):
     version = rest_framework.serializers.CharField(read_only=True)
+    event_matrix_enabled = rest_framework.serializers.BooleanField(read_only=True)
 
 
 class StatusView(generics.RetrieveAPIView):
@@ -25,4 +27,8 @@ class StatusView(generics.RetrieveAPIView):
     serializer_class = VersionSerializer
 
     def get_object(self):
-        return {'version': __version__} #request.version}
+        resp = {'version': __version__} #request.version}
+
+        resp['event_matrix_enabled'] = settings.EVENT_MATRIX_ENABLED
+
+        return resp
