68e911dd64e3cdab465d73787cb232b66d74c252
Chris Doehring <chrisdoehring@gmail.com>
2016-10-09 09:16:18 -0700

    Update timestamp representation in report title.

--- das/reports/tests.py
+++ das/reports/tests.py
@@ -7,7 +7,7 @@ import string
 
 class TestReports(TestCase):
 
-    fixtures = 'oauth_clients'
+    fixtures = ( 'activity_eventreference',)
 
     pwd = ''.join(random.SystemRandom().choice((string.ascii_uppercase + string.ascii_lowercase + string.digits)) for _ in range(12))
     user_data = dict(username='sometestuser', password=pwd, email='testuser@tempuri.org', is_superuser=True,
@@ -22,7 +22,6 @@ class TestReports(TestCase):
     def setUp(self):
 
         User.objects.create_superuser(**self.user_data)
-
 
 
     def test_sitrep(self):
@@ -40,3 +39,20 @@ class TestReports(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.using, 'docx_template')
         self.assertTemplateUsed('lewa_sitrep_template.docx')
+
+
+    def test_sitrep_html(self):
+        response = self.client.post('/oauth2/token', data=self.login_data)
+
+        self.assertEqual(response.status_code, 200)
+
+        body = json.loads(str(response.content, 'utf8'))
+        self.assertTrue(body['access_token'] is not None)
+
+        extras = dict(Authorization='Bearer {}'.format(body['access_token']))
+        response = self.client.get('/api/v1.0/reports/sitrep.html', follow=True, **extras)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.using, 'jinja2')
+        self.assertTemplateUsed('lewa_sitrep_template.html')
+
