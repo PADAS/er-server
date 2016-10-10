b3aad37ee6e79e17305d693160c08c5cd325e981
Chris Doehring <chrisdoehring@gmail.com>
2016-10-09 19:44:43 -0700

    Add list of security events.

--- das/reports/reports.py
+++ das/reports/reports.py
@@ -5,17 +5,30 @@ from django.db.models import *
 from activity.models import Event
 from choices.models import Conservancy
 from observations.models import Subject
+
+
 def get_events(start, end):
-    events = Event.objects.filter(event_time__range=[start,end]).prefetch_related('event_type', 'reported_by')
+    events = Event.objects.filter(event_time__range=[start,end]).prefetch_related('event_type', 'reported_by')\
+        .order_by('event_time')
     return events
+
 
 def get_conservancies():
     return Conservancy.objects.all().values()
 
+
 def get_rhino_sightings(start, end):
-    events = Event.objects.filter(event_type__value__in=('black_rhino_sighting', 'white_rhino_sighting'), event_time__range=[start,end])
+    events = Event.objects.filter(event_type__value__in=('black_rhino_sighting', 'white_rhino_sighting'),
+                                  event_time__range=[start,end])
     return events
+
 
 def get_rhinos():
     rhinos = Subject.objects.filter(subject_type=Subject.TYPE_WILDLIFE, subject_subtype=Subject.SUBTYPE_RHINO)
     return rhinos
+
+
+def get_security_event(start, end):
+    security_events = Event.objects.filter(event_time__range=[start, end], event_type__category__value='security')
+    security_events = security_events.prefetch_related('event_type', 'reported_by').order_by('-event_time')
+    return security_events
