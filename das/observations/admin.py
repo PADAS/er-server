6e8eb1fd2cc8f1336174cfe463bc0ae277c81c83
Chris Doehring <chrisdoehring@gmail.com>
2016-10-11 22:26:52 -0700

    Update search fields for SubjectAdmin.

--- das/observations/admin.py
+++ das/observations/admin.py
@@ -16,8 +16,7 @@ class SubjectAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'subject_type', 'subject_subtype',
                     'is_active', 'additional', 'all_groups', 'all_sources')
 
-    # TODO: Including common_name in search_fields has caused an exception. Figure out how to solve it.
-    search_fields = ('name', 'subject_subtype',) # 'common_name')
+    search_fields = ('name', 'subject_subtype', 'common_name__display')
 
     fields = ('id', 'name', 'common_name', 'additional', 'groups', SubjectForm.SUBTYPE_FIELD)
     list_filter = ('is_active', 'subject_type', 'subject_subtype', 'common_name')
