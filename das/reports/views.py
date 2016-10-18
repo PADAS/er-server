4d5cb5808802130395c0a2f7435930a5aaebc364
Chris Doehring <chrisdoehring@gmail.com>
2016-10-17 22:41:25 -0700

    Revert inadvertent commits in reports.

--- das/reports/views.py
+++ das/reports/views.py
@@ -14,15 +14,9 @@ from django.views.generic.base import TemplateResponseMixin, ContextMixin
 from reports.reports import get_events, get_conservancies, get_rhino_sightings, get_rhinos, get_security_event
 from reports.accumulator import accumulator, broadcast
 
-class ReportQueryParameters(serializers.Serializer):
+class ReportDateParameters(serializers.Serializer):
     since = serializers.DateTimeField(default=None)
-    before = serializers.DateTimeField(default=serializers.CreateOnlyDefault(timezone.now))
-
-
-    def validate(self, attrs):
-        if self.since >= self.before:
-            raise serializers.ValidationError('since must not be greater than before.')
-        return attrs
+    before = serializers.DateTimeField(default=None)
 
 
 class ReportView(views.APIView):
@@ -78,14 +72,15 @@ class SituationReportView(views.APIView, TemplateResponseMixin, ContextMixin, ):
 
     def get(self, request, *args, **kwargs):
 
-        qs = ReportQueryParameters(data=request.query_params)
+        qs = ReportDateParameters(data=request.query_params)
         if not qs.is_valid():
             return Response(data=qs.errors, status=status.HTTP_400_BAD_REQUEST)
 
         qs = qs.validated_data
         now = timezone.now()
+        since = qs.get('since') or  (now - datetime.timedelta(hours=24))
         before = qs.get('before') or now
-        since = qs.get('since') or  (before - datetime.timedelta(hours=24))
+
 
         context = self.get_context_data(since=since, before=before, **kwargs)
         return self.render_to_response(context)
