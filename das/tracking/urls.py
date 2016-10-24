d9fc9ab50a36fcdda2c5c3b1ebff7dc0ef4ddd1f
Chris Doehring <chrisdoehring@gmail.com>
2016-10-24 13:57:12 -0700

    Remove deprecated code in tracking.

--- das/tracking/urls.py
+++ das/tracking/urls.py
@@ -1,7 +1,1 @@
 from django.conf.urls import url
-from tracking import views
-
-urlpatterns = [
-    url(r'^observations/?$', views.observation_list, ),
-    url(r'^messages/?$', views.message_list, ),
-]
