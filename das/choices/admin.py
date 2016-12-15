5dc854078c4823a9feaa1e92ff014c213b0feddc
Shawn Hickey <ShawnH@vulcan.com>
2016-12-15 11:04:51 -0800

    Merging choices added within /rwanda branch.

--- das/choices/admin.py
+++ das/choices/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from choices.models import Choice, DynamicChoice, Conservancy, Behavior, Station, Color, Health, Species, CauseOfDeath, FenceSection, ActionTaken, SectionArea, Team, PoachingMean, Tribe, IllegalActivity, Livestock, ContactType, TrophyStatus, WildlifeGap, IncidentStatus
+import choices.models as models
 
 
 class BaseChoiceAdmin(admin.ModelAdmin):
@@ -10,94 +10,149 @@ class BaseChoiceAdmin(admin.ModelAdmin):
     list_editable = ('name', 'ordernum')
 
 
-@admin.register(Choice)
+@admin.register(models.SectionArea)
+class SectionAreaAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Station)
+class StationAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.FenceLocation)
+class FenceLocationAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.FenceDamage)
+class FenceDamageAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.KeySpecies)
+class KeySpeciesAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Species)
+class SpeciesAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.AnimalSex)
+class AnimalSexAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.AnimalAge)
+class AnimalAgeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.CarcassAge)
+class CarcassAgeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.TrophyStatus)
+class TrophyStatusAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.CauseOfDeath)
+class CauseOfDeathAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.InjuryCause)
+class InjuryCauseAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.InjuryType)
+class InjuryTypeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.FireStatus)
+class FireStatusAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.FireCause)
+class FireCauseAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Direction)
+class DirectionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Crops)
+class CropsAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.TypeOfIllegalActivity)
+class TypeOfIllegalActivityAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.SnareAge)
+class SnareAgeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.SnareStatus)
+class SnareStatusAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.PoacherCampAge)
+class PoacherCampAgeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.TypeOfShots)
+class TypeOfShotsAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.TypeOfTrophy)
+class TypeOfTrophyAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.VehicleTypes)
+class VehicleTypesAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.IncidentStatus)
+class IncidentStatusAdmin(BaseChoiceAdmin):
+
+@admin.register(models.WeaponTypes)
+class WeaponTypesAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.TrafficType)
+class TrafficTypeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.TrafficActivity)
+class TrafficActivityAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.AccidentType)
+class AccidentTypeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.CriticalSightingType)
+class CriticalSightingTypeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.TracksType)
+class TracksTypeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.VehicleType)
+class VehicleTypeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.MedicalEquipmentRequired)
+class MedicalEquipmentRequiredAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.MedicalEvacSecurity)
+class MedicalEvacSecurityAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.DetectionType)
+class DetectionTypeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Choice)
 class ChoiceAdmin(admin.ModelAdmin):
     pass
 
-
-@admin.register(DynamicChoice)
+@admin.register(models.DynamicChoice)
 class DynamicChoiceAdmin(admin.ModelAdmin):
     pass
-
-
-@admin.register(Conservancy)
-class ConservancyAdmin(BaseChoiceAdmin):
-    pass
-
-@admin.register(Behavior)
-class BehaviorAdmin(BaseChoiceAdmin):
-    pass
-
-@admin.register(Station)
-class StationAdmin(BaseChoiceAdmin):
-    pass
-
-@admin.register(Color)
-class ColorAdmin(BaseChoiceAdmin):
-    pass
-
-@admin.register(Health)
-class HealthAdmin(BaseChoiceAdmin):
-    pass
-
-@admin.register(Species)
-class SpeciesAdmin(BaseChoiceAdmin):
-    pass
-
-@admin.register(CauseOfDeath)
-class CauseOfDeathAdmin(BaseChoiceAdmin):
-    pass
-
-@admin.register(FenceSection)
-class FenceSectionAdmin(BaseChoiceAdmin):
-    pass
-
-@admin.register(ActionTaken)
-class ActionTakenAdmin(BaseChoiceAdmin):
-    pass
-
-@admin.register(SectionArea)
-class SectionAreaAdmin(BaseChoiceAdmin):
-    pass
-
-@admin.register(Team)
-class TeamAdmin(BaseChoiceAdmin):
-    pass
-
-@admin.register(PoachingMean)
-class PoachingMeanAdmin(BaseChoiceAdmin):
-    pass
-
-@admin.register(Tribe)
-class TribeAdmin(BaseChoiceAdmin):
-    pass
-
-@admin.register(IllegalActivity)
-class IllegalActivityAdmin(BaseChoiceAdmin):
-    pass
-
-@admin.register(Livestock)
-class LivestockAdmin(BaseChoiceAdmin):
-    pass
-
-@admin.register(ContactType)
-class ContactTypeAdmin(BaseChoiceAdmin):
-    pass
-
-@admin.register(TrophyStatus)
-class TrophyStatusAdmin(BaseChoiceAdmin):
-    pass
-
-@admin.register(WildlifeGap)
-class WildlifeGapAdmin(BaseChoiceAdmin):
-    pass
-
-@admin.register(IncidentStatus)
-class IncidentStatusAdmin(BaseChoiceAdmin):
-    pass
-
-
-
-
-
-
