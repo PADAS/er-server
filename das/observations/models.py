c6311d033d1cbf655bad21cb704135b8e8daaf98
chrisj <chrisj@vulcan.com>
2016-12-24 23:31:16 -0800

    added 2016-12-21 event icons

--- das/observations/models.py
+++ das/observations/models.py
@@ -536,6 +536,7 @@ class Subject(TimestampedModel, PermissionSetGroupMixin):
     SUBTYPE_RHINO = 'rhino'
     SUBTYPE_LION = 'lion'
     SUBTYPE_GIRAFFE = 'giraffe'
+    SUBTYPE_ANTELOPE = 'antelope'
 
     SUBTYPE_SECURITY = 'security_vehicle'
     SUBTYPE_RESEARCH = 'research'
@@ -564,6 +565,7 @@ class Subject(TimestampedModel, PermissionSetGroupMixin):
                 (SUBTYPE_RHINO, 'Rhino'),
                 (SUBTYPE_LION, 'Lion'),
                 (SUBTYPE_GIRAFFE, 'Giraffe'),
+                (SUBTYPE_ANTELOPE, 'Antelope'),
             )
 
         },
