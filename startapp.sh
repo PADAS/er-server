8f7291749fce16e4690d4e259118d77414faed92
Matt Korwel <matt.korwel@gmail.com>
2017-02-27 18:40:08 -0800

    Adding in socket.io and messagequeue listeners. MK

--- startapp.sh
+++ startapp.sh
@@ -27,5 +27,5 @@ export DJANGO_SETTINGS_MODULE=das_server.local_settings
 python3 manage.py migrate
 # python3 manage.py createsuperuser
 python3 manage.py runserver 0.0.0.0:8000 &
-python manage.py rt_server 8000 &
+python manage.py rt_server 8001 &
 python manage.py message_queue_listeners
