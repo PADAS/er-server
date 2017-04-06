37350b03401da90277e2e49f7a3252fe9e91768b
Chris Doehring <chrisdoehring@gmail.com>
2017-04-06 13:34:42 -0700

    Adding immobility test.

--- das/analyzers/tests/test_immobility.py
+++ das/analyzers/tests/test_immobility.py
@@ -1,14 +1,43 @@
 import copy
 from datetime import datetime, timedelta
-
+import dateutil.parser as dp
 from django.test import TestCase
 import pytz
 
 from analyzers.models.immobility import ImmobilityAnalyzer
-from analyzers.models.analyzer import NOMINAL, WARNING, CRITICAL
+from .immobility_test_data import ISHANGO_IMMOBILE
 from observations.track import Track
 import observations.models
+from django.contrib.gis.db import models
+from django.contrib.gis.geos import Point, Polygon
+import random
+import copy
 
+from observations import models
+
+
+def generate_random_positions(start_time=None, x=37.5, y=1.41):
+    recorded_at = start_time or pytz.utc.localize(datetime.utcnow()) - timedelta(hours=24)
+
+    while True:
+        yield recorded_at, Point(x=x, y=y)
+        x += (random.random()  - 0.5)/10000
+        y += (random.random()  - 0.5)/10000
+        recorded_at = recorded_at + timedelta(minutes=30)
+
+
+def time_shift(items, start_time=None, time_key='recorded_at'):
+    fake_start = start_time or pytz.utc.localize(datetime.utcnow()) - timedelta(hours=24)
+    for i, item in enumerate(items):
+        if i == 0:
+            actual_start = dp.parse(item[time_key])
+            fake_time = fake_start
+        else:
+            fake_time = (dp.parse(item[time_key]) - actual_start) + fake_start
+
+        new_item = copy.copy(item)
+        new_item[time_key] = fake_time
+        yield new_item
 
 class TestImmobilityAnalyzer(TestCase):
 
@@ -17,104 +46,55 @@ class TestImmobilityAnalyzer(TestCase):
 
     def setUp(self):
 
-        self.n_points = 50
-
-        mobile_points = [
-            [200 * i * 10 ** -6, 0]
-            for i in range(self.n_points)
-        ]
-
-        # dead track
-        immobile_points = [
-            [0, 0]
-            for i in range(self.n_points)
-        ]
-
-        immobile_points_with_outliers = copy.deepcopy(immobile_points)
-
-        # insert noise:
-        immobile_points_with_outliers[49][0] = immobile_points_with_outliers[49][0] + .0004
-
-        times = [
-            datetime(2000,1,1,0,0,0,tzinfo=pytz.utc) + timedelta(hours=i)
-            for i in range(self.n_points)
-        ]
-
-        self.mobile_track = Track(mobile_points, times)
-        self.immobile_track = Track(immobile_points, times)
-        self.immobile_track_with_outliers = Track(immobile_points_with_outliers, times)
+        source = models.Source.objects.get(id='a91e0366-898c-475b-830f-e0fae46e6efe')
 
 
-    def test_generic(self):
+    def test_ishango_immobile(self):
 
-        f = observations.models.SubjectTrackSegmentFilter(subject_type='elephant')
+        source = models.Source.objects.get(id='a91e0366-898c-475b-830f-e0fae46e6efe')
+        end = pytz.utc.localize(datetime.utcnow())
+
+        for item in time_shift(ISHANGO_IMMOBILE):
+
+            recorded_at = item['recorded_at']
+            location = Point(x=item['longitude'], y=item['latitude'])
+            obs = models.Observation.objects.create(recorded_at=recorded_at,
+                                             location=location,
+                                                    source=source, additional={})
+            if obs.recorded_at > end:
+                break
+
         ia = ImmobilityAnalyzer.objects.create(subject_id='9342973f-b369-4d21-9f1f-ae89d523e05a', threshold_time=1000)
+
+        test_subject = models.Subject.objects.get(id='9342973f-b369-4d21-9f1f-ae89d523e05a')
+        print (test_subject)
 
         r = ia.analyze()
 
         print(r)
 
-    def xtest_immobility_analyzer_is_mobile(self):
-        """
-        Test a mobile Track
-        """
+    def test_random(self):
 
-        immobility_analyzer = ImmobilityAnalyzer()
-        analyzer_result = immobility_analyzer.analyze(self.mobile_track)
+        n = pytz.utc.localize(datetime.utcnow())
+        source = models.Source.objects.get(id='a91e0366-898c-475b-830f-e0fae46e6efe')
+        positions = generate_random_positions()
+        positions.send(None)
+        while True:
 
-        expected = NOMINAL
-        actual = analyzer_result.level
+            recorded_at, location = positions.send(None)
+            obs = models.Observation.objects.create(recorded_at=recorded_at,
+                                             location=location,
+                                                    source=source, additional={})
+            if obs.recorded_at > n:
+                break
 
-        self.assertEqual(actual, expected, 'actual value: {}'.format(analyzer_result.value))
+        ia = ImmobilityAnalyzer.objects.create(subject_id='9342973f-b369-4d21-9f1f-ae89d523e05a', threshold_time=1000)
 
-    def xtest_immobility_analyzer_is_critical_immobile(self):
-        """
-        Test an immobile Track
-        """
+        test_subject = models.Subject.objects.get(id='9342973f-b369-4d21-9f1f-ae89d523e05a')
+        print (test_subject)
 
-        analyzer = ImmobilityAnalyzer()
-        analyzer_result = analyzer.analyze(self.immobile_track)
+        r = ia.analyze()
 
-        actual = analyzer_result.level
-        expected = CRITICAL
+        print(r)
 
-        self.assertEqual(actual, expected)
 
-    def xtest_immobility_analyzer_is_warning_immobile(self):
-        """
-        Test an immobile Track
-        """
-        analyzer = ImmobilityAnalyzer()
-        analyzer_result = analyzer.analyze(self.immobile_track)
-
-        expected = CRITICAL
-        actual = analyzer_result.level
-
-        self.assertEqual(actual, expected, "actual value: {}".format(analyzer_result.value))
-
-    def xtest_immobility_analyzer_is_nominal_with_a_few_outliers_but_less_than_threshold_ratio(self):
-        """
-        Test an immobile Track with a few outliers, but less than the threshold ratio.  Should still
-        be classified as immobile.
-        """
-
-        analyzer = ImmobilityAnalyzer()
-        analyzer_result = analyzer.analyze(self.immobile_track_with_outliers)
-
-        expected = WARNING
-        actual = analyzer_result.level
-
-        self.assertEqual(actual, expected, "actual value: {}".format(analyzer_result.value))
-
-    def xtest_immobility_analyzer_is_warning_with_more_outliers_than_the_threshold_ratio(self):
-        """
-        Test an immobile Track with enough outliers to exceed threshold ratio.
-        """
-
-        analyzer = ImmobilityAnalyzer()
-        analyzer_result = analyzer.analyze(self.immobile_track_with_outliers)
-
-        expected = NOMINAL
-        actual = analyzer_result.level
-
-        self.assertEqual(actual, expected, "actual value: {}".format(analyzer_result.value))
