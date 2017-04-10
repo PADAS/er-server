7be9dff0d74f1e8500dedd4b3544fd7ede4cd6b4
Matt Korwel <matt.korwel@gmail.com>
2017-04-10 16:55:02 -0700

    Moving from das to app for portability. MK GM

--- docker/managelocal.sh
+++ docker/managelocal.sh
@@ -11,9 +11,9 @@ DOCKER_COMMAND="exec"
 if [[ -n "$COMMAND" ]]; then
     CID=$(docker ps -q -f name=$CONTAINER_NAME)
     if [[ -z $CID ]]; then
-        docker run -it --entrypoint="python3 /var/www/das/manage.py $COMMAND --settings=das_server.local_settings_docker" $IMAGE_NAME 
+        docker run -it --entrypoint="python3 /var/www/app/manage.py $COMMAND --settings=das_server.local_settings_docker" $IMAGE_NAME 
     else
-        docker exec -it $CONTAINER_NAME python3 /var/www/das/manage.py $COMMAND --settings=das_server.local_settings_docker
+        docker exec -it $CONTAINER_NAME python3 /var/www/app/manage.py $COMMAND --settings=das_server.local_settings_docker
     fi
 
 else
