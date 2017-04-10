29764959a3039c4e2e41304ad10c90bf54353cc9
Matt Korwel <matt.korwel@gmail.com>
2017-04-10 14:11:42 -0700

    Updating to actual remotes. MK

--- docker/keys/push.sh
+++ docker/keys/push.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker login -e 1234@5678.com -u _json_key -p "$(cat $DIR/push.json)" https://gcr.io
+docker login -e 1234@5678.com -u _json_key -p "$(cat $DIR/../../../das-push.json)" https://gcr.io
 
-docker push gcr.io/padas-app/base
-docker push gcr.io/padas-app/postgis
-docker push gcr.io/padas-app/api
-docker push gcr.io/padas-app/rt_api
-docker push gcr.io/padas-app/beat
-docker push gcr.io/padas-app/worker
-docker push gcr.io/padas-app/mql
-docker push gcr.io/padas-app/nginx
-docker push gcr.io/padas-app/web
+docker push gcr.io/padas-app/base:latest
+docker push gcr.io/padas-app/postgis:latest
+docker push gcr.io/padas-app/api:latest
+docker push gcr.io/padas-app/rt_api:latest
+docker push gcr.io/padas-app/beat:latest
+docker push gcr.io/padas-app/worker:latest
+docker push gcr.io/padas-app/mql:latest
+docker push gcr.io/padas-app/nginx:latest
+docker push gcr.io/padas-app/web:latest
