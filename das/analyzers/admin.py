75370cfe76f97df9d31f1dd0dcc0cc41b39edcb0
Chris Doehring <chrisdoehring@gmail.com>
2017-04-28 14:51:14 -0700

    Update admin page for Immobility Analyzer.

--- das/analyzers/admin.py
+++ das/analyzers/admin.py
@@ -3,17 +3,15 @@ from django.contrib.admin import ModelAdmin
 from django.conf import settings
 import analyzers.models as models
 
-if settings.DEBUG:
+@admin.register(models.ObservationAnnotator)
+class ObservationAnnotatorAdmin(admin.ModelAdmin):
+    pass
 
-    @admin.register(models.ObservationAnnotator)
-    class ObservationAnnotatorAdmin(admin.ModelAdmin):
-        pass
+@admin.register(models.ImmobilityAnalyzer)
+class ImmobilityAnalyzerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'subject_group_name',)
 
-    @admin.register(models.ImmobilityAnalyzer)
-    class ImmobilityAnalyzerAdmin(admin.ModelAdmin):
-        list_display = ('id', 'name', 'subject_group_name',)
+    search_fields = ('subject_group__name',)
 
-        search_fields = ('subject_group__name',)
-
-        def subject_group_name(self, o):
-            return o.subject_group.name
+    def subject_group_name(self, o):
+        return o.subject_group.name
