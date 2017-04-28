e150612bd47021270a299cde1da205ebe7da838e
Chris Doehring <chrisdoehring@gmail.com>
2017-04-27 18:44:00 -0700

    Adding admin for ImmobilityAnalyzer.

--- das/analyzers/admin.py
+++ das/analyzers/admin.py
@@ -3,26 +3,17 @@ from django.contrib.admin import ModelAdmin
 from django.conf import settings
 import analyzers.models as models
 
-# if settings.DEBUG:
-#
-#     @admin.register(models.GeofenceAnalyzer)
-#     class GeofenceAnalyzerAdmin(ModelAdmin):
-#         pass
-#
-#     @admin.register(models.SubjectAnalyzer)
-#     class SubjectAnalyzerAdmin(admin.ModelAdmin):
-#         pass
-#
-#     @admin.register(models.ObservationAnnotator)
-#     class ObservationAnnotatorAdmin(admin.ModelAdmin):
-#         pass
-#
-#     @admin.register(models.ImmobilityAnalyzer)
-#     class ImmobilityAnalyzerAdmin(admin.ModelAdmin):
-#         list_display = ('id', 'name', 'get_subject_name',
-#                         )
-#
-#         search_fields = ('subject__name',)
-#
-#         def get_subject_name(self, o):
-#             return o.subject.name
+if settings.DEBUG:
+
+    @admin.register(models.ObservationAnnotator)
+    class ObservationAnnotatorAdmin(admin.ModelAdmin):
+        pass
+
+    @admin.register(models.ImmobilityAnalyzer)
+    class ImmobilityAnalyzerAdmin(admin.ModelAdmin):
+        list_display = ('id', 'name', 'subject_group_name',)
+
+        search_fields = ('subject_group__name',)
+
+        def subject_group_name(self, o):
+            return o.subject_group.name
