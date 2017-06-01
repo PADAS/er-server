a7cea2618f71c7cf5fe2c405eda81c08cbff6bd7
Chris Doehring <chrisdoehring@gmail.com>
2017-06-01 15:36:10 -0700

    Fix model attributes for analyzer models.

--- das/analyzers/models/__init__.py
+++ das/analyzers/models/__init__.py
@@ -1,3 +1,4 @@
-from .base import SubjectAnalyzerConfig, SubjectAnalyzerResult, OK, CRITICAL, WARNING
-from .annotations import ObservationAnnotator
-from .immobility import ImmobilityAnalyzerConfig
+from analyzers.models.base import SubjectAnalyzerConfig, SubjectAnalyzerResult, OK, CRITICAL, WARNING
+from analyzers.models.annotations import ObservationAnnotator
+from analyzers.models.immobility import ImmobilityAnalyzerConfig
+
