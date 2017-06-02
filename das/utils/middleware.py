fd3ab81a98f4c67e88be8eb7a7d088f446499525
chrisj <chrisj@vulcan.com>
2017-06-02 16:09:48 -0700

    Change logging to output to json. Use the python-json-logger package to format. Update our request logger to output using a dictionary of values instead of putting all of the data in a message field. Default logging will output only to stdout.
    Include first run of autopep8.

--- das/utils/middleware.py
+++ das/utils/middleware.py
@@ -20,7 +20,7 @@ class RequestLoggingMiddleware(object):
             logname = '-'
             remote_addr = request.META.get('REMOTE_ADDR')
             remote_addr = request.META.get(
-                    'HTTP_X_FORWARDED_FOR') or remote_addr
+                'HTTP_X_FORWARDED_FOR') or remote_addr
             user_id = '-'
             if hasattr(request, 'user'):
                 user_id = getattr(request.user, 'id', '-')
@@ -28,19 +28,33 @@ class RequestLoggingMiddleware(object):
                 req_time = time.time() - self.start_time
             except AttributeError:
                 req_time = 0
-            content_len = len(getattr(response, 'content', []))
+            content_length = len(getattr(response, 'content', []))
             referer = request.META.get('HTTP_REFERER', '')
-            agent = request.META.get('HTTP_USER_AGENT', '')
+            user_agent = request.META.get('HTTP_USER_AGENT', '')
             status = response.status_code
             path = request.get_full_path()
             method = request.method
             protocol = request.META.get('SERVER_PROTOCOL', '')
 
-            request_info = '{0} {1} {2}'.format(method, path, protocol)
+            extra = dict(
+                remote_addr=remote_addr,
+                user_id=user_id,
+                req_time=req_time,
+                content_length=content_length,
+                referer=referer,
+                user_agent=user_agent,
+                status=status,
+                path=path,
+                method=method,
+                protocol=protocol,
+            )
 
-            self.logger.info('%s %s %s [] "%s" %s %s "%s" "%s" (%.02f seconds)' % (
-            remote_addr, logname, user_id, request_info,
-            status, content_len, referer, agent, req_time))
+            request_info = '{0} {1} {2}'.format(method, path, protocol)
+            method = '%s %s %s [] "%s" %s %s "%s" "%s" (%.02f seconds)' % (
+                remote_addr, logname, user_id, request_info,
+                status, content_length, referer, user_agent, req_time)
+
+            self.logger.info('request', extra=extra)
 
         except Exception as e:
             logging.exception('RequestLoggingMiddleware Error')
