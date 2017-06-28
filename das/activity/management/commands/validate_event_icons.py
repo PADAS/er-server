5ccfe8151da872729b802ececb9ba97c5b1b577e
Chris Doehring <chrisdoehring@gmail.com>
2017-06-28 14:22:17 -0700

    Use logger, and print warning or error when appropriate.

--- das/activity/management/commands/validate_event_icons.py
+++ das/activity/management/commands/validate_event_icons.py
@@ -1,7 +1,10 @@
+import logging
 from django.core.management.base import BaseCommand
 from django.contrib.staticfiles import finders
 
 from activity.models import Event, EventType
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -12,12 +15,14 @@ class Command(BaseCommand):
 
         for et in EventType.objects.all():
 
-            print('Validating images for EvenType {}'.format(et.value))
+            logger.info('Validating images for EvenType {}'.format(et.value))
             for priority, priority_name in Event.PRIORITY_CHOICES:
                 for state, state_name in Event.STATE_CHOICES:
                     image = Event.marker_icon(et.value, priority, state)
                     image = image[8:]
 
-                    print('{0} -> {1}'.format(et.value, image))
+                    if 'generic' in image:
+                        logger.warning('{0} -> {1}'.format(et.value, image))
+
                     if not finders.find(image):
-                        print('\tMissing image {0}'.format(image))
+                        logger.error('\tMissing image {0}'.format(image))
