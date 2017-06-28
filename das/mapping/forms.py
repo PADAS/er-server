ce10654dcb4e73af091ab6173cb5333dfaa6a21f
Shawn Hickey <shawnh@vulcan.com>
2017-06-28 16:45:25 -0700

    Bugfix in custom latlon form

--- das/mapping/forms.py
+++ das/mapping/forms.py
@@ -23,19 +23,37 @@ class MapCenterForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.coordinates = self.initial.get('center', None)
-        if isinstance(self.coordinates, Point):
-            self.initial['latitude'], self.initial['longitude'] = self.coordinates.tuple
+        self.starting_center = self.initial.get('center', None)
+        if not isinstance(self.starting_center, Point):
+            self.data['center'] = Point(0, 0)
+            self.starting_center = self.data['center']
+
+        if 'longitude' not in self.initial:
+            self.initial['longitude'], self.initial['latitude'] = self.starting_center.tuple
 
     def clean(self):
         data = super().clean()
-        map_control_center = data.get('center')
-        # If they've manipulated the map, use the coordinates from that.
-        # However, if they haven't touched the map, AND have entered new
-        # coordinates manually, then we want to use the new coordinates
-        if map_control_center.tuple == self.coordinates.tuple:
-            latitude = data.get('latitude', None)
-            longitude = data.get('longitude', None)
-            if latitude != self.initial['latitude'] or longitude != self.initial['longitude']:
-                data['center'] = Point(latitude, longitude)
+        map_control_center = data.get('center', None)
+        latitude = data.get('latitude', None)
+        longitude = data.get('longitude', None)
+        if latitude and longitude:
+            manual_center = Point(float(longitude), float(latitude))
+        else:
+            manual_center = None
+
+        # If map control center exists and it's changed from the starting center
+        # Use the data specified in the map control
+        if map_control_center and not self.samepoint(map_control_center, self.starting_center):
+            data['center'] = map_control_center
+        # If the map control has not been changed, see if the manual latlon has
+        # been changed, and use those as the new values
+        elif manual_center and not self.samepoint(manual_center, self.starting_center):
+            data['center'] = manual_center
+        else:
+            pass
+
         return data
+
+    def samepoint(self, point_a, point_b):
+        import math
+        return math.isclose(point_a.x, point_b.x, rel_tol=1e-10) and math.isclose(point_a.y, point_b.y, rel_tol=1e-10)
