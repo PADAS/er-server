7ea48761a7ae2d143e8d542b6c1b870b59679f13
Chris Doehring <chrisdoehring@gmail.com>
2017-07-08 15:17:24 -0700

    Code cleanup in get_alerts.

--- das/activity/alerts.py
+++ das/activity/alerts.py
@@ -8,20 +8,22 @@ def get_alert_users(priority):
     if not isinstance(priority, (list, set)):
         priority = [priority, ]
 
-    map = {
+    priority_to_permissionset = {
         Event.PRI_URGENT: settings.NOTIFY_HIGH_PRIORITY_EVENT,
         Event.PRI_IMPORTANT: settings.NOTIFY_MEDIUM_PRIORITY_EVENT,
         Event.PRI_REFERENCE: settings.NOTIFY_LOW_PRIORITY_EVENT,
     }
-    pset_names = [map.get(int(p), None) for p in priority]
-    pset_names = set([_ for _ in pset_names if _ is not None])
+    pset_names = [priority_to_permissionset.get(
+        int(p), None) for p in priority]
+    pset_names = set([v for v in pset_names if v is not None])
     return get_users_in_permission_sets(pset_names)
 
 
-def get_users_in_permission_sets(names):
+def get_users_in_permission_sets(permissionset_names):
 
-    if not names:
+    if not permissionset_names:
         return User.objects.none()
 
-    permission_sets = PermissionSet.objects.filter(name__in=names)
+    permission_sets = PermissionSet.objects.filter(
+        name__in=permissionset_names)
     return User.objects.filter(permission_sets__in=permission_sets).distinct().order_by('username')
