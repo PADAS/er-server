185156894fdd1c4eba4703e9dc1d8c0c01b2b95c
Chris Jones <chrisj@merledog.com>
2017-07-20 11:55:24 -0700

    Update __init__.py

--- das/das_server/__init__.py
+++ das/das_server/__init__.py
@@ -9,4 +9,4 @@ VERSION = (1, 15, 2, 'dev', BUILD_NUMBER)
 if VERSION[3]:  # pragma: no cover
     __version__ = '{0}.{1}.{2}-{3}.{4}'.format(*VERSION)
 else:  # pragma: no cover
-    __version__ = '.'.join(map(str, VERSION[0:2]))
+    __version__ = '.'.join(map(str, VERSION[0:3]))
