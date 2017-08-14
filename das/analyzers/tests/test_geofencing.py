df38ba0f09c24c018bf1ab9e89b8680aeb2b2f21
Chris Doehring <chrisdoehring@gmail.com>
2017-08-14 10:18:12 -0700

    Load fixture in geofence integration test.

--- das/analyzers/tests/test_geofencing.py
+++ das/analyzers/tests/test_geofencing.py
@@ -2,7 +2,8 @@ import logging
 from analyzers.models import SubjectAnalyzerResult, GeofenceAnalyzerConfig
 from analyzers.geofence import GeofenceAnalyzer
 #from django.test import TestCase
-from unittest import TestCase  # Use python unit test here to persist results in test DB
+# Use python unit test here to persist results in test DB
+from unittest import TestCase
 from django.core import management
 logger = logging.getLogger(__name__)
 
@@ -16,6 +17,8 @@ from analyzers.tasks import analyze_subject
 
 class TestGeofenceAnalyzer(TestCase):
 
+    fixtures = ['analyzer_eventtype.yaml', ]
+
     def setUp(self):
 
         # Load the geojson files into the database
@@ -27,13 +30,15 @@ class TestGeofenceAnalyzer(TestCase):
     def test_geofence_integration(self):
         """ Test the functioning of the geofence algorithm logic"""
 
-
         # Create models (Subject, SubjectSource and Source)
-        sub = Subject.objects.create(name='Olchoda', subject_type='wildlife', subject_subtype='elephant')
+        sub = Subject.objects.create(
+            name='Olchoda', subject_type='wildlife', subject_subtype='elephant')
         source = Source.objects.create(manufacturer_id='007')
-        SubjectSource.objects.create(subject=sub, source=source, assigned_range=DEFAULT_ASSIGNED_RANGE)
+        SubjectSource.objects.create(
+            subject=sub, source=source, assigned_range=DEFAULT_ASSIGNED_RANGE)
 
-        sg = SubjectGroup.objects.create(name='geofence_subject_analyzer_group', )
+        sg = SubjectGroup.objects.create(
+            name='geofence_subject_analyzer_group', )
         sg.subjects.add(sub)
         sg.save()
 
@@ -44,32 +49,41 @@ class TestGeofenceAnalyzer(TestCase):
         for item in time_shift(test_observations):
             recorded_at = item['recorded_at']
             location = Point(x=item['longitude'], y=item['latitude'])
-            Observation.objects.create(recorded_at=recorded_at, location=location, source=source, additional={})
+            Observation.objects.create(
+                recorded_at=recorded_at, location=location, source=source, additional={})
 
-        # Create a SpatialFeatureGroupStatic group with the 'Ol Donyo Farm 2' geofence
-        geofences = SpatialFeature.objects.filter(name__iexact='Ol Donyo Farm 2')
+        # Create a SpatialFeatureGroupStatic group with the 'Ol Donyo Farm 2'
+        # geofence
+        geofences = SpatialFeature.objects.filter(
+            name__iexact='Ol Donyo Farm 2')
         logger.info('Geofence count: %s' % str(len(geofences)))
-        gf_grp = SpatialFeatureGroupStatic.objects.create(name='Mara Geofences',)
+        gf_grp = SpatialFeatureGroupStatic.objects.create(
+            name='Mara Geofences',)
         gf_grp.features.add(*geofences)
         gf_grp.save()
 
         # Create a containment regions grp
-        contain_rgns = SpatialFeature.objects.filter(name='Pardamat Conservancy')
+        contain_rgns = SpatialFeature.objects.filter(
+            name='Pardamat Conservancy')
         logger.info('Containment region count: %s' % str(len(contain_rgns)))
-        cr_grp = SpatialFeatureGroupStatic.objects.create(name='Geofence Containment Regions',)
+        cr_grp = SpatialFeatureGroupStatic.objects.create(
+            name='Geofence Containment Regions',)
         cr_grp.features.add(*contain_rgns)
         cr_grp.save()
 
         logger.info('got here...')
 
         # Create the Geofence Analyzer Config object
-        config = GeofenceAnalyzerConfig.objects.create(subject_group=sg, geofences=gf_grp, containment_regions=cr_grp)
+        config = GeofenceAnalyzerConfig.objects.create(
+            subject_group=sg, geofences=gf_grp, containment_regions=cr_grp)
 
         # Run the analyzer
         analyze_subject(str(sub.id))
 
-        # There should be several Geofence Breaks fom this analysis. Query and assert number
-        results = SubjectAnalyzerResult.objects.all()  # Todo: not sure how to get results specific to this subject?
+        # There should be several Geofence Breaks fom this analysis. Query and
+        # assert number
+        # Todo: not sure how to get results specific to this subject?
+        results = SubjectAnalyzerResult.objects.all()
         logger.info('There were %s geofence breaks' % len(results))
-        #assert(len(results)>0)
+        # assert(len(results)>0)
         assert (True)
