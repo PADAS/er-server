a785fb067d4a372af6e4ea147b7b67f6406e37e4
Matt Skone <matts@vulcan.com>
2017-09-08 10:59:52 -0700

    WIP: merged and updated /ci contents from Skylight -MS

---
+++ ci/environments/create.dev.environment.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -e
+
+############################################################################
+# 
+# This is to be run by humans
+# This attempts to create an end to end environment with:
+# - a ci pipeline
+# - a k8s cluster
+# - disks with a standard known name
+# - All these things wired together
+#
+#   We know this is not the solution we want to have this and the infra repo dependent on each other
+#   And to have limited access and several other reasons we haven't done this
+#   Much like the delete cluster script, its a matter of pragmatism. We have to do this anyway
+#   The more automated, the safer.
+#
+# ARGS:
+#  1: SERVER_BRANCH_NAME=REQUIRED Which vic_server project branch to pull from
+############################################################################
+
+if [ $# -eq 0 ]; then
+    echo "Create a pipeline, Create a cluster, and Enable that pipeline to deploy to that cluster"
+    echo "USAGE:"
+    echo "  1: SERVER_BRANCH_NAME=REQUIRED Which vic_server project branch to pull from"
+    echo "  Note: SERVER_BRANCH_NAME will also be the name of the k8s cluster and Concoures pipeline that are created"
+    echo "  Note: SERVER_BRANCH_NAME will override the 'server-branch-name' setting in your custom params file, if you created one"
+    echo "  Note: Google does not like _ and special characters in cluster name lowercase leters/numbers with - is best"
+    echo "This script can be run from anywhere."
+    exit 1
+fi
+
+DEV_CREATE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT=padas-app
+SERVER_BRANCH_NAME=$1
+
+##################################################################################
+# Create CI Pipeline
+##################################################################################
+$DEV_CREATE_DIR/includes/set.dev.pipeline.happy.path.sh $SERVER_BRANCH_NAME
+fly -t $PROJECT unpause-pipeline -p $SERVER_BRANCH_NAME
+
+# NOT CREATING DISKS. THE CI BUILD ALREADY DOES THAT, BETTER TO HAVE A RECORD OF THEM THERE
+# BEFORE TO LONG WE CAN HAVE THE CI BUILD CREATE THE CLUSTER TO AND THEN THIS ISN'T NEEDED
+
+##################################################################################
+# Create Cluster
+##################################################################################
+LABELS="creator=$(whoami),environment-name=$SERVER_BRANCH_NAME,source-type=create-dev-env"
+$DEV_CREATE_DIR/../../../infrastructure/resources/k8s/create.gcp.cluster.sh $PROJECT $SERVER_BRANCH_NAME n1-highmem-4 6 false skip $LABELS
---
+++ ci/environments/destroy.dev.environment.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -e
+
+############################################################################
+# 
+# This is to be run by humans
+# This attempts to destroy an end to end environment with:
+# - a ci pipeline
+# - a k8s cluster
+# - disks with a standard known name
+# - All these things wired together
+#
+#   We know this is not the solution we want to have this and the infra repo dependent on each other
+#   And to have limited access and several other reasons we haven't done this
+#   Much like the delete cluster script, its a matter of pragmatism. We have to do this anyway
+#   The more automated, the safer.
+#
+# ARGS:
+#  1: ENVIONMENT_NAME=REQUIRED What is the name of the pipeline to remove
+############################################################################
+
+if [ $# -eq 0 ]; then
+    echo "Delete a dev pipeline, cluster, and disks"
+    echo "USAGE:"
+    echo "  1: ENVIONMENT_NAME=REQUIRED What is the name of the envionment to destroy: This will be branch name/cluster name/pipeline name"
+    echo "This script can be run from anywhere."
+    exit 1
+fi
+
+DEV_CREATE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT=padas-app
+ENVIONMENT_NAME=$1
+
+##################################################################################
+# Delete CI Pipeline
+##################################################################################
+fly -t $PROJECT dp -p $ENVIONMENT_NAME
+
+##################################################################################
+# Delete Cluster
+##################################################################################
+INFRA_DIR_X="$DEV_CREATE_DIR/../../../infrastructure"
+$INFRA_DIR_X/resources/k8s/delete.gcp.cluster.sh $PROJECT $ENVIONMENT_NAME
+
+##################################################################################
+# Delete Disks
+##################################################################################
+DEFAULT_DISKS_LOCATION="$DEV_CREATE_DIR/../../deployment/storage/default.disks.csv"
+$INFRA_DIR_X/resources/cloud/gcp/disks/delete.from.csv.sh $PROJECT $ENVIONMENT_NAME $DEFAULT_DISKS_LOCATION
