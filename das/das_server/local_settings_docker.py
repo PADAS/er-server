2c6518c1a2bfe49f0a706fc1bed0b26a4e7a438b
Chris Doehring <chrisdoehring@gmail.com>
2017-09-20 13:18:52 -0700

    Reverse inadvertent change in comment.

--- das/das_server/local_settings_docker.py
+++ das/das_server/local_settings_docker.py
@@ -26,7 +26,7 @@ CSRF_COOKIE_SECURE = False
 
 STATIC_URL = '/static/'
 
-# add the path to your local copy of the das-web static root dir that contains realtime.html
+# add the path to your local copy of the das-web static root dir that contains index.html
 #STATICFILES_DIRS = STATICFILES_DIRS + (os.path.join(BASE_DIR, 'www'),)
 
 """
