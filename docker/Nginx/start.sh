c57ac57e676735f9e531d1b4cf544c52888f8035
Chris Jones <chrisj@vulcan.com>
2017-09-20 15:03:52 -0700

    fix start.sh for nginx

--- docker/Nginx/start.sh
+++ docker/Nginx/start.sh
@@ -2,8 +2,8 @@
 
 WEB_SERVICE_NAME=${WEB_SERVICE_NAME-web}
 
-if ! grep -q "upstream web_server" /etc/nginx/default.conf; then
-  (echo ""; echo "upstream web_server{ server $WEB_SERVICE_NAME:9000; }") >> /etc/nginx/default.conf
+if ! grep -q "upstream web_server" /etc/nginx/sites-available/default.conf; then
+  (echo ""; echo "upstream web_server{ server $WEB_SERVICE_NAME:9000; }") >> /etc/nginx/sites-available/default.conf
 fi
 
 /usr/sbin/nginx -c /etc/nginx/nginx.conf -g "daemon off;"
