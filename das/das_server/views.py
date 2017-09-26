74166e0ada9763d76f6bf5468d62985fbcc5188b
Chris Doehring <chrisdoehring@gmail.com>
2017-09-26 09:59:51 -0700

    Add event_search_enabled to API status response.

--- das/das_server/views.py
+++ das/das_server/views.py
@@ -16,6 +16,8 @@ class VersionSerializer(rest_framework.serializers.Serializer):
     version = rest_framework.serializers.CharField(read_only=True)
     event_matrix_enabled = rest_framework.serializers.BooleanField(
         read_only=True)
+    event_search_enabled = rest_framework.serializers.BooleanField(
+        read_only=True)
 
 
 class StatusView(generics.RetrieveAPIView):
