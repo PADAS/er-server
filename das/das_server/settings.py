5020a9eba277ebbbb2f343fd45f16b05bfaff027
Chris Jones <chrisj@vulcan.com>
2017-10-05 12:44:28 -0700

    break out our SuperUserSessionAuthentication class into its own module to address a circular dependency.

--- das/das_server/settings.py
+++ das/das_server/settings.py
@@ -157,7 +157,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'accounts.backends.NoLoginOAuth2Authentication',
-        'utils.drf.SuperUserSessionAuthentication',
+        'utils.authentication.SuperUserSessionAuthentication',
     ),
     #'VIEW_DESCRIPTION_FUNCTION': 'rest_framework_swagger.views.get_restructuredtext',
     'DEFAULT_RENDERER_CLASSES': (
