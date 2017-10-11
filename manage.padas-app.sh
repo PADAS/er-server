e9d6265c5df094421dc43df9f635baf90a1b7821
Caine Jette <C-CaineJ@vulcan.com>
2017-10-11 11:18:34 -0700

    Update manage.padas-app.sh to mount ci and deployment directories, bump platform tools version

--- manage.padas-app.sh
+++ manage.padas-app.sh
@@ -2,13 +2,13 @@
 
 ###############################################################
 # Manage the padas-app infrastructure resources
-#
 ###############################################################
 
+MANAGE_PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT=padas-app
 CONCOURSE_URL=https://35.197.64.22
 
-IMAGE_NAME=gcr.io/ss-infrastructure-public/platform/tools:0.0.7
+IMAGE_NAME=gcr.io/ss-infrastructure-public/platform/tools:0.0.15
 CONTAINER_NAME=vp_tools_$PROJECT
 VAULT_ADDR=https://35.197.70.36:8200
 
@@ -20,16 +20,18 @@ if [ ! "$(docker ps -aq -f status=exited -f name=$CONTAINER_NAME)" ]; then
         -e CONCOURSE_URL=$CONCOURSE_URL \
         --name $CONTAINER_NAME \
         -v $CONTAINER_NAME-root:/root \
-        -v $(pwd):/workdir \
-        --entrypoint /run/startup.sh \
-        $IMAGE_NAME
-else
-    docker run -it --rm \
-        --volumes-from $CONTAINER_NAME \
-        -e PROJECT=$PROJECT \
-        -e CONCOURSE_URL=$CONCOURSE_URL \
-        -e VAULT_ADDR=$VAULT_ADDR \
-        -e VAULT_SKIP_VERIFY=true \
-        -v $(pwd):/workdir \
+        --entrypoint run/startup.sh \
         $IMAGE_NAME
 fi
+
+docker run -it --rm \
+    --volumes-from $CONTAINER_NAME \
+    -e PROJECT=$PROJECT \
+    -e CONCOURSE_URL=$CONCOURSE_URL \
+    -e VAULT_ADDR=$VAULT_ADDR \
+    -e VAULT_SKIP_VERIFY=true \
+    -e USERNAME=$(whoami) \
+    -v $MANAGE_PROJECT_DIR/ci:/vulcan-platform-tools/ci \
+    -v $MANAGE_PROJECT_DIR/deployment:/vulcan-platform-tools/deployment \
+    -v $(pwd):/vulcan-platform-tools/workdir \
+    $IMAGE_NAME
