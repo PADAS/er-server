8e83bbde7e16fb5deec002e77a89332ed296e267
Chris Jones <chrisj@merledog.com>
2017-10-28 14:13:44 -0700

    bump up to version 1.23.1

--- das/das_server/__init__.py
+++ das/das_server/__init__.py
@@ -4,7 +4,7 @@ try:
 except ImportError:
     BUILD_NUMBER = 1
 
-VERSION = (1, 22, 1, 'ct', BUILD_NUMBER)
+VERSION = (1, 23, 1, 'dev', BUILD_NUMBER)
 
 if VERSION[3]:  # pragma: no cover
     __version__ = '{0}.{1}.{2}-{3}.{4}'.format(*VERSION)
