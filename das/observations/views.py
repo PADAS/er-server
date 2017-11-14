c507a8f927e80c21f681468838c6e753f9fa4306
Shawn Hickey <shawnh@vulcan.com>
2017-11-14 11:23:21 -0800

    Add another header field that only lists the filename instead of content-disposition which has the filename and some other info

--- das/observations/views.py
+++ das/observations/views.py
@@ -511,13 +511,15 @@ class ObservationsView(generics.ListCreateAPIView):
 
 
 def render_to_kmz(kml_str, filename):
+    full_filename = '{}.kmz'.format(filename)
     zip_io = BytesIO()
     with zipfile.ZipFile(zip_io, mode='w', compression=zipfile.ZIP_DEFLATED) as kmz:
         kmz.writestr('document.kml', kml_str.encode('utf-8'))
     response = Response(zip_io.getvalue(),
                         content_type='application/vnd.google-earth.kmz')
-    response['Content-Disposition'] = 'attachment; filename={}.kml'.format(
-        filename)
+    response['Content-Disposition'] = 'attachment; filename={}'.format(
+        full_filename)
+    response['x-das-download-filename'] = full_filename
     response['Content-Length'] = zip_io.tell()
     return response
 
