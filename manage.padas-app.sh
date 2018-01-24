fa491f3157702cce60f27bf1fb1beaf1192c3bd2
Seth <sarudak@gmail.com>
2018-01-24 10:20:52 -0800

    Updated manage script

--- manage.padas-app.sh
+++ manage.padas-app.sh
@@ -11,10 +11,14 @@ K8S_PROXY_PORT=$1
 MANAGE_PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT=padas-app
 CONCOURSE_URL=https://ci.pamdas.org
-TOOLS_VERSION=0.0.146
+ELASTIC_URL=35.203.167.191
+TOOLS_VERSION=0.0.178
 
 ### DO NOT EDIT BELOW THIS LINE
 ### Below this line is generic copy pasted from the master in infrastructure
+
+# If this is set we will serve up 8001 in this container to this port on the host
+K8S_PROXY_PORT=$1
 
 IMAGE_NAME=gcr.io/ss-infrastructure-public/platform/tools:$TOOLS_VERSION
 CONTAINER_NAME=vp_tools_$PROJECT
@@ -22,9 +26,8 @@ VAULT_ADDR=https://35.197.70.36:8200
 
 function forward_port_if_set()
 {
-    local PROXY_PORT=$1
-    if [ -n "$PROXY_PORT" ]; then
-        echo "-p$PROXY_PORT:8001"
+    if [ -n "$K8S_PROXY_PORT" ]; then
+        echo "-p$K8S_PROXY_PORT:8001"
     else
         echo "-eCANNOT_PROXY=true"
     fi
@@ -36,6 +39,7 @@ if [ ! "$(docker ps -aq -f status=exited -f name=$CONTAINER_NAME)" ]; then
         -e VAULT_ADDR=$VAULT_ADDR \
         -e VAULT_SKIP_VERIFY=true \
         -e CONCOURSE_URL=$CONCOURSE_URL \
+        -e ELASTIC_URL=$ELASTIC_URL \
         --name $CONTAINER_NAME \
         -v $CONTAINER_NAME-root:/root \
         --entrypoint run/startup.sh \
@@ -52,11 +56,13 @@ docker run -it --rm \
     --volumes-from $CONTAINER_NAME \
     -e PROJECT=$PROJECT \
     -e CONCOURSE_URL=$CONCOURSE_URL \
+    -e ELASTIC_URL=$ELASTIC_URL \
     -e VAULT_ADDR=$VAULT_ADDR \
     -e VAULT_SKIP_VERIFY=true \
     -e K8S_PROXY_PORT=$K8S_PROXY_PORT \
     "$(forward_port_if_set $K8S_PROXY_PORT)" \
     -e USERNAME=$(whoami) \
+    -e TOOLS_CONTAINER_VERSION=$TOOLS_VERSION \
     -v $MANAGE_PROJECT_DIR/ci:/vulcan-platform-tools/ci \
     -v $MANAGE_PROJECT_DIR/deployment:/vulcan-platform-tools/deployment \
     -v $(pwd):/vulcan-platform-tools/workdir \
