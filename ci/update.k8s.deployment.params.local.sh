aadcb21e161429f021cd0ac12c7d5804e2e358eb
Chris Jones <ChrisJ@vulcan.com>
2018-02-26 16:02:12 -0800

    update shared services tools version.

--- ci/update.k8s.deployment.params.local.sh
+++ ci/update.k8s.deployment.params.local.sh
@@ -13,7 +13,7 @@ OUTPUT_DIR=${1:?Output Dir Required}
 INPUT_DIR=${2:?Input Dir Required}
 export GIT_SERVER_SHA=${3:?Server SHA Required}
 export GIT_WEB_SHA=${4:?WEB SHA Required}
-export GIT_UTILITY_SHA=${6:?Utility SHA Required}
+export GIT_UTILITY_SHA=${5:?Utility SHA Required}
 
 echo "OUTPUT_DIR=$OUTPUT_DIR"
 echo "INPUT_DIR=$INPUT_DIR"
