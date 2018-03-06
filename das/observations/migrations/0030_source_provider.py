c4f72efb673d6f3c375ed0bade0c971fd9cf41d4
Chris Doehring <chrisdoehring@gmail.com>
2018-03-06 09:53:43 -0800

    Another coding error in early source provider migration.

--- das/observations/migrations/0030_source_provider.py
+++ das/observations/migrations/0030_source_provider.py
@@ -29,7 +29,8 @@ SOURCE_PROVIDER_UPDATE = '''with provider as (select id, name from observations_
                     '''
 
 INSERT_DEFAULT_SOURCEPROVIDER = '''
-INSERT INTO observations_sourceprovider (id, name) values ('{provider_id}', '{name}')
+INSERT INTO observations_sourceprovider (created_at, updated_at, id, name) 
+                         values (current_timestamp, current_timestamp, '{provider_id}', '{name}')
 ON CONFLICT DO NOTHING;
 '''.format(provider_id=observations.models.DEFAULT_SOURCE_PROVIDER_ID,
            name=observations.models.DEFAULT_SOURCE_PROVIDER_KEY)
