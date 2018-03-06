ed495e993ee2cbcb5f3454e1b8220a5e2fb0a8b7
Chris Doehring <chrisdoehring@gmail.com>
2018-03-06 09:10:03 -0800

    Add rate_limit on emitting service_status messages.

--- das/rt_api/tasks.py
+++ das/rt_api/tasks.py
@@ -143,7 +143,7 @@ def _broadcast_service_status(service_status_data=None):
         close_old_connections()
 
 
-@celery.app.task(base=QueueOnce, once={'graceful': True, 'timeout': 60})
+@celery.app.task(base=QueueOnce, once={'graceful': True, 'timeout': 60}, rate_limit='4/m')
 def broadcast_service_status():
     _broadcast_service_status()
 
