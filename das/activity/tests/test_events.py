0a0644c2907e789c4971749ddd0fd5428a098452
Chris Jones <ChrisJ@vulcan.com>
2018-03-29 09:04:43 -0700

    Add priority as a column in export csv. DAS-2421

--- das/activity/tests/test_events.py
+++ das/activity/tests/test_events.py
@@ -7,6 +7,7 @@ import copy
 import collections
 import string
 import random
+import io
 
 import django.contrib.auth
 from django.db import transaction
@@ -25,7 +26,7 @@ from core.tests import BaseAPITest
 from choices.models import Choice
 from accounts.models import PermissionSet
 from activity.models import Event, EventAttachment, EventType, EventCategory,\
-    EventRelationship, EventRelationshipType
+    EventRelationship, EventRelationshipType, EventNote
 
 from activity.models import get_sentinel_user
 from activity import views
@@ -92,13 +93,17 @@ class TestEventView(BaseAPITest):
             'all_perms_user', 'das_all_perms@vulcan.com', 'all_perms_user',
             **self.user_const)
 
+        self.notes_line1_prefix = 'note1 text'
+        self.notes_line2_prefix = 'note2 text'
         self.event_data = dict(
             message=lorem_ipsum.paragraph(),
             time=DateTimeField().to_representation(timezone.now()),
             provenance=Event.PC_SYSTEM,
             event_type=ET_OTHER,
             priority=Event.PRI_REFERENCE,
-            location=dict(longitude='40.1353', latitude='-1.891517')
+            location=dict(longitude='40.1353', latitude='-1.891517'),
+            notes=[{'text': self.notes_line1_prefix + lorem_ipsum.paragraph()},
+                   {'text': self.notes_line2_prefix + lorem_ipsum.paragraph()}]
         )
 
         self.sample_event = self.create_event(self.event_data)
@@ -146,7 +151,7 @@ class TestEventView(BaseAPITest):
     def tearDown(self):
         shutil.rmtree(self.temporary_folder)
 
-    def create_event(self, event_data):
+    def create_event(self, event_data, created_by_user=None):
         data = copy.deepcopy(event_data)
         if 'time' in event_data:
             data['event_time'] = DateTimeField().to_internal_value(
@@ -159,14 +164,21 @@ class TestEventView(BaseAPITest):
         if 'location' in data:
             data['location'] = PointField().to_internal_value(
                 data['location'])
-        return Event.objects.create_event(**data)
 
-        request = self.factory.get(self.api_base + '/events/schema')
-        self.force_authenticate(request, self.all_perms_user)
+        notes = None
+        if 'notes' in data:
+            notes = data['notes']
+            del data['notes']
 
-        response = views.EventSchemaView.as_view()(request)
-        response_data = response.data
-        self.assertEqual(response.status_code, 200)
+        data['created_by_user'] = created_by_user if created_by_user else self.radio_room_user
+
+        event = Event.objects.create_event(**data)
+        if notes:
+            for note in notes:
+                EventNote.objects.create_note(
+                    event=event, created_by_user=event.created_by_user, **note)
+
+        return event
 
     def test_find_all_event_type_icons(self):
 
@@ -788,6 +800,33 @@ class TestEventView(BaseAPITest):
         response = views.EventsView.as_view()(request)
         self.assertEqual(response.status_code, 500)
 
+    def _export_template_response(self, request):
+        return views.EventsExportView.as_view(
+            content_type='text/csv',
+            template_engine='jinja2',
+            template_name='event_export_template.html')(request)
+
+    def test_export_csv(self):
+        event = self.create_event(self.event_data)
+        carcass_data = json.loads("""{"event_details":{"sectionArea":["bbbe77a9-f829-47dd-8a6f-bca76920f706","957a8bfa-ad0d-4b94-bc86-983cab105910"],"team":[],"conservancy":"346f5449-52b0-4b52-9d10-b44b8aa313a6","beginning_of_incident":"2017-10-13 12:00","end_of_incident":"2017-10-14 12:00","details":"interesting details","results_and_findings":"very interesting results and findings","species":"ad26adde-1261-4133-8d3f-a22d12ceae1f","sex":"Male","causeOfDeath":"ab468ffc-9745-4c71-a19d-c34b8c9c3b18"},"event_type":"carcass","priority":200,"title":"Carcass","location":{"latitude":47.65636923655089,"longitude":-122.30770111083983}}""")
+        request = self.factory.post(self.api_base + '/events/', carcass_data)
+        self.force_authenticate(request, self.all_perms_user)
+        response = views.EventsView.as_view()(request)
+        self.assertEqual(response.status_code, 201)
+
+        url = """/activity/events/export"""
+
+        request = self.factory.get(
+            self.api_base + url)
+
+        self.force_authenticate(request, self.all_perms_user)
+        response = self._export_template_response(request)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('Priority' in response.rendered_content)
+        self.assertTrue('Notes' in response.rendered_content)
+        self.assertTrue(self.notes_line2_prefix in response.rendered_content)
+
     def test_export_csv_with_filter(self):
         event = self.create_event(self.event_data)
         carcass_data = json.loads("""{"event_details":{"sectionArea":["bbbe77a9-f829-47dd-8a6f-bca76920f706","957a8bfa-ad0d-4b94-bc86-983cab105910"],"team":[],"conservancy":"346f5449-52b0-4b52-9d10-b44b8aa313a6","beginning_of_incident":"2017-10-13 12:00","end_of_incident":"2017-10-14 12:00","details":"interesting details","results_and_findings":"very interesting results and findings","species":"ad26adde-1261-4133-8d3f-a22d12ceae1f","sex":"Male","causeOfDeath":"ab468ffc-9745-4c71-a19d-c34b8c9c3b18"},"event_type":"carcass_rep","priority":200,"title":"Carcass","location":{"latitude":47.65636923655089,"longitude":-122.30770111083983}}""")
@@ -802,12 +841,8 @@ class TestEventView(BaseAPITest):
             self.api_base + url)
 
         self.force_authenticate(request, self.all_perms_user)
+        response = self._export_template_response(request)
 
-        response = views.EventsExportView.as_view(
-            content_type='text/csv',
-            template_engine='jinja2',
-            template_name='event_export_template.html')(request)
-        response_data = response.rendered_content
         self.assertEqual(response.status_code, 200)
 
     def test_reported_by_filtering(self):
