9a7af2b4e15f348b9ce898181c4c49544da7e9d0
Chris Doehring <chrisdoehring@gmail.com>
2018-07-16 12:09:18 -0700

    Adding templates and widget for icon selection in EvenType admin.

--- das/activity/forms.py
+++ das/activity/forms.py
@@ -1,6 +1,7 @@
 from django.utils.translation import ugettext_lazy as _
 
 from django import forms
+from django.forms.widgets import Widget
 from django.contrib.admin.helpers import ActionForm
 from django.contrib.admin.widgets import FilteredSelectMultiple
 
@@ -27,6 +28,29 @@ class SchemaWidget(forms.Textarea):
         }
 
 
+class IconIdTextInput(Widget):
+    input_type = 'text'
+    template_name = 'admin/activity/eventtype/icon_key_text.html'
+
+    def __init__(self, attrs=None):
+        if attrs is not None:
+            attrs = attrs.copy()
+            self.input_type = attrs.pop('type', self.input_type)
+        super().__init__(attrs)
+
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        context['widget']['type'] = self.input_type
+        return context
+
+    class Media:
+        css = {
+            'all': ('css/icon_key_text.css',),
+        }
+
+
 class EventTypeForm(forms.ModelForm):
     schema = forms.CharField(widget=SchemaWidget(
         attrs={'rows': 30, 'cols': 100}))
+
+    icon = forms.CharField(widget=IconIdTextInput())
