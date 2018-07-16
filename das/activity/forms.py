39909073114e46a491c13b187c98b49a501a5de3
Chris Doehring <chrisdoehring@gmail.com>
2018-07-16 14:06:17 -0700

    Populate image_list for EventType icon widget.

--- das/activity/forms.py
+++ das/activity/forms.py
@@ -1,12 +1,9 @@
-from django.utils.translation import ugettext_lazy as _
+import os
+
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 from django import forms
 from django.forms.widgets import Widget
-from django.contrib.admin.helpers import ActionForm
-from django.contrib.admin.widgets import FilteredSelectMultiple
-
-from observations.models import Subject, Source, SubjectGroup, SubjectSource, SubjectSubType
-from observations.forms_utils import JSONFieldFormMixin, ColorPickerWidget, AssignedDateTimeRangeField
 
 import logging
 logger = logging.getLogger(__name__)
@@ -28,19 +25,21 @@ class SchemaWidget(forms.Textarea):
         }
 
 
-class IconIdTextInput(Widget):
+class IconKeyInput(Widget):
     input_type = 'text'
-    template_name = 'admin/activity/eventtype/icon_key_text.html'
+    template_name = 'admin/activity/eventtype/icon_key_widget.html'
 
-    def __init__(self, attrs=None):
+    def __init__(self, attrs=None, image_list_fn=None):
         if attrs is not None:
             attrs = attrs.copy()
             self.input_type = attrs.pop('type', self.input_type)
+        self.image_list_fn = image_list_fn
         super().__init__(attrs)
 
     def get_context(self, name, value, attrs):
         context = super().get_context(name, value, attrs)
         context['widget']['type'] = self.input_type
+        context['image_list'] = list(self.image_list_fn())
         return context
 
     class Media:
@@ -49,8 +48,20 @@ class IconIdTextInput(Widget):
         }
 
 
+def get_event_icon_select_list(dirname='sprite-src'):
+    icon_list = [
+        {
+            'key': item.split('.')[0],
+            'file_path': staticfiles_storage.url(os.sep.join((dirname, item)))
+        }
+        for item in staticfiles_storage.listdir(dirname)[1]
+    ]
+    return icon_list
+
+
 class EventTypeForm(forms.ModelForm):
     schema = forms.CharField(widget=SchemaWidget(
         attrs={'rows': 30, 'cols': 100}))
 
-    icon = forms.CharField(widget=IconIdTextInput())
+    icon = forms.CharField(required=False,
+                           widget=IconKeyInput(image_list_fn=get_event_icon_select_list))
