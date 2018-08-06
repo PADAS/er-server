1c46d05141c3ed26558ef6a5e5a38c1a6cb722a5
Jay Lorenzo <cxplanet@hotmail.com>
2018-08-06 13:58:49 -0700

    Back to using sources

--- das/sensors/handlers.py
+++ das/sensors/handlers.py
@@ -78,7 +78,7 @@ class GenericSensorHandler():
 
         # Short-circuit if we already have this observation.
         if Observation.objects.filter(source=src, recorded_at=recorded_at).exists():
-            logger.info("Processed duplicate observation %s", subject_subtype, extra={'dup_obs': subject_subtype})
+            logger.info("Processed duplicate observation %s", subject_subtype, extra={'dup_obs': source_type})
             return Response({}, status=status.HTTP_201_CREATED)
 
         observation = {
@@ -91,7 +91,7 @@ class GenericSensorHandler():
         serializer = ObservationSerializer(data=observation)
         if serializer.is_valid():
             serializer.save()
-            logger.info("Added new observation %s", observation, extra={'new_obs': subject_subtype})
+            logger.info("Added new observation %s", observation, extra={'new_obs': source_type})
             notify_new_tracks(src.id)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
@@ -170,7 +170,7 @@ class DasRadioAgentHandler():
 
         # Short-circuit if we already have this observation.
         if Observation.objects.filter(source=src, recorded_at=recorded_at).exists():
-            logger.info("Processed duplicate %s observation", cls.DEFAULT_SUBJECT_SUBTYPE, extra={'dup_obs': cls.DEFAULT_SUBJECT_SUBTYPE})
+            logger.info("Processed duplicate %s observation", cls.DEFAULT_SUBJECT_SUBTYPE, extra={'dup_obs': cls.SOURCE_TYPE})
             return Response({}, status=status.HTTP_201_CREATED)
 
         observation = {
@@ -189,7 +189,7 @@ class DasRadioAgentHandler():
         if serializer.is_valid():
             serializer.save()
             notify_new_tracks(src.id)
-            logger.info("Added new observation %s", observation, extra={'new_obs': cls.DEFAULT_SUBJECT_SUBTYPE})
+            logger.info("Added new observation %s", observation, extra={'new_obs': cls.SOURCE_TYPE})
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -309,7 +309,7 @@ class GsatHandler():
             serializer.save()
 
             notify_new_tracks(src.id)
-
+            logger.info("Processed duplicate %s observation", cls.DEFAULT_SUBJECT_SUBTYPE, extra={'new_obs': cls.SOURCE_TYPE})
             # GSAT service expects 200 and considers anything else bad.
             return Response(serializer.data, status=status.HTTP_200_OK)
 
