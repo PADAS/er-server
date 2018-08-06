f99f71931dc289a40e51ecc8ebacd8031305d880
Chris Jones <chrisj@vulcan.com>
2018-08-06 09:36:49 -0700

    simple /analyzers/spatial api endpoint

--- das/analyzers/serializers.py
+++ das/analyzers/serializers.py
@@ -8,11 +8,36 @@ import analyzers.models
 
 logger = logging.getLogger(__name__)
 
+
+class SubjectGroupSerializer(rest_framework.serializers.Serializer):
+    id = rest_framework.serializers.UUIDField(read_only=True, required=False)
+    name = rest_framework.serializers.CharField(required=True)
+
+
+class SpatialAnalyzerConfigSerializer(rest_framework.serializers.Serializer):
+    id = rest_framework.serializers.UUIDField(read_only=True, required=False)
+    name = rest_framework.serializers.CharField(required=True)
+    notes = rest_framework.serializers.CharField(required=False)
+    schedule = rest_framework.serializers.ListField(child=rest_framework.serializers.CharField(required=False,
+                                                                                               max_length=50),
+                                                    required=False)
+    is_active = rest_framework.serializers.BooleanField(required=False)
+    subject_group = SubjectGroupSerializer()
+    analyzer_category = rest_framework.serializers.CharField(read_only=True)
+    search_time_hours = rest_framework.serializers.FloatField()
+
+
+class GeofenceAnalyzerConfigSerializer(SpatialAnalyzerConfigSerializer):
+    pass
+
+
+class ProximityAnalyzerConfigSerializer(SpatialAnalyzerConfigSerializer):
+    pass
+
+
 class SubjectAnalyzerResultSerializer(rest_framework.serializers.Serializer):
 
     content_type = ContentTypeField(read_only=True)
 
     class Meta:
         model = analyzers.models.SubjectAnalyzerResult
-
-
---
+++ das/analyzers/views.py
@@ -0,0 +1,26 @@
+import itertools
+
+from rest_framework import generics, status, response
+from rest_framework.response import Response
+
+from utils.drf import StandardResultsSetPagination
+from analyzers.models import GeofenceAnalyzerConfig, ProximityAnalyzerConfig
+from analyzers.serializers import GeofenceAnalyzerConfigSerializer, ProximityAnalyzerConfigSerializer
+from analyzers.permissions import ModelPermissions
+
+
+class SpatialAnalyzerListView(generics.ListAPIView):
+    #permission_classes = (ModelPermissions,)
+    #pagination_class = StandardResultsSetPagination
+
+    MODEL_TO_SERIALIZER = ((GeofenceAnalyzerConfig, GeofenceAnalyzerConfigSerializer),
+                           (ProximityAnalyzerConfig, ProximityAnalyzerConfigSerializer))
+
+    def list(self, request, *args, **kwargs):
+        results = []
+        for spatial_model, spatial_serializer in self.MODEL_TO_SERIALIZER:
+            for row in spatial_model.objects.all():
+                serializer = spatial_serializer(row)
+                results.append(serializer.data)
+
+        return Response(results)
