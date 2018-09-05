43e3aa7405e3e84a327352ade2e99146bc5377ad
Chris Doehring <chrisdoehring@gmail.com>
2018-09-05 15:11:25 -0700

    Remove rate limit from broadcast_service_status task.
    This top-level task should be a simple task with no limiting. It delegates (via apply_async) to a task that handles rejecting superfluous tasks.

--- das/rt_api/tasks.py
+++ das/rt_api/tasks.py
@@ -143,7 +143,7 @@ def _broadcast_service_status(service_status_data=None):
         close_old_connections()
 
 
-@celery.app.task(base=QueueOnce, once={'graceful': True, 'timeout': 60}, rate_limit='4/m')
+@celery.app.task()
 def broadcast_service_status():
     _broadcast_service_status.apply_async()
 
