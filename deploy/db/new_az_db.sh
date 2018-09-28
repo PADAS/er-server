11ed0a8dedb17356d340c8c159d38eca2736a353
Jay Lorenzo <cxplanet@hotmail.com>
2018-09-28 13:35:37 -0700

    More wip

--- deploy/db/new_az_db.sh
+++ deploy/db/new_az_db.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 # WARNING: Used to populate dev instances. Review script security before using in production
 
-# create a 32 char random password
+if [ -z "$1" ]
+  then
+    echo "Must specify DB name"
+    exit 1
+fi
+
+DB_NAME="$1"
+DB_OWNER="$1"
+DB_USER="$1_user"
+
+# create a 32 char random password without dashes
+# will use a RNG if present, otherwise its based off time 
 RANDOM_UUID="uuidgen | tr -d '-'"
 DB_OWNER_PWD=`eval ${RANDOM_UUID}`
-DB_USER_PWD=`eval ${RANDOM_UUID}`
+RANDOM_UUID2="uuidgen | tr -d '-'"
+DB_USER_PWD=`eval ${RANDOM_UUID2}`
 
 # dump it to a json file
-echo -e "{\"$DB_OWNER\":\""$DB_OWNER_PWD"\", \"$DB_USER\":\""$DB_USER_PWD"\", \"db_name\":\""$DB_NAME"\"}" 
-echo $OWNER_PWD
-echo $USER_PWD
+DB_DATA="{\"$DB_OWNER\":\""$DB_OWNER_PWD"\", \"$DB_USER\":\""$DB_USER_PWD"\", \"db_name\":\""$DB_NAME"\"}" 
+echo -e $DB_DATA
 
