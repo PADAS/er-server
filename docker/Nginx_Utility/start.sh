35f18b3a2c0cf9d7b11382e6fb6972b67d0e31ef
Satyarth Negi <satyarthnegi@MAC-SatyarthN-1.corp.vnw.com>
2018-10-04 14:27:27 -0700

    Progress commit

--- docker/Nginx_Utility/start.sh
+++ docker/Nginx_Utility/start.sh
@@ -3,10 +3,12 @@
 SECRET_PATH=/etc/secrets
 SSL_PATH=/etc/ssl
 
-echo "$SSL_SECRET" > $SSL_PATH/ssl.key
+echo "$SSL_KEY" > $SSL_PATH/ssl.key
+echo "$SSL_CERT" > $SSL_PATH/certificate.pem
+echo "$SSL_CA" > $SSL_PATH/ca.pem
 
 HASHED_PASSWORD=$(openssl passwd -apr1 "$ELASTIC_BASIC_AUTH_PASSWORD")
-echo "logger:$HASHED_PASSWORD" > /etc/nginx/passwords
+echo "$ELASTIC_BASIC_AUTH_USERNAME:$HASHED_PASSWORD" > /etc/nginx/passwords
 
 
 /usr/sbin/nginx -g 'daemon off;'
