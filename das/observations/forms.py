f7234a253be13c2fdc38ee6fbfefaea9a812a06b
Chris Doehring <chrisdoehring@gmail.com>
2018-10-09 16:56:17 -0700

    Remove parsing of birthdate from tm_animal_id.

--- das/observations/forms.py
+++ das/observations/forms.py
@@ -122,8 +122,8 @@ class SubjectFormWithAttributes(JSONFieldFormMixin, SubjectForm):
         verbose_name='Country Choices', is_stacked=False), required=False,
         help_text='This is the country that will be shown in the DAS Mobile '
                   'App.')
-    birthdate = forms.CharField(required=False, label='Birth Date')
-    other_id = forms.CharField(required=False, label='Other id')
+    tm_animal_id = forms.CharField(required=False, label='Animal ID')
+    # other_id = forms.CharField(required=False, label='Other id')
 
     @staticmethod
     def fetch_region_choices():
@@ -148,33 +148,34 @@ class SubjectFormWithAttributes(JSONFieldFormMixin, SubjectForm):
 
         # Split tm_animal_id from additional json field by separator as'%',
         #  to put value in birthdate and other_id form fields
-        if 'tm_animal_id' in self.instance.additional.keys():
-            tm_animal_id = self.instance.additional['tm_animal_id']
-            self.fields['birthdate'].initial, self.fields[
-                'other_id'].initial = tm_animal_id.split('%')
+        # if 'tm_animal_id' in self.instance.additional.keys():
+        #     tm_animal_id = self.instance.additional['tm_animal_id']
+        #     if '%' in tm_animal_id:
+        #         self.fields['birthdate'].initial, self.fields[
+        #             'other_id'].initial = tm_animal_id.split('%')
 
         # Get country and region choices from static methods
         self.fields['region'].choices = self.fetch_region_choices()
         self.fields['country'].choices = self.fetch_country_choices()
 
     class Meta(SubjectForm.Meta):
-        json_fields = ('rgb', 'sex', 'region', 'country')
+        json_fields = ('rgb', 'sex', 'region', 'country', 'tm_animal_id')
 
     json_field = 'additional'
 
     def save(self, *args, **kwargs):
-        # Concatenate birthdate and other_id field's value with separator as '%'
-        if self.cleaned_data['birthdate'] or self.cleaned_data['other_id']:
-            tm_animal_id = '{0}%{1}'.format(self.cleaned_data['birthdate'],
-                                            self.cleaned_data['other_id'])
-        else:
-            tm_animal_id = None
+        # # Concatenate birthdate and other_id field's value with separator as '%'
+        # if self.cleaned_data['birthdate'] or self.cleaned_data['other_id']:
+        #     tm_animal_id = '{0}%{1}'.format(self.cleaned_data['birthdate'],
+        #                                     self.cleaned_data['other_id'])
+        # else:
+        #     tm_animal_id = None
         commit = kwargs.pop('commit', True)
         instance = super(SubjectFormWithAttributes, self).save(*args,
                                                                commit=False,
                                                                **kwargs)
-        if tm_animal_id:
-            instance.additional['tm_animal_id'] = tm_animal_id
+        # if tm_animal_id:
+        #     instance.additional['tm_animal_id'] = tm_animal_id
         if commit:
             instance.save()
         return instance
