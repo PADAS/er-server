14ca94d1137d59929861691838243aa682d0cef1
Jay Lorenzo <cxplanet@hotmail.com>
2018-11-07 15:36:18 -0800

    Rolling back the migration change, until I understand the unit test failure

--- das/activity/migrations/0046_serial_number_unique.py
+++ das/activity/migrations/0046_serial_number_unique.py
@@ -29,7 +29,7 @@ class Migration(migrations.Migration):
                       START 1
                       CACHE 1;
                     ALTER TABLE public.activity_event_serial_number_seq
-                      OWNER TO current_user;''',
+                      OWNER TO postgres;''',
             reverse_sql='''drop sequence activity_event_serial_number_seq ;'''
         ),
         migrations.RunSQL(
