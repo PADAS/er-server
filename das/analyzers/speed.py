836cac1a9bcc2364a8319a3fe9ad70d4a68a007b
Vishal Palasgaonkar <Vishal.Palasgaonkar@talentica.com>
2018-12-04 16:56:15 +0530

    Subject Group inheritance check added for Analyzer invocation

--- das/analyzers/speed.py
+++ das/analyzers/speed.py
@@ -24,8 +24,9 @@ class LowSpeedPercentileAnalyzer(SubjectAnalyzer):
 
     @classmethod
     def get_subject_analyzers(cls, subject=None):
-        for ac in LowSpeedPercentileAnalyzerConfig.objects.filter(subject_group__subjects=subject,
-                                                                  is_active=True):
+        subject_groups = subject.get_ancestor_subject_groups()
+        for ac in LowSpeedPercentileAnalyzerConfig.objects.filter(
+                subject_group__in=subject_groups, is_active=True):
             yield cls(subject=subject, config=ac)
 
     def default_observations(self):
@@ -176,7 +177,9 @@ class LowSpeedWilcoxAnalyzer(SubjectAnalyzer):
 
     @classmethod
     def get_subject_analyzers(cls, subject=None):
-        for ac in LowSpeedWilcoxAnalyzerConfig.objects.filter(subject_group__subjects=subject, is_active=True):
+        subject_groups = subject.get_ancestor_subject_groups()
+        for ac in LowSpeedWilcoxAnalyzerConfig.objects.filter(
+                subject_group__in=subject_groups, is_active=True):
             yield cls(subject=subject, config=ac)
 
     def _normal_movement_distro(self, trajectory_filter=None, end=None, last_hours=30 * 24):
--- das/analyzers/tasks.py
+++ das/analyzers/tasks.py
@@ -47,6 +47,7 @@ def handle_source(source_id):
 def analyze_subject(subject_id):
 
     logger.info('Analyze subject for id=%s', subject_id)
+    print('in analyze_subject')
     try:
         subject = Subject.objects.get(id=subject_id)
     except Subject.DoesNotExist:
@@ -55,6 +56,7 @@ def analyze_subject(subject_id):
 
     logger.info('Running analyzers for subject: %s', subject)
     for analyzer in get_subject_analyzers(subject):
+        print(analyzer)
 
         try:
             analyzer_results = analyzer.analyze()
@@ -73,6 +75,7 @@ def analyze_subject(subject_id):
 def annotate_observations_for_subject(subject_id):
 
     logger.debug('Annotating observations for subject: %s', str(subject_id))
+    print('in annotator')
 
     try:
         sub = Subject.objects.get(id=subject_id)
