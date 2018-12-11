b3ff2da890d408abca1a67899ef773fde0afdcfb
James Lorenzo <cxplanet@hotmail.com>
2018-12-11 15:59:03 -0800

    Forgot to format the query literal

--- das/activity/migrations/0046_serial_number_unique.py
+++ das/activity/migrations/0046_serial_number_unique.py
@@ -33,7 +33,7 @@ class Migration(migrations.Migration):
                       START 1
                       CACHE 1;
                     ALTER TABLE public.activity_event_serial_number_seq
-                      OWNER TO current_user;''',
+                      OWNER TO {}};'''.format(db_user),
             reverse_sql='''drop sequence activity_event_serial_number_seq ;'''
         ),
         migrations.RunSQL(
