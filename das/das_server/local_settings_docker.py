8e23ecf95b0e06e0039c7f1c023cf717648aa5f5
Jay Lorenzo <cxplanet@hotmail.com>
2018-12-19 17:21:27 -0800

    Rename the blob storage to be more generic

--- das/das_server/local_settings_docker.py
+++ das/das_server/local_settings_docker.py
@@ -56,6 +56,9 @@ if USE_AZURE_STORAGE == 'true':
     # Azure storage - see https://django-storages.readthedocs.io/en/latest/backends/azure.html
     DEFAULT_FILE_STORAGE = 'storages.backends.azure_storage.AzureStorage'
     STATICFILES_STORAGE = 'storages.backends.azure_storage.AzureStorage'
+    AZURE_ACCOUNT_NAME = os.getenv('STORAGE_ACCOUNT', '')
+    AZURE_ACCOUNT_KEY = os.getenv('STORAGE_KEY', '')
+    AZURE_CONTAINER = os.getenv('STORAGE_CONTAINER', '')
     # todo - add the blob storage keys here
 
 
