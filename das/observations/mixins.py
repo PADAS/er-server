7c71fd16f72370e197e623a7339da6a05272e92c
Chris Doehring <chrisdoehring@gmail.com>
2019-01-16 16:29:34 -0800

    Validate the subject_ids are valid UUIDs.

--- das/observations/mixins.py
+++ das/observations/mixins.py
@@ -1,7 +1,8 @@
+import uuid
+
 class FilterMixin(object):
 
     def by_id(self, primary_keys):
         if isinstance(primary_keys, str):
-            primary_keys = [primary_key.strip() for primary_key in 
-            primary_keys.split(',') if primary_key]
+            primary_keys = [uuid.UUID(pk.strip()) for pk in primary_keys.split(',')]
         return self.filter(id__in=primary_keys)
