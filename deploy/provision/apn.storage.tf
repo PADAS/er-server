84515eb8ad80b72311b168f63d9d9a59f8087dff
Jay Lorenzo <cxplanet@hotmail.com>
2019-01-28 17:22:37 -0800

    Devops foo

--- deploy/provision/apn.storage.tf
+++ deploy/provision/apn.storage.tf
@@ -10,5 +10,6 @@ resource "azurerm_storage_container" "bangweulu-assets" {
   name                  = "bangweulu-assets"
   resource_group_name   = "${azurerm_resource_group.er-west-eur.name}"
   storage_account_name  = "${azurerm_storage_account.bangweulu.name}"
+  container_access_type = "blob"
 }
 
