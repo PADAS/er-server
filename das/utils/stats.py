f57fbb016e299bf9686d7490c37ec9cc94aea6a8
Chris Doehring <chrisdoehring@gmail.com>
2019-03-19 11:39:04 -0700

    Add statsd gauges for session count and redis memory.

--- das/utils/stats.py
+++ das/utils/stats.py
@@ -2,4 +2,13 @@ from datadog import statsd
 
 
 def increment(metric, value=1, tags=None, sample_rate=1):
-    statsd.increment(metric, value=value, tags=tags, sample_rate=sample_rate)
+    statsd.increment(metric.lower(), value=value, tags=tags, sample_rate=sample_rate)
+
+
+def increment_for_view(view_name):
+    statsd.increment(metric=view_name.lower())
+
+
+def update_gauge(metric, value, tags=None, sample_rate=1):
+    statsd.gauge(metric.lower(), value=value, tags=tags, sample_rate=sample_rate)
+
