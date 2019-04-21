3ccfd1d68b7466fd44bb4c90a6f091201a0e593c
Jay Lorenzo <cxplanet@hotmail.com>
2019-04-20 22:22:56 -0700

    remove arg test

--- docker/API/start.sh
+++ docker/API/start.sh
@@ -2,10 +2,6 @@
 . /startup/wait_for.sh
 wait_for $DB_HOST $DB_PORT
 
-if [[ -z "${AZURE_STORAGE}" ]]; then
-  #python3 cfgload.py
-fi
-
 python3 manage.py migrate --no-input
 python3 manage.py collectstatic --no-input
 python3 manage.py loaddata initial_admin initial_groups initial_eventdata initial_dev_map initial_features initial_tilelayers event_data_model
