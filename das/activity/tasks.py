98c18ef7475241480fe2cb54c56776404b3972da
Jay Lorenzo <cxplanet@hotmail.com>
2019-05-03 09:29:57 -0700

    mail task sans template rendering

--- das/activity/tasks.py
+++ das/activity/tasks.py
@@ -1,10 +1,11 @@
 import logging
 
 from versatileimagefield.image_warmer import VersatileImageFieldWarmer
-from das_server import celery
-from activity.models import EventPhoto, Event
-
+from django.template.loader import render_to_string
 from activity.alertingservice import evaluate_event
+from activity.models import EventPhoto, Event, EventType, NotificationMethod
+from das_server import celery, mailer, settings
+from reports.distribution import send_report
 
 logger = logging.getLogger(__name__)
 
@@ -16,8 +17,7 @@ def evaluate_alert_rules(self, event_id):
         logger.info('Evaluating Event %s for alerting.', event_id)
         event = Event.objects.get(id=event_id)
         action_list = evaluate_event(event)
-
-        print(f'Action List: {action_list}')
+        #send_alert_to_user.delay((event.id, notification_id))
     except Exception as e:
         logger.exception('Failed when evaluating alert rules for event {}'.format(event_id))
 
@@ -38,3 +38,43 @@ def warm_eventphotos(self, event_photo_id):
     except Exception as e:
         logger.exception('Failed when warming images for event_photo_id {}'.format(event_photo_id))
 
+
+# apparently a tuple is needed for the passing parameters to a subtask and delay()
+@celery.app.task(bind=True)
+def send_alert_to_user(self, payload):
+    (event_id, notif_id) = payload
+    logger.info(f"Sending alert of event {event_id} to notification id {notif_id}")
+    event = Event.objects.get(id=event_id)
+    notification = NotificationMethod.objects.get(id=notif_id)
+    recip = notification.value
+    method = notification.method
+    subject = create_email_subject(event)
+    if method.lower() == 'email':
+        send_report(
+            subject=subject,
+            to_email=recip,
+            text_content=str(event.__dict__)
+        )
+        logger.info(f"Sent email alert {event_id} to {recip}")
+    elif method.lower() == 'sms':
+        parameters = {
+            'serial': event.id,
+            'color': priority,
+            'title': event.title
+        }
+        msg = render_to_string('new_event_sms.txt', parameters).strip()
+        mailer.send_sms(msg, recip)
+        logger.info(f"Sent sms alert {event_id} to {recip}")
+    else:
+        logger.error(f"Failed to send alert {event_id} to {recip} via {method}")
+
+
+# TODO - move me to a good location
+def create_email_subject(event):
+    priority = event.get_display_value('priority', 'Grey')
+    title = event.title
+    if title is None:
+        etype = EventType.objects.get(id=event.event_type_id)
+        title = etype.display
+
+    return f"DAS {priority} Alert: {event.serial_number} {title}"
