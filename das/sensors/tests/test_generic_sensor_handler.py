dd424803425eb85ccb4c2c51a4ecfb69e74d220d
rohitc <rohitc@vulcan.com>
2019-05-07 12:48:52 -0700

    more test cases, code cleanup

--- das/sensors/tests/test_generic_sensor_handler.py
+++ das/sensors/tests/test_generic_sensor_handler.py
@@ -1,9 +1,11 @@
 import json
+import datetime
+from django.utils import timezone
 from rest_framework import status
 
 from core.tests import BaseAPITest
 from sensors.views import SensorObservation
-from observations.models import Subject, SourceProvider, Source, SubjectSource
+from observations.models import Subject, SourceProvider, Source, Observation
 
 
 class GenericSensorHandlerTest(BaseAPITest):
@@ -22,34 +24,85 @@ class GenericSensorHandlerTest(BaseAPITest):
 
     def setUp(self):
         super().setUp()
-        # setup db: create subject, source, provider, subjectsource
 
-        self.test_subject = Subject.objects.create(name="test_subject")
+        # setup db: create subject, source, provider
+        Subject.objects.create(name="test_subject")
         self.test_sourceprovider = SourceProvider.objects.create(
             display_name=self.provider, provider_key=self.provider)
         self.test_source = Source.objects.create(
             source_type=self.source_type, provider=self.test_sourceprovider,
             manufacturer_id=self.manufacturer_id)
-        self.test_subjectsource = SubjectSource.objects.create(
-            source=self.test_source, subject=self.test_subject)
 
-        self. api_path = '/'.join((self.api_base, 'sensors', self.sensor_type, self.provider, 'status'))
+        self.api_path = '/'.join((self.api_base, 'sensors', self.sensor_type, self.provider, 'status'))
 
-    def post_data(self, payload):
-        request = self.factory.post(self.api_path, data=payload, content_type='application/json')
-        self.force_authenticate(request, self.app_user)
-        return SensorObservation.as_view()(request, sensor_type=self.sensor_type, provider_key=self.provider)
+    def test_badrequest_manufacturer_id_missing(self):
+        local_obs = dict(self.one_observation)
+        local_obs.pop("manufacturer_id", None)
+        response = self._post_data(json.dumps(local_obs))
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_post_one_sample(self):
-        response = self.post_data(json.dumps(self.one_observation))
-        self.assertTrue(response.status_code, status.HTTP_201_CREATED)
+    def test_badrequest_recorded_at_missing(self):
+        local_obs = dict(self.one_observation)
+        local_obs.pop('recorded_at', None)
+        response = self._post_data(json.dumps(local_obs))
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_badrequest_location_missing(self):
+        local_obs = dict(self.one_observation)
+        local_obs.pop("location", None)
+        response = self._post_data(json.dumps(local_obs))
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_post_one(self):
+        response = self._post_data(json.dumps(self.one_observation))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(1, Observation.objects.filter(source=self.test_source).count())
 
     def test_post_with_additional(self):
-        self.one_observation["additional"] = {
-            "event_action": "device_location_changed"
-        }
-        response = self.post_data(json.dumps(self.one_observation))
-        self.assertTrue(response.status_code, status.HTTP_201_CREATED)
+        self.one_observation.update({"additional": {"event_action": "device_location_changed"}})
+        response = self._post_data(json.dumps(self.one_observation))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(1, Observation.objects.filter(source=self.test_source).count())
 
+    def test_post_ten_has_dups(self):
+        obs_list = [x for x in self._generate_observations()]
+
+        response = self._post_data(json.dumps(obs_list))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(1, Observation.objects.filter(
+            source__provider=self.test_sourceprovider, source=self.test_source).count())
+
+    def test_post_ten(self):
+        obs_list = [x for x in self._generate_observations(distinct=True)]
+
+        response = self._post_data(json.dumps(obs_list))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(10, Observation.objects.filter(source__provider=self.test_sourceprovider).count())
+
+    def test_post_two_different_ids(self):
+        response = self._post_data(json.dumps(self.one_observation))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        Source.objects.create(source_type=self.source_type, provider=self.test_sourceprovider, manufacturer_id='mfg_id')
+        local_obs = dict(self.one_observation)
+        local_obs.update(manufacturer_id='mfg_id')
+        response = self._post_data(json.dumps(local_obs))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(2, Observation.objects.filter(source__provider=self.test_sourceprovider).count())
+
+    def _generate_observations(self, n=10, distinct=False):
+        for i in range(n):
+            obs = dict(self.one_observation)
+            if distinct:
+                timestamp = timezone.now() - datetime.timedelta(days=i)
+                obs.update(recorded_at=timestamp.strftime("%Y-%m-%d %H:%M:%S"))
+
+            yield obs
+
+    def _post_data(self, payload):
+        request = self.factory.post(self.api_path, data=payload, content_type='application/json')
+        self.force_authenticate(request, self.app_user)
+        response = SensorObservation.as_view()(request, sensor_type=self.sensor_type, provider_key=self.provider)
+        return response
 
 
