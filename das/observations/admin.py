c6136d4874c1f2082d54967209b32a33193a5c2f
rohitc <rohitc@vulcan.com>
2019-07-30 16:02:22 -0700

    - Active Subjects is sorted in Admin/Observations/SubjectGroup - Exported subject information includes subject groups and subject subtype

--- das/observations/admin.py
+++ das/observations/admin.py
@@ -698,7 +698,7 @@ class RegionAdmin(admin.ModelAdmin):
 class SubjectGroupChangeForm(forms.ModelForm):
     filter_horizontal = ('children', 'permission_sets', 'subjects')
     active_subjects = forms.ModelMultipleChoiceField(
-        queryset=models.Subject.objects.by_is_active(True),
+        queryset=models.Subject.objects.order_by('name').by_is_active(True),
         required=False,
         widget=FilteredSelectMultiple(
             verbose_name=_('Subjects'),
