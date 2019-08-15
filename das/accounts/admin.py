717679b79a84ed9a34e7e1ad88f9d4c0b6b577c9
rohitc <rohitc@vulcan.com>
2019-08-15 11:21:31 -0700

    role shows up right under user lastname when creating/changing a user in Admin

--- das/accounts/admin.py
+++ das/accounts/admin.py
@@ -261,14 +261,14 @@ class UserAdmin(DefaultFilterMixin, DjangoUserAdmin):
     ordering = ('last_name', 'first_name', 'username')
     fieldsets = (
         (None, {
-            'fields': ('first_name', 'last_name',
+            'fields': ('first_name', 'last_name', 'role',
                        'email', 'phone',
                        'is_email_alert', 'is_sms_alert',
                        'username', 'password')
         }),
-        ('Additiona JSON Fields', {
+        ('Additional JSON Fields', {
             'fields': ('notes', 'expiry', 'moudatesigned', 'moutype', 'moufilename',
-                       'organization', 'tech', 'role')
+                       'organization', 'tech',)
         }),
         ('Additional Data', {
             'fields': ['additional']}
@@ -289,7 +289,7 @@ class UserAdmin(DefaultFilterMixin, DjangoUserAdmin):
     add_form = CustomUserCreationForm
     add_fieldsets = (
         (None, {
-            'fields': ('first_name', 'last_name',
+            'fields': ('first_name', 'last_name', 'role',
                        'email', 'phone',
                        'is_email_alert', 'is_sms_alert',
                        'username'
@@ -297,7 +297,7 @@ class UserAdmin(DefaultFilterMixin, DjangoUserAdmin):
         }),
         ('Additional JSON Fields', {
             'fields': ('notes', 'expiry', 'moudatesigned', 'moutype', 'moufilename',
-                       'organization', 'tech', 'role',)
+                       'organization', 'tech',)
         }),
         ('Additional JSON Data', {
             'fields': ['additional']
