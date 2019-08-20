49dd72f596bdc4c6bee3eac6a658a56be90ef2b7
Chris Doehring <chrisdoehring@gmail.com>
2019-08-20 13:45:08 -0700

    Combine points and tracks in the same KML document.

--- docker/RT_API/start.sh
+++ docker/RT_API/start.sh
@@ -6,7 +6,7 @@ python3 cfgloader.py
 python3 manage.py collectstatic --no-input
 
 if [ "$DEV" = "True" ]; then
-    python3 manage.py rtserver 0.0.0.0:8000 --nothreading
+    python3 manage.py rtserver 0.0.0.0:8000 --nothreading --noreload
 else
     python3 manage.py rtserver 0.0.0.0:8000 --noreload --nothreading
 fi
