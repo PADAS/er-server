32f98978d36b302b72fa543d341f52f23ec7efcd
Loraine Kanervisto <lorainek@vulcan.com>
2019-08-29 14:15:21 -0700

    prune!

--- docker/Beat/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-. /startup/wait_for.sh
-wait_for $API_HOST $API_PORT
-
-python3 cfgloader.py
-celery -A das_server beat -l info -s /tmp/celerybeat-schedule
--- docker/Worker_RT/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-. /startup/wait_for.sh
-wait_for $API_HOST $API_PORT
-
-python3 cfgloader.py
-python3 manage.py collectstatic --no-input
-celery -A das_server worker -Q realtime_p1,realtime_p2,realtime_p3 -l info -c 15 -P gevent --without-gossip -n priority
