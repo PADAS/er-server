815385829b0fbcd01447b89025affadeae956c46
Loraine Kanervisto <lorainek@vulcan.com>
2019-09-12 14:04:24 -0700

    Dynamically include a variable

--- terraform/template.tf
+++ terraform/template.tf
@@ -2,7 +2,7 @@ resource "template_dir" "deployments" {
   source_dir      = "${path.root}/templated_deployments"
   destination_dir = "${path.root}/rendered_deployments"
   vars = {
-    NAMESPACE = "namespace"
+    NAMESPACE = "${yamldecode(file("${path.root}/../ci/params/circleci.params.yaml"))["namespace"]}"
     SERVER_VERSION = "server-version"
   }
 }
