39d0e8da12885da3720e4463758066b994e52368
Loraine Kanervisto <lorainek@vulcan.com>
2019-09-12 14:34:24 -0700

    Default params

--- terraform/templates.tf
+++ terraform/templates.tf
@@ -2,7 +2,6 @@ resource "template_dir" "deployments" {
   source_dir      = "${path.root}/templated_deployments"
   destination_dir = "${path.root}/rendered_deployments"
   vars = {
-    NAMESPACE = "${yamldecode(file("${path.root}/../ci/params/circleci.params.yaml"))["namespace"]}"
-    SERVER_VERSION = "server-version"
+    NAMESPACE = var.namespace
   }
 }
