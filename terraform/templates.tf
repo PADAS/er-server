7bbab4822bcd87c2af0b8eec9a0facefc3d8aed2
Loraine Kanervisto <lorainek@vulcan.com>
2019-09-12 15:11:01 -0700

    Typo

--- terraform/templates.tf
+++ terraform/templates.tf
@@ -7,6 +7,6 @@ resource "template_dir" "deployments" {
     DB_PORT = var.db_port
     DB_NAME = var.db_name
     DB_USER = var.db_user
-    USER_AZURE_STORAGE = var.use_azure_storage
+    USE_AZURE_STORAGE = var.use_azure_storage
   }
 }
