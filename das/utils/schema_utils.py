a9f5800ca4fab11e4c1371362a8cc7e311505b7d
blac-siren <husnein.zac@gmail.com>
2019-09-18 20:36:36 +0300

    update eventtype schema payload to contain inactive choices

--- das/utils/schema_utils.py
+++ das/utils/schema_utils.py
@@ -386,6 +386,28 @@ def get_replacement_fields_in_schema(schema):
     return fields
 
 
+def map_schema(schema, load_schema):
+
+    lookups = []
+    keys = load_schema['schema']['properties'].keys()
+    for key in keys:
+        if ('enum' or 'query'
+                or 'table') in load_schema['schema']['properties'][key].keys():
+            lookups.append(key)
+
+    fields = []
+    template = Template(schema)
+    for node in template.nodelist:
+        if type(node) is VariableNode:
+            field_tag = node.token.contents
+            field_details = field_tag.split('___')
+
+            if field_details[1] not in fields:
+                fields.append(field_details[1])
+
+    return dict(zip(lookups, fields))
+
+
 def format_key_for_title(key):
     titleStr = re.sub('(.)([A-Z][a-z]+)', r'\1 \2', key)
     titleStr = re.sub('([a-z0-9])([A-Z])', r'\1 \2', titleStr).lower()
