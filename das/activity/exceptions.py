b3407c2cc72199d518631c985b4a19153b6fca79
Chris Doehring <chrisdoehring@gmail.com>
2019-09-19 17:03:36 -0700

    Add translate for error messages.

--- das/activity/exceptions.py
+++ das/activity/exceptions.py
@@ -1,10 +1,12 @@
-SCHEMA_ERROR_INCORRECT_RENDER_TAG = "Incorrect event render tag, tag should " \
-                                    "be in the form 'xxx___xxx___xxx'"
-SCHEMA_ERROR_JSON_DECODE_ERROR = "Schema can not be decoded"
+from django.utils.translation import ugettext_lazy as _
 
-SCHEMA_ERROR_EMPTY_PROPERTY = "Each property must contain at minimum a type and a title"
+SCHEMA_ERROR_INCORRECT_RENDER_TAG = _("Incorrect event render tag, tag should " \
+                                    "be in the form 'xxx___xxx___xxx'")
+SCHEMA_ERROR_JSON_DECODE_ERROR = _("Schema can not be decoded")
 
-SCHEMA_ERROR_MISMATCHED_PROPERTIES_IN_DEFINITION = "Some property keys missing in the definition"
+SCHEMA_ERROR_EMPTY_PROPERTY = _("Each property must contain at minimum a type and a title")
+
+SCHEMA_ERROR_MISMATCHED_PROPERTIES_IN_DEFINITION = _("Some property keys missing in the definition")
 
 
 class SchemaValidationError(Exception):
