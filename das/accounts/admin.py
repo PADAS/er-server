7421416402bb93698103f8bc7c1ed91d191afabb
Chris Doehring <chrisdoehring@gmail.com>
2019-09-20 00:12:57 -0700

    Removed some stale references to email and sms flags.

--- das/accounts/admin.py
+++ das/accounts/admin.py
@@ -175,8 +175,7 @@ class CustomUserCreationForm(JSONFieldFormMixin, UserCreationForm):
         model = User
         json_fields = ('notes', 'expiry', 'moudatesigned', 'moutype', 'moufilename',
                        'organization', 'tech', 'role')
-        fields = ('first_name', 'last_name', 'email', 'phone',
-                  'is_email_alert', 'is_sms_alert', 'username') + json_fields
+        fields = ('first_name', 'last_name', 'email', 'phone', 'username') + json_fields
 
     json_field = 'additional'
 
@@ -228,8 +227,7 @@ class UserAdditionalForm(JSONFieldFormMixin, UserChangeForm):
         json_fields = ('notes', 'expiry', 'moudatesigned', 'moutype', 'moufilename',
                        'organization', 'tech', 'role')
         json_date_fields = ('expiry', 'moudatesigned')
-        fields = ('first_name', 'last_name', 'email', 'phone',
-                  'is_email_alert', 'is_sms_alert', 'username') + json_fields
+        fields = ('first_name', 'last_name', 'email', 'phone', 'username') + json_fields
 
     json_field = 'additional'
 
@@ -297,11 +295,10 @@ class UserAdmin(DefaultFilterMixin, DjangoUserAdmin):
     )
 
     list_display = ('display_name', 'member_permission_sets',
-                    'all_permission_sets', 'is_email_alert', 'is_sms_alert', 'is_active')
-    list_editable = ('is_email_alert', 'is_sms_alert', 'is_active')
+                    'all_permission_sets', 'is_active')
+    list_editable = ('is_active',)
     list_display_links = ('display_name', )
-    list_filter = ('is_active', 'is_staff', 'is_email_alert',
-                   'is_sms_alert', 'permission_sets')
+    list_filter = ('is_active', 'is_staff', 'permission_sets')
     filter_horizontal = ('permission_sets',)
     form = UserAdditionalForm
     add_form = CustomUserCreationForm
@@ -309,7 +306,6 @@ class UserAdmin(DefaultFilterMixin, DjangoUserAdmin):
         (None, {
             'fields': ('first_name', 'last_name', 'role',
                        'email', 'phone',
-                       'is_email_alert', 'is_sms_alert',
                        'username'
                        )
         }),
