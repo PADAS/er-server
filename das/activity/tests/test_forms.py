819b3673a5d4d3e45aaf42d1f10e0f9536d96a89
Chris Doehring <chrisdoehring@gmail.com>
2019-10-03 16:23:25 -0700

    Fix unit test.

--- das/activity/tests/test_forms.py
+++ das/activity/tests/test_forms.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 
-from activity.exceptions import SCHEMA_ERROR_EMPTY_PROPERTY, \
-    SCHEMA_ERROR_MISMATCHED_PROPERTIES_IN_DEFINITION
+from activity.exceptions import SCHEMA_ERROR_EMPTY_PROPERTY
 from activity.forms import EventTypeForm, SCHEMA_ERROR_JSON_DECODE_ERROR, \
     SCHEMA_ERROR_INCORRECT_RENDER_TAG
 
@@ -15,7 +14,51 @@ EVENT_SCHEMA_INCORRECT_TAG = """{\n    "schema": {\n        "$schema": "http://j
 
 EVENT_SCHEMA_EMPTY_PROPERTY = """{\n    "schema": {\n        "$schema": "http://json-schema.org/draft-04/schema#",\n        "title": "Rhino Sighting (rhino_sighting_rep)",\n      \n        "type": "object",\n\n        "properties": \n        {\n            "rhinosightingrep_Rhino": {\n            },\n            "rhinosightingrep_earnotchcount": {\n                "type":"number",\n                "title": "Ear notch count"\n            },\n            "rhinosightingrep_condition":{\n                "type": "string",\n                "title": "Condition",\n               "enum": {{enum___rhinosightingrep_condition___values}},\n               "enumNames": {{enum___rhinosightingrep_condition___names}}                   \n            },\n            "rhinosightingrep_activity": {\n                "type": "string",\n                "title": "Activity",\n               "enum": {{enum___rhinosightingrep_activity___values}},\n               "enumNames": {{enum___rhinosightingrep_activity___names}}            \n            }\n        }\n    },\n    "definition": [\n    {\n        "key":         "rhinosightingrep_Rhino",\n        "htmlClass": "col-lg-6"\n    }, \n    {\n        "key":         "rhinosightingrep_earnotchcount",\n        "htmlClass": "col-lg-6"\n    }, \n    {\n        "key":         "rhinosightingrep_condition",\n        "htmlClass": "col-lg-6"\n    }, \n    {\n        "key":         "rhinosightingrep_activity",\n        "htmlClass": "col-lg-6"\n    }\n    ]\n}"""
 
-EVENT_SCHEMA_WITH_MISSING_PROPERTY_IN_DEFINITION = """{\n    "schema": {\n        "$schema": "http://json-schema.org/draft-04/schema#",\n        "title": "Rhino Sighting (rhino_sighting_rep)",\n      \n        "type": "object",\n\n        "properties": \n        {\n            "rhinosightingrep_Rhino": {\n                "type": "string",\n                "title": "Individual Rhino ID",\n                "enum": {{query___blackRhinos___values}},\n                "enumNames": {{query___blackRhinos___names}}\n            },\n            "rhinosightingrep_earnotchcount": {\n                "type":"number",\n                "title": "Ear notch count"\n            },\n            "rhinosightingrep_condition":{\n                "type": "string",\n                "title": "Condition",\n               "enum": {{enum___rhinosightingrep_condition___values}},\n               "enumNames": {{enum___rhinosightingrep_condition___names}}                   \n            },\n            "rhinosightingrep_activity": {\n                "type": "string",\n                "title": "Activity",\n               "enum": {{enum___rhinosightingrep_activity___values}},\n               "enumNames": {{enum___rhinosightingrep_activity___names}}            \n            }\n        }\n    },\n    "definition": [ \n    {\n        "key":         "rhinosightingrep_earnotchcount",\n        "htmlClass": "col-lg-6"\n    }, \n    {\n        "key":         "rhinosightingrep_condition",\n        "htmlClass": "col-lg-6"\n    }, \n    {\n        "key":         "rhinosightingrep_activity",\n        "htmlClass": "col-lg-6"\n    }\n    ]\n}"""
+EVENT_SCHEMA_WITH_MISSING_PROPERTY_IN_DEFINITION = """{
+    "schema": {
+        "$schema": "http://json-schema.org/draft-04/schema#",
+        "title": "Rhino Sighting (rhino_sighting_rep)",
+
+        "type": "object",
+
+        "properties": 
+        {
+            "rhinosightingrep_earnotchcount": {
+                "type":"number",
+                "title": "Ear notch count"
+            },
+            "rhinosightingrep_condition":{
+                "type": "string",
+                "title": "Condition",
+               "enum": {{enum___rhinosightingrep_condition___values}},
+               "enumNames": {{enum___rhinosightingrep_condition___names}}                   
+            },
+            "rhinosightingrep_activity": {
+                "type": "string",
+                "title": "Activity",
+               "enum": {{enum___rhinosightingrep_activity___values}},
+               "enumNames": {{enum___rhinosightingrep_activity___names}}            
+            }
+        }
+    },
+    "definition": [ 
+    {
+        "key":         "rhinosightingrep_earnotchcount",
+        "htmlClass": "col-lg-6"
+    }, 
+    {
+        "key":         "rhinosightingrep_condition",
+        "htmlClass": "col-lg-6"
+    }, 
+    {
+        "key":         "rhinosightingrep_activity",
+        "htmlClass": "col-lg-6"
+    },
+    {
+         "key": "this_key_should_be_missing_from_schema",
+         "htmlClass": "col-lg-6"
+    ]
+}"""
 
 
 class TestEventTypeForm(TestCase):
@@ -41,19 +84,16 @@ class TestEventTypeForm(TestCase):
         form = EventTypeForm(
             data={"schema": EVENT_SCHEMA_INCORRECT_TAG})
         self.assertFalse(form.is_valid())
-        self.assertEqual(form.errors["schema"],
-                         [SCHEMA_ERROR_INCORRECT_RENDER_TAG])
+        self.assertTrue('query_blackRhinos___names' in str(
+            form.errors["schema"]))
 
     def test_empty_property_throws_an_error(self):
         form = EventTypeForm(
             data={"schema": EVENT_SCHEMA_EMPTY_PROPERTY})
         self.assertFalse(form.is_valid())
-        self.assertEqual(form.errors["schema"],
-                         [SCHEMA_ERROR_EMPTY_PROPERTY])
+        self.assertTrue('rhinosightingrep_Rhino' in str(form.errors["schema"]))
 
     def test_missing_properties_in_definition(self):
         form = EventTypeForm(
             data={"schema": EVENT_SCHEMA_WITH_MISSING_PROPERTY_IN_DEFINITION})
         self.assertFalse(form.is_valid())
-        self.assertEqual(form.errors["schema"],
-                         [SCHEMA_ERROR_MISMATCHED_PROPERTIES_IN_DEFINITION])
