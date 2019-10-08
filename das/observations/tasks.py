cda1775dc36da30fa444eba4179880e755e0a5e3
blac-siren <husnein.zac@gmail.com>
2019-10-08 17:15:00 +0300

    remove commented code

--- das/observations/tasks.py
+++ das/observations/tasks.py
@@ -31,74 +31,36 @@ def maintain_subjectstatus_for_subject(subject_id):
     SubjectStatus.objects.maintain_subject_status(subject_id)
 
 
+def query_source_provider():
+    for ssprovider in SourceProvider.objects.annotate(
+            name=F('display_name'),
+            info=F('additional')).values('name', 'info'):
+        try:
+            if ssprovider["info"]["days_data_retain"] != None:
+                yield ssprovider
+        except KeyError:
+            pass
 
 
-# class ObservationMixin:
-#     def source_provider(self):
-#         source_provider = SourceProvider.objects.annotate(
-#             name=F('display_name'),
-#             info=F('additional')).values('name', 'info')
-#         return source_provider
-
-#     def source_id(self):
-#         return Source.objects.values('id')
-
-#     def observation_data(self, name, id_):
-#         return Observation.objects.filter(
-#             source__provider__display_name=o['name'], source__id=i['id'])
-
-#     def time_difference(self, recorded_time):
-#         difference_time = recorded_time - latest_recorded_time
-#         return difference_time
-
-#     def latest_recorded_time(self):
-#         return Observation.objects.last().recorded_at
-
-#     def check_time_difference(self, time_difference, configured_days):
-#         if time_difference == o['info']['maximum_number_of_days']:
-#             # TODO: Delete observed object.
-#             # pass
-
-#     def __call__(self):
-#         for o in self.source_provider():
-#             for i in self.source_id():
-#                 for x in self.observation_data():
-#                     time = self.time_difference(x.recorded_at)
-#                     configured_days = o['info']['maximum_number_of_days']
-#                     self.check_time_difference(time.days, configured_days)
-
-
-# observation_mixin = ObservationMixin()
-
-
-
-# @celery.app.task
-# def maintain_observation_data():
-
-#     source_provider = SourceProvider.objects.annotate(
-#         name=F('display_name'), info=F('additional')).values('name', 'info')
-#     from celery.contrib import rdb
-#     rdb.set_trace()
-
-#     source = Source.objects.values('id')
-#     for o in source_provider:
-#         for i in source:
-#             for x in Observation.objects.filter(source__provider__display_name=o['name'], source__id=i['id']):
-#                 difference_time = x.recorded_at - Observation.objects.last().recorded_at
-#                 if difference_time.days == o['info']['maximum_number_of_days']:
-#                     # TODO: delete observed data
-#                     pass
-
-def source_provider_func():
-    for o in SourceProvider.objects.annotate(name=F("display_name"), info=F("additional")).values("name", "info"):
-        if o["info"]["days_data_retain"] != None:
-            yield o
+def query_observation(source_provider, source):
+    for o in Observation.objects.filter(
+            source__provider__display_name=source_provider['name'],
+            source__id=source['id']):
+        yield o
 
 
 @celery.app.task
 def maintain_observation_data():
+    source_provider = query_source_provider()
+    source = Source.objects.values('id')
 
-    source_provider = source_provider_func()
-    # x = list(source_provider)
-    from celery.contrib import rdb
-    rdb.set_trace()
+    for o in source_provider:
+        for i in source:
+            observation_record = list(
+                query_observation(source_provider=o, source=i))
+            if len(observation_record) >= 2:
+                for n in range(1, len(observation_record)):
+                    difference_time = observation_record[
+                        0].recorded_at - observation_record[-n].recorded_at
+                    if difference_time.days == o['info']['days_data_retain']:
+                        observation_record[-n].delete()
