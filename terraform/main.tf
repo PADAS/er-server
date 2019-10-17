ccb6ad6f1043f918038960927162642a1242d35d
Chris Doehring <chrisdoehring@gmail.com>
2019-10-17 16:52:53 -0700

    variable interpolation'

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 resource "google_sql_database" "database" {
   name = "${var.site}_dasdb"
   # instance is set explicitly on the db
-  instance = "${data.terraform_remote_state.earthranger_app_infra.outputs.db_instance_name}"
+  instance = data.terraform_remote_state.earthranger_app_infra.outputs.db_instance_name
 }
 
 # Store in vault (pattern in earthranger_app_infra)
