31c1ea651816fda4991c839c5c3f785fa9564d22
blac-siren <husnein.zac@gmail.com>
2019-10-17 16:36:14 +0300

    rotatem map

--- das/mapping/templates/admin/ol.js
+++ das/mapping/templates/admin/ol.js
@@ -55,6 +55,32 @@ var {{ module }} = {};
 
 // var map = new ol.Map('id_feature_geometry_map', options)
 
+var raster = new ol.layer.Tile({
+    source: new ol.source.OSM()
+})
+
+var source = new ol.source.Vector();
+var vector = new ol.layer.Vector({
+    source: source,
+    style: new ol.style.Style({
+        fill: new ol.style.Fill({
+            color: 'rgba(255, 255, 255, 0.2)'
+        }),
+        stroke: new ol.style.Stroke({
+            color: '#ffcc33',
+            width: 2
+        }),
+        image: new ol.style.Circle({
+            radius: 7,
+            fill: new ol.style.Fill({
+                color: '#ffcc33'
+            })
+        })
+    })
+});
+
+
+
 var map = new ol.Map({
     view: new ol.View({
         center: [0, 0],
@@ -62,22 +88,52 @@ var map = new ol.Map({
         zoom: 3,
         projection: options.projection.projection_
     }),
-    layers: [
-        new ol.layer.Tile({
-            source: new ol.source.OSM()
-        })
-    ],
+    layers: [raster, vector],
     target: '{{ id }}_map',
-
-    interactions: new ol.interaction.defaults().extend([
-        new ol.interaction.DragRotateAndZoom()
+    controls: new ol.control.defaults().extend([
+        new ol.control.FullScreen()
     ]),
+
+    // interactions: new ol.interaction.defaults().extend([
+    //     new ol.interaction.DragRotateAndZoom(),
+    // ]),
 
 
 });
 
 
+var modify = new ol.interaction.Modify({ source: source });
+map.addInteraction(modify);
 
+var draw, snap; // global so we can remove them later
+var typeSelect = document.getElementById('type');
+
+
+function addInteractions() {
+    draw = new ol.interaction.Draw({
+        source: source,
+        type: typeSelect.value
+    });
+    map.addInteraction(draw);
+    snap = new ol.interaction.Snap({ source: source });
+    map.addInteraction(snap);
+
+}
+
+var dragrotate = new ol.interaction.DragRotateAndZoom()
+map.addInteraction(dragrotate)
+
+
+/**
+ * Handle change event.
+ */
+typeSelect.onchange = function () {
+    map.removeInteraction(draw);
+    map.removeInteraction(snap);
+    addInteractions();
+};
+
+addInteractions();
 
 
 
