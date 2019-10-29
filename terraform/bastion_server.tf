4e30fc152ae1ad276b184cd081d062fb3e0459d4
Loraine Kanervisto <lorainek@vulcan.com>
2019-10-29 15:29:35 -0700

    Tidy up POC code.

--- terraform/bastion_server.tf
+++ terraform/bastion_server.tf
@@ -1,3 +1,10 @@
+locals {
+  is_production = (terraform.workspace == "prod")
+  dev_subnetwork_name = data.terraform_remote_state.terraform_gcp_networking.outputs.dev_us_west_1_subnetwork_name
+  prod_subnetwork_name = data.terraform_remote_state.terraform_gcp_networking.outputs.prod_europe_west_3_subnetwork_name
+  subnetwork_name = local.is_production ? local.prod_subnetwork_name : local.dev_subnetwork_name
+}
+
 module "vulcan_firewall_access_to_bastion_server" {
   source = "git@github.com:vulcantechnologies/ss-terraform-modules.git//gcp-vulcan-corpnet-firewall?ref=v2.21.0"
 
@@ -7,4 +14,65 @@ module "vulcan_firewall_access_to_bastion_server" {
   project_id    = data.google_project.this.project_id
   protocol      = "tcp"
   target_tags   = [bastion_server.name]
+}
+
+resource "tls_private_key" "bastion_server" {
+  algorithm = "RSA"
+}
+
+data "google_compute_image" "container_optimized_os" {
+  provider = google
+
+  family  = "ubuntu-1804-lts"
+  project = "gce-uefi-images"
+}
+
+resource "google_compute_instance" "bastion_server" {
+  allow_stopping_for_update = "true"
+  machine_type              = "g1-small"
+  name                      = "psql-bastion-server-${terraform.workspace}"
+  project                   = data.google_project.this.project_id
+  zone                      = data.terraform_remote_state.earthranger_app_infra.outputs.gcp_zone
+  boot_disk {
+    initialize_params {
+      image = data.google_compute_image.container_optimized_os.self_link
+    }
+  }
+
+  labels = {
+    role      = "psql-bastion-server"
+    workspace = terraform.workspace
+  }
+
+  metadata = {
+    ssh-keys = "bastion_server:${tls_private_key.bastion_server.public_key_openssh}"
+  }
+
+  network_interface {
+
+    subnetwork = local.subnetwork_name
+
+    access_config { # necessary to allocate public ip
+    }
+  }
+
+  lifecycle {
+    ignore_changes = ["attached_disk"]
+  }
+
+  provisioner "file" {
+    source      = "${path.root}/postgres_bootstrapping.sql"
+    destination = "/home/bastion_server/postgres_bootstrapping.sql"
+
+    connection {
+      host        = google_compute_instance.bastion_server.network_interface.0.access_config.0.nat_ip
+      type        = "ssh"
+      private_key = "${tls_private_key.bastion_server.private_key_pem}"
+      user        = "bastion_server"
+    }
+  }
+
+  tags = [
+    google_compute_instance.bastion_server.name,
+  ]
 }
--- terraform/databases.tf
+++ terraform/databases.tf
@@ -1,6 +1,33 @@
+locals {
+  psql_bootstrapping_executor = <<EOF
+sudo docker run --rm --interactive --env=PGSSLMODE=require --env=PGPASSWORD=${data.vault_generic_secret.db_password.data["value"]} \
+--mount=type=bind,source=$PWD/postgres_bootstrapping.sql,destination=/tmp/postgres_bootstrapping.sql,readonly postgres:9.6 \
+psql --host=172.20.0.2 --username=postgres --dbname=${google_sql_database.database.name} --file=/tmp/postgres_bootstrapping.sql \
+--variable=db_owner=${google_sql_database.database.name} --variable=db_passwd=${data.vault_generic_secret.db_password.data["value"]} \
+--variable=db_name=${google_sql_database.database.name} --single-transaction --variable=ON_ERROR_STOP=1
+EOF
+}
+
+data "vault_generic_secret" "db_password" {
+  path = "padas-app/main/earthranger-app-infra-postgres-server-${terraform.workspace}"
+}
+
 resource "google_sql_database" "database" {
   name = "${var.site}_dasdb"
-  # instance is set explicitly on the db
   instance = data.terraform_remote_state.earthranger_app_infra.outputs.db_instance_name
+
+  provisioner "remote-exec" {
+        connection {
+          host        = google_compute_instance.bastion_server.network_interface.0.access_config.0.nat_ip
+          port        = "22"
+          private_key = "${tls_private_key.bastion_server.private_key_pem}"
+          type        = "ssh"
+          user        = "bastion_server"
+        }
+
+     inline = [
+      local.psql_bootstrapping_executor,
+      ]
+    }
 }
 
