87e0e81fc96f94905cac52c39ce39d266733a2d7
Loraine Kanervisto <lorainek@vulcan.com>
2019-10-30 08:56:54 -0700

    Add vault provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -3,3 +3,8 @@ provider "google" {
   region  = data.terraform_remote_state.earthranger_app_infra.outputs.gcp_region
 }
 
+provider "vault" {
+  address         = "https://vault.vulcancloud.io:8200"
+  skip_tls_verify = "true"
+  version         = ">= 2.1"
+}
