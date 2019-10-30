6020b712738d0a5c54a132669afe2603e6c4ee64
Loraine Kanervisto <lorainek@vulcan.com>
2019-10-30 14:49:55 -0700

    Default for now

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,6 +4,11 @@ variable "site" {
   default     = "dev"
 }
 
+variable "bastion_server_count" {
+  type    = string
+  default = "1"
+}
+
 variable "firewall_priority_threshold" {
   description = "For historical reasons. vCloud has provisioned many GCP projects with default networks with a default-deny-all set at 900."
   type        = string
