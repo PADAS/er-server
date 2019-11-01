e24af2955ae43d1791a92ff55a4679a63f356974
Loraine Kanervisto <lorainek@vulcan.com>
2019-11-01 09:57:14 -0700

    Open the bastion_server to public traffic, CircleCI in this case

--- terraform/bastion_server.tf
+++ terraform/bastion_server.tf
@@ -11,15 +11,24 @@ locals {
   network_name = local.is_production ? local.prod_network_name : local.dev_network_name
 }
 
-module "vulcan_firewall_access_to_bastion_server" {
-  source = "git@github.com:vulcantechnologies/ss-terraform-modules.git//gcp-vulcan-corpnet-firewall?ref=v2.21.0"
 
-  firewall_name = "vulcan-to-bastion-server"
-  network_name  = local.network_name
-  priority      = var.firewall_priority_threshold - 1
-  project_id    = data.google_project.this.project_id
-  protocol      = "tcp"
-  target_tags   = ["psql-bastion-server-${terraform.workspace}"]
+# CircleCI needs to connect to bastion server
+resource "google_compute_firewall" "public_to_bastion_server" {
+  provider = google-beta
+
+  direction               = "INGRESS"
+  disabled                = false
+  enable_logging          = true
+  name                    = "public-to-bastion-server"
+  network                 = local.network_name
+  priority                = var.firewall_priority_threshold - 1
+  project                 = data.google_project.this.project_id
+  target_tags             = ["psql-bastion-server-${terraform.workspace}"]
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
 }
 
 resource "tls_private_key" "bastion_server" {
