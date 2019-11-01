4fdb46bc791c487cf04875da2bc357ad350dcbf6
Loraine Kanervisto <lorainek@vulcan.com>
2019-11-01 16:28:54 -0700

    Get rid of bastion server

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "site" {
 variable "need_bastion_server" {
   description = "Only set to true when postgres bootstrapping is necessary. Meant to be short lived, make sure to revert to false, so that a bastion server with a public IP does not persist."
   type    = bool
-  default = true
+  default = false
 }
 
 variable "firewall_priority_threshold" {
