d61b0d5a00260e88023b4d9f17236b643e64fdd2
SaraC <sarac@vulcan.com>
2019-11-07 14:44:37 -0800

    update providers

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,17 +1,19 @@
 provider "google" {
   project = "earthranger-78ca55ca"
   region  = data.terraform_remote_state.earthranger_app_infra.outputs.gcp_region
+  version = ">=2.19"
 }
 
 provider "vault" {
+  address         = "https://vault.vulcancloud.io:8200"
   skip_tls_verify = "true"
   version         = ">= 2.1"
 }
 
 provider "tls" {
-  version    = ">= 2.0"
+  version    = ">= 2.1"
 }
 
 provider "google-beta" {
-  version = ">= 2.11"
+  version = ">= 2.19"
 }
