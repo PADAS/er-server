8b5026373ca0aba8e214ac22cecd3106d35612bd
Loraine Kanervisto <lorainek@vulcan.com>
2019-11-08 14:28:39 -0800

    Toggle dev/prod for path to db password.

--- terraform/databases.tf
+++ terraform/databases.tf
@@ -1,24 +1,28 @@
+locals {
+  db_secret_path = (local.is_production ? "prod1" : "dev")
+}
+
 data "vault_generic_secret" "db_password" {
-  path = "padas-app/main/earthranger-app-infra-postgres-server-${terraform.workspace}"
+  path = "padas-app/main/earthranger-app-infra-postgres-server-${local.db_secret_path}"
 }
 
 resource "google_sql_database" "database" {
-  project = data.google_project.earthranger.project_id
-  name = "${var.site}_dasdb"
+  project  = data.google_project.earthranger.project_id
+  name     = "${var.site}_dasdb"
   instance = data.terraform_remote_state.earthranger_app_infra.outputs.db_instance_name
 
   provisioner "remote-exec" {
-        connection {
-          host        = google_compute_instance.bastion_server[0].network_interface.0.access_config.0.nat_ip
-          port        = "22"
-          private_key = "${tls_private_key.bastion_server.private_key_pem}"
-          type        = "ssh"
-          user        = "bastion_server"
-        }
-
-     inline = [
-        "((sudo docker run --rm --interactive --env=PGSSLMODE=require --env=PGPASSWORD=${data.vault_generic_secret.db_password.data["value"]} --mount=type=bind,source=$PWD/postgres_bootstrapping.sql,destination=/tmp/postgres_bootstrapping.sql,readonly postgres:9.6 psql --host=${data.terraform_remote_state.earthranger_app_infra.outputs.db_instance_private_ip} --username=postgres --dbname=${google_sql_database.database.name} --file=/tmp/postgres_bootstrapping.sql --variable=db_owner=${google_sql_database.database.name} --variable=db_passwd=${data.vault_generic_secret.db_password.data["value"]} --variable=db_name=${google_sql_database.database.name} --single-transaction --variable=ON_ERROR_STOP=1) && sudo rm -rf /tmp/terraform* && exit 0) || (sudo rm -rf /tmp/terraform* && exit 1)"
-      ]
+    connection {
+      host        = google_compute_instance.bastion_server[0].network_interface.0.access_config.0.nat_ip
+      port        = "22"
+      private_key = "${tls_private_key.bastion_server.private_key_pem}"
+      type        = "ssh"
+      user        = "bastion_server"
     }
+
+    inline = [
+      "((sudo docker run --rm --interactive --env=PGSSLMODE=require --env=PGPASSWORD=${data.vault_generic_secret.db_password.data["value"]} --mount=type=bind,source=$PWD/postgres_bootstrapping.sql,destination=/tmp/postgres_bootstrapping.sql,readonly postgres:9.6 psql --host=${data.terraform_remote_state.earthranger_app_infra.outputs.db_instance_private_ip} --username=postgres --dbname=${google_sql_database.database.name} --file=/tmp/postgres_bootstrapping.sql --variable=db_owner=${google_sql_database.database.name} --variable=db_passwd=${data.vault_generic_secret.db_password.data["value"]} --variable=db_name=${google_sql_database.database.name} --single-transaction --variable=ON_ERROR_STOP=1) && sudo rm -rf /tmp/terraform* && exit 0) || (sudo rm -rf /tmp/terraform* && exit 1)"
+    ]
+  }
 }
 
