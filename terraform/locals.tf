9b81a76bf2c92ef8e9eeb2fe9961bb7a9777a048
Loraine Kanervisto <lorainek@vulcan.com>
2019-11-14 10:35:29 -0800

    CA cert

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -5,5 +5,7 @@ locals {
   }
 
   default_infra_workspace_when_not_mapped_here = "dev"
+
+  b64_encoded_cluster_or_proxy_ca_certificate = var.is_running_in_automation ? data.terraform_remote_state.earthranger_app_infra.outputs.b64_encoded_proxy_ca_certificate : data.terraform_remote_state.earthranger_app_infra.outputs.b64_encoded_cluster_ca_certificate
 }
 
