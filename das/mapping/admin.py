876a95178f4f6ea4b0f0c2ab8c2f1ee2d84362d2
phillipahereza <pahereza@gmail.com>
2019-11-18 16:47:43 +0300

    setting location cookie for observations

--- das/mapping/admin.py
+++ das/mapping/admin.py
@@ -65,46 +65,6 @@ class BaseFeatureAdmin(OSMGeoExtendedAdmin):
     list_display = ('name', 'type', 'featureset')
     search_fields = ('name', )
 
-    def get_single_coordinate_pair(self, coords):
-        try:
-            if not isinstance(coords[0], tuple):
-                return coords
-            return self.get_single_coordinate_pair(coords[0])
-        except Exception:
-            return (0, 0)
-
-    def get_form(self, request, obj=None, **kwargs):
-        if not obj:
-            lon, lat = 0, 0
-            try:
-                lon = float(request.COOKIES.get('longitude', 0))
-                lat = float(request.COOKIES.get('latitude', 0))
-            except ValueError:
-                pass
-
-            self.default_lat = lat
-            self.default_lon = lon
-        return super(BaseFeatureAdmin, self).get_form(request, obj=None, **kwargs)
-
-    def set_coordinates_cookie(self, http_response, obj):
-        coords = obj.feature_geometry.coords
-        long, lat = self.get_single_coordinate_pair(coords)
-        http_response.set_cookie("latitude", lat, max_age=365 * 24 * 60 * 60)
-        http_response.set_cookie("longitude", long, max_age=365 * 24 * 60 * 60)
-        return http_response
-
-    def response_post_save_add(self, request, obj):
-        http_response = super(BaseFeatureAdmin,
-                              self).response_post_save_add(request, obj)
-        response = self.set_coordinates_cookie(http_response, obj)
-        return response
-
-    def response_post_save_change(self, request, obj):
-        http_response = super(BaseFeatureAdmin,
-                              self).response_post_save_change(request, obj)
-        response = self.set_coordinates_cookie(http_response, obj)
-        return response
-
 
 @admin.register(models.PolygonFeature)
 class PolygonFeatureAdmin(BaseFeatureAdmin):
--- das/observations/admin.py
+++ das/observations/admin.py
@@ -321,6 +321,13 @@ class ObservationAdmin(ExportCsvMixin, OSMGeoExtendedAdmin):
 
     actions = ['export_as_csv', ]
 
+    def set_coordinates_cookie(self, http_response, obj):
+        coords = obj.location.coords
+        long, lat = self.get_single_coordinate_pair(coords)
+        http_response.set_cookie("latitude", lat, max_age=365 * 24 * 60 * 60)
+        http_response.set_cookie("longitude", long, max_age=365 * 24 * 60 * 60)
+        return http_response
+
 
 class SourceProviderFilter(admin.SimpleListFilter):
     title = 'Source Provider'
