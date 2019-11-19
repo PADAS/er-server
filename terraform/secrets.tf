c6443e0698e634785cbdf5b97b22c36520270cdd
Loraine Kanervisto <lorainek@vulcan.com>
2019-11-19 12:02:21 -0800

    Adding first secret.

---
+++ terraform/secrets.tf
@@ -0,0 +1,9 @@
+resource "kubernetes_secret" "alerts_slack_url" {
+  metadata {
+    name      = "alerts-slack-url"
+    namespace = kubernetes_namespace.this.metadata.0.name
+  }
+
+  data = data.vault_generic_secret.alerts_slack_url.data
+}
+
