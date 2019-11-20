d56a1977b29276598a61c538ae34db3c1f212536
Loraine Kanervisto <lorainek@vulcan.com>
2019-11-20 11:15:11 -0800

    Formatted

--- terraform/databases.tf
+++ terraform/databases.tf
@@ -1,5 +1,5 @@
 locals {
-  db_secret_path = (local.is_production ? "prod1" : "dev")
+  db_secret_path    = (local.is_production ? "prod1" : "dev")
   sanitized_db_name = substr(replace(terraform.workspace, "/[^A-Za-z0-9_]/", "_"), 0, 28)
 }
 
