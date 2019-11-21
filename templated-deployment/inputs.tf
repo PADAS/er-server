73c28cbb53d19a00ce1361cc5418c19bce4ba140
Loraine Kanervisto <lorainek@vulcan.com>
2019-11-20 16:45:32 -0800

    Render default templates

--- templated-deployment/inputs.tf
+++ templated-deployment/inputs.tf
@@ -1,51 +1,41 @@
 variable "api_endpoint" {
   type = string
-  default = "localhost"
 }
 
 variable "api_host" {
   type = string
-  default = "api"
 }
 
 variable "api_port" {
   type = string
-  default = "8000"
 }
 
 variable "config_container" {
   type = string
-  default = "dev-az"
 }
 
 variable "db_name" {
   type = string
-  default = "das"
 }
 
 variable "db_port" {
   type = string
-  default = "5432"
 }
 
 variable "kml_export" {
   type = string
-  default = "true"
 }
 
 variable "storage_container" {
   type = string
-  default = ""
 }
 
 variable "use_azure_storage" {
   type = string
-  default = "false"
 }
 
 variable "web_service_name" {
   type = string
-  default = "web"
 }
 
 
