c818dba45275ee5dadc15cf5fd9dc49a009c42c5
Loraine Kanervisto <lorainek@vulcan.com>
2019-12-02 12:56:32 -0800

    remove target for gcloud auth config, add output for ca cert

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "gcloud_kubectl_configuration" {
   value = data.terraform_remote_state.earthranger_app_infra.outputs.gcloud_kubectl_configuration
 }
+
+output "cluster_b64_encoded_proxy_ca_certificate" {
+ value = data.terraform_remote_state.earthranger_app_infra.outputs.b64_encoded_proxy_ca_certificate
+}
