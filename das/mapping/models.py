14e9c400e068a92a7718f457009a74a8534c3973
rohitc <rohitc@vulcan.com>
2019-12-14 22:57:13 -0800

    plural for feature class

--- das/mapping/models.py
+++ das/mapping/models.py
@@ -479,6 +479,7 @@ class SpatialFeatureTypeManager(models.Manager):
 class SpatialFeatureType(TimestampedModel):
     class Meta:
         verbose_name = 'Feature Class'
+        verbose_name_plural = 'Feature Classes'
 
     objects = SpatialFeatureTypeManager()
 
