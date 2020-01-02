9325d0023bd0d7130bf620c2cc0f283ccbe2b8f9
rohitc <rohitc@vulcan.com>
2020-01-02 12:33:36 -0800

    view returns DisplayCategory/SpatialFeatureType/SpatialFeature or FeatureSet/FeatureType/Point/Line/Polygon based on a feature flag.

--- das/mapping/views.py
+++ das/mapping/views.py
@@ -1,27 +1,28 @@
-import simplejson as json
+import hashlib
 import logging
 from itertools import chain
-import hashlib
 
-from django.db.models import F
-
+import simplejson as json
+from django.conf import settings
 from django.core.serializers import serialize
-from django.urls import reverse
+from django.db.models import F
 from django.http import HttpResponse, Http404
+from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 from rest_framework import generics
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.views import APIView
 from rest_framework.parsers import JSONParser
+from rest_framework.views import APIView
 from rest_framework_extensions.etag.decorators import etag
 
-from mapping.models import PolygonFeature, LineFeature, PointFeature, FeatureSet
-from mapping.models import MBTiles, MBTilesNotFoundError, MissingTileError, Map, TileLayer
 import mapping.serializers as serializers
 from mapping import app_settings
-
+from mapping.models import MBTiles, MBTilesNotFoundError, MissingTileError, Map, TileLayer
+from mapping.models import PolygonFeature, LineFeature, PointFeature, FeatureSet
+from mapping.models import SpatialFeature, DisplayCategory
 
 logger = logging.getLogger(__name__)
+MAPPING_FEATURES_V2 = getattr(settings, 'MAPPING_FEATURES_V2', False)
 
 
 class FeatureListJsonView(APIView):
@@ -32,12 +33,18 @@ class FeatureListJsonView(APIView):
     def get(self, request):
         # todo:  add api docs
         response_data = {'features': []}
-        features = list(chain(PolygonFeature.objects.all(),
-                              LineFeature.objects.all(), PointFeature.objects.all()))
+        features = SpatialFeature.objects.all() if MAPPING_FEATURES_V2 \
+            else list(chain(PolygonFeature.objects.all(),
+                            LineFeature.objects.all(),
+                            PointFeature.objects.all()))
+
         for feature in features:
+            type_dict = dict(name=feature.feature_type.name, id=str(feature.feature_type.id)) if MAPPING_FEATURES_V2 \
+                else dict(name=feature.type.name, id=str(feature.type.id))
+
             response_data['features'].append({
                 'name': feature.name,
-                'type': dict(name=feature.type.name, id=str(feature.type.id)),
+                'type': type_dict,
                 'description': feature.description if feature.description else '',
                 'geojson_url': reverse('mapping:mapping-feature-geojson', args=[feature.id.hex]),
             })
@@ -46,10 +53,13 @@ class FeatureListJsonView(APIView):
 
 class FeatureGeoJsonView(APIView):
     def get(self, request, id):
+        all_features = SpatialFeature.objects.filter(id=id) if MAPPING_FEATURES_V2 \
+            else list(chain(PolygonFeature.objects.filter(id=id),
+                            LineFeature.objects.filter(id=id),
+                            PointFeature.objects.filter(id=id)))
+
         feature = serialize('geojson',
-                            list(chain(PolygonFeature.objects.filter(id=id),
-                                       LineFeature.objects.filter(id=id),
-                                       PointFeature.objects.filter(id=id))),
+                            all_features,
                             properties={'name': 'title',
                                         'default_presentation': 'presentation'},
                             geometry_field='feature_geometry'
@@ -64,11 +74,15 @@ class FeatureSetListJsonView(APIView):
 
     def get(self, request):
         def feature_types(featureset):
-            for t in featureset.types.all():
+            feature_types_qs = featureset.spatialfeaturetype_set.all() if MAPPING_FEATURES_V2 \
+                else featureset.types.all()
+
+            for t in feature_types_qs:
                 yield dict(name=t.name, id=str(t.id), feature_count=t.feature_count)
 
         response_data = {'features': []}
-        featuresets = FeatureSet.objects.all()
+        featuresets = DisplayCategory.objects.all() if MAPPING_FEATURES_V2 else FeatureSet.objects.all()
+
         for featureset in featuresets:
             response_data['features'].append({
                 'name': featureset.name,
@@ -81,13 +95,21 @@ class FeatureSetListJsonView(APIView):
 
 
 def calculate_featureset_etag(view_instance, view_method, request, args, kwargs):
-    featureset = FeatureSet.objects.get(id=kwargs['id'])
-    field_list = ('updated_at', 'type__updated_at')
-    objects = chain(PolygonFeature.objects.filter(featureset=featureset).values(*field_list),
-                    LineFeature.objects.filter(featureset=featureset).values(*field_list),
-                    PointFeature.objects.filter(featureset=featureset).values(*field_list))
-    etag = ','.join((str(f['updated_at']) + str(f['type__updated_at'])
-                     for f in objects))
+
+    if MAPPING_FEATURES_V2:
+        featureset = DisplayCategory.objects.get(id=kwargs['id'])
+        field_list = ('updated_at', 'feature_type__updated_at')
+        objects = SpatialFeature.objects.filter(feature_type__display_category=featureset).values(*field_list)
+        etag = ','.join((str(f['updated_at']) + str(f['feature_type__updated_at'])
+                         for f in objects))
+    else:
+        featureset = FeatureSet.objects.get(id=kwargs['id'])
+        field_list = ('updated_at', 'type__updated_at')
+        objects = chain(PolygonFeature.objects.filter(featureset=featureset).values(*field_list),
+                        LineFeature.objects.filter(featureset=featureset).values(*field_list),
+                        PointFeature.objects.filter(featureset=featureset).values(*field_list))
+        etag = ','.join((str(f['updated_at']) + str(f['type__updated_at'])
+                         for f in objects))
     etag += str(featureset.updated_at)
     return hashlib.md5(etag.encode('utf-8')).hexdigest()
 
@@ -99,14 +121,20 @@ class FeatureSetGeoJsonView(APIView):
     @etag(etag_func=calculate_featureset_etag)
     def get(self, request, **kwargs):
         # todo:  better 404 handling, what to do with empty featureset
-        featureset = FeatureSet.objects.get(id=kwargs['id'])
-
-        querysets = (PolygonFeature.objects.filter(featureset=featureset),
-                     LineFeature.objects.filter(featureset=featureset),
-                     PointFeature.objects.filter(featureset=featureset))
-        # So type-name can appear in geojson properties.
-        querysets = (q.prefetch_related('type').annotate(
-            type_name=F('type__name')) for q in querysets)
+        if MAPPING_FEATURES_V2:
+            featureset = DisplayCategory.objects.get(id=kwargs['id'])
+            querysets = (SpatialFeature.objects.filter(feature_type__display_category=featureset),)
+            # So type-name can appear in geojson properties.
+            querysets = (q.prefetch_related('feature_type').annotate(
+                type_name=F('feature_type__name')) for q in querysets)
+        else:
+            featureset = FeatureSet.objects.get(id=kwargs['id'])
+            querysets = (PolygonFeature.objects.filter(featureset=featureset),
+                         LineFeature.objects.filter(featureset=featureset),
+                         PointFeature.objects.filter(featureset=featureset))
+            # So type-name can appear in geojson properties.
+            querysets = (q.prefetch_related('type').annotate(
+                type_name=F('type__name')) for q in querysets)
 
         feature = serialize('geojson',
                             list(chain(*querysets)),
