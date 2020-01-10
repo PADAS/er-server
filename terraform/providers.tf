5781759608cf57cc71582aed198d9afbb46a2487
Dominic Motuka <dominic.motuka@gmail.com>
2020-01-10 23:36:23 +0300

    Add AWS provider
      - Used to interract with resources supported by AWS
    
    Signed-off-by: Dominic Motuka <dominic.motuka@gmail.com>

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -40,3 +40,9 @@ provider "vault" {
   version         = ">= 2.1"
 }
 
+provider "aws" {
+  version    = "~>2.44"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region     = "us-east-1"
+}
