b3120042e9a0c7e566ab95e83bad1853dd94e996
phillipahereza <pahereza@gmail.com>
2020-01-15 22:35:44 +0300

    fix coerce method

--- das/activity/tests/test_alerts.py
+++ das/activity/tests/test_alerts.py
@@ -1,11 +1,34 @@
+from django.forms import DateTimeField
 from django.test import TestCase
+from django.utils import lorem_ipsum, timezone
 
 from activity.alerting.message import coerce_state_value
+from activity.models import Event
 
+ET_OTHER = 'other'
 
 class TestAlerts(TestCase):
-    def test_alert_coarces_to_the_right_state(self):
-        states = [{'name': 'New', 'value': 'new'}, {'name': 'Active', 'value': 'active'}, {'name': 'Resolved', 'value': 'resolved'}]
+    def setUp(self) -> None:
+        self.states = [{'name': 'New', 'value': 'new'},
+                  {'name': 'Active', 'value': 'active'},
+                  {'name': 'Resolved', 'value': 'resolved'}]
 
-        for state in states:
-            self.assertEqual(state.get('name'), coerce_state_value(state.get('value')))
+        self.event_data = dict(
+            message=lorem_ipsum.paragraph(),
+            time=DateTimeField().to_representation(timezone.now()),
+            provenance=Event.PC_SYSTEM,
+            event_type=ET_OTHER,
+            priority=Event.PRI_REFERENCE,
+            location=dict(longitude='40.1353', latitude='-1.891517'),
+        )
+
+    def test_alert_coerces_to_the_right_state_val(self):
+        for state in self.states:
+            self.assertEqual(state.get('name'), coerce_state_value(val=state.get('value')))
+
+    def test_alert_coerces_event_to_right_state(self):
+        self.fail()
+
+
+
+
