86afbf1f91f270eb58db20bbecf031f805a652f0
rohitc <rohitc@vulcan.com>
2020-01-16 16:57:57 -0800

    unittests for is_visible field in SpatialFeatureType model and include_hidden query parameter for mapping API

--- das/mapping/tests/test_features.py
+++ das/mapping/tests/test_features.py
@@ -118,7 +118,6 @@ class TestFeatures(BaseAPITest):
         data = json.loads(response.content)
         self.assertEqual(len(data['features']), 0)
 
-
     def test_with_feature_class_is_visible_false_include_hidden_true(self):
         self.feature_class.is_visible = False
         self.feature_class.save()
