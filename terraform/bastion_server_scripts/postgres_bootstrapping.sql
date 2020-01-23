a2e20b14d1f0954b368a5151de485968acc00ef2
Dominic Motuka <dominic.motuka@gmail.com>
2020-01-23 17:43:52 +0300

    Revoke new user access and table creation in public schema
    Signed-off-by: Dominic Motuka <dominic.motuka@gmail.com>

--- terraform/bastion_server_scripts/postgres_bootstrapping.sql
+++ terraform/bastion_server_scripts/postgres_bootstrapping.sql
@@ -3,3 +3,7 @@ CREATE EXTENSION IF NOT EXISTS "unaccent";
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS "postgis";
 CREATE EXTENSION IF NOT EXISTS "postgis_topology";
+
+-- Revoke new user access and table creation in public schema
+REVOKE ALL ON DATABASE :db_owner FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
