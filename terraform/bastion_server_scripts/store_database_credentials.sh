030d7f82f92076097757d7fc7e26ac00f252f4c8
Dominic Motuka <dominic.motuka@gmail.com>
2020-01-28 01:41:35 +0300

    Store credentials from database creation
    Signed-off-by: Dominic Motuka <dominic.motuka@gmail.com>

--- terraform/bastion_server_scripts/store_database_credentials.sh
+++ terraform/bastion_server_scripts/store_database_credentials.sh
@@ -39,16 +39,46 @@ require() {
 
 activate_google_sdk() {
     info "Activating Google Cloud  sdk"
-    gcloud auth activate-service-account secret-manager-api-access@bench-projects.iam.gserviceaccount.com --key-file=/tmp/test-service-account.json --project=bench-projects
+    gcloud auth activate-service-account secret-manager-key@earthranger-78ca55ca.iam.gserviceaccount.com --key-file=/tmp/secret_manager_key.json --project=earthranger-78ca55ca
     is_success "The Gcloud SDK has successfully been activated"
 }
 
 store_migrator_secret() {
-    info "Add database migrator secret"
-
+    if [[ $(gcloud beta secrets list --format text | grep "$migrator") = *migrator* ]]; then
+        # Next: Compare stored pass and new pass if != make new version of password
+        info "Compare migrator role stored passoword and new password"
+        (echo -n "$migrator_pass" | gcloud beta secrets versions add "$migrator" --data-file=-)
+    else
+        info "Create database migrator role password"
+        (echo -n "$migrator_pass" | gcloud beta secrets create $migrator --data-file=- --replication-policy=automatic)
+    fi
 }
+store_app_secret() {
+    if [[ $(gcloud beta secrets list --format text | grep "$app_user") = *app* ]]; then
+        # Next: Compare stored pass and new pass if != make new version of password
+        info "Update database app role password"
+        (echo -n "$app_user_pass" | gcloud beta secrets versions add "$app_user" --data-file=-)
+    else
+        info "Add database app role password"
+        (echo -n "$app_user_pass" | gcloud beta secrets create "$app_user" --data-file=- --replication-policy=automatic)
+    fi
+}
+store_analytics_secret() {
+    if [[ $(gcloud beta secrets list --format text | grep "$analytics_user") = *analytics* ]]; then
+        # Next: Compare stored pass and new pass if != make new version of password
+        info "Update database analytics role password"
+        (echo -n "$analytics_user_pass" | gcloud beta secrets versions add "$analytics_user" --data-file=-)
+    else
+        info "Create database analytics role password"
+        (echo -n "$analytics_user_pass" | gcloud beta secrets create "$analytics_user" --data-file=- --replication-policy=automatic)
+    fi
+}
+
 main() {
     activate_google_sdk
+    store_migrator_secret
+    store_app_secret
+    store_analytics_secret
 }
 
 main
