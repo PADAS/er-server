d52d68f03174e4a6b1d69791e0e1e04c2f6d3d4d
blac-siren <husnein.zac@gmail.com>
2020-01-27 19:54:38 +0300

    Add MultiWidget

--- das/observations/admin.py
+++ das/observations/admin.py
@@ -277,11 +277,12 @@ class LargeTablePaginator(Paginator):
 
     count = cached_property(_get_count)
 
-
+from observations.custom_widget import OForm
 @admin.register(models.Observation)
 class ObservationAdmin(ExportCsvMixin, ValidateFilterMixin, OSMGeoExtendedAdmin):
+    form = OForm
     readonly_fields = ("created_at", "id")
-    fields = ("id", "recorded_at", "created_at",
+    fields = ("id", "recorded_at", "created_at", "Coordinate",
               "location", "exclusion_flags", "source", "additional")
     list_display = ('subject_link', '_manufacturer_id', '_recorded_at', '_created_at',
                     '_longitude', '_latitude', '_state', '_event_action', 'exclusion_flags')
