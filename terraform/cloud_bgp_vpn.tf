7d2842b9d44eb8d5982b93e9cbb77ea41a7a606f
Dominic Motuka <dominic.motuka@gmail.com>
2020-02-06 18:26:06 +0300

    Create cloud router and advertise subnet IP CIDR
    Signed-off-by: Dominic Motuka <dominic.motuka@gmail.com>

--- terraform/cloud_bgp_vpn.tf
+++ terraform/cloud_bgp_vpn.tf
@@ -14,3 +14,29 @@ resource "google_compute_subnetwork" "dev_subnet" {
   region                   = "us-west1" #dynamically add
 }
 
+# Cloud Router enables you to dynamically exchange routes between your Virtual Private Cloud (VPC) 
+# and on-premises networks by the using of Borderway Gateway Protocol
+resource "google_compute_router" "dev" {
+  name    = "aws-gcp-router"
+  network = google_compute_network.dev_vpc_network.name
+  project = data.google_project.earthranger.project_id
+  region  = "us-west1" #dynamically add
+  bgp {
+    asn               = 65001
+    advertise_mode    = "CUSTOM"
+    advertised_groups = ["ALL_SUBNETS"]
+    advertised_ip_ranges {
+      range = "10.100.0.0/20"
+    }
+    advertised_ip_ranges {
+      range = "10.48.0.0/14"
+    }
+    advertised_ip_ranges {
+      range = "10.241.0.0/20"
+    }
+
+  }
+
+}
+
+
