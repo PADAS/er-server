ef1f015db62cf56718b10d40f96a17af6b18fef7
Dominic Motuka <dominic.motuka@gmail.com>
2020-02-06 18:30:14 +0300

    Create two VPN tunnels for high availability
    Signed-off-by: Dominic Motuka <dominic.motuka@gmail.com>

--- terraform/cloud_bgp_vpn.tf
+++ terraform/cloud_bgp_vpn.tf
@@ -70,3 +70,58 @@ resource "google_compute_vpn_gateway" "dev_gateway" {
   network  = google_compute_network.dev_vpc_network.name
 }
 
+# Each tunnel is responsible for encrypting and decrypting traffic exiting
+# and leaving its associated gateway
+# We will create 2 tunnels to aws on same GCP VPN gateway
+
+# Create Tunnel1
+#--------------------------------------------
+resource "google_compute_vpn_tunnel" "dev_tunnel1" {
+  name          = "vpn-1-tunnel-1"
+  ike_version   = 1
+  peer_ip       = "52.36.71.252"
+  shared_secret = "" #store in vault
+  project       = data.google_project.earthranger.project_id
+  region        = "us-west1" #dynamically add
+
+  timeouts {
+    create = "4m"
+    delete = "4m"
+  }
+
+  router             = google_compute_router.dev.self_link
+  target_vpn_gateway = google_compute_vpn_gateway.dev_gateway.self_link
+
+  depends_on = [
+    google_compute_forwarding_rule.vpn_1_rule_esp,
+    google_compute_forwarding_rule.vpn_1_rule_udp500,
+    google_compute_forwarding_rule.vpn_1_rule_udp4500,
+  ]
+
+}
+
+# VPN Tunnel2
+resource "google_compute_vpn_tunnel" "dev_tunnel2" {
+  name          = "vpn-1-tunnel-2"
+  ike_version   = 1
+  peer_ip       = "54.68.48.172"
+  shared_secret = "" #store in vault
+  project       = data.google_project.earthranger.project_id
+  region        = "us-west1" #dynamically add
+
+  timeouts {
+    create = "4m"
+    delete = "4m"
+  }
+
+  router             = google_compute_router.dev.self_link
+  target_vpn_gateway = google_compute_vpn_gateway.dev_gateway.self_link
+
+  depends_on = [
+    google_compute_forwarding_rule.vpn_1_rule_esp,
+    google_compute_forwarding_rule.vpn_1_rule_udp500,
+    google_compute_forwarding_rule.vpn_1_rule_udp4500,
+  ]
+
+}
+
