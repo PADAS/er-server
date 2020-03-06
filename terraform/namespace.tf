83831133de9ae25e40dd43ca1c7798b2e0b32763
Chris Doehring <chrisdoehring@gmail.com>
2020-03-06 11:39:50 -0800

    Update cleaning branch name for K8s namespace.

--- terraform/namespace.tf
+++ terraform/namespace.tf
@@ -1,6 +1,6 @@
 resource "kubernetes_namespace" "this" {
   metadata {
     # To be DNS compliant, replace _ with -
-    name = lower(replace(terraform.workspace, "_", "-"))
+    name = lower(replace(terraform.workspace, "/([[:punct:]]|_)/" , "-"))
   }
 }
