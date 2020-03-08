0784ff9d52a85bfafb9c96c23874fc0d05b08d98
Kezzy Angiro <kezzyangiro@Kezzys-MacBook-Pro.local>
2020-03-09 02:01:04 +0300

    exclude conditions with eventtypes of invalid schema

--- das/activity/alerts_views.py
+++ das/activity/alerts_views.py
@@ -10,7 +10,7 @@ from activity.alerting.businessrules import render_aggregate_event_variables
 
 from utils.drf import StandardResultsSetPagination
 from utils.json import parse_bool
-
+from utils.schema_utils import get_schema_renderer_method
 logger = logging.getLogger(__name__)
 
 # Views for Advanced Alert Functionality.
@@ -34,8 +34,19 @@ class EventAlertConditionsListView(generics.ListAPIView):
     def get(self, *args, **kwargs):
         only_common_factors = parse_bool(
             self.request.query_params.get('only_common_factors', False))
-        rules = render_aggregate_event_variables(self.get_queryset(
-        ), only_common_factors=only_common_factors, user=self.request.user)
+        
+        # Exclude conditions with eventtypes of invalid schema
+        queryset, errored_types = self.get_queryset(), []
+        for eventype in queryset:
+            try:
+                get_schema_renderer_method()(eventype.schema)
+            except Exception:
+                logger.exception(f"{eventype} event type skipped, invalid schema")
+                errored_types.append(eventype.display)
+
+        queryset = queryset.exclude(display__in=errored_types)
+        rules = render_aggregate_event_variables(
+            queryset, only_common_factors=only_common_factors, user=self.request.user)
 
         return response.Response(rules, status=status.HTTP_200_OK)
 
