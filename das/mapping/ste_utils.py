6c51f621341abfb5051ac68e5ab5877ed4ac8e07
Kezzy Angiro <kezzyangiro@Kezzys-MacBook-Pro.local>
2020-03-18 03:06:46 +0300

    update gcp files download

--- das/mapping/ste_utils.py
+++ das/mapping/ste_utils.py
@@ -1,8 +1,9 @@
 import io
 import logging
 import os
-from zipfile import ZipFile, is_zipfile
+import zipfile
 
+import requests
 from django.core.exceptions import ValidationError
 from django.core.files.storage import default_storage
 
@@ -12,7 +13,8 @@ from mapping import utils
 logger = logging.getLogger(__name__)
 
 googlestorage = default_storage.__class__.__name__ == 'GoogleCloudStorage'
-ste_folder = 'spatialfiles' if googlestorage else 'mapping/spatialfiles'
+local_ste_folder = 'mapping/spatialfiles'
+ste_folder = 'spatialfiles' if googlestorage else local_ste_folder
 bucket_name = "kezzy-ste-test"
 storage_client = storage.Client()
 
@@ -27,16 +29,13 @@ def extract_features_from_files(spatial_file, model, presentation):
         types_file = None
 
     if googlestorage:
-        if filename.lower().endswith('.zip'):
-            download_file_from_gcp(spatial_file)
-            data_file = get_upload_file(spatial_file.data, model, spatial_file.id)
-        else:
-            data_file = spatial_file.data.url
-            feature_types_file = types_file.url if types_file else None
-    else:
-        data_file = get_upload_file(spatial_file.data, model, spatial_file.id)
-        if types_file:
-            feature_types_file = get_upload_file(spatial_file.feature_types_file, model, spatial_file.id)
+        if not os.path.exists(local_ste_folder):
+            os.makedirs(local_ste_folder)
+
+        download_files_from_gcp(spatial_file, types_file)
+    data_file = get_upload_file(spatial_file.data, model, spatial_file.id)
+    if types_file:
+        feature_types_file = get_upload_file(types_file, model, spatial_file.id)
 
     # extract data from upload file
     if types_file:
@@ -50,14 +49,12 @@ def extract_features_from_files(spatial_file, model, presentation):
 
 def get_upload_file(upload_file, model, spatial_file_id):
     if googlestorage:
-        uploaded_file_directory = f'{os.getcwd()}/mapping/spatialfiles'
         path = f'mapping/{upload_file.name}'
     else:
-        uploaded_file_directory = os.path.dirname(upload_file.path)
         path = upload_file.path
 
     try:
-        return import_spatial_file(path, uploaded_file_directory)
+        return import_spatial_file(path)
     except ValidationError as err:
         model.objects.filter(id=spatial_file_id).delete()
         raise ValidationError(
@@ -66,7 +63,7 @@ def get_upload_file(upload_file, model, spatial_file_id):
         )
 
 
-def import_spatial_file(uploaded_file_path, uploaded_file_directory):
+def import_spatial_file(uploaded_file_path):
     """
     Import features by invoking importlayer management command.
     :param uploaded_file_path: Path of uploaded file.
@@ -76,16 +73,14 @@ def import_spatial_file(uploaded_file_path, uploaded_file_directory):
         import_file = None
         if uploaded_file_path.lower().endswith('.zip'):
             # Extract user-uploaded zip file.
-            with ZipFile(
-                    uploaded_file_path, 'r') as zip_file_object:
-                zip_file_object.extractall(uploaded_file_directory)
+            if not googlestorage:
+                with zipfile.ZipFile(uploaded_file_path, 'r') as zip_file_object:
+                    zip_file_object.extractall(local_ste_folder)
 
-            import_file = fetch_shape_file_path(
-                uploaded_file_directory)
+            import_file = fetch_shape_file_path(local_ste_folder)
             # If zip contains a directory encapsulating all the shape files
             if not import_file:
-                import_file = fetch_shape_file_path(
-                    uploaded_file_path[:-4])
+                import_file = fetch_shape_file_path(uploaded_file_path[:-4])
         else:
             import_file = uploaded_file_path
 
@@ -113,11 +108,20 @@ def fetch_shape_file_path(directory_path):
     return import_file
 
 
-def download_file_from_gcp(spatial_file):
-    filename = spatial_file.data.name.split('/')[-1]
-    blobs = storage_client.list_blobs(bucket_name, prefix=ste_folder)
-    for blob in blobs:
-        folder = f'mapping/{ste_folder}'
-        if not os.path.exists(folder):
-            os.makedirs(folder)
-        blob.download_to_filename(f'{folder}/{filename}')
+def download_files_from_gcp(spatial_file, types_file):
+
+    files = [spatial_file.data]
+
+    if types_file:
+        files.append(spatial_file.feature_types_file)
+
+    for upload_file in files:
+        request = requests.get(upload_file.url)
+        name = upload_file.name
+
+        if upload_file.name.lower().endswith('.zip'):
+            downloaded_zip = zipfile.ZipFile(io.BytesIO(request.content), 'r')
+            downloaded_zip.extractall(local_ste_folder)
+        else:
+            with open(f'{local_ste_folder}/{name.split("/")[-1]}', 'w+') as fd:
+                fd.write(request.content.decode())
