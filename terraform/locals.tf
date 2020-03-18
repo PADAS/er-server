654307abfe073d281ff89598dc3cd1c290b21015
Dominic Motuka <dominic.motuka@gmail.com>
2020-03-18 09:26:14 +0300

    Add sabiegamereserve to prod cluster
    Signed-off-by: Dominic Motuka <dominic.motuka@gmail.com>

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -37,6 +37,7 @@ locals {
     "gonarezhou"             = "prod1"
     "matlamamba"             = "prod1"
     "sabisands"              = "prod1"
+    "sabiegamereserve"       = "prod1"
   }
 
   default_infra_workspace_when_not_mapped_here = "dev"
