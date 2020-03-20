a8e98aa41c57c09e7d656c91f80d5e42c620d8f8
blac-siren <husnein.zac@gmail.com>
2020-03-20 20:47:29 +0300

    All fields in spatialfile editable

--- das/mapping/admin.py
+++ das/mapping/admin.py
@@ -395,10 +395,6 @@ class BaseSpatialFileAdmin(admin.ModelAdmin):
                                       "Delete selected spatial files")
         return actions
 
-    def get_readonly_fields(self, request, obj=None):
-        if obj:
-            return [f.name for f in self.model._meta.fields]
-        return self.readonly_fields
 
 
 if MAPPING_FEATURES_V2:
