aac73c0f43e9c3191193f28dc5e04410e856786f
Dominic Motuka <dominic.motuka@gmail.com>
2020-03-24 17:00:10 +0300

    Add elephantsalive to GCP prod cluster
    Signed-off-by: Dominic Motuka <dominic.motuka@gmail.com>

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -40,6 +40,7 @@ locals {
     "sabiegamereserve"       = "prod1"
     "oljogi"                 = "prod1"
     "balule"                 = "prod1"
+    "elephantsalive"         = "prod1"
   }
 
   default_infra_workspace_when_not_mapped_here = "dev"
