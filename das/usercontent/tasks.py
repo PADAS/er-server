50cc6b50ae8da27713563e2ce6d0af29fa44ef7c
Chris Doehring <chrisdoehring@gmail.com>
2020-04-10 12:04:14 -0700

    Notify on when new renditions are created.

--- das/usercontent/tasks.py
+++ das/usercontent/tasks.py
@@ -21,11 +21,13 @@ def warm_imagefilecontent(self, imagefile_content_id):
             rendition_key_set='default',
             image_attr='file'
         )
-        print('warmed images.')
+
         num_created, failed_to_create = warmer.warm()
         logger.info('Warmed images for imagefile_content_id=%s', imagefile_content_id)
 
-        imagefile_rendered.send(sender=None, usercontent_id=instance.id)
+        # If any images have been created, signal receivers.
+        if num_created:
+            imagefile_rendered.send(sender=None, usercontent_id=instance.id)
 
     except Exception as e:
        logger.exception('Failed when warming images for imagefile_content_id {}'.format(imagefile_content_id))
