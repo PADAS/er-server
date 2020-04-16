f2d0f065132ddddff75ac713f74b91f3b25e4493
kzyGit <kezzyangiro@gmail.com>
2020-04-16 21:25:26 +0300

    Feedback

--- das/sensors/capturs.py
+++ das/sensors/capturs.py
@@ -1,10 +1,8 @@
 import logging
 from datetime import datetime
-from typing import NamedTuple
 
 import pytz
-from django.conf import settings
-from rest_framework import status
+from rest_framework import serializers, status
 from rest_framework.response import Response
 
 from observations.models import Observation, Source
@@ -13,62 +11,34 @@ from observations.serializers import ObservationSerializer
 logger = logging.getLogger(__name__)
 
 
-DAS_SUBJECT_TYPE = 'vehicle'
-DAS_SUBJECT_SUBTYPE = 'car'
+DAS_SUBJECT_TYPE = 'person'
+DAS_SUBJECT_SUBTYPE = 'ranger'
 DAS_SOURCE_TYPE = 'tracking-device'
-OBS_MIN_YEAR = 2010
 
 
-class CaptursDeviceObs(NamedTuple):
-    """"
-    Data object that represents the common fields retrieved from capturs
-    payload on https://api.capturs.com/device/{device}/position
-    """
-    device_id: str
-    name: str
-    recorded_at: datetime
-    lat: float
-    lon: float
-    additional: dict
-
-    def is_valid(self):
-        # omit old dates, and those with no pos fix
-        valid_obs = (self.recorded_at.year >
-                     OBS_MIN_YEAR) and self.lat and self.lon
-        return valid_obs
+class CaptursObservationSerializer(serializers.Serializer):
+    device_id = serializers.CharField()
+    name = serializers.CharField(default=None)
+    recorded_at = serializers.DateTimeField()
+    lat = serializers.FloatField()
+    lon = serializers.FloatField()
+    additional = serializers.DictField()
 
 
 class CaptursAdapter:
     @staticmethod
     def create_capturs_obs(data):
         """
-        Validate and create capturs observation
+        Serialize and create capturs observation
         """
-        obs = CaptursDeviceObs(
-            device_id=data['device'],
-            name=data['device'],
-            recorded_at=CaptursAdapter.create_tz_date_from_timestamp(
-                data['timestamp']),
-            lat=data['latitude'],
-            lon=data['longitude'],
-            additional=CaptursAdapter.build_additional(data)
-        )
+        obs = CaptursObservationSerializer(data=dict(
+            device_id=data.pop('device'),
+            recorded_at=datetime.fromtimestamp(int(data.get('timestamp')), tz=pytz.UTC),
+            lat=data.pop('latitude'),
+            lon=data.pop('longitude'),
+            additional=data
+        ))
         return obs
-
-    @staticmethod
-    def build_additional(data):
-        extra = {}
-        extra['altitude'] = data.get('altitude')
-        extra['speed'] = data.get('speed')
-        extra['move'] = data.get('move')
-        return extra
-
-    @staticmethod
-    def create_tz_date_from_timestamp(timestamp):
-        tz = pytz.timezone(settings.TIME_ZONE)
-        localized = tz.localize(datetime.fromtimestamp(timestamp))
-        utc_dt = localized.astimezone(pytz.UTC)
-        return utc_dt
 
     @staticmethod
     def create_das_obs(capturs_obs):
@@ -78,16 +48,16 @@ class CaptursAdapter:
         :return: a DasObservation instance
         """
         das_obs = dict(
-            location={'latitude': capturs_obs.lat,
-                      'longitude': capturs_obs.lon},
-            recorded_at=capturs_obs.recorded_at,
-            manufacturer_id=capturs_obs.device_id,
-            subject_name=capturs_obs.device_id,
+            location={'latitude': capturs_obs['lat'],
+                      'longitude': capturs_obs['lon']},
+            recorded_at=capturs_obs['recorded_at'],
+            manufacturer_id=capturs_obs['device_id'],
+            subject_name=capturs_obs['device_id'],
             subject_type=DAS_SUBJECT_TYPE,
             subject_subtype=DAS_SUBJECT_SUBTYPE,
             model_name='capturs',
             source_type=DAS_SOURCE_TYPE,
-            additional=capturs_obs.additional
+            additional=capturs_obs['additional']
         )
         return das_obs
 
@@ -98,21 +68,29 @@ class CaptursPushHandler:
     @classmethod
     def post(cls, request, sensor_type, provider_key):
         pos_data = request.data.get('position')
-        new_observations = []
+        observations_count = 0
 
         for data in pos_data:
-            capturs_obs = CaptursAdapter.create_capturs_obs(data)
+
+            # Validate input
+            serializer = CaptursAdapter.create_capturs_obs(data)
+            if not serializer.is_valid():
+                logger.error(f'Invalid observation records {serializer.errors}')
+                return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+            capturs_obs = serializer.data
+            capturs_obs['name'] = capturs_obs['device_id']
 
             # ensure source
             src = Source.objects.ensure_source(
                 provider=provider_key,
-                manufacturer_id=capturs_obs.device_id,
+                manufacturer_id=capturs_obs['device_id'],
                 subject={
                     'subject_subtype_id': DAS_SUBJECT_SUBTYPE,
-                    'name': capturs_obs.device_id
+                    'name': capturs_obs['device_id']
                 })
 
-            if Observation.objects.filter(recorded_at=capturs_obs.recorded_at,
+            if Observation.objects.filter(recorded_at=capturs_obs['recorded_at'],
                                           source=src).exists():
                 logger.info(f'Ignoring duplicate observation from {src}')
             else:
@@ -124,14 +102,16 @@ class CaptursPushHandler:
                 if validator.is_valid():
                     validator.save()
                     logger.info(f'New observation created from source {src}')
-                    new_observations.append(validator.data.get('id'))
+                    observations_count += 1
                 else:
-                    logger.error(f'Invalid observation {observation}')
+                    logger.error(f'Invalid observation records {validator.errors}')
                     return Response(data=validator.errors,
                                     status=status.HTTP_400_BAD_REQUEST)
 
-        if new_observations:
-            return Response(data=new_observations, status=status.HTTP_201_CREATED)
+        if observations_count > 0:
+            return Response(
+                data={"message": f"{observations_count} new observations added"},
+                status=status.HTTP_201_CREATED)
 
         else:
             return Response(
