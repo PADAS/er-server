8594cac8f4489dc30d1b8cb54c6ac18194172df0
Dominic Motuka <dominic.motuka@gmail.com>
2020-04-17 03:24:33 +0300

    Migrate lowerzambezi to GCP prod1 cluster
    Signed-off-by: Dominic Motuka <dominic.motuka@gmail.com>

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -46,6 +46,7 @@ locals {
     "gile"                   = "prod1"
     "dwnp"                   = "prod1"
     "parcw"                  = "prod1"
+    "lowerzambezi"           = "prod1"
   }
 
   default_infra_workspace_when_not_mapped_here = "dev"
