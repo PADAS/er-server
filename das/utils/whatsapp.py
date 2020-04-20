d3d2c07b4138434ceeb6f13ff1f194ba49fb6dc2
phillipahereza <pahereza@gmail.com>
2020-04-20 14:48:05 +0300

    added logger incase of error

--- das/utils/whatsapp.py
+++ das/utils/whatsapp.py
@@ -13,12 +13,13 @@ TWILIO_FROM_NUMBER = getattr(settings, 'WHATSAPP_FROM_NUMBER', '')
 def send_whatsapp(body, to, fail_silently=True):
     logger.info("Send message via Twilio")
     if not TWILIO_FROM_NUMBER:
-        raise ValueError("TWILIO_FROM_NUMBER not configured in settings.py")
+        raise ValueError("WHATSAPP_FROM_NUMBER not configured in settings.py")
     client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
     from_number = '+' + TWILIO_FROM_NUMBER if not TWILIO_FROM_NUMBER.startswith('+') else TWILIO_FROM_NUMBER
     try:
         client.messages.create(body=body, from_=f"whatsapp:{from_number}", to=f"whatsapp:{to}")
-    except:
+    except Exception as e:
+        logger.error("Error sending whatsapp message {0}".format(str(e)))
         if not fail_silently:
             raise
 
