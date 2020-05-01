3bb523af44de8e9ef242dfb71fe3a095603e8ab0
Chris Doehring <chrisdoehring@gmail.com>
2020-05-01 09:06:44 -0700

    Update CORS settings.

--- das/das_server/local_settings_docker.py
+++ das/das_server/local_settings_docker.py
@@ -53,17 +53,20 @@ SERVER_NAMES = [
     SERVER_FQDN,
     SERVER_FQDN.replace('pamdas.org', 'apn.pamdas.org'),
     SERVER_FQDN.replace('pamdas.org', 'wps.pamdas.org'),
-    SERVER_FQDN.replace('pamdas.org', 'fzs.pamdas.org')
+    SERVER_FQDN.replace('pamdas.org', 'fzs.pamdas.org'),
+    'localhost:9000',
+
 ]
 
 # Django allowed-hosts
-ALLOWED_HOSTS = SERVER_NAMES + ['localhost', '']
+ALLOWED_HOSTS = SERVER_NAMES
 
+CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = env.bool('CORS_ORIGIN_ALLOW_ALL', False)
 
 # Rest and realtime API allowed hosts.
 CORS_ORIGIN_WHITELIST = [f'{prefix}{servername}' for servername in SERVER_NAMES for prefix in ('', 'http://', 'https://')]
-CORS_ORIGIN_WHITELIST = CORS_ORIGIN_WHITELIST + ['localhost', 'http://localhost:9000',]
+
 
 CORS_REPLACE_HTTPS_REFERER = env.bool('CORS_REPLACE_HTTPS_REFERER', True)
 
