eb07392aa46d7a8f848668807da5c5b54ced0286
phillipahereza <pahereza@gmail.com>
2020-05-06 01:20:50 +0300

    add more details to fire alert schema

--- das/analyzers/gfw_inbound.py
+++ das/analyzers/gfw_inbound.py
@@ -195,6 +195,21 @@ def create_event_from_downloadedalert(downloaded_sample, common_event_fields, us
         longitude = deserialized_sample.validated_data.get('longitude')
         confidence = deserialized_sample.validated_data.get('confidence')
         time = deserialized_sample.validated_data.get('acq_date')
+
+        bright_ti4 = deserialized_sample.validated_data.get('bright_ti4')
+        bright_ti5 = deserialized_sample.validated_data.get('bright_ti5')
+        scan = deserialized_sample.validated_data.get('scan')
+        track = deserialized_sample.validated_data.get('track')
+        frp = deserialized_sample.validated_data.get('frp')
+        test = deserialized_sample.validated_data.get('test')
+
+        common_event_fields['event_details']['bright_ti4'] = bright_ti4
+        common_event_fields['event_details']['bright_ti5'] = bright_ti5
+        common_event_fields['event_details']['scan'] = scan
+        common_event_fields['event_details']['track'] = track
+        common_event_fields['event_details']['frp'] = frp
+        common_event_fields['event_details']['test'] = test
+
     else:
         julian_day = deserialized_sample.validated_data.get('julian_day')
         year = deserialized_sample.validated_data.get('year')
--- das/analyzers/migrations/0035_num_clustered_events.py
+++ das/analyzers/migrations/0035_num_clustered_events.py
@@ -1,12 +1,69 @@
 from django.db import migrations, models
 
 
-GENERIC_GFW_ALERT_SCHEMA = """{
+GENERIC_GFW_TREE_LOSS_SCHEMA = """{
    "schema":{
       "$schema":"http://json-schema.org/draft-04/schema#",
-      "title":"Event Type Global Forest Watch Alert",
+      "title":"Event Type Global Forest Watch Tree Loss Alert",
       "type":"object",
       "properties":{
+         "subscription_name":{
+            "type":"string",
+            "title":"Name of subscription with Global Forest Watch"
+         },
+         "alert_link":{
+            "type":"string",
+            "title":"URL of the map for this alert",
+            "format":"uri"
+         },
+         "confidence":{
+            "type":"number",
+            "title":"Confidence level of alert"
+         },
+         "num_clustered_alerts":{
+            "type":"number",
+            "title":"Number of clustered alerts"
+         }
+      }
+   },
+   "definition":[
+      "subscription_name",
+      "alert_link",
+      "confidence",
+      "num_clustered_alerts"
+   ]
+}"""
+
+GENERIC_GFW_ACTIVE_FIRE_SCHEMA = """{
+   "schema":{
+      "$schema":"http://json-schema.org/draft-04/schema#",
+      "title":"Event Type Global Forest Watch Active Fire Alert",
+      "type":"object",
+      "properties":{
+         "bright_ti4": {
+            "type": "string",
+            "title": "VIIRS I-4 channel brightness (Kelvin)"
+         },
+         "bright_ti5": {
+            "type": "string",
+            "title": "VIIRS I-5 channel brightness (Kelvin)"
+         },
+         "scan": {
+            "type": "string",
+            "title": "VIIRS Scan"
+         },
+         "track": {
+            "type": "string",
+            "title": "Satellite Track"
+         },
+         "frp": {
+            "type": "string",
+            "title": "Fire Radiative Power (MW)"
+         },
+         "test": {
+            "type": "string",
+            "title": "This is a test field on an Analyzer Event"
+         },
          "subscription_name":{
             "type":"string",
             "title":"Name of subscription with Global Forest Watch"
@@ -27,6 +84,12 @@ GENERIC_GFW_ALERT_SCHEMA = """{
       }
    },
    "definition":[
+      "bright_ti4",
+      "bright_ti5",
+      "scan",
+      "track",
+      "frp",
+      "test",
       "subscription_name",
       "alert_link",
       "confidence",
@@ -46,15 +109,15 @@ def forwards(apps, schema_editor):
         display='Analyzer Event',
         ordernum=1)
 
-    gfw_glad_alert_defaults = {'schema': GENERIC_GFW_ALERT_SCHEMA, 'display': 'GLAD Tree-Loss Alert (GFW)',
+    gfw_tree_loss_defaults = {'schema': GENERIC_GFW_TREE_LOSS_SCHEMA, 'display': 'GLAD Tree-Loss Alert (GFW)',
                 'category_id': category.id, 'icon': 'deforestation_rep'}
 
     EventType.objects.using(db_alias).update_or_create(
         value="gfw_glad_alert",
-        defaults=gfw_glad_alert_defaults
+        defaults=gfw_tree_loss_defaults
     )
 
-    gfw_activefire_alert_defaults = {'schema': GENERIC_GFW_ALERT_SCHEMA, 'display': 'Active Fire Alert (GFW)',
+    gfw_activefire_alert_defaults = {'schema': GENERIC_GFW_ACTIVE_FIRE_SCHEMA, 'display': 'Active Fire Alert (GFW)',
                 'category_id': category.id, 'icon': 'fire_rep'}
 
     EventType.objects.using(db_alias).update_or_create(
