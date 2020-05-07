724c9352eef4e57ed5145f235e4a50fd1ad5118b
Chris Doehring <chrisdoehring@gmail.com>
2020-05-07 15:32:52 -0700

    Remove calls to cfgloader in start scripts.

--- das/start_scripts/start_default_worker.sh
+++ das/start_scripts/start_default_worker.sh
@@ -2,6 +2,5 @@
 . $(dirname "$0")/wait_for.sh
 wait_for $API_HOST $API_PORT
 
-python3 cfgloader.py
 python3 manage.py collectstatic --no-input
 celery -A das_server worker -Q default,maintenance -l info -c 10 -P gevent --without-gossip -n default
