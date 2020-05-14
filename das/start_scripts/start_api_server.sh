83b75ef496ef37113d694d1347f30c05e5538f47
Chris Doehring <chrisdoehring@gmail.com>
2020-05-14 13:25:50 -0700

    Run api server using gunicorn.

--- das/start_scripts/start_api_server.sh
+++ das/start_scripts/start_api_server.sh
@@ -5,8 +5,15 @@ wait_for $DB_HOST $DB_PORT
 python3 manage.py migrate --no-input
 python3 manage.py collectstatic --no-input
 
-if [ "$DEV" = "True" ]; then
-    python3 manage.py runserver 0.0.0.0:8000
-else
-    python3 manage.py runserver 0.0.0.0:8000 --noreload
-fi
+# Override GUNICORN_CMD_ARGS at deployment if desired.
+# Keep in mind that the flags specified below, when running gunicorn, take 
+# precedence.
+export GUNICORN_CMD_ARGS=${GUNICORN_CMD_ARGS:-"--bind 0.0.0.0:8000 --workers 6"}
+
+echo "Notice GUNICORN_CMD_ARGS: ${GUNICORN_CMD_ARGS}"
+
+gunicorn das_server.wsgi --name das \
+    --user www-data \
+    --group www-data \
+    --limit-request-line 6000 \
+    --env DJANGO_SETTINGS_MODULE=das_server.local_settings_docker
