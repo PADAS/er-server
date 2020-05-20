f0c897a49eaab10e8ebb19e7016509468a01b0b9
blac-siren <husnein.zac@gmail.com>
2020-05-20 11:07:07 +0300

    rename id to observation_id and put it after column recordserial

--- das/observations/views.py
+++ das/observations/views.py
@@ -1280,7 +1280,7 @@ class TrackingDataCsvView(generics.RetrieveAPIView):
             tz_offset) if result_format == 'csv' else 'fixtime'
         dloadtime_label = 'dloadtime ({})'.format(
             tz_offset) if result_format == 'csv' else 'dloadtime'
-        fieldnames = ['id', 'chronofile', 'recordserial', 'collar_id', fixtime_label, dloadtime_label,
+        fieldnames = ['chronofile', 'recordserial', 'observation_id', 'collar_id', fixtime_label, dloadtime_label,
                       'lon', 'lat', 'height', 'temp', 'voltage']
         csv_data = []
         cur_record_serial = record_serial_base
@@ -1345,7 +1345,7 @@ class TrackingDataCsvView(generics.RetrieveAPIView):
                 if item['subjectsource_additional'] else ''
 
         collar_id = item['collar_id']
-        data = {'id': item['id'],
+        data = {'observation_id': item['id'],
                 'lat': item['location'].y,
                 'lon': item['location'].x,
                 'height': item['location'].z,
