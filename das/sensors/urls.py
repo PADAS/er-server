6ed7e7282cd41836c95473d0018d4bcc08b1a666
kzyGit <kezzyangiro@gmail.com>
2020-05-22 14:42:21 +0300

    Setup openapi schema configuration for sensors

--- das/sensors/urls.py
+++ das/sensors/urls.py
@@ -1,7 +1,17 @@
 from django.conf.urls import url
+from rest_framework.renderers import JSONOpenAPIRenderer
+from rest_framework.schemas import get_schema_view
+
 from sensors import views
 
+schema_view = get_schema_view(
+    title="DAS API Documentation",
+    description="Sensors API",
+    urlconf="sensors.urls",
+    renderer_classes=[JSONOpenAPIRenderer]
+)
 urlpatterns = [
+    url(r'^openapi-schema/', schema_view, name='openapi-schema'),
     url(r'^(?P<sensor_type>[\w-]{3,20})/(?P<provider_key>[\w-]{3,20})/status/?$', views.SensorObservation.as_view(),
         name='sensor-observation-view'),
 ]
