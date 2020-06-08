28d5dfe01ae978955d17810f5254855b599da391
Dominic Motuka <dominic.motuka@gmail.com>
2020-06-08 19:17:50 +0300

    Create 51 degrees on Prod Cluster
    Signed-off-by: Dominic Motuka <dominic.motuka@gmail.com>

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -60,6 +60,7 @@ locals {
     "welgevonden"            = "prod1"
     "wildhorizons"           = "prod1"
     "zakouma"                = "prod1"
+    "degrees51"              = "prod1"
     "hello-asia"             = "prod-asia"
     "socp"                   = "prod-asia"
     "westernsiempang"        = "prod-asia"
