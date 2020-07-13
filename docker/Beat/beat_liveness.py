ae941e42382fca5cf9629e7e9c1a7446db564fb3
kzyGit <kezzyangiro@gmail.com>
2020-07-13 14:13:36 +0300

    add mql liveness check

--- docker/Beat/beat_liveness.py
+++ docker/Beat/beat_liveness.py
@@ -1,11 +1,15 @@
-from datetime import datetime
-import pytz
 import shelve
+import sys
+from datetime import datetime
+
+import pytz
 
 now = datetime.now(tz=pytz.utc)
-file_data = shelve.open('celerybeat-schedule')  # file that store the last run times of periodic tasks. 
-
+file_data = shelve.open('celerybeat-schedule')  # file that store the last run times of periodic tasks.
 for task_name, task in file_data['entries'].items():
-    if now > task.last_run_at + task.schedule.run_every:
-        exit(1)
-exit(0)
+    try:
+        if now > task.last_run_at + task.schedule.run_every:
+            sys.exit(1)
+    except Exception:
+        pass
+sys.exit(0)
