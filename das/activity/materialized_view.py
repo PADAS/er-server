e633eb8898eec73bbeaee2badb76fb4a091f6377
kzyGit <kezzyangiro@gmail.com>
2020-07-16 22:26:50 +0300

    update event_details_view ddl

--- das/activity/materialized_view.py
+++ das/activity/materialized_view.py
@@ -1,5 +1,5 @@
 import json
-from activity.models import Event, EventType
+from activity.models import EventType
 from utils import schema_utils
 
 from django.db import connection
@@ -45,12 +45,17 @@ def query_statement(json_path, data_type):
     path = ','.join(json_path)
 
     if data_type == 'TEXT[]':
-        query_string = f"case when jsonb_typeof(data#> '{{{array_path}}}') = 'array' then array(select jsonb_array_elements(data#>'{{{array_path}}}')->>'name') end as {json_path[1]}"
+        array_elements = f"select jsonb_array_elements(data#>'{{{array_path}}}')"
+        query_string = f"case when jsonb_typeof(data#> '{{{array_path}}}') = 'array' then case\
+            when array_position(array({array_elements}->>'value'), null) is not null\
+                then array({array_elements})::text[] else array({array_elements}->>'value') end end as {json_path[1]}"
     elif data_type == 'NUMERIC':
         # Wrap in a function that'll safely coerce values to NUMERIC.
         query_string = f'TO_NUMERIC((data#>>\'{{{path}}}\')::TEXT) as "{json_path[1]}"'
     else:
-        query_string = f'(data#>>\'{{{path}}}\')::{data_type} as "{json_path[1]}"'
+        removed_value = ','.join(json_path[:-1])  # value removed
+        query_string = f'case when data#>>\'{{{path}}}\' IS NOT NULL THEN (data#>>\'{{{path}}}\')::{data_type}\
+            else (data#>>\'{{{removed_value}}}\')::{data_type} end as "{json_path[1]}"'
     return query_string
 
 
@@ -93,7 +98,7 @@ def refresh_materialized_view():
 def generate_field_details(schema_accumulator):
     used_properties = set()
 
-    for k, v in schema_accumulator.items():
+    for v in schema_accumulator.values():
         properties = v['schema']['properties']
 
         for prop_key, prop_val in properties.items():
@@ -101,17 +106,12 @@ def generate_field_details(schema_accumulator):
             if prop_key in used_properties:
                 continue
             used_properties.add(prop_key)
-
-            if prop_val.get('enum'):
-                if prop_val.get('type') == 'string':
-                    yield ('event_details', prop_key, 'name'), 'TEXT'
-
-            elif prop_val.get('type') == 'string':
-                yield ('event_details', prop_key), 'TEXT'
+            if prop_val.get('type') == 'string':
+                yield ('event_details', prop_key, 'value'), 'TEXT'
 
             elif prop_val.get('type') == 'number':
                 yield ('event_details', prop_key), 'NUMERIC'
 
             # elif bool({'checkboxes', 'array'} & set(prop_val.values())):
             elif prop_val.get('type') == 'array' or prop_val.get('type') == "checkboxes":
-                yield ('event_details', prop_key, 'name'), 'TEXT[]'
+                yield ('event_details', prop_key), 'TEXT[]'
