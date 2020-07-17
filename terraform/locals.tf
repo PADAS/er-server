59805c75140c25745387bb38af0915c051bd4d72
Chris Doehring <chrisdoehring@gmail.com>
2020-07-17 00:19:58 -0700

    Fix lionalert. Wire alt-subdomains.

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -32,6 +32,7 @@ locals {
     "liuwa"                  = "prod1"
     "lowerzambezi"           = "prod1"
     "liwonde"                = "prod1"
+    "lionalert"              = "prod1"
     "lionlandscapes"         = "prod1"
     "madikwe"                = "prod1"
     "majete"                 = "prod1"
