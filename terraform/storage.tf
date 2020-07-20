10964b6ff5df84dde5536879c7839228003a3b52
Chris Doehring <chrisdoehring@gmail.com>
2020-07-20 08:51:16 -0700

    Removing resources that are moved to das-metrics project.

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -32,30 +32,3 @@ resource "kubernetes_secret" "google-application-credentials" {
     credentials_json = base64decode(google_service_account_key.er_app_account_key.private_key)
   }
 }
-
-# Service Account for writing metrics to padas-app project.
-resource "google_service_account" "metrics_writer_sa" {
-  provider   = google
-  account_id = "metrics-${kubernetes_namespace.this.metadata.0.name}"
-  project    = local.legacy_project_id
-}
-
-resource "google_project_iam_member" "project" {
-  project    = local.legacy_project_id
-  role    = "roles/monitoring.metricWriter"
-  member  = "serviceAccount:${google_service_account.metrics_writer_sa.email}"
-}
-
-resource "google_service_account_key" "metrics_writer_account_key" {
-  service_account_id = google_service_account.metrics_writer_sa.name
-}
-
-resource "kubernetes_secret" "metrics-writer-credentials" {
-  metadata {
-    name      = "metrics-writer-credentials"
-    namespace = kubernetes_namespace.this.metadata.0.name
-  }
-  data = {
-    credentials_json = base64decode(google_service_account_key.metrics_writer_account_key.private_key)
-  }
-}
