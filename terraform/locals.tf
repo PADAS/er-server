c4939f09ce294a02cdb3857aaf5c133194e3576e
Dominic Motuka <dominic.motuka@gmail.com>
2020-07-29 18:27:29 +0300

    New site serengeti.pamdas.org
    Signed-off-by: Dominic Motuka <dominic.motuka@gmail.com>

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -65,6 +65,7 @@ locals {
     "sabiegamereserve"       = "prod1"
     "sabisands"              = "prod1"
     "sawc"                   = "prod1"
+    "serengeti"              = "prod1"
     "snowleopardtrust"       = "prod-asia"
     "socp"                   = "prod-asia"
     "spaceforgiants"         = "prod1"
@@ -93,6 +94,7 @@ locals {
     "meru"             = 1
     "aberdares"        = 1
     "baminguibangoran" = 1
+    "serengeti"        = 1
   }
 
   db_instance_index = lookup(local.workspace_to_database_instance, terraform.workspace, 0)
