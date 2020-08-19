aed1dea847ef8ed797566be6f9da2f9292f69701
Chris Doehring <chrisdoehring@gmail.com>
2020-08-18 19:53:41 -0700

    Attempt to run tests using compose.

---
+++ das/test_scripts/run_unittests.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+. $(dirname "$0")/../start_scripts/wait_for.sh
+wait_for $DB_HOST $DB_PORT
+
+export PYTHONPATH=$(dirname "$0"):$PYTHONPATH
+
+python3 -m pip install -r /workspace/dependences/requirements-dev.txt \
+   --find-links /workspace/dependencies/wheelhouse/ --upgrade
+
+python3 manage.py test --keepdb --settings=unittest_settings
