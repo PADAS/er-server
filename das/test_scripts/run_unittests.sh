82a12cdd3a68e085b04e4d2cb2b3bb29bcbd9939
Chris Doehring <chrisdoehring@gmail.com>
2020-08-20 15:17:30 -0700

    Reorder installing dependencies.

--- das/test_scripts/run_unittests.sh
+++ das/test_scripts/run_unittests.sh
@@ -5,10 +5,9 @@ wait_for $DB_HOST $DB_PORT
 
 export PYTHONPATH=$(dirname "$0"):$PYTHONPATH
 
+python3 -m pip install --upgrade keyrings.alt
 python3 -m pip install -r /workspace/dependencies/requirements-dev.txt \
    --find-links /workspace/dependencies/wheelhouse/ --upgrade
-
-python3 -m pip install --upgrade keyrings.alt
 
 export DJANGO_SETTINGS_MODULE=unittest_settings
 pytest --create-db --junitxml=/reports/result.xml
