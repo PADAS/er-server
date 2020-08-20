819c137e732f5eb976eadb1ca2d1f2d88bab74da
Chris Doehring <chrisdoehring@gmail.com>
2020-08-20 15:07:00 -0700

    Fix missing keyrings dependency

--- das/test_scripts/run_unittests.sh
+++ das/test_scripts/run_unittests.sh
@@ -8,5 +8,7 @@ export PYTHONPATH=$(dirname "$0"):$PYTHONPATH
 python3 -m pip install -r /workspace/dependencies/requirements-dev.txt \
    --find-links /workspace/dependencies/wheelhouse/ --upgrade
 
+python3 -m pip install --upgrade keyrings.alt
+
 export DJANGO_SETTINGS_MODULE=unittest_settings
 pytest --create-db --junitxml=/reports/result.xml
