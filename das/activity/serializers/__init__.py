0d50bdbecd336fcfc5a2b0bee062b797a5c4db4d
kzyGit <kezzyangiro@gmail.com>
2020-08-24 23:19:08 +0300

    Add patrol api urls, views and serializers

--- das/activity/serializers/__init__.py
+++ das/activity/serializers/__init__.py
@@ -7,6 +7,7 @@ import django.db
 import drf_extra_fields.geo_fields
 import pytz
 import rest_framework.serializers
+from rest_framework import serializers
 import rest_framework.status
 import versatileimagefield.files
 from django.contrib.auth import get_user_model
@@ -19,12 +20,14 @@ from django.urls import reverse
 from django.utils import timezone
 from django.utils.encoding import force_text
 from django.template.defaultfilters import truncatechars
+from drf_extra_fields.fields import DateTimeRangeField
 from drf_extra_fields.geo_fields import PointField
 from rest_framework.exceptions import ValidationError, APIException
 from rest_framework.fields import DateTimeField
 from rest_framework.metadata import BaseMetadata
 from rest_framework.request import clone_request
 from rest_framework.utils.field_mapping import ClassLookupDict
+from rest_framework.validators import UniqueValidator
 from rest_framework_gis.serializers import GeoFeatureModelListSerializer
 from versatileimagefield.serializers import VersatileImageFieldSerializer
 # Make dictionaries from the IMAGE_SETS, to make lookups a little easier.
@@ -1788,3 +1791,19 @@ class PatrolTypeSerializer(rest_framework.serializers.ModelSerializer):
         model = activity.models.PatrolType
         read_only_fields = ('id', 'value', 'display', 'ordernum', 'icon_id', 'default_priority',)
         fields = read_only_fields
+
+
+from activity import models
+class PatrolSerializer(serializers.Serializer):
+    id = serializers.UUIDField(read_only=True)
+    serial_number = serializers.IntegerField(
+        validators=[UniqueValidator(queryset=models.Patrol.objects.all())])
+    priority = serializers.ChoiceField(choices=models.PRIORITY_CHOICES, default=models.PRI_NONE)
+    new_state = serializers.ChoiceField(choices=models.PATROL_STATE_CHOICES, default=models.PC_ACTIVE)
+    title = serializers.CharField(required=False)
+    objective = serializers.CharField(style={'base_template': 'textarea.html'}, required=False)
+    time_range = DateTimeRangeField()
+
+    def create(self, validated_data):
+        return models.Patrol.objects.create(**validated_data)
+    
--- das/activity/views.py
+++ das/activity/views.py
@@ -38,7 +38,7 @@ from activity.filters import EventObjectPermissionsFilter
 from activity.models import Event, EventNote, EventClass, \
     EventFactor, EventClassFactor, EventType, EventRelationship, EventCategory, \
     EventFile, Community, \
-    EventFilter, EventSource, EventProvider, PatrolType
+    EventFilter, EventSource, EventProvider, PatrolType, Patrol
 from activity.permissions import EventCategoryPermissions, \
     EventNotesCategoryPermissions, IsOwner
 from activity.serializers import EventSerializer, EventNoteSerializer, \
@@ -48,7 +48,7 @@ from activity.serializers import EventSerializer, EventNoteSerializer, \
     EventFileSerializer, \
     EventFilterSerializer, EventSourceSerializer, EventProviderSerializer, \
     EventGeoJsonSerializer, \
-    PatrolTypeSerializer
+    PatrolTypeSerializer, PatrolSerializer
 from choices.models import Choice
 from observations.models import Subject
 from utils.drf import StandardResultsSetPagination, \
@@ -1092,3 +1092,14 @@ class PatrolTypeView(generics.RetrieveAPIView):
     lookup_field = 'id'
     serializer_class = PatrolTypeSerializer
     queryset = PatrolType.objects.all()
+
+
+class PatrolsView(generics.ListCreateAPIView):
+    serializer_class = PatrolSerializer
+    queryset = Patrol.objects.all()
+
+
+class PatrolView(generics.RetrieveAPIView):
+    lookup_field = 'id'
+    serializer_class = PatrolSerializer
+    queryset = Patrol.objects.all()
