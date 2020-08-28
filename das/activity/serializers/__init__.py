e2eb08b1f0a52a89692a04dedbb556d6a33bd3ba
blac-siren <husnein.zac@gmail.com>
2020-08-28 16:14:50 +0300

    remove import

--- das/activity/serializers/__init__.py
+++ das/activity/serializers/__init__.py
@@ -50,8 +50,6 @@ import utils.schema_utils as schema_utils
 import usercontent.serializers
 
 from activity.alerting.conditions import Conditions
-from observations.serializers import SourceSerializer
-
 
 logger = logging.getLogger(__name__)
 
