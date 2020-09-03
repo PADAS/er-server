93e39279cc92982a8b3b4d5c4f24218c02fb7b7a
kzyGit <kezzyangiro@gmail.com>
2020-09-03 22:58:00 +0300

    Add v2 handler

--- das/sensors/sigfox_foundation_push_handler.py
+++ das/sensors/sigfox_foundation_push_handler.py
@@ -1,12 +1,15 @@
 import logging
 import math
+import os
 import re
+from base64 import b64encode
 from datetime import datetime, timezone
 
+import requests
 from rest_framework import serializers, status
 from rest_framework.response import Response
 
-from observations.models import Source, Observation
+from observations.models import Observation, Source
 from observations.serializers import ObservationSerializer
 
 logger = logging.getLogger(__name__)
@@ -24,7 +27,7 @@ class PayloadValidator(serializers.Serializer):
     deviceId = serializers.CharField()
     time = serializers.IntegerField()
     seqNumber = serializers.IntegerField()
-    data = serializers.CharField(min_length=24, max_length=24, required=False)
+    data = serializers.CharField(min_length=2, required=False)
     computedLocation = ComputedLocation(required=False)
     duplicate = serializers.BooleanField(required=False)
     reception = serializers.ListField(required=False)
@@ -32,27 +35,29 @@ class PayloadValidator(serializers.Serializer):
 
 class SigfoxFoundationPushHandler:
     SENSOR_TYPE = 'sff-tracker'
+    SENSOR_TYPE_V2 = 'sff-tracker-v2'
     DEFAULT_SUBJECT_SUBTYPE = 'wildlife'
     serializer_class = PayloadValidator
 
     @classmethod
-    def post(cls, request, provider_key):
+    def post(cls, request, provider_key, version):
         sigfox_data = PayloadValidator(data=request.data)
         if sigfox_data.is_valid():
             validated_data = sigfox_data.validated_data
             if validated_data.get('data'):
-                return cls.process_data_uplink(validated_data, cls.SENSOR_TYPE, provider_key)
+                parser = SigfoxPayloadParserV1 if version == 1 else SigfoxPayloadParserV2
+                return cls.process_data_uplink(validated_data, provider_key, parser)
             elif validated_data.get('computedLocation'):
                 return Response(data=dict(message='Message received'), status=status.HTTP_200_OK)
 
         return Response(data=sigfox_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
     @classmethod
-    def process_data_uplink(cls, payload, sensor_type, provider_key):
+    def process_data_uplink(cls, payload, provider_key, parser):
         data = payload.pop('data')
-        parsed_data = SigfoxPayloadParser.parse(data)
+        device_id = payload.pop('deviceId')
+        parsed_data = parser.parse(data, device_id)
         if parsed_data:
-            device_id = payload.pop('deviceId')
             src = Source.objects.ensure_source(provider=provider_key,
                                                manufacturer_id=device_id,
                                                subject={
@@ -94,38 +99,9 @@ class SigfoxFoundationPushHandler:
         return Response(data=dict(message='Unable to parse data'), status=status.HTTP_400_BAD_REQUEST)
 
 
-class SigfoxPayloadParser:
-
-    # look at the rhinosparser.txt linked in the JIRA ticket for a javascript example
-    # https://vulcan.atlassian.net/browse/DAS-4392
-
+class SigfoxParser:
     BYTE_PATTERN = '.{1,2}'
-    SIGFOX_PAYLOAD_PATTERN = '(.)(.{31})(.)(.{31})(.{2})(.{2})(.{4})(.{4})(.{4})(.{8})(.{8})'
-
     byte_re = re.compile(BYTE_PATTERN)
-    sigfox_payload_re = re.compile(SIGFOX_PAYLOAD_PATTERN)
-
-    @classmethod
-    def parse(cls, data):
-        try:
-            bin_string = cls._to_binary_string(data)
-            components = cls._get_components(bin_string)
-        except Exception as ex:
-            logger.exception(ex)
-            return None
-        else:
-            logger.debug('parsed components', components)
-            return {
-                'latitude': cls._parse_coordinate(components[0], components[1]),
-                'longitude': cls._parse_coordinate(components[2], components[3]),
-                'hdop': cls._parse_hdop(components[4]),
-                'sat': cls._parse_sat(components[5]),
-                'unknown_field': int(components[6], 2),
-                'gps_acq_time': cls._parse_gps_acq_time(components[7]),
-                'speed': cls._parse_speed(components[8]),
-                'batt_level': cls._parse_battery_volts(components[9]),
-                'alert': cls._parse_alert(components[10]),
-            }
 
     @classmethod
     def _to_binary_string(cls, payload):
@@ -152,12 +128,13 @@ class SigfoxPayloadParser:
             raise ParseException('byte_re did not find any bytes')
 
     @classmethod
-    def _get_components(cls, binary_string):
-        parsed_payload = cls.sigfox_payload_re.findall(binary_string)
+    def _get_components(cls, binary_string, pattern):
+        sigfox_payload_re = re.compile(pattern)
+        parsed_payload = sigfox_payload_re.findall(binary_string)
         if parsed_payload:
             return parsed_payload[0]
         else:
-            raise ParseException('sigfox_payload_re didnot find any sigfox components in binary string')
+            raise ParseException('sigfox_payload_re did not find any sigfox components in binary string')
 
     @classmethod
     def _parse_coordinate(cls, sign_bit, coordinate_bits):
@@ -170,6 +147,52 @@ class SigfoxPayloadParser:
         minutes = payload_component % 1 / 60 * 100
         minutes = round(minutes * 1000000) / 1000000
         return degrees + minutes
+
+    @staticmethod
+    def _parse_gps_acq_time(bits):
+        return int(bits, 2) * 5
+
+    @staticmethod
+    def _parse_battery_volts(bits, version=1):
+        if version == 1:
+            battery = int(bits, 2) * 15 / 1000
+        else:  # version 2
+            battery = (int(bits, 2) * 75 + 2000) / 1000
+        return battery
+
+
+class SigfoxPayloadParserV1(SigfoxParser):
+
+    # look at the rhinosparser.txt linked in the JIRA ticket for a javascript example
+    # https://vulcan.atlassian.net/browse/DAS-4392
+
+    SIGFOX_PAYLOAD_PATTERN = '(.)(.{31})(.)(.{31})(.{2})(.{2})(.{4})(.{4})(.{4})(.{8})(.{8})'
+
+    @classmethod
+    def parse(cls, data, device_id):
+        if len(data) != 24:
+            logger.debug("Invalid data, expecting only 24 bit data")
+            return None
+        try:
+            bin_string = cls._to_binary_string(data)
+            components = cls._get_components(bin_string, cls.SIGFOX_PAYLOAD_PATTERN)
+        except Exception as ex:
+            logger.exception(ex)
+            return None
+        else:
+            logger.debug('parsed components', components)
+            return {
+                'latitude': cls._parse_coordinate(components[0], components[1]),
+                'longitude': cls._parse_coordinate(components[2], components[3]),
+                'hdop': cls._parse_hdop(components[4]),
+                'sat': cls._parse_sat(components[5]),
+                'unknown_field': int(components[6], 2),
+                'gps_acq_time': cls._parse_gps_acq_time(components[7]),
+                'speed': cls._parse_speed(components[8]),
+                'batt_level': cls._parse_battery_volts(components[9]),
+                'alert': cls._parse_alert(components[10]),
+            }
+
 
     @staticmethod
     def _parse_hdop(bits):
@@ -191,20 +214,127 @@ class SigfoxPayloadParser:
         return int(bits, 2) * 2 + 2
 
     @staticmethod
-    def _parse_gps_acq_time(bits):
-        return int(bits, 2) * 5
-
-    @staticmethod
     def _parse_speed(bits):
         return int(bits, 2) * 5
 
     @staticmethod
-    def _parse_battery_volts(bits):
-        return int(bits, 2) * 15 / 1000
-
-    @staticmethod
     def _parse_alert(bits):
         return int(bits, 2)
+
+
+class SigfoxPayloadParserV2(SigfoxParser):
+
+    @classmethod
+    def get_ubi_credentials(cls):
+        username = os.getenv('UBI_API_USERNAME', 'username')
+        # password = os.getenv('UBI_API_PASSWORD', 'password')
+        password = 'N]_4~q(+BF?Z'
+        credentials = f"{username}:{password}"
+
+        encoded_credentials = str(b64encode(credentials.encode("utf-8")), "utf-8")
+        return encoded_credentials
+
+
+
+    @classmethod
+    def get_position_from_ubi(cls, data, device_id):
+        ubiscale_payload = data[4:24]
+        ubi_api_url = 'https://api.ubignss.com/position'
+        payload = {
+            "type": "ubiwifi",
+            "device": device_id,
+            "data": ubiscale_payload}
+        credentials = cls.get_ubi_credentials()
+
+        headers = {'Content-Type': 'application/json', 'Authorization': f'Basic {credentials}'}
+        response = requests.post(url=ubi_api_url, headers=headers, json=payload)
+
+        if response.status_code != 200:
+            logger.debug("Error when retrieving device position: ", response.text)
+            return
+        else:
+            return response.json()
+
+    @classmethod
+    def prepare_data(cls, data):
+        mode_value, mode_display, components, pattern = None, None, None, None
+        try:
+            bin_string = cls._to_binary_string(data)
+            mode_value, mode_display = cls._parse_mode(bin_string[:3])
+            if mode_value == 1:
+                # gps tracking (Location provided)
+                pattern = '(.{3})(.{5})(.)(.)(.{6})(.)(.{31})(.)(.{31})'
+            elif mode_value == 2:
+                # ubiscale tracking (To get location from ubi api)
+                pattern = '(.{3})(.{5})(.)(.)(.{6})(.{20})'
+            else:
+                logger.debug("skipping setup, Tracking GPS and unknown modes")
+            components = cls._get_components(bin_string, pattern) if pattern else None
+        except Exception as exc:
+                logger.exception(exc)
+        return mode_value, mode_display, components
+
+    @classmethod
+    def parse(cls, data, device_id):
+        result = None
+        if len(data) == 2 or len(data) == 4:
+            logger.debug("skipping Boot/Reboot and Sigfox geolocation data")
+            return None
+
+        mode_value, mode_display, components = cls.prepare_data(data)
+        if not mode_value:
+            logger.debug("Error when preparing data, only gps and ubiscale modes allowed")
+            return None
+
+        logger.debug(f'sigfox version 2, mode: {mode_display}, parsed components: {components}')
+        if mode_value == 1:
+            # gps tracking -> Location provided
+            result = {
+                'batt_level': cls._parse_battery_volts(components[1], version=2),
+                'mode': mode_display,
+                'movement_it': cls._parse_movement_it(components[2]),
+                'gps_state': cls._parse_state(components[3]),
+                'gps_acq_time': cls._parse_gps_acq_time(components[4]),
+                'latitude': cls._parse_coordinate(components[5], components[6]),
+                'longitude': cls._parse_coordinate(components[7], components[8])
+            }
+        elif mode_value == 2:
+            # ubiscale tracking -> Get location from ubi api
+            device_position = cls.get_position_from_ubi(data, device_id)
+            if device_position:
+                result = {
+                    'batt_level': cls._parse_battery_volts(components[1], version=2),
+                    'mode': mode_display,
+                    'movement_it': cls._parse_movement_it(components[2]),
+                    'gps_state': cls._parse_state(components[3]),
+                    'gps_acq_time': cls._parse_gps_acq_time(components[4]),
+                    'latitude': device_position.get('lat'),
+                    'longitude': device_position.get('lng'),
+                    'altitude': device_position.get('alt'),
+                    'accuracy': device_position.get('accuracy')
+                }
+        return result
+
+    @staticmethod
+    def _parse_movement_it(bit):
+        movement_it = False if bit == 0 else True
+        return movement_it
+
+
+    @staticmethod
+    def _parse_state(bit):
+        state = 'Acquisition GPS successful' if bit == 0 else 'Acquisition GPS failed'
+        return state
+
+    @staticmethod
+    def _parse_mode(bits):
+
+        value, display = int(bits, 2), None
+        if value == 1:
+            display = 'Tracking GPS'
+        elif value == 2:
+            display = 'Tracking Ubiscale'
+        return value, display
 
 
 class ParseException(Exception):
