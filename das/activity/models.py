b11d1bf003838e8b3e564092cdb5a7ea703b1e36
Kezzy Angiro <kezzyangiro@gmail.com>
2020-10-20 17:20:16 +0300

    [DAS-5763-fix] Patrol filters - Cancelled patrols (#1417)
    * patrols cancelled today  as part of current patrols
    
    * Update end_date when patrol is cancel, update filters
    
    * revert serializer updates, update filter
    
    * cleanup filter

--- das/activity/models.py
+++ das/activity/models.py
@@ -17,7 +17,7 @@ from django.contrib.postgres.fields import JSONField
 from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 from django.db import transaction
-from django.db.models import Q, F, Func
+from django.db.models import Q, F, Func, Exists, OuterRef
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import dateparse
@@ -1485,15 +1485,18 @@ class PatrolFilteringQuerySet(models.QuerySet, FilterFieldMixin):
                 patrol_segment__time_range__endswith__isnull=True)
             start_filter = Q(patrol_segment__time_range__startswith__lte=upper) | Q(
                 patrol_segment__scheduled_start__lte=upper)
-            q1 = queryset.filter(start_filter, end_filter)
+            q1 = queryset.filter(start_filter, end_filter).exclude(state=PC_CANCELLED)
 
-            if upper > datetime.datetime.now(tz=pytz.utc):
-                q1 = q1.filter(state="open")
+            # Get patrols cancelled within given range
+            q2 = queryset.annotate(cancel_rev_exists=Exists(
+                Patrol.revision.model.objects.filter(
+                    data__state=PC_CANCELLED, object_id=OuterRef('id'),
+                    data__updated_at__range=(lower.isoformat(), upper.isoformat())))).filter(cancel_rev_exists=True)
 
             # End_date past but patrol still active
-            q2 = queryset.filter(
-                patrol_segment__time_range__endswith__lte=datetime.datetime.today(), state="open")
-            queryset = q1.union(q2)
+            q3 = queryset.filter(
+                patrol_segment__time_range__endswith__lte=datetime.datetime.today(), state=PC_OPEN)
+            queryset = (q1 | q2 | q3).distinct()
 
         elif lower:
             queryset = queryset.filter(
