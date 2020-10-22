ce40a58710a1571bf3045bc637a951cbe8dd66e5
blac-siren <husnein.zac@gmail.com>
2020-10-22 18:36:34 +0300

    sort active patrol alphabetically only

--- das/activity/models.py
+++ das/activity/models.py
@@ -1509,9 +1509,6 @@ class PatrolFilteringQuerySet(models.QuerySet, FilterFieldMixin):
 
     def sort_patrols(self):
         return self.annotate(
-            start_time=Case(
-                When(Q(patrol_segment__time_range__startswith=RangeStartsWith(F('patrol_segment__time_range')),
-                       state=PC_OPEN), then=F('patrol_segment__time_range')), default=None, output_field=DateTimeField()),
             scheduled=Case(
                 When(Q(patrol_segment__scheduled_start=F('patrol_segment__scheduled_start'), state=PC_OPEN) &
                      Q(patrol_segment__time_range__startswith__isnull=True),
@@ -1519,7 +1516,7 @@ class PatrolFilteringQuerySet(models.QuerySet, FilterFieldMixin):
         ).order_by(Case(When(state=PC_OPEN, then=Value(1)),
                         When(state=PC_DONE, then=Value(2)),
                         When(state=PC_CANCELLED, then=Value(3)),
-                        default=Value(4)), 'scheduled', 'start_time', 'title')
+                        default=Value(4)), 'scheduled', 'title')
 
 
 class Patrol(TimestampedModel, RevisionMixin):
