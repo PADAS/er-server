efc86ff66ad76968cb2334f496d27968d75e2c6c
kzyGit <kezzyangiro@gmail.com>
2020-11-16 18:06:07 +0300

    Add trackingconfig model

--- das/tracking/models/__init__.py
+++ das/tracking/models/__init__.py
@@ -1,23 +1,22 @@
-import uuid
-
-from core.models import TimestampedModel
+from tracking.models.plugin_base import SourcePlugin
+from tracking.models.savannah import SavannahPlugin
+from tracking.models.demo import DemoSourcePlugin
+from tracking.models.inreach import InreachPlugin
+from tracking.models.awtgsm import AWTHttpPlugin
+from tracking.models.inreachkml import InreachKMLPlugin
+from tracking.models.skygistics import SkygisticsSatellitePlugin
+from tracking.models.firms import FirmsPlugin
+from tracking.models.awetelemetry import AWETelemetryPlugin
+from tracking.models.spidertracks import SpiderTracksPlugin
+from tracking.models.plugin_base import TrackingPlugin
+from tracking.models.sirtrack import SirtrackPlugin
+from tracking.models.vectronics import VectronicsPlugin
+from tracking.models.awt import AwtPlugin
 from django.contrib.gis.db import models
+from core.models import TimestampedModel
 from django.utils.translation import ugettext_lazy as _
 from observations.models import SubjectType
-from tracking.models.awetelemetry import AWETelemetryPlugin
-from tracking.models.awt import AwtPlugin
-from tracking.models.awtgsm import AWTHttpPlugin
-from tracking.models.demo import DemoSourcePlugin
-from tracking.models.firms import FirmsPlugin
-from tracking.models.inreach import InreachPlugin
-from tracking.models.inreachkml import InreachKMLPlugin
-from tracking.models.plugin_base import SourcePlugin, TrackingPlugin
-from tracking.models.savannah import SavannahPlugin
-from tracking.models.sirtrack import SirtrackPlugin
-from tracking.models.skygistics import SkygisticsSatellitePlugin
-from tracking.models.spidertracks import SpiderTracksPlugin
-from tracking.models.vectronics import VectronicsPlugin
-
+import uuid
 runnable_plugins = (SavannahPlugin, DemoSourcePlugin, InreachPlugin, InreachKMLPlugin, AWTHttpPlugin,
                     SkygisticsSatellitePlugin, SpiderTracksPlugin, AWETelemetryPlugin, SirtrackPlugin,
                     VectronicsPlugin, AwtPlugin)
---
+++ das/tracking/static/toggle_subject_types.js
@@ -0,0 +1,27 @@
+(function($) {
+    $(function() {
+        var new_subject_config = $('#id_new_subject_config'), verified = $('.new_subject_types');
+        var name_change_config = $('#id_name_change_config'), verified2 = $('.name_change_types');
+
+        function toggleVerified(value) {
+            console.log(value[0].name)
+
+            types_class = (value[0].name == 'new_subject_config') ? verified : verified2
+
+            if (value.val() === 'use_existing') {
+                types_class.show();
+            } else {
+                types_class.hide();
+            }
+        }
+
+        var mylist = [new_subject_config, name_change_config];
+        for (var i = 0; i < mylist.length; i++) {
+            field = mylist[i]
+            toggleVerified(field);
+            field.change(function() {
+                toggleVerified($(this));
+            });
+            }
+    });
+})(jQuery);
