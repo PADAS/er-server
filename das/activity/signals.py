08b72f34ca4d4cf7c74fc7db350a3da57348a409
kzyGit <kezzyangiro@gmail.com>
2020-12-07 21:05:23 +0300

    Add patrol rt messaging when report is added to patrol

--- das/activity/signals.py
+++ das/activity/signals.py
@@ -23,6 +23,10 @@ def event_post_save(sender, instance, created, **kwargs):
                           celery.app.send_task(
                               'activity.tasks.evaluate_alert_rules', args=(str(instance.id), created))
                           )
+    if getattr(instance.patrol_segment, 'patrol', None):
+        # Send patrol_update rt message
+        segment = instance.patrol_segment
+        verify_patrol_constituent_for_rt_messaging(segment)
 
 
 @receiver(post_delete, sender=Event)
