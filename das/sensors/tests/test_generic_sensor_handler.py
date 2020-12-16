5c9f9f07a1c156bf7c7eb6ca5cc0a0c3c6bc4891
kzyGit <kezzyangiro@gmail.com>
2020-12-17 01:59:36 +0300

    Add unittests

--- das/sensors/tests/test_generic_sensor_handler.py
+++ das/sensors/tests/test_generic_sensor_handler.py
@@ -378,13 +378,12 @@ class GenericSensorHandlerTest(BaseAPITest):
         config = self.config
         config.new_device_config = CREATE_NEW
         config.save()
-
-        self.assertEqual(Subject.objects.count(), 0)
+        self.assertTrue(Subject.objects.count() == 0 and SubjectSource.objects.count() == 0)
         response = self._post_data(json.dumps(self.one_observation))
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(1, Observation.objects.filter(
             source=self.test_source).count())
-        self.assertEqual(Subject.objects.count(), 1)  # New subject created
+        self.assertTrue(SubjectSource.objects.count() == 1 and Subject.objects.count() == 1)  # New subject created
 
     @override_settings(CELERY_TASK_ALWAYS_EAGER=True)
     def test_post_new_device_handling_with_use_existing_config(self):
@@ -413,6 +412,92 @@ class GenericSensorHandlerTest(BaseAPITest):
 
         # New source assignment added
         self.assertEqual(2, SubjectSource.objects.filter(subject=matching_subject).count())
+
+    @override_settings(CELERY_TASK_ALWAYS_EAGER=True)
+    def test_device_handling_with_use_existing_config_and_person_subtype_match(self):
+        subject_type = SubjectType.objects.create(value='Cats')
+        person_subject_type = SubjectType.objects.create(value='Person')
+        subject_subtype = SubjectSubType.objects.create(value='queens', subject_type=subject_type)
+        ranger_subject_subtype = SubjectSubType.objects.create(value='Ranger', subject_type=person_subject_type)
+
+        subject_name = 'Katie Kitten'
+
+        person_subject = Subject.objects.create(
+            name=subject_name, subject_subtype=ranger_subject_subtype)  # Person Match
+
+        other_subject = Subject.objects.create(
+            name=subject_name, subject_subtype=subject_subtype)  # Other match
+
+        obs_copy = copy.deepcopy(self.one_observation)
+        obs_copy['subject_name'] = 'Katie Kitten'
+        obs_copy['manufacturer_id'] = 'new_source'
+
+        self.assertEqual(Subject.objects.count(), 2)
+        self.assertTrue(
+            len(Subject.objects.get(name=subject_name, subject_subtype=ranger_subject_subtype).observations()) == 0 and
+            len(Subject.objects.get(name=subject_name, subject_subtype=subject_subtype).observations()) == 0
+        )
+
+        self.assertEqual(0, SubjectSource.objects.filter(source=self.test_source).count())
+        response = self._post_data(json.dumps(obs_copy), user=self.super_user)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # No new subject created
+        self.assertEqual(Subject.objects.count(), 2)
+
+        # Observation added to matching Subject
+        self.assertTrue(
+            len(Subject.objects.get(name=subject_name, subject_subtype=ranger_subject_subtype).observations()) == 1 and
+            len(Subject.objects.get(name=subject_name, subject_subtype=subject_subtype).observations()) == 0
+        )
+
+        # New source assignment added
+        self.assertEqual(1, SubjectSource.objects.filter(subject=person_subject).count())
+        self.assertEqual(0, SubjectSource.objects.filter(subject=other_subject).count())
+
+    @override_settings(CELERY_TASK_ALWAYS_EAGER=True)
+    def test_device_handling_with_use_existing_config_and_other_subtype_matches(self):
+        subject_type = SubjectType.objects.create(value='Cats')
+        subject_subtype = SubjectSubType.objects.create(value='queens', subject_type=subject_type)
+
+        wildlife_subject_type = SubjectType.objects.create(value='Wildlife')
+        ranger_subject_subtype = SubjectSubType.objects.create(value='Rhinos', subject_type=wildlife_subject_type)
+
+        subject_name = 'Katie Kitten'
+
+        Subject.objects.create(name=subject_name, subject_subtype=ranger_subject_subtype)  # Other match 1
+        Subject.objects.create(name=subject_name, subject_subtype=subject_subtype)  # Other match 2
+
+        obs_copy = copy.deepcopy(self.one_observation)
+        obs_copy['subject_name'] = subject_name
+        obs_copy['manufacturer_id'] = 'new_source'
+
+        self.assertEqual(Subject.objects.count(), 2)
+        self.assertTrue(
+            len(Subject.objects.get(name=subject_name, subject_subtype=ranger_subject_subtype).observations()) == 0 and
+            len(Subject.objects.get(name=subject_name, subject_subtype=subject_subtype).observations()) == 0
+        )
+
+        self.assertEqual(0, SubjectSource.objects.filter(source=self.test_source).count())
+        response = self._post_data(json.dumps(obs_copy), user=self.super_user)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # One more subject created, more than one other (Not person) match found
+        self.assertEqual(Subject.objects.count(), 3)
+
+        self.assertEqual(1, SubjectSource.objects.count())
+
+        # Match on only one other subtype subjects
+        subject_2_name = 'Katrina Kitten'
+        obs_copy = copy.deepcopy(self.one_observation)
+        obs_copy['subject_name'] = subject_2_name
+        Subject.objects.create(name=subject_2_name, subject_subtype=subject_subtype)  # Other match 1
+
+        self.assertEqual(Subject.objects.count(), 4)
+
+        self._post_data(json.dumps(obs_copy), user=self.super_user)
+        self.assertEqual(Subject.objects.count(), 4)  # No new subject created, only one other (than Person) match found
+        self.assertEqual(2, SubjectSource.objects.count())
 
     @override_settings(CELERY_TASK_ALWAYS_EAGER=True)
     def test_device_handling_with_name_update_config(self):
