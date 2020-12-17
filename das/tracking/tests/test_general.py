c58aa547aa5b0cb1c3fb36e81b4e3d4c5a693e64
Chris Doehring <chrisdoehring@gmail.com>
2020-12-17 09:29:50 -0800

    Unit test for checking runnable plugins set.

--- das/tracking/tests/test_general.py
+++ das/tracking/tests/test_general.py
@@ -1,10 +1,12 @@
 import logging
+
 from django.test import TestCase
 
-from tracking.models.plugin_base import TrackingPlugin
 from tracking.models import runnable_plugins
+from tracking.models.plugin_base import TrackingPlugin
 
 logger = logging.getLogger(__name__)
+
 
 class GeneralTestsForTracking(TestCase):
 
@@ -16,4 +18,5 @@ class GeneralTestsForTracking(TestCase):
         Ensure that all the classes included in runnable_plugins are of the right type.
         '''
         for rp in runnable_plugins:
-            self.assertTrue(issubclass(rp, (TrackingPlugin)))
+            self.assertTrue(issubclass(rp, (TrackingPlugin)),
+                            msg=f'Please remove {rp.__module__}.{rp.__name__} from the list of runnable_plugins.')
