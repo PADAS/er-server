71f48fe5d47631d88c867216a75bbb97eb4e4747
Dominic Motuka <dominic.motuka@gmail.com>
2021-01-07 23:15:14 +0300

    SSL expiry alerting policy
    Signed-off-by: Dominic Motuka <dominic.motuka@gmail.com>

--- terraform/uptime_checks.tf
+++ terraform/uptime_checks.tf
@@ -20,3 +20,39 @@ resource "google_monitoring_uptime_check_config" "this" {
   }
 
 }
+
+resource "google_monitoring_alert_policy" "ssl_alert_policy" {
+  display_name = "SSL certificate expiring soon"
+  project      = data.google_project.earthranger.project_id
+  combiner     = "OR"
+  conditions {
+    display_name = "SSL certificate expiring soon"
+    condition_threshold {
+      filter          = "metric.type=\"monitoring.googleapis.com/uptime_check/time_until_ssl_cert_expires\" AND resource.type=\"uptime_url\""
+      duration        = "600s"
+      threshold_value = 15
+      trigger {
+        count = 1
+      }
+      comparison = "COMPARISON_LT"
+      aggregations {
+        alignment_period     = "1200s"
+        per_series_aligner   = "ALIGN_NEXT_OLDER"
+        cross_series_reducer = "REDUCE_MEAN"
+        group_by_fields = [
+          "resource.label.*"
+        ]
+      }
+    }
+  }
+
+  user_labels = {
+    uptime  = "ssl_cert_expiration"
+    version = "1"
+  }
+
+  notification_channels = [
+    "projects/earthranger-78ca55ca/notificationChannels/17849266361997779646"
+  ]
+}
+
