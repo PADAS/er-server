7faa8488b0dc899f53a60f42d2d3cd07526acfe8
blac-siren <husnein.zac@gmail.com>
2021-01-14 20:23:35 +0300

    add unittest & add permission for patroltype

--- das/activity/permissions.py
+++ das/activity/permissions.py
@@ -174,7 +174,6 @@ class PatrolObjectPermissions(DjangoObjectPermissions):
         return [perm % kwargs for perm in self.perms_map[method]]
 
     def has_object_permission(self, request, view, obj):
-        # authentication checks have already executed via has_permission
         model_cls = Patrol
         user = request.user
 
@@ -184,17 +183,25 @@ class PatrolObjectPermissions(DjangoObjectPermissions):
             # If the user does not have permissions we need to determine if
             # they have read permissions to see 403, or not, and simply see
             # a 404 response.
-
             if request.method in SAFE_METHODS:
-                # Read permissions already checked and failed, no need
-                # to make another lookup.
                 raise Http404
 
             read_perms = self.get_required_object_permissions('GET', model_cls)
             if not user.has_perms(read_perms, obj):
                 raise Http404
-
-            # Has read permissions.
             return False
-
         return True
+
+
+class StandardModelPermissions(DjangoModelPermissions):
+    view_perms = ['%(app_label)s.view_%(model_name)s']
+
+    perms_map = {
+        'GET': view_perms,
+        'OPTIONS': view_perms,
+        'HEAD': view_perms,
+        'POST': ['%(app_label)s.add_%(model_name)s'],
+        'PUT': ['%(app_label)s.change_%(model_name)s'],
+        'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+    }
