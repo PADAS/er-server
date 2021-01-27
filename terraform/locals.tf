eaced1db926b6d9e97db36221d14c9d9590eb566
Dominic Motuka <dominic.motuka@gmail.com>
2021-01-27 17:37:49 +0300

    Provision new site: waterberg.pamdas.org
    Signed-off-by: Dominic Motuka <dominic.motuka@gmail.com>

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -123,6 +123,7 @@ locals {
     "welgevonden"            = "prod1"
     "upemba"                 = "prod1"
     "velebit"                = "prod1"
+    "waterberg"              = "prod1"
     "westernsiempang"        = "prod-asia"
     "wildhorizons"           = "prod1"
     "wisentproject"          = "prod1"
@@ -191,6 +192,7 @@ locals {
     "osaconservation"     = 1
     "marabustopisaaten"   = 1
     "koro"                = 1
+    "waterberg"           = 1
   }
 
   db_instance_index = lookup(local.workspace_to_database_instance, terraform.workspace, 0)
