0182b897cc2ae14194561cb4fc702da78453a7ed
Dominic Motuka <dominic.motuka@gmail.com>
2021-02-10 00:11:26 +0300

    Specify provider versions
    Signed-off-by: Dominic Motuka <dominic.motuka@gmail.com>

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,10 +1,8 @@
 provider "google" {
   region  = data.terraform_remote_state.earthranger_app_infra.outputs.gcp_region
-  # version = ">=2.20"
 }
 
 provider "google" {
-  # version = "~> 2.20"
   alias   = "k8s_cluster"
 
   project = data.terraform_remote_state.earthranger_app_infra.outputs.cluster_project_id
@@ -15,10 +13,8 @@ provider "google" {
 }
 
 provider "google-beta" {
-  # version = ">=2.19"
 }
 provider "kubernetes" {
-  # version = "~> 1.10"
 
   cluster_ca_certificate = base64decode(local.b64_encoded_cluster_or_proxy_ca_certificate)
   host                   = "https://${local.cluster_or_proxy_k8s_endpoint}"
@@ -26,18 +22,9 @@ provider "kubernetes" {
   token                  = data.google_client_config.k8s.access_token
 }
 
-# provider "random" {
-#   version = ">=2.1"
-# }
-
-# provider "tls" {
-#   version = ">=2.1"
-# }
-
 provider "vault" {
   address         = "https://vault-prod.erboh.cloud"
   skip_tls_verify = "false"
-  # version         = ">= 2.1"
 }
 
 provider "aws" {
