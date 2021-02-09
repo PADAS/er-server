d5e6de28ca593b308b37487705d26f7277af4a6b
Dominic Motuka <dominic.motuka@gmail.com>
2021-02-09 23:44:14 +0300

    Specify provider versions
    Signed-off-by: Dominic Motuka <dominic.motuka@gmail.com>

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,25 +1,32 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~>2.44"
     }
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
+      version = "~> 2.20"
     }
     google-beta = {
-      source = "hashicorp/google-beta"
+      source  = "hashicorp/google-beta"
+      version = ">=2.19"
     }
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
+      version = "~> 1.10"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
+      version = ">=2.1"
     }
     tls = {
-      source = "hashicorp/tls"
+      source  = "hashicorp/tls"
+      version = ">=2.1"
     }
     vault = {
-      source = "hashicorp/vault"
+      source  = "hashicorp/vault"
+      version = ">= 2.1"
     }
   }
   required_version = ">= 0.13"
