2461df1c6380f19b64bd1d39744c9acdc84b6c1f
blac-siren <husnein.zac@gmail.com>
2021-03-05 02:00:46 +0300

    AC-6

--- das/observations/models.py
+++ das/observations/models.py
@@ -1384,7 +1384,7 @@ def update_subject_status(source, recorded_at, location,
     if reported_subject_name:
         status_updates['additional'] = {'subject_name': reported_subject_name}
 
-    if transformed_additional_data:
+    if transformed_additional_data is not None:
         status_updates.setdefault('additional', {})['device_status_properties'] = transformed_additional_data
 
     SubjectStatus.objects.filter(subject__subjectsource__source=source,
