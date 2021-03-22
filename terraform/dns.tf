9790c2c8d2a02ee3125888c908a960af780aedea
Dominic Motuka <dominic.motuka@gmail.com>
2021-03-22 17:41:49 +0300

    Change dns name: abokouamekro ==> elephants-ci
    Signed-off-by: Dominic Motuka <dominic.motuka@gmail.com>

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -11,6 +11,7 @@ locals {
     "pantheraolympic"        = "pantheraolympicpenninsula"
     "wildhorizons"           = "victoriafalls"
     "westernsiempang"        = "spws"
+    "abokouamekro"           = "elephants-ci"
   }
 
   subdomain_name = lookup(local.alt_subdomains, terraform.workspace, kubernetes_namespace.this.metadata.0.name)
