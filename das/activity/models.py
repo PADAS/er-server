9e860be0cb2ec80badf10c5641329398cfae15dd
blac-siren <husnein.zac@gmail.com>
2021-03-27 00:29:08 +0300

    support subject-group selection for trackedby

--- das/activity/models.py
+++ das/activity/models.py
@@ -34,9 +34,9 @@ from django.contrib.postgres.fields import DateTimeRangeField
 from django.core.serializers.json import DjangoJSONEncoder
 
 from accounts.models.permissionset import PermissionSet
-from core.models import TimestampedModel
+from core.models import TimestampedModel, SingletonModel
 from core.utils import static_image_finder
-from observations.models import Subject, Source
+from observations.models import Subject, Source, SubjectGroup
 from revision.manager import Revision, RevisionMixin, RevisionAdapter
 from utils.html import clean_user_text
 from observations.utils import dateparse as dparse
@@ -1775,22 +1775,21 @@ class PatrolSegmentMembership(TimestampedModel):
 
 
 class PatrolSegmentManager(models.Manager):
-    def get_leader_for_provenance(self, provenance, user=None):
+
+    @staticmethod
+    def get_leader_for_provenance(provenance, user=None):
         if PC_STAFF == provenance:
-            def get_staff():
-                # We also want subjects who are staff (rangers are tracked as
-                # subjects via their radio, but can report events
-                staff_subject = Subject.objects.all().get_staff().by_is_active()
+            def get_subjects():
+                active_subjects = Subject.objects.all().by_is_active()
+                subject_grps = PatrolConfiguration.objects.first().subject_groups.all()
 
-                for obj in staff_subject.by_user_subjects(user) if user else staff_subject:
-                    yield obj.name.lower(), obj
-            for staff in sorted(get_staff(), key=itemgetter(0)):
-                yield staff[1]
+                for o in active_subjects.by_subjectgroups(subject_grps, user=user):
+                    yield o.name.lower(), o
 
-        elif PC_COMMUNITY == provenance:
-            for community in sorted(Community.objects.all(),
-                                    key=attrgetter('name')):
-                yield community
+            subjects = get_subjects()
+            for sub in sorted(subjects, key=itemgetter(0)):
+                yield sub[1]
+
 
 
 class PatrolSegmentRevisionAdapter(RevisionAdapter):
@@ -1856,6 +1855,11 @@ class PatrolSegment(TimestampedModel, RevisionMixin):
     revision = PatrolSegmentRevision()
 
     objects = PatrolSegmentManager()
+
+
+class PatrolConfiguration(SingletonModel):
+    name = models.CharField(max_length=255)
+    subject_groups = models.ManyToManyField(SubjectGroup, related_name='groups', blank=True)
 
 
 # class PatrolTemplate(models.Model):
