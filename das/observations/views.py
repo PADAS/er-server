233c8adf4566a50619ec3182f98a5b919045a32c
blac-siren <husnein.zac@gmail.com>
2021-05-19 18:03:37 +0300

    coerce device id from UUID to string

--- das/observations/views.py
+++ das/observations/views.py
@@ -1903,7 +1903,7 @@ class MessagesView(generics.ListCreateAPIView):
                                                                       assigned_range__contains=dt).distinct('subject'):
                 data['sender'] = {
                     "content_type": "observations.subject", "id": subject_source.subject.id}
-                data['device'] = subject_source.source.id
+                data['device'] = str(subject_source.source.id)
                 data['status'] = models.RECEIVED   # update incoming message status to received.
                 ser_data = self.save_message(request, data)
         else:
