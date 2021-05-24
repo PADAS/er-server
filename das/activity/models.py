b6667aec74ce6e31e230a296cd2a4a5e3def5130
blac-siren <husnein.zac@gmail.com>
2021-05-24 09:14:06 +0300

    inclusive filter

--- das/activity/models.py
+++ das/activity/models.py
@@ -485,14 +485,10 @@ class EventFilteringQuerySet(models.QuerySet, FilterFieldMixin):
 
         return self
 
-    def by_updated_date(self, lower=None, upper=None):
-        if lower and upper:
-            return self.filter(updated_at__range=(lower, upper))
-        elif lower:
-            return self.filter(updated_at__gt=lower)
-        elif upper:
-            return self.filter(updated_at__lt=upper)
-        return self
+    def by_updated_date(self,
+                        lower=datetime.datetime.min.replace(tzinfo=pytz.utc),
+                        upper=datetime.datetime.max.replace(tzinfo=pytz.utc)):
+        return self.filter(updated_at__range=(lower, upper))
 
 
 class EventManager(models.Manager):
