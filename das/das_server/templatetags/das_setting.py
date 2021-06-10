4c9ac687d837cf0c5cd3df111040df889829be0e
blac-siren <husnein.zac@gmail.com>
2021-06-10 21:16:00 +0300

    send a pageview with site_name attached to it

--- das/das_server/templatetags/das_setting.py
+++ das/das_server/templatetags/das_setting.py
@@ -1,5 +1,6 @@
 from django import template
 from django.conf import settings
+from core.utils import get_site_name
 
 register = template.Library()
 
@@ -7,3 +8,10 @@ register = template.Library()
 @register.simple_tag()
 def get_settings_value(name):
     return getattr(settings, name, "")
+
+
+@register.simple_tag()
+def retrieve_site_name():
+    return get_site_name()
+
+
