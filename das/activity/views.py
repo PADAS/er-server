0538e25e5f2f377f01797f9490e1fd92ae89b1f9
Chris Jones <chrisj@vulcan.com>
2021-06-23 15:30:37 -0700

    User doesn't have an updated_at field, so use the first_name,last_name tuple instead for the etag calculation.

--- das/activity/views.py
+++ das/activity/views.py
@@ -17,6 +17,7 @@ import rest_framework.exceptions
 import versatileimagefield.files
 from django.conf import settings
 from django.contrib.postgres.aggregates import StringAgg, ArrayAgg
+from django.contrib.auth import get_user_model
 from django.db import transaction, IntegrityError
 from django.db.models import CharField, Value
 from django.db.models import Prefetch, F, Count, Max
@@ -39,7 +40,6 @@ import accounts.models
 import accounts.serializers
 import utils
 import utils.schema_utils as schema_utils
-from accounts.models import User
 from activity.filters import EventObjectPermissionsFilter
 from activity.models import Event, EventNote, EventClass, \
     EventFactor, EventClassFactor, EventType, EventRelationship, EventCategory, \
@@ -67,6 +67,7 @@ from activity.util import get_permitted_event_categories, return_409_response
 
 logger = logging.getLogger(__name__)
 
+User = get_user_model()
 LAST_DAYS = timedelta(days=3)
 
 USERCONTENT_FORCE_DOWNLOAD = getattr(settings, 'USERCONTENT_SETTINGS', {}).get(
@@ -85,7 +86,8 @@ def calculate_event_schema_etag(view_instance, view_method, request, *args, **kw
     for providence, people in Event.objects.get_reported_by(user):
         if people:
             for person in people:
-                latest_reported_by_list.append(person.updated_at)
+                latest_reported_by_list.append((person.first_name, person.last_name) if isinstance(
+                    person, User) else person.updated_at)
 
     latest_reported_by_count = len(latest_reported_by_list)
 
