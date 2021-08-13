5d46c616a8d91c81f55be301af83729f51532a51
Dan Cortes <daniel@nimblegravity.com>
2021-08-13 21:58:20 +0000

    Provisioned site NG26

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -183,6 +183,7 @@ locals {
     "matlabas"                = "prod1"
     "karongwe"                = "prod1"
     "alladale"                = "prod1"
+    "ng26"                    = "prod1"
   }
 
   default_infra_workspace_when_not_mapped_here = "dev"
@@ -282,6 +283,7 @@ locals {
     "bisonpark"             = 1
     "wcpzambia"             = 1
     "lionlandscapeske"      = 1
+    "ng26"                  = 1
   }
 
   db_instance_index = lookup(local.workspace_to_database_instance, terraform.workspace, 0)
