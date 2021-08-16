bb8fb55453e2d7043ac0d7f2c42c8525f2ca4325
Daniel Cortés <daniel@nimblegravity.com>
2021-08-16 14:50:18 -0500

    Update the provider

--- terraform/data.tf
+++ terraform/data.tf
@@ -3,46 +3,55 @@ data "google_project" "earthranger" {
 }
 
 data "google_secret_manager_secret_version" "alerts_slack_url" {
+  provider = "gsm"
   project = data.google_project.earthranger.project_id
   secret  = "slack_webhook_url_alertmanager"
 }
 
 data "google_secret_manager_secret_version" "email_username" {
+  provider = "gsm"
   project = data.google_project.earthranger.project_id
   secret  = "email_username"
 }
 
 data "google_secret_manager_secret_version" "email_password" {
+  provider = "gsm"
   project = data.google_project.earthranger.project_id
   secret  = "email_password"
 }
 
 data "google_secret_manager_secret_version" "ssl_privatekey_pem" {
+  provider = "gsm"
   project = data.google_project.earthranger.project_id
   secret  = var.ssl_privatekey_gsm_id
 }
 
 data "google_secret_manager_secret_version" "ssl_certificate_chain" {
+  provider = "gsm"
   project = data.google_project.earthranger.project_id
   secret  = var.ssl_certificate_gsm_id
 }
 
 data "google_secret_manager_secret_version" "twilio_account_settings" {
+  provider = "gsm"
   project = data.google_project.earthranger.project_id
   secret  = "twilio_default"
 }
 
 data "google_secret_manager_secret_version" "ubi_api_credentials" {
+  provider = "gsm"
   project = data.google_project.earthranger.project_id
   secret  = "ubi_api_credentials"
 }
 
 data "google_secret_manager_secret_version" "kerlink_credentials" {
+  provider = "gsm"
   project = data.google_project.earthranger.project_id
   secret  = "kerlink_credentials"
 }
 
 data "google_secret_manager_secret_version" "tableau_api_credentials" {
+  provider = "gsm"
   project = data.google_project.earthranger.project_id
   secret  = "tableau_api_credentials"
 }
