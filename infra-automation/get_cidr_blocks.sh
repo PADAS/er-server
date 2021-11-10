fcbbdd73b52d1aea2d5e28eb2d9442dc29a311e9
Daniel Cortés <daniel@nimblegravity.com>
2021-11-09 18:01:51 -0600

    Fixed command order and permissions

--- infra-automation/get_cidr_blocks.sh
+++ infra-automation/get_cidr_blocks.sh
@@ -1,3 +1,3 @@
-apt-get install jq -y
-gcloud config set project earthranger-78ca55ca
+apt-get -qq install jq -y
+gcloud config set project earthranger-78ca55ca -q --no-user-output-enabled
 gcloud container clusters describe das-dev --zone us-west1-a --format json | jq -r ".masterAuthorizedNetworksConfig.cidrBlocks[] | .cidrBlock" | sed -z 's/\n/,/g;s/,$/\n/'
