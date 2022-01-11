c7e84996229f89f317fd87b274738ca052660d1a
Daniel Cortés <daniel@nimblegravity.com>
2022-01-11 13:08:18 -0600

    Update to test out if jq is installed

--- infra-automation/get_cidr_blocks.sh
+++ infra-automation/get_cidr_blocks.sh
@@ -1,4 +1,4 @@
-apt-get -qq update
 apt-get -qq install jq -y
+jq
 gcloud config set project earthranger-78ca55ca -q --no-user-output-enabled > /dev/null
 gcloud container clusters describe das-dev --zone us-west1-a --format json | jq -r ".masterAuthorizedNetworksConfig.cidrBlocks[] | .cidrBlock" | sed -z 's/\n/,/g;s/,$/\n/'
