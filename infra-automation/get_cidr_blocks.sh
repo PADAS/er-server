bab51a6a6a00ab9242361d8c48fb54020cc37012
Daniel Cortés <daniel@nimblegravity.com>
2022-01-11 12:39:04 -0600

    Adding validation to fetching IPs

--- infra-automation/get_cidr_blocks.sh
+++ infra-automation/get_cidr_blocks.sh
@@ -1,3 +1,4 @@
-apt-get -qq install jq -y > /dev/null
+apt-get -qq update
+apt-get -qq install jq -y
 gcloud config set project earthranger-78ca55ca -q --no-user-output-enabled > /dev/null
 gcloud container clusters describe das-dev --zone us-west1-a --format json | jq -r ".masterAuthorizedNetworksConfig.cidrBlocks[] | .cidrBlock" | sed -z 's/\n/,/g;s/,$/\n/'
