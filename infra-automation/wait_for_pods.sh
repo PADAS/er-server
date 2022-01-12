b79be5b6f000b2f65fb83b3b18599cd4dcc51348
Daniel Cortés <daniel@nimblegravity.com>
2022-01-12 15:15:07 -0600

    Complete test

--- infra-automation/wait_for_pods.sh
+++ infra-automation/wait_for_pods.sh
@@ -3,16 +3,19 @@
 set -e
 
 function __is_pod_ready() {
-  [[ "$(kubectl get po "$1" -o 'jsonpath={.status.conditions[?(@.type=="Ready")].status}')" == 'True' ]]
+  [[ "$(kubectl get po "$1" -n $2 -o 'jsonpath={.status.conditions[?(@.type=="Ready")].status}')" == 'True' ]]
 }
 
 function __pods_ready() {
   local pod
 
+  echo $pods
+  echo $namespace
+
   [[ "$#" == 0 ]] && return 0
 
   for pod in $pods; do
-    __is_pod_ready "$pod" || return 1
+    __is_pod_ready "$pod" "$namespace" || return 1
   done
 
   return 0
@@ -31,10 +34,11 @@ function __wait-until-pods-ready() {
 
   period="$1"
   interval="$2"
+  namespace="$3"
 
   for ((i=0; i<$period; i+=$interval)); do
-    pods="$(kubectl get po -o 'jsonpath={.items[*].metadata.name}')"
-    if __pods_ready $pods; then
+    pods="$(kubectl get po -n $namespace -o 'jsonpath={.items[*].metadata.name}')"
+    if __pods_ready $pods $namespace; then
       return 0
     fi
 
