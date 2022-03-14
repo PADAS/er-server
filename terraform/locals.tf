59abe13fdaa32fde71ac64f52c8901e1bd9c9890
David Bwire <israelbwire@gmail.com>
2022-03-14 09:44:28 +0300

    New site creation kaingo.pamdas.org

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -236,6 +236,7 @@ locals {
     "chimanimani"             = "prod1"
     "ukraine"                 = "prod1"
     "sfggabon"                = "prod1"
+    "kaingo"                  = "prod1"
   }
 
   default_infra_workspace_when_not_mapped_here = "dev"
