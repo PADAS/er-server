23418407305fef0f1587275bc07dc9e214798399
Chris Doehring <chrisdoehring@gmail.com>
2022-03-24 07:39:17 -0700

    Reduce number of workers. Fix for conditional assignment in bourne shell. (#1938)

--- das/start_scripts/start_api_server.sh
+++ das/start_scripts/start_api_server.sh
@@ -9,7 +9,8 @@ python3 manage.py migrate --no-input
 # Override GUNICORN_CMD_ARGS at deployment if desired.
 # Keep in mind that the flags specified below, when running gunicorn, take 
 # precedence.
-export GUNICORN_CMD_ARGS=${GUNICORN_CMD_ARGS:-"--workers 4 --threads 4 --worker-class gthread --max-requests 500 --max-requests-jitter 25"}
+GUNICORN_CMD_ARGS=${GUNICORN_CMD_ARGS:-"--workers 2 --threads 4 --worker-class gthread --max-requests 500 --max-requests-jitter 25"}
+export GUNICORN_CMD_ARGS
 
 echo "Notice GUNICORN_CMD_ARGS: ${GUNICORN_CMD_ARGS}"
 
