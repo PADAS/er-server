13a33cf6dd48f347468bca9cff3bce8d5cffcf67
David Bwire <israelbwire@gmail.com>
2022-04-13 09:16:15 +0300

    Add new site lolldaiga

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -244,6 +244,7 @@ locals {
     "agriec"                  = "prod1"
     "madikwefutures"          = "prod1"
     "suyian"                  = "prod1"
+    "lolldaiga"               = "prod1"
   }
 
   default_infra_workspace_when_not_mapped_here = "dev"
