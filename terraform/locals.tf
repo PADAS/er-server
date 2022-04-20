805a8a1365da29ac1c1a07be8d8728478fc28fc3
David Bwire <israelbwire@gmail.com>
2022-04-20 11:48:41 +0300

    Move sucp and zellerzigaitis to the second db instance

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -363,6 +363,8 @@ locals {
     "elkarama"              = 1
     "noaa"                  = 1
     "nust"                  = 1
+    "sucp"                  = 1
+    "zellerzigaitis"        = 1
   }
 
   db_instance_index = lookup(local.workspace_to_database_instance, terraform.workspace, 0)
