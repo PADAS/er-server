e2951d7a97c8883e42c7ed9dd5e3874f5434e41d
David Bwire <israelbwire@gmail.com>
2022-05-11 11:21:32 +0300

    New site configuration raptorrescue

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -254,6 +254,7 @@ locals {
     "termit"                  = "prod1"
     "creaturehero"            = "prod1"
     "nzdoc"                   = "prod-asia"
+    "raptorrescue"            = "prod1"
   }
 
   default_infra_workspace_when_not_mapped_here = "dev"
@@ -374,6 +375,7 @@ locals {
     "binderlere"            = 1
     "termit"                = 1
     "creaturehero"          = 1
+    "raptorrescue"          = 1
   }
 
   db_instance_index = lookup(local.workspace_to_database_instance, terraform.workspace, 0)
