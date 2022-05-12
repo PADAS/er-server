7495e0fd6e45bd3c74e9cf75cd97515077fedee8
Daniel Cortés <daniel@nimblegravity.com>
2022-05-11 19:07:20 -0500

    delete unused files and move others to utils

--- docker/initDB.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker-compose run --entrypoint=/var/www/app/initDB.sh api
--- docker/reset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-COMMAND=$@
-docker-compose stop $COMMAND
-docker-compose rm -f $COMMAND 
-docker-compose -f docker-compose.yml -f compose-dev.yml up -d $COMMAND
-docker-compose logs -f $COMMAND
