b0a037b09c1a1f6a65329e4438f5671d0ce93304
David Bwire <israelbwire@gmail.com>
2022-05-17 09:19:32 +0300

    New site configuration birdlifebotswana

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -258,6 +258,7 @@ locals {
     "kruger"                  = "prod1"
     "cdass"                   = "prod1"
     "rhinorevolution"         = "prod1"
+    "birdlifebotswana"        = "prod1"
   }
 
   default_infra_workspace_when_not_mapped_here = "dev"
@@ -382,6 +383,7 @@ locals {
     "kruger"                = 1
     "cdass"                 = 1
     "rhinorevolution"       = 1
+    "birdlifebotswana"      = 1
   }
 
   db_instance_index = lookup(local.workspace_to_database_instance, terraform.workspace, 0)
