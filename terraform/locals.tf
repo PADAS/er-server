d20f7ad700761c801aae09903dd2c3356836d838
David Bwire <israelbwire@gmail.com>
2022-06-15 10:58:43 +0300

    New site configuration natural state

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -264,6 +264,7 @@ locals {
     "iona"                    = "prod1"
     "thuma"                   = "prod1"
     "maputo"                  = "prod1"
+    "naturalstate"            = "prod1"
   }
 
   default_infra_workspace_when_not_mapped_here = "dev"
@@ -394,6 +395,7 @@ locals {
     "iona"                  = 1
     "thuma"                 = 1
     "maputo"                = 1
+    "naturalstate"          = 1
   }
 
   db_instance_index = lookup(local.workspace_to_database_instance, terraform.workspace, 0)
