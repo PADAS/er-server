f09c4b5245f51e2ec1d2f14c01cff7a3c36b5d2a
David Bwire <israelbwire@gmail.com>
2022-06-15 12:36:37 +0300

    New site configuration umkhuze

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -265,6 +265,7 @@ locals {
     "thuma"                   = "prod1"
     "maputo"                  = "prod1"
     "naturalstate"            = "prod1"
+    "umkhuze"                 = "prod1"
   }
 
   default_infra_workspace_when_not_mapped_here = "dev"
@@ -396,6 +397,7 @@ locals {
     "thuma"                 = 1
     "maputo"                = 1
     "naturalstate"          = 1
+    "umkhuze"               = 1
   }
 
   db_instance_index = lookup(local.workspace_to_database_instance, terraform.workspace, 0)
