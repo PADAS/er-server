004358b3e41e7dde54c51ab5e4124d43a2032b7e
Azarael-Romero <93936271+Azarael-Romero@users.noreply.github.com>
2022-07-27 14:23:25 -0500

    ERA-7659 (#2116)
    * Replace eventlet with gevent

--- das/das_server/rt_wsgi.py
+++ das/das_server/rt_wsgi.py
@@ -1,5 +1,3 @@
-# !/bin/python3
-# isort: skip_file
 """
 WSGI config for real-time das project.
 
@@ -11,20 +9,19 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 from gevent import monkey  # nopep8
+
 monkey.patch_all()  # nopep8
 
-from socketio import WSGIApp
-
-from django.core.wsgi import get_wsgi_application
-
-from das_server.log import init_logging
-from rt_api.views import create_rt_socketio
-
+from django.core.wsgi import get_wsgi_application  # isort:skip
+from socketio import WSGIApp  # isort:skip
+from das_server.log import init_logging  # isort:skip
 
 init_logging()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "das_server.settings")
 app = get_wsgi_application()
+
+from rt_api.views import create_rt_socketio  # nopep8
 
 sio = create_rt_socketio()
 application = WSGIApp(sio, app)
