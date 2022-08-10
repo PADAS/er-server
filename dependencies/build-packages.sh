d2f3a5fb77cdbf0f1d8878a078420f88e8bf4e61
Chris Jones <41801350+chrisj-er@users.noreply.github.com>
2022-08-10 12:09:33 -0700

    DAS-8356 Run a development das server on an M1 or ARM processor (#2089)
    * run on M1
    
    * dockerfile config to test for cpu architecture to decide which gdal package to install.
    
    * move development environment to python 3.7
    
    * fix build-packages.sh to support cpu type
    
    * generalize building the vscode dockerfile, downloading gdal from our dependency bucket.
    
    * vs code python extension deprecated python 3.6. Pin to specific version. Update dev dockerfile to specify python version in two places.
    
    * move to gdal 3.4.1
    
    * add curly bracket to build-packages script. Remove unused deb packages now that they can be found in a GCP storage bucket.

--- dependencies/build-packages.sh
+++ dependencies/build-packages.sh
@@ -1,9 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # build in a docker container
-    # docker run -it -w="/workspace" -v ~/projects/das/das/dependencies/:/workspace ubuntu:18.04 bash
+    # docker run -it -w="/workspace" -v ~/projects/er/das/dependencies/:/workspace ubuntu:18.04 bash
 
-apt-get update -y
+PYTHON_PACKAGES="python3.7 python3.7-dev python3.7-distutils"
+apt-get update && apt-get install --no-install-recommends -yq software-properties-common \
+     && add-apt-repository ppa:deadsnakes/ppa && apt-get update \
+     && apt-get install -yq --no-install-recommends ${PYTHON_PACKAGES} 
+
 apt-get install -y build-essential \
                  software-properties-common \
                  ca-certificates \
@@ -11,13 +15,26 @@ apt-get install -y build-essential \
                  autoconf \
                  zip \
                  checkinstall \
-                 python3-dev \
                  wget \
                  pkg-config \
                  libtiff5-dev \
                  libcurl4-openssl-dev \
                  sqlite3 libsqlite3-dev \
 
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 4
+wget https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py
+python3 -m pip install pip --upgrade ; pip3 install pyopenssl setuptools wheel
+
+
+arch=$(uname -i)
+if [[ $arch == x86_64* ]]; then
+    arch_name="amd64"
+elif  ([[ $arch == arm* ]] || [[ $arch == aarch* ]]) ; then
+    arch_name="arm64"
+fi
+
+cp_ver="cp37"
 
 wget http://download.osgeo.org/geos/geos-3.9.1.tar.bz2; tar -xjf geos-3.9.1.tar.bz2
 cd geos-3.9.1
@@ -37,20 +54,20 @@ sed -i -e 's/return version/return GEOS_CAPI_VERSION/' capi/geos_ts_c.cpp
 
 ./configure; make; checkinstall -y;
 cd ..
-cp geos-3.9.1/geos_3.9.1-1_amd64.deb ./geos_3.9.1-1_cp36_amd64.deb
+cp geos-3.9.1/geos_3.9.1-1_${arch_name}.deb ./geos_3.9.1-1_${cp_ver}_${arch_name}.deb
 #rm -rf geos-3.9.1
 ldconfig
 
 wget http://download.osgeo.org/proj/proj-7.2.1.tar.gz; tar -xzvf proj-7.2.1.tar.gz; cd proj-7.2.1; ./configure --prefix=/usr; make; checkinstall -y;
 cd ..
-cp proj-7.2.1/proj_7.2.1-1_amd64.deb ./proj_7.2.1-1_cp36_amd64.deb
+cp proj-7.2.1/proj_7.2.1-1_${arch_name}.deb ./proj_7.2.1-1_${cp_ver}_${arch_name}.deb
 #rm -rf proj-7.2.1
 ldconfig
 
-wget http://download.osgeo.org/gdal/3.2.3/gdal-3.2.3.tar.gz; tar -xzvf gdal-3.2.3.tar.gz; cd gdal-3.2.3; ./configure --prefix=/usr --with-python=/usr/bin/python3 --with-geos=/usr/local/bin/geos-config --with-proj=/usr; make; checkinstall -y;
+wget http://download.osgeo.org/gdal/3.4.1/gdal-3.4.1.tar.gz; tar -xzvf gdal-3.4.1.tar.gz; cd gdal-3.4.1; ./configure --prefix=/usr --with-python=/usr/bin/python3.7 --with-geos=/usr/local/bin/geos-config --with-proj=/usr; make; checkinstall -y;
 cd ..
-cp gdal-3.2.3/gdal_3.2.3-1_amd64.deb ./gdal_3.2.3-1_cp36_amd64.deb
-#rm -rf gdal-3.2.3
+cp gdal-3.4.1/gdal_3.4.1-1_${arch_name}.deb ./gdal_3.4.1-1_${cp_ver}_${arch_name}.deb
+#rm -rf gdal-3.4.1
 
 # RUN if [ ! -e /usr/lib/libproj.so ]; then \
 #   cd /opt; wget http://download.osgeo.org/proj/proj-4.9.2.tar.gz; tar -xzvf proj-4.9.2.tar.gz; cd proj-4.9.2; ./configure --prefix=/usr; make; make install; fi
