e6733003a5d996ac88d29556b3b586120e08830c
Dan Cortes <daniel@nimblegravity.com>
2022-09-28 16:47:46 +0000

    Add Damai again

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -295,6 +295,7 @@ locals {
     "mukutan"                 = "prod1"
     "pangolinconservation"    = "prod1"
     "zingela"                 = "prod1"
+    "damai"                   = "prod-asia"
   }
 
   default_infra_workspace_when_not_mapped_here = "dev"
