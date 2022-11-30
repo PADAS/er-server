d316b0da7c48ed2422e312cf737f9731723ef3f6
Francisco Suárez <tarkin88@users.noreply.github.com>
2022-11-30 15:59:54 -0600

    Fix 2-61-1 into Develop (#2352)
    * Update __init__.py
    
    Update version for the release candidate.
    
    * ERA-7957: Fix SQL injection vulnerability on `events` API (#2286)
    
    * fix: don't render updates for event_details if this was disabled by the api parameter include_updates (#2313)
    
    fix: add a few more qparam descriptions to the Events API interactive schema
    
    * ERA-7957: catch SQL errors exposing table structure (#2336)
    
    * fix: add missing function from fix sql injection
    
    * fix: add missing condition
    
    Co-authored-by: Daniel Cortés <daniel@nimblegravity.com>
    Co-authored-by: JoshuaVulcan <38018017+JoshuaVulcan@users.noreply.github.com>
    Co-authored-by: Diego Guzmán <diegog@earthranger.com>
    Co-authored-by: Chris Jones <41801350+chrisj-er@users.noreply.github.com>

---
+++ das/activity/tests/events/test_filters.py
@@ -0,0 +1,40 @@
+import json
+from unittest.mock import patch
+
+import pytest
+from psycopg2.errors import InvalidTextRepresentation
+
+from django.urls import reverse
+from rest_framework import status
+
+
+@pytest.mark.django_db
+class TestEventViewFilters:
+    def test_filter_param_is_not_vulnerable_to_sql_injection(self, superuser_client, five_events_with_details):
+        url = reverse("events")
+        term = (
+            "'||(SELECT (CHR(97)||CHR(102)||CHR(107)||CHR(68)) WHERE 7152=7152 AND 8130=CAST((CHR(113)||CHR(106)||"
+            "CHR(118)||CHR(107)||CHR(113))||(SELECT (CASE WHEN (8130=8130) THEN 1 ELSE 0 END))::text||(CHR(113)||"
+            "CHR(107)||CHR(112)||CHR(113)||CHR(113)) AS NUMERIC))||'"
+        )
+        query_params = {
+            "include_notes": True,
+            "include_related_events": True,
+            "state": ["active", "new"],
+            "filter": json.dumps({"text": term}),
+            "sort_by": "-updated_at",
+        }
+
+        response = superuser_client.get(url, query_params)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["count"] == 0
+
+    def test_filter_raises_invalid_text_representation(self, superuser_client, five_events_with_details):
+        url = reverse("events")
+        with patch("rest_framework.generics.mixins.ListModelMixin.list") as list_mock:
+            list_mock.side_effect = InvalidTextRepresentation("This message should not reach the user")
+            response = superuser_client.get(url)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["count"] == 0
