fda23fa5911e7f8a896e66fefe165634c95a12a3
Chris Jones <chrisj@earthranger.com>
2023-01-19 02:08:02 +0000

    ERA-8222 - configure logging level feat: override the ER Server default logging level of Warning by setting the LOGGING_LEVEL environment variable to a noiser level INFO or DEBUG. chore: Update a few of the vs code launch configurations to INFO

--- das/das_server/log.py
+++ das/das_server/log.py
@@ -2,6 +2,8 @@ import logging
 import logging.config
 import sys
 
+import environ
+
 try:
     # local_log.py should contain an override of DEFAULT_LOGGING as seen below
     from . import local_log
@@ -10,6 +12,12 @@ except ImportError:
 
 logger = logging.getLogger(__name__)
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env()
 
 DEFAULT_LOGGING = {
     "version": 1,
@@ -63,22 +71,44 @@ DEFAULT_LOGGING = {
 has_initialized = False
 
 
+def update_level_in_log_config(config: dict, level: str) -> dict:
+
+    loggers = config["loggers"]
+
+    loggers[""]["level"] = level
+    loggers["rt_api.socketio"]["level"] = level
+    if level == "DEBUG":
+        config["handlers"]["console"]["level"] = level
+        loggers["django"]["level"] = level
+        loggers["django.request"]["level"] = level
+        loggers["django.server"]["level"] = level
+        loggers["rt_api"]["level"] = level
+        loggers["rt_api.pubsub_listener"]["level"] = level
+
+    return config
+
+
 def init_logging(service=None):
     global has_initialized
+
+    if not service:
+        service = "default_logging"
+
     if has_initialized:
         logger.debug("logging already initialized, not loading %s", service, exc_info=True)
         return
 
     has_initialized = True
 
-    if not service:
-        service = "default_logging"
+    level = env.str("LOGGING_LEVEL", "")
 
     try:
         module = sys.modules[__name__]
         if local_log:
             module = local_log
         log_settings = getattr(module, service.upper())
+        if level:
+            log_settings = update_level_in_log_config(log_settings, level)
     except AttributeError:
         message = "No logging configuration" " found for {0} in {1}".format(service, repr(module))
         logger.warning(message)
