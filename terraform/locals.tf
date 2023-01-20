364d3e0d29171c3299a06124668569073c26b50a
David Bwire <israelbwire@gmail.com>
2023-01-20 14:44:06 +0300

    new site configuration - mindoro

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -355,6 +355,7 @@ locals {
     "conkouatidouli"          = "prod1"
     "anac"                    = "prod1"
     "stoprhinopoaching"       = "prod1"
+    "mindoro"                 = "prod-asia"
   }
 
   default_infra_workspace_when_not_mapped_here = "dev"
