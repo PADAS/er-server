34a256c889b794e54d2710b54566d18cf4e41fe6
David Bwire <israelbwire@gmail.com>
2023-01-23 15:17:00 +0300

    use distant future time as rotation values

--- terraform/databases.tf
+++ terraform/databases.tf
@@ -208,9 +208,10 @@ resource "google_secret_manager_secret" "er_sql_analytics_info" {
     name = local.pgb_credentials_topic[local.kubernetes_cluster_name]
   }
   # rotation block is needed to add topics
+  # use distant time as we are not currently rotating the secret
   rotation {
-    next_rotation_time = "2023-01-23T11:30:00Z"
-    rotation_period    = "3600s"
+    next_rotation_time = "2033-01-23T11:30:00Z"
+    rotation_period    = "315360000s"
   }
 }
 
