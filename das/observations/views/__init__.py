4bab4c2dfddc1510b36dd14a1fb2f7b1d0675f04
Juan Herrera <90631459+juanearthranger@users.noreply.github.com>
2023-02-14 15:51:42 -0600

    ERA-8256: Add support for sort by in observations endpoint (#2443)
    * feat: Add new module for testing views
    
    * feat: Move current test_views.py file into tests/views module
    
    * feat: Create helper and exception files into observation views module
    
    * feat: Create observations file into observations views
    
    * feat: Move observations views into their corresponding file
    
    * feat: Add new order_by param in observations endpiont
    
    * feat: Add five_observations fixture
    
    * feat: Finish test for order_by
    
    * feat: Remove all order_by statement in ObservationsView
    
    * feat: Rename param by sort_by to be consistent with activity app
    
    * feat: Add improvements in observations.py and helpers.py

--- das/observations/views/__init__.py
+++ das/observations/views/__init__.py
@@ -23,12 +23,7 @@ from django.urls import reverse
 from django.utils import timezone
 from django.utils.dateparse import parse_datetime
 from rest_framework import generics, status
-from rest_framework.exceptions import (
-    APIException,
-    ParseError,
-    PermissionDenied,
-    ValidationError,
-)
+from rest_framework.exceptions import ParseError, PermissionDenied, ValidationError
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.renderers import StaticHTMLRenderer
 from rest_framework.response import Response
@@ -59,12 +54,15 @@ from utils import add_base_url
 from utils.drf import (
     BadRequestAPIException,
     OptionalResultsSetPagination,
-    StandardResultsSetCursorPagination,
     StandardResultsSetGeoJsonPagination,
     StandardResultsSetPagination,
     return_409_response,
 )
 from utils.json import ExtendedGEOJSONRenderer, parse_bool, zeroout_microseconds
+
+from .exceptions import UnauthorizedView
+from .helpers import check_valid_date_string
+from .observations import ObservationsView
 
 logger = logging.getLogger(__name__)
 
@@ -104,18 +102,6 @@ def default_since():
     return datetime.datetime.now(pytz.utc) - get_track_days()
 
 
-def check_valid_date_string(date_str, parameter_name):
-    if date_str:
-        try:
-            result = dateparse(date_str)
-        except ValueError:
-            raise ValueError(f'Invalid value for {parameter_name}: "{date_str}"')
-        else:
-            return True, result
-    else:
-        return False, None
-
-
 def get_subjects_with_observations_in_daterange(start_date=None, end_date=None):
     observations_qs = models.Observation.objects.all()
 
@@ -137,15 +123,6 @@ def get_subjects_with_observations_in_daterange(start_date=None, end_date=None):
     ]
 
     return models.Subject.objects.filter(id__in=subject_ids)
-
-
-class UnauthorizedView(APIException):
-    """
-    User does not have view permission, return empty data
-    """
-
-    status_code = 200
-    default_detail = {"data": []}
 
 
 class RegionsView(generics.ListAPIView):
@@ -914,185 +891,6 @@ class SourceProvidersViewPartial(generics.UpdateAPIView):
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
-        return context
-
-
-class ObservationsViewSchema(CustomSchema):
-    def get_operation(self, path, method):
-        operation = super().get_operation(path, method)
-        if method == "GET":
-            query_params = [
-                {"name": "subject_id", "in": "query", "description": "filter to a single subject"},
-                {"name": "source_id", "in": "query", "description": "filter to a single source"},
-                {
-                    "name": "subjectsource_id",
-                    "in": "query",
-                    "description": "filter to a subjectsource_id, rather than source_id + time range",
-                },
-                {
-                    "name": "since",
-                    "in": "query",
-                    "description": "get observations after this ISO8061 date, include timezone",
-                },
-                {
-                    "name": "until",
-                    "in": "query",
-                    "description": "get observations up to this ISO8061 date, include timezone",
-                },
-                {
-                    "name": "filter",
-                    "in": "query",
-                    "description": "filter using exclusion_flags for an observation. one of [null, 0, 1, 2  or 3].",
-                },
-                {
-                    "name": "include_details",
-                    "in": "query",
-                    "description": " one of [true,false], default is false. This brings back the observation additional field",
-                },
-                {
-                    "name": "created_after",
-                    "in": "query",
-                    "description": "get observations created (saved in EarthRanger) after this ISO8061 date, include timezone",
-                },
-                {
-                    "name": "use_cursor",
-                    "in": "query",
-                    "description": "default is to use a page based paginator, which does not scale to a large dataset. Set use_cursor=true to employ a paginator that can handle millions of rows by using next/prev urls.",
-                },
-            ]
-            operation["parameters"].extend(query_params)
-        return operation
-
-
-class ObservationsCursorPagination(StandardResultsSetCursorPagination):
-    cursor_query_Param = "id"
-    ordering = "recorded_at"
-
-
-class ObservationsView(generics.ListCreateAPIView):
-    serializer_class = serializers.ObservationSerializer
-    pagination_class = StandardResultsSetPagination
-    permission_classes = (StandardObjectPermissions,)
-    schema = ObservationsViewSchema()
-
-    @property
-    def paginator(self):
-        """The paginator instance associated with the view, or `None`.
-           API caller can request to use a cursor based paginator.
-
-        Returns:
-            paginator: the requested paginator
-        """
-        if not hasattr(self, "_paginator"):
-            if self.pagination_class is None:
-                self._paginator = None
-            else:
-                self._paginator = (
-                    ObservationsCursorPagination()
-                    if parse_bool(self.request.query_params.get("use_cursor"))
-                    else self.pagination_class()
-                )
-        return self._paginator
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        page = self.paginate_queryset(queryset)
-
-        output = []
-        for item in page:
-            output.append(self.serializer_class.dict_to_representation(item, request.query_params))
-
-        return self.get_paginated_response(output)
-
-    def get_queryset(self):
-        if not self.request.user.has_any_perms(VIEW_OBSERVATION_PERMS):
-            raise UnauthorizedView
-
-        query_params = self.request.query_params
-        since = query_params.get("since", None)
-        until = query_params.get("until", None)
-        recorded_since_is_valid, recorded_since = check_valid_date_string(since, "recorded_since")
-        recorded_until_is_valid, recorded_until = check_valid_date_string(until, "recorded_until")
-        subject_id = query_params.get("subject_id", None)
-        source_id = query_params.get("source_id", None)
-        subjectsource_id = query_params.get("subjectsource_id", None)
-        created_after = query_params.get("created_after", None)
-        filter_flag = 0
-        filter_qparam = query_params.get("filter", 0)
-        try:
-            filter_flag = int(filter_qparam)
-        except (ValueError, TypeError):
-            filter_flag = None if filter_qparam == "null" else filter_flag
-
-        if len([id for id in (subject_id, source_id, subjectsource_id) if id]) > 1:
-            raise ValueError("Can only specify one of: subject_id and source_id and subjectsource_id")
-        elif subject_id:
-            subject = get_object_or_404(models.Subject, pk=subject_id)
-            if not self.request.user.has_any_perms(VIEW_SUBJECT_PERMS, subject):
-                raise PermissionDenied
-
-            queryset = models.Observation.objects.get_subject_observations(
-                subject, since=recorded_since, until=recorded_until, filter_flag=filter_flag, order_by="recorded_at"
-            )
-        elif source_id:
-            queryset = models.Observation.objects.get_source_observations(
-                source_id, since=recorded_since, until=recorded_until, filter_flag=filter_flag, order_by="recorded_at"
-            )
-        elif subjectsource_id:
-            queryset = models.Observation.objects.get_subjectsource_observations(
-                subjectsource_id,
-                since=recorded_since,
-                until=recorded_until,
-                filter_flag=filter_flag,
-                order_by="recorded_at",
-            )
-        else:
-            queryset = models.Observation.objects.by_since_until(recorded_since, recorded_until)
-            queryset = queryset.by_exclusion_flags(filter_flag)
-            queryset = queryset.order_by("recorded_at")
-
-        mou_date = self.request.user.additional.get("expiry", None)
-        mou_expiry_date = dateparse(mou_date) if mou_date else None
-        created_after = dateparse(created_after) if created_after else None
-
-        if mou_expiry_date:
-            queryset = queryset.filter(recorded_at__lte=mou_expiry_date)
-
-        if created_after:
-            queryset = queryset.by_created_after(created_after)
-
-        queryset = queryset.annotate_transforms()
-        queryset = queryset.prefetch_related("source__provider__transforms")
-
-        return queryset.values()
-
-    def create(self, request, *args, **kwargs):
-        """
-         On condition of post body being a list, let it bulk insert.
-        :param request:
-        :param args:
-        :param kwargs:
-        :return:
-        """
-        serializer = serializers.ObservationSerializer(many=isinstance(request.data, list), data=request.data)
-        if not serializer.is_valid():
-            return Response(
-                serializer.errors,
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-
-    def get_serializer_context(self):
-        context = super(ObservationsView, self).get_serializer_context()
-
-        # Check request before accessing params since self.request is None when
-        # generating docs schema
-        context["include_details"] = (
-            parse_bool(self.request.query_params.get("include_details", False)) if self.request else False
-        )
-
         return context
 
 
