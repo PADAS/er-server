25a5788acf765548b445ebbe3a898e68ff731615
Diego Guzmán <diegog@earthranger.com>
2023-03-21 09:28:29 -0600

    ERA-8406: pods should not fail on InconsistencyError (#2483)
    Co-authored-by: Chris Jones <chrisj@earthranger.com>

--- das/das_server/rt_wsgi.py
+++ das/das_server/rt_wsgi.py
@@ -7,6 +7,7 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 
 import eventlet
+
 eventlet.monkey_patch()
 
 from django.core.wsgi import get_wsgi_application
@@ -19,5 +20,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "das_server.settings")
 app = get_wsgi_application()
 
 from rt_api.views import create_rt_socketio
+
 sio = create_rt_socketio()
 application = WSGIApp(sio, app)
