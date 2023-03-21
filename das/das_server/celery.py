e5645b98fa7cb3439eec4812dad72dc81a86e022
Diego Guzmán <diegog@earthranger.com>
2023-03-21 09:28:29 -0600

    ERA-8406: pods should not fail on InconsistencyError (#2483)
    Co-authored-by: Chris Jones <chrisj@earthranger.com>

--- das/das_server/celery.py
+++ das/das_server/celery.py
@@ -18,6 +18,7 @@ from kombu import Exchange, Queue
 from django.conf import settings
 
 import utils.stats
+from das_server.redis import TRANSPORT_ALIASES  # pylint: disable=unused-import
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "das_server.settings")
@@ -217,4 +218,4 @@ def task_failure_handler(sender, *args, **kwargs):
 
 @task_revoked.connect
 def task_revoked_handler(request, *args, **kwargs):
-    utils.stats.increment("task", tags=[f"name:{request.name}", "state:revoked"])
+    utils.stats.increment("task", tags=[f"name:{request.task}", "state:revoked"])
--- das/tracking/tests/test_savannah.py
+++ das/tracking/tests/test_savannah.py
@@ -1,28 +1,33 @@
-import io
+import json
 from datetime import datetime, timedelta
 from unittest import mock
+
 import pytz
-import json
+import requests_mock
 
 from django.apps import apps
 from django.contrib.contenttypes.models import ContentType
-from django.test import TestCase
-import requests_mock
+from django.test import TestCase, override_settings
 
-from core.tests import BaseAPITest, fake_get_pool
-from activity.models import EventCategory, EventType
-from observations.models import Source, SourceProvider, Subject, SubjectType, \
-    SubjectSubType, SubjectSource
+from core.tests import fake_get_pool
+from observations.models import (
+    Source,
+    SourceProvider,
+    Subject,
+    SubjectSource,
+    SubjectSubType,
+    SubjectType,
+)
 from tracking.models import SavannahPlugin, SourcePlugin
-from tracking.models.savannah import SavannaClient, STObservation, STAlert
-from tracking.tasks import run_source_plugin
+from tracking.tasks import run_plugin_class
 
 
 def make_data_download(request_mock, host):
     def match_data_download(request):
-        return 'data_download' in request.text
+        return "data_download" in request.text
 
-    data = json.loads("""{
+    data = json.loads(
+        """{
         "sucess": true, "error_msg": "", "has_more_records": false,
         "records": [
             {
@@ -46,16 +51,19 @@ def make_data_download(request_mock, host):
                 "speed_accuracy": 0, "altitude": 0, "temperature": 28.8,
                 "initial_data": "", "battery": 3.71
                 }
-        ]}""")
-    request_mock.register_uri('POST', host + "/savannah_data/data_request", json=data,
-                              status_code=200, additional_matcher=match_data_download)
+        ]}"""
+    )
+    request_mock.register_uri(
+        "POST", host + "/savannah_data/data_request", json=data, status_code=200, additional_matcher=match_data_download
+    )
 
 
 def make_exceptions_download(request_mock, host):
     def match_exceptions_download(request):
-        return 'exceptions_download' in request.text
+        return "exceptions_download" in request.text
 
-    data = json.loads("""{
+    data = json.loads(
+        """{
     "sucess": true,
     "error_msg": "",
     "has_more_records": false,
@@ -163,9 +171,15 @@ def make_exceptions_download(request_mock, host):
             "battery": 0
         }
     ]
-}""")
-    request_mock.register_uri('POST', host + "/savannah_data/data_request", json=data,
-                              status_code=200, additional_matcher=match_exceptions_download)
+}"""
+    )
+    request_mock.register_uri(
+        "POST",
+        host + "/savannah_data/data_request",
+        json=data,
+        status_code=200,
+        additional_matcher=match_exceptions_download,
+    )
 
 
 class SavannahPluginTest(TestCase):
@@ -174,64 +188,67 @@ class SavannahPluginTest(TestCase):
     def setUp(self):
         latest_timestamp = datetime.now(tz=pytz.utc) - timedelta(days=20)
         latest_timestamp = latest_timestamp.isoformat()
-        cursor_data = {'latest_timestamp': latest_timestamp}
+        cursor_data = {"latest_timestamp": latest_timestamp}
 
         # Create source and savannah plugin object
         # Link source and savannah plugin object using SourcePlugin
-        self.source_provider = SourceProvider.objects.create(
-            provider_key='savannah', display_name='Savannah')
+        self.source_provider = SourceProvider.objects.create(provider_key="savannah", display_name="Savannah")
         self.source = Source.objects.create(
-            provider=self.source_provider, manufacturer_id='ST2010-3083',
-            source_type=('tracking-device', 'Tracking Device'))
+            provider=self.source_provider,
+            manufacturer_id="ST2010-3083",
+            source_type=("tracking-device", "Tracking Device"),
+        )
 
         savannah_plugin = SavannahPlugin.objects.create(
-            name='Savannah', provider=self.source_provider,
-            service_username="random", service_password="random",
-            service_api_host=self.api_host)
-        plugin_type = ContentType.objects.get(
-            app_label='tracking', model='savannahplugin')
+            name="Savannah",
+            provider=self.source_provider,
+            service_username="random",
+            service_password="random",
+            service_api_host=self.api_host,
+        )
+        plugin_type = ContentType.objects.get(app_label="tracking", model="savannahplugin")
         self.source_plugin = SourcePlugin.objects.create(
-            plugin_type=plugin_type, plugin_id=savannah_plugin.id,
-            source=self.source, cursor_data=cursor_data)
+            plugin_type=plugin_type, plugin_id=savannah_plugin.id, source=self.source, cursor_data=cursor_data
+        )
 
         # Create Subject and link with source using SubjectSource
-        subject_type, created = SubjectType.objects.get_or_create(
-            value='wildlife')
+        subject_type, created = SubjectType.objects.get_or_create(value="wildlife")
         subject_subtype, created = SubjectSubType.objects.get_or_create(
-            value='elephant', defaults=dict(subject_type=subject_type))
-        self.henry = Subject.objects.create(
-            name='Henry', subject_subtype=subject_subtype)
+            value="elephant", defaults=dict(subject_type=subject_type)
+        )
+        self.henry = Subject.objects.create(name="Henry", subject_subtype=subject_subtype)
         SubjectSource.objects.create(source=self.source, subject=self.henry)
 
+    @override_settings(CELERY_TASK_ALWAYS_EAGER=True)
     @mock.patch("das_server.pubsub.get_pool", fake_get_pool)
     @requests_mock.Mocker()
     def test_savannah(self, request_mock):
         make_data_download(request_mock, self.api_host)
         make_exceptions_download(request_mock, self.api_host)
 
-        plugin_class = apps.get_model('tracking', 'SavannahPlugin')
+        plugin_class = apps.get_model("tracking", "SavannahPlugin")
 
         # run plugin to fetch observations and alert type data
-        for plugin in plugin_class.objects.all():
-            if plugin.run_source_plugins:
-                for sp in plugin.source_plugins.filter(status='enabled'):
-                    if sp.should_run():
-                        run_source_plugin(sp.id)
-            else:
-                plugin.execute()
+        run_plugin_class(plugin_class)
 
         self.assertEqual(len(self.henry.observations()), 8)
 
         # Check battery values
         self.assertTrue(
-            any(observation.__dict__['additional'].get('battery', None)
-                for observation in self.henry.observations()))
+            any(observation.__dict__["additional"].get("battery", None) for observation in self.henry.observations())
+        )
 
         # Check alert types immobility, immobility_all_clear in observations
-        self.assertTrue(any(observation.__dict__['additional'].get(
-            'device_alert', None) == 'immobility'
-            for observation in self.henry.observations()))
+        self.assertTrue(
+            any(
+                observation.__dict__["additional"].get("device_alert", None) == "immobility"
+                for observation in self.henry.observations()
+            )
+        )
 
-        self.assertTrue(any(observation.__dict__['additional'].get(
-            'device_alert', None) == 'immobility_all_clear'
-            for observation in self.henry.observations()))
+        self.assertTrue(
+            any(
+                observation.__dict__["additional"].get("device_alert", None) == "immobility_all_clear"
+                for observation in self.henry.observations()
+            )
+        )
