e1319278a3ec8be83eb957f1d12da57de9457bb2
Diego Guzmán <diegog@earthranger.com>
2023-04-17 13:02:38 -0600

    ERA-8465: validate user pin uniqueness at admin form (#2516)

--- das/accounts/tests/test_forms.py
+++ das/accounts/tests/test_forms.py
@@ -1,6 +1,79 @@
 import pytest
 
-from accounts.forms import UserAdditionalForm
+from accounts.forms import CustomUserCreationForm, UserAdditionalForm
+from accounts.models import User
+
+
+@pytest.mark.django_db
+class TestCustomUserCreationForm:
+    @pytest.mark.parametrize("pin", ["1234", "0000", "0012"])
+    def test_create_user_with_valid_pin(self, pin):
+        form = CustomUserCreationForm(data={"username": "username", "pin": pin})
+
+        form.is_valid()
+
+        assert not form.errors
+
+    def test_create_user_with_empty_pin(self):
+        form = CustomUserCreationForm(data={"username": "username"})
+
+        form.is_valid()
+
+        assert not form.errors
+
+    def test_create_user_with_string_pin(self):
+        form = CustomUserCreationForm(data={"username": "username", "pin": "this"})
+
+        form.is_valid()
+
+        assert form.errors["pin"][0] == "The value should be four digits."
+
+    def test_create_user_with_long_pin(self):
+        form = CustomUserCreationForm(data={"username": "username", "pin": 123456})
+
+        form.is_valid()
+
+        assert form.errors["pin"][0] == "The size should be four digits."
+
+    @pytest.mark.parametrize("pin", [12, 0])
+    def test_create_user_with_short_pin(self, pin):
+        form = CustomUserCreationForm(data={"username": "username", "pin": pin})
+
+        form.is_valid()
+
+        assert form.errors["pin"][0] == "The size should be four digits."
+
+    @pytest.mark.parametrize("field_name", ["first_name", "last_name"])
+    def test_create_user_rejected_by_xss_protection(self, field_name):
+        form = CustomUserCreationForm(data={"username": "username", field_name: "<script>alert('boo')</script>"})
+
+        form.is_valid()
+
+        assert (
+            form.errors[field_name][0]
+            == "The field contains invalid characters. Only aphanumeric characters and period are allowed."
+        )
+
+    @pytest.mark.parametrize("invalid_name", ["`", "-", "<", ">", ";", "$", "@", "(", ")"])
+    def test_special_characters_are_not_allowed_on_first_name_field(self, invalid_name):
+        form = CustomUserCreationForm(data={"username": "username", "first_name": invalid_name})
+
+        form.is_valid()
+
+        assert (
+            form.errors["first_name"][0]
+            == "The field contains invalid characters. Only aphanumeric characters and period are allowed."
+        )
+
+    def test_create_user_with_duplicate_pin(self):
+        user = User.objects.first()
+        user.pin = "1234"
+        user.save()
+        form = CustomUserCreationForm(data={"username": "username", "pin": "1234"})
+
+        form.is_valid()
+
+        assert form.errors["pin"][0] == "User PINs must be unique, please select another PIN value."
 
 
 @pytest.mark.django_db
@@ -63,3 +136,13 @@ class TestUserAdditionalForm:
             form.errors["first_name"][0]
             == "The field contains invalid characters. Only aphanumeric characters and period are allowed."
         )
+
+    def test_create_user_with_duplicate_pin(self):
+        user = User.objects.first()
+        user.pin = "1234"
+        user.save()
+        form = UserAdditionalForm(data={"username": "username", "pin": "1234"})
+
+        form.is_valid()
+
+        assert form.errors["pin"][0] == "User PINs must be unique, please select another PIN value."
