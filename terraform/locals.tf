3e726ca1e08d490fe977481d196fc1e322807c5c
David Bwire <israelbwire@gmail.com>
2023-04-24 07:33:08 +0300

    new site config - olarroconservancy

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -406,6 +406,7 @@ locals {
     "focen"                   = "prod1"
     "standrews"               = "prod1"
     "earthrace"               = "prod1"
+    "olarroconservancy"       = "prod1"
   }
 
   default_infra_workspace_when_not_mapped_here = "dev"
