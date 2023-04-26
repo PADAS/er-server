4f266d9a5c11a5e4c112844f878485d6e911f811
Chris Jones <41801350+chrisj-er@users.noreply.github.com>
2023-04-26 08:09:56 -0700

    DASOPS-1106 manupont124 site (#2534)

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -408,6 +408,7 @@ locals {
     "earthrace"               = "prod1"
     "kisampa"                 = "prod1"
     "olarroconservancy"       = "prod1"
+    "manupont124"             = "prod1"
   }
 
   default_infra_workspace_when_not_mapped_here = "dev"
@@ -605,6 +606,7 @@ locals {
     "alturas"                 = 1
     "focen"                   = 1
     "earthrace"               = 1
+    "manupont124"             = 1
   }
 
   db_instance_index  = lookup(local.workspace_to_database_instance, terraform.workspace, 0)
