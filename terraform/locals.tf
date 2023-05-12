58de68b2a88c4bc9fbb7770239ee17cc827fccea
David Bwire <israelbwire@gmail.com>
2023-05-12 15:24:17 +0300

    new site configuration - ybycui (#2556)

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -414,6 +414,7 @@ locals {
     "swt"                     = "prod1"
     "chum"                    = "prod1"
     "mtverdesanctuary"        = "prod1"
+    "ybycui"                  = "prod1"
   }
 
   default_infra_workspace_when_not_mapped_here = "dev"
