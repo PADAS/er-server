14a7198ea7863315dc6bd193caad6fc78867469c
Diego Guzmán <diegog@earthranger.com>
2023-05-29 17:00:59 -0600

    ERA-8503: SocketClient and UserSession updates raise DatabaseError from green threads (#2582)

---
+++ das/utils/tests/test_db.py
@@ -0,0 +1,33 @@
+from unittest.mock import MagicMock
+
+import pytest
+
+from utils.db import DatabaseConnectionCloser
+
+
+class CustomException(Exception):
+    pass
+
+
+class TestUseSharedResourceUsageOnDatabaseConnectionCloser:
+    def test_use_shared_resource_successfully(self):
+        resource = MagicMock()
+        resource_handler = DatabaseConnectionCloser(resource)
+
+        resource_handler.close_connection()
+
+        assert resource.inc_thread_sharing.called_once
+        assert resource.close_if_unusable_or_obsolete.called_once
+        assert resource.dec_thread_sharing.called_once
+
+    def test_use_shared_resource_unsuccessfully(self):
+        resource = MagicMock()
+        resource.close_if_unusable_or_obsolete.side_effect = CustomException
+        resource_handler = DatabaseConnectionCloser(resource)
+
+        with pytest.raises(CustomException):
+            resource_handler.close_connection()
+
+        assert resource.inc_thread_sharing.called_once
+        assert resource.close_if_unusable_or_obsolete.called_once
+        assert resource.dec_thread_sharing.called_once
