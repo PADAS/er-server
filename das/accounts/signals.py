da48c8616fd25507c568c4454a78f1cbad437892
Francisco Suárez <tarkin88@users.noreply.github.com>
2023-06-07 17:21:56 -0600

    ERA-8514 Fix. Patch problematic migration  (#2596)
    * bump develop to 2.76
    
    * fix: moved business logic from signal to admin save
    
    To avoid issue on migrations
    
    * fix: patch problematic migration to use signals on user
    
    * fix: returns to signals approach to update subject name
    
    ---------
    
    Co-authored-by: Chris Jones <41801350+chrisj-er@users.noreply.github.com>

--- das/accounts/signals.py
+++ das/accounts/signals.py
@@ -6,9 +6,19 @@ from oauth2_provider.models import AccessToken
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from accounts.models import User
+
 
 @receiver(post_save, sender=AccessToken, dispatch_uid="record_last_login")
 def record_login(sender, instance, created, **kwargs):
     if created:
         instance.user.last_login = datetime.now(tz=pytz.utc)
         instance.user.save()
+
+
+@receiver(post_save, sender=User, dispatch_uid="user_linked_subject_name")
+def update_linked_subject_name(sender, instance, created, **kwargs):
+    if hasattr(instance, "linked_subject"):
+        full_name = instance.get_full_name()
+        instance.linked_subject.name = full_name if full_name else instance.username
+        instance.linked_subject.save(update_fields=["name"])
