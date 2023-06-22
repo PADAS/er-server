609a393d6a7f1d8fc1443373daeb8ccc1a608964
Chris Jones <41801350+chrisj-er@users.noreply.github.com>
2023-06-22 15:44:22 -0700

    Update bastion_server.tf to use ubuntu-2004-lts

--- terraform/bastion_server.tf
+++ terraform/bastion_server.tf
@@ -15,7 +15,7 @@ resource "tls_private_key" "bastion_server" {
 data "google_compute_image" "ubuntu" {
   provider = google
 
-  family  = "ubuntu-1804-lts"
+  family  = "ubuntu-2004-lts"
   project = "ubuntu-os-cloud"
 }
 
