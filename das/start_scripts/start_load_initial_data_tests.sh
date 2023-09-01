66e947c547781770383fdb5ad80be5e12841f3ff
Chris Jones <41801350+chrisj-er@users.noreply.github.com>
2023-09-01 12:45:08 -0700

    remove migrate command in start_load_initial_data_tests.sh

--- das/start_scripts/start_load_initial_data_tests.sh
+++ das/start_scripts/start_load_initial_data_tests.sh
@@ -4,5 +4,7 @@ wait_for $DB_HOST $DB_PORT
 
 . $(dirname "$0")/django_common_startup.sh
 
-python3 manage.py migrate
+# can't run a migration here, because the API pod coming up is already performing a migration
+# maybe we should wait for that to finish. Could loop in showmigrations until all migrations are done.
+#python3 manage.py migrate
 python3 manage.py loaddata initial_admin_tests initial_groups initial_eventdata initial_dev_map initial_features initial_tilelayers event_data_model
