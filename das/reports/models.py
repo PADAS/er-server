a3998533585606e3c6693223b82e3bcef6622387
Francisco Suárez <tarkin88@users.noreply.github.com>
2023-09-15 10:06:09 -0600

    ERA-8811 Add tenant id in reports models (#2728)
    * feat: reports models tenant awareness
    
    * fix: address comment

--- das/reports/models.py
+++ das/reports/models.py
@@ -1,33 +1,47 @@
+from django_multitenant.fields import TenantForeignKey
+from django_multitenant.mixins import TenantModelMixin
+
 from django.db import models
 
 from core.models import TimestampedModel, UUIDModel
+from core.models.core import DASTenant
 
 
-class SourceProviderEvent(TimestampedModel, UUIDModel):
-    source_provider = models.ForeignKey(
+class SourceProviderEvent(TenantModelMixin, TimestampedModel, UUIDModel):
+    source_provider = TenantForeignKey(
         "observations.SourceProvider",
         on_delete=models.CASCADE,
         related_name="events_reached_threshold",
         related_query_name="event_reached_threshold",
     )
-    event = models.ForeignKey(
+    event = TenantForeignKey(
         "activity.Event",
         on_delete=models.CASCADE,
         related_name="sources_provider_event",
         related_query_name="source_provider_event",
     )
+    das_tenant = models.ForeignKey(DASTenant, on_delete=models.CASCADE, blank=True, null=True)
+    tenant_id = "das_tenant_id"
+
+    class Meta:
+        unique_together = ["id", "das_tenant"]
 
 
-class SourceEvent(TimestampedModel, UUIDModel):
-    source = models.ForeignKey(
+class SourceEvent(TenantModelMixin, TimestampedModel, UUIDModel):
+    source = TenantForeignKey(
         "observations.Source",
         on_delete=models.CASCADE,
         related_name="events_reached_threshold",
         related_query_name="event_reached_threshold",
     )
-    event = models.ForeignKey(
+    event = TenantForeignKey(
         "activity.Event",
         on_delete=models.CASCADE,
         related_name="sources_event",
         related_query_name="source_event",
     )
+    das_tenant = models.ForeignKey(DASTenant, on_delete=models.CASCADE, blank=True, null=True)
+    tenant_id = "das_tenant_id"
+
+    class Meta:
+        unique_together = ["id", "das_tenant"]
