60034591c7e73411b73290da8b7743e98dc69d74
Chris Jones <41801350+chrisj-er@users.noreply.github.com>
2023-09-19 09:39:57 -0700

    revert unittest runner changes until we resolve general unit test order flakiness

--- das/test_scripts/run_unittests.sh
+++ das/test_scripts/run_unittests.sh
@@ -8,7 +8,7 @@ function echo_b() {
 }
 
 function review_results() {
-  if grep -i 'failures="0"' /testresults/result_suite_*.xml; then
+  if grep -i 'failures="0"' /testresults/result.xml; then
     echo "Suite executed successfully"
   else
     exit 1
@@ -17,25 +17,43 @@ function review_results() {
 
 function run_test_suite_one() {
   echo_b "Running test suite one...";
-  pytest --reuse-db --junitxml=/testresults/result_suite_one.xml --maxfail=15 accounts/tests mapping/tests reports/tests rt_api/tests tracking/tests
+  pytest --reuse-db --junitxml=/testresults/result.xml --maxfail=15 accounts/tests
+  review_results
+  pytest --reuse-db --junitxml=/testresults/result.xml --maxfail=15 mapping/tests
+  review_results
+  pytest --reuse-db --junitxml=/testresults/result.xml --maxfail=15 reports/tests
+  review_results
+  pytest --reuse-db --junitxml=/testresults/result.xml --maxfail=15 rt_api/tests
+  review_results
+  pytest --reuse-db --junitxml=/testresults/result.xml --maxfail=15 tracking/tests
   review_results
 }
 
 function run_test_suite_two() {
   echo_b "Running test suite two...";
-  pytest --reuse-db --junitxml=/testresults/result_suite_two.xml --maxfail=15 activity/tests sensors/tests
+  pytest --reuse-db --junitxml=/testresults/result.xml --maxfail=15 activity/tests
+  review_results
+  pytest --reuse-db --junitxml=/testresults/result.xml --maxfail=15 sensors/tests
   review_results
 }
 
 function run_test_suite_three() {
   echo_b "Running test suite three...";
-  pytest --reuse-db --junitxml=/testresults/result_suite_three.xml --maxfail=15 analyzers/tests utils/tests
+  pytest --reuse-db --junitxml=/testresults/result.xml --maxfail=15 analyzers/tests
+  review_results
+  pytest --reuse-db --junitxml=/testresults/result.xml --maxfail=15 utils/tests
   review_results
 }
 
 function run_test_suite_four() {
   echo_b "Running test suite four...";
-  pytest --reuse-db --junitxml=/testresults/result_suite_four.xml --maxfail=15 choices/tests das_server/tests observations/tests core/tests
+  pytest --reuse-db --junitxml=/testresults/result.xml --maxfail=15 choices/tests
+  review_results
+  pytest --reuse-db --junitxml=/testresults/result.xml --maxfail=15 das_server/tests
+  review_results
+  pytest --reuse-db --junitxml=/testresults/result.xml --maxfail=15 observations/tests
+  review_results
+  pytest --reuse-db --junitxml=/testresults/result.xml --maxfail=15 core/tests
   review_results
 }
 
@@ -54,7 +72,7 @@ echo "${CIRCLE_NODE_TOTAL}"
 echo "${CIRCLE_NODE_INDEX}"
 
 IS_OAUTH2_PROVIDER_MIGRATION=true python3 manage.py migrate
-IS_OAUTH2_PROVIDER_MIGRATION=true pytest --reuse-db --create-db --maxfail=15  core/tests/test_utils.py
+IS_OAUTH2_PROVIDER_MIGRATION=true pytest --reuse-db --create-db --junitxml=/testresults/result.xml --maxfail=15  core/tests/test_utils.py
 
 # Execute based on number of Circle CI nodes, and which Circle CI node is running
 case "$CIRCLE_NODE_TOTAL" in
