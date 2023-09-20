e223fb6cb8193123f936f1982e01a03c7b1caee1
Chris Jones <41801350+chrisj-er@users.noreply.github.com>
2023-09-20 10:16:48 -0700

    save then restore the current_tenant when using the five_tenant fixture

--- das/core/tests/conftest.py
+++ das/core/tests/conftest.py
@@ -1,8 +1,10 @@
 import pytest
 
 from factories import TenantFactory
-
+from django_multitenant.utils import set_current_tenant, get_current_tenant
 
 @pytest.fixture
 def five_tenants():
-    return TenantFactory.create_batch(5)
+    previous_tenant = get_current_tenant()
+    yield TenantFactory.create_batch(5)
+    set_current_tenant(previous_tenant)
