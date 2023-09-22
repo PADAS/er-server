c6322da67b77212956c0d4c6c640a380da3b1716
Diego Guzmán <diegog@earthranger.com>
2023-09-22 17:01:21 -0600

    ERA-8668: [fix] Remove tenant awareness from Celery workers (#2742)

--- das/das_server/tasks.py
+++ das/das_server/tasks.py
@@ -1,10 +1,12 @@
 import logging
 
+import redis
+from celery_once import QueueOnce
+
 from django.conf import settings
 from django.core.management import call_command
 
 from das_server import celery
-from utils.tenant.cache import MultitenantRedisClient
 from utils.tenant.celery import OverAllTenantTask
 
 logger = logging.getLogger(__name__)
@@ -19,11 +21,11 @@ def publish_daily_site_metrics():
 CELERYBEAT_PULSE_SENTINEL_KEY = "celerybeat-pulse-sentinel"
 
 
-@celery.app.task(base=OverAllTenantTask, once={"graceful": True})
+@celery.app.task(base=QueueOnce, once={"graceful": True})
 def celerybeat_pulse():
     """
     Set a sentinel key to expire in 120 seconds.
     :return: None
     """
-    redis_client = MultitenantRedisClient(settings.CELERY_BROKER_URL)
+    redis_client = redis.from_url(settings.CELERY_BROKER_URL)
     redis_client.setex(CELERYBEAT_PULSE_SENTINEL_KEY, 120, "n/a")
--- das/rt_api/client.py
+++ das/rt_api/client.py
@@ -5,6 +5,7 @@ import signal
 import socket
 
 import pytz
+import redis
 from psycopg2.extras import DateTimeTZRange
 
 from django.conf import settings
@@ -12,10 +13,9 @@ from django.contrib.gis.geos import MultiPolygon, Polygon
 
 from observations.models import SocketClient, UserSession
 from utils import json
-from utils.tenant.cache import MultitenantRedisClient
 
 logger = logging.getLogger(__name__)
-redis_client = MultitenantRedisClient(settings.REALTIME_BROKER_URL)
+redis_client = redis.from_url(settings.REALTIME_BROKER_URL)
 
 
 EXPIRED_CLIENT_TRACES_LIST = "rt_api.expired_traces"
