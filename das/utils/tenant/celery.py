c8e4955532503db82094b1232d774d7fc4c5b873
Chris Jones <41801350+chrisj-er@users.noreply.github.com>
2023-09-25 22:25:48 -0700

    fix: calling threading.local() gives us a new local thread dict for each call. Instead, use a module level variable to hold it (#2756)
    fix: update our TenantTaskMixin to include marking the current domain in kwargs when a caller uses apply or apply_async.
    fix: iterate over all tenants found in DASTenant until we have an API from TMS
    
    Co-authored-by: “Chris <“chrisj@earthranger.com”>

--- das/utils/tenant/celery.py
+++ das/utils/tenant/celery.py
@@ -3,9 +3,9 @@ import logging
 from celery import Task
 from celery_once import QueueOnce
 
-from django.conf import settings
-
+from core.models import DASTenant
 from utils.features import features
+from utils.tenant import get_tenant_settings
 from utils.tenant.managers import TenantContextManager
 
 logger = logging.getLogger(__name__)
@@ -24,6 +24,20 @@ class TenantTaskMixin:
         else:
             return super().__call__(*args, **kwargs)
 
+    def apply(self, args=None, kwargs=None, *arg, **kw):
+        kwargs = kwargs or {}
+        if "domain" not in kwargs:
+            kwargs["domain"] = get_tenant_settings().domain
+
+        return super().apply(args, kwargs, *arg, **kw)
+
+    def apply_async(self, args=None, kwargs=None, *arg, **kw):
+        kwargs = kwargs or {}
+        if "domain" not in kwargs:
+            kwargs["domain"] = get_tenant_settings().domain
+
+        return super().apply_async(args, kwargs, *arg, **kw)
+
 
 class TenantTask(TenantTaskMixin, Task):
     pass
@@ -36,12 +50,10 @@ class TenantQueueOnceTask(TenantTaskMixin, QueueOnce):
 class OverAllTenantTask(QueueOnce):
     def __call__(self, *args, **kwargs):
         if features.tms.is_on():
-            domain = settings.SERVER_FQDN
-            if domain:
-                with TenantContextManager(domain):
-                    logger.info("Running: %s for Tenant domain: %s" % (self.name, domain))
+            # TODO: update with the TMS call to get all tenants
+            for tenant in DASTenant.objects.all():
+                with TenantContextManager(tenant.domain):
+                    logger.info("Running: %s for Tenant domain: %s" % (self.name, tenant.domain))
                     self.run(*args, **kwargs)
-            else:
-                logger.warning("Task %s will not run because no tenants were found." % self.name)
         else:
             self.run(*args, **kwargs)
