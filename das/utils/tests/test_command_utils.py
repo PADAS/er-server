5a07a3fcac478544ba86a89276ce23bf52b2c8da
Mariano Martinez Grasso <marianom@earthranger.com>
2023-09-28 10:53:48 -0300

    test: update tests for tenant-aware commands

--- das/utils/tests/test_command_utils.py
+++ das/utils/tests/test_command_utils.py
@@ -28,10 +28,10 @@ class TestTenantBaseCommand:
         call_command("dummy_tenant_command", tenant_domain=das_tenant.domain)
         captured = capsys.readouterr()
 
-        assert set_current_tenant_mock.assert_called_once
+        set_current_tenant_mock.assert_called_once()
         set_current_tenant_mock.assert_called_with(tenant=das_tenant)
-        assert tenant_data_mock.return_value.get_tenant_data.assert_called_once
-        assert set_tenant_settings_mock.assert_called_once
+        tenant_data_mock.return_value.get_tenant_data.assert_called_once()
+        set_tenant_settings_mock.assert_called_once()
         set_tenant_settings_mock.assert_called_with(value=tenant_response)
         assert "dummy tenant-aware command executed." in captured.out
 
@@ -57,24 +57,42 @@ class TestTenantBaseCommand:
         captured = capsys.readouterr()
         expected_extra_details = f"Executing command with tenant id {das_tenant.id}..."
 
-        assert get_tenant_settings_mock.assert_called_once
+        get_tenant_settings_mock.assert_called_once()
         assert expected_extra_details in captured.out
         assert "dummy tenant-aware command executed." in captured.out
 
     @patch("utils.tenant.commands.TenantData")
     @patch("utils.tenant.commands.set_current_tenant")
     @patch("utils.tenant.commands.set_tenant_settings")
-    def test_tenant_domain_is_required(
-        self, set_tenant_settings_mock, set_current_tenant_mock, das_tenant, tenant_response
+    @patch("utils.tenant.commands.get_tenant_settings")
+    def test_tenant_is_required(
+        self, get_tenant_settings_mock, set_tenant_settings_mock, set_current_tenant_mock, das_tenant, tenant_response
     ):
         with pytest.raises(CommandError):
             call_command("dummy_tenant_command")
 
+        get_tenant_settings_mock.assert_called_once()
+
     @patch("utils.tenant.commands.TenantData")
     @patch("utils.tenant.commands.set_current_tenant")
     @patch("utils.tenant.commands.set_tenant_settings")
-    def test_raise_error_on_tenant_not_found(
+    def test_raise_error_on_tenant_domain_not_found(
         self, set_tenant_settings_mock, set_current_tenant_mock, das_tenant, tenant_response
     ):
         with pytest.raises(CommandError):
             call_command("dummy_tenant_command", tenant_domain="notatenantdomain")
+
+    @patch("utils.tenant.commands.set_current_tenant")
+    @patch("utils.tenant.commands.set_tenant_settings")
+    @patch("utils.tenant.commands.get_tenant_settings")
+    def test_get_tenant_from_thread(
+        self, get_tenant_settings_mock, set_tenant_settings_mock, set_current_tenant_mock, das_tenant, tenant
+    ):
+        get_tenant_settings_mock.return_value = tenant
+
+        call_command("dummy_tenant_command")
+
+        get_tenant_settings_mock.assert_called_once()
+        assert not set_tenant_settings_mock.called
+        set_current_tenant_mock.assert_called_once()
+        set_current_tenant_mock.assert_called_with(tenant=das_tenant)
