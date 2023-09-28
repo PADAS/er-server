7b5fd634676f6ac1e142765691b5d926b133d4a1
Francisco Suárez <tarkin88@users.noreply.github.com>
2023-09-28 11:15:10 -0600

    ERA-8883 HierarchyModel tenant aware, replace  with new das_tenant+id column (#2757)
    * feat: make Hierachy Models tenant awere and apply into accounts
    
    * fix: broken tests cause use call_commands
    
    * feat: Add migration for alter PK of activity tables
    
    * feat: Rename function
    
    * fix? fix collission between migrations at observations
    
    * feat: Add update_primary_key.py implementation
    
    * test: Add regenerate_primary_keys command
    
    * feat: Finish regenerate_primary_key command
    
    * fix: mix apporachs to handle the migrations issues with call_commands
    
    * feat: Add 0151 migration in activity app
    
    * feat: Move creation of event types to a new migration
    
    * feat: Add __all__ in analyzers models
    
    * feat: Add migrations for regenerate the PK of activity and analyzers app
    
    * fix: use das_tenant_monkeypatch fixture in BaseAPITest
    
    * feat: Add migration for regenerate PK of mapping
    
    * feat: Add migration in observation app
    
    * feat: Finish report, tracking and usercontent apps
    
    * feat: Add __all__ in tracking models
    
    * feat: Add SocketClient and UserSession
    
    * feat: Add edge cases for tables
    
    * feat: Add migration for choices and users app
    
    * feat: Add migration for regenerate PK of choice models
    
    * ERA-8891 Update rt_api client class to support tenants (#2747)
    
    feat: add tenant id and domain to ClientData, refactor ClientData to use a frozen dataclass
    feat: add the TenantContextManager to the authentication stage in the rt_api.
    fix: update some unit tests, waiting for the subject tenant_id+id index to be fixed before this test will pass
    fix: remove the need to start the rt_api wsgi server with the default tenant
    feat: for me, added a DjangoSettingsClient() so that I can load Django Settings as TenantSettings, just for testing.
    
    Co-authored-by: “Chris <“chrisj@earthranger.com”>
    
    * fix: update migrations to call command approach
    
    * fix: migrations sequences and bad dependencies
    
    * fix: update dependeices
    
    * fix: migrate left problematics
    
    * fix: missing migrations with primarykey rework
    
    * feat: Add migration for regenerate PK in core
    
    * feat: Rename migration that regenerate PK in core app
    
    * feat: generate key for core models
    
    * fix: remove duplicated migration
    
    * fix: add missing unique constraint at Event model
    
    * fix: add unique together to missing models
    
    * fix: remove all regenerate pkeys to avoid issues
    
    * fix: update signals to use tenant from FQDN just for migrations
    
    * chore: enhance signals conditions
    
    ---------
    
    Co-authored-by: Juan Herrera <juanh@earthranger.com>
    Co-authored-by: Azarael Itai Romero Ramirez <azar@earthranger.com>
    Co-authored-by: Chris Jones <chrisj@earthranger.com>
    Co-authored-by: Chris Jones <41801350+chrisj-er@users.noreply.github.com>
    Co-authored-by: “Chris <“chrisj@earthranger.com”>
    Co-authored-by: JoshuaVulcan <38018017+JoshuaVulcan@users.noreply.github.com>

--- das/accounts/migrations/0014_reported_by_permission_set.py
+++ das/accounts/migrations/0014_reported_by_permission_set.py
@@ -1,19 +1,11 @@
 from django.db import migrations
-from django.core.management import call_command
-import utils.models
-
-
-def create_reported_by_permission_set(apps, schema_editor):
-    utils.models.migrate_permissions(apps)
-    call_command('loaddata', 'reported_by_permission_set')
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('accounts', '0013_profiles'),
+        ("accounts", "0013_profiles"),
     ]
 
     operations = [
-        migrations.RunPython(create_reported_by_permission_set),
+        # migrations.RunPython(create_reported_by_permission_set),   Move to accounts 0031
     ]
