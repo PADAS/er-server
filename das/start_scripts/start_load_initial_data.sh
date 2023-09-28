9b3e2f30b1715c3758d37fea4073151bedb7c1f5
Mariano Martinez Grasso <marianom@earthranger.com>
2023-09-28 15:20:31 -0300

    refactor: require the tenant domain in scripts calling loaddata_with_tenant

--- das/start_scripts/start_load_initial_data.sh
+++ das/start_scripts/start_load_initial_data.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
+# Check arguments
+if [ $# -ne 1 ]; then
+  echo "Usage: ./start_load_initial_data_tests.sh <domain>"
+  exit 1
+fi
+
+TENANT_DOMAIN=$1
+
 . $(dirname "$0")/wait_for.sh
 wait_for $DB_HOST $DB_PORT
 
 . $(dirname "$0")/django_common_startup.sh
 
 python3 manage.py create_tenant
-python3 manage.py loaddata_with_tenant initial_admin initial_groups initial_eventdata initial_dev_map initial_features initial_tilelayers event_data_model
+python3 manage.py loaddata_with_tenant --tenant_domain "$TENANT_DOMAIN" initial_admin initial_groups initial_eventdata initial_dev_map initial_features initial_tilelayers event_data_model
