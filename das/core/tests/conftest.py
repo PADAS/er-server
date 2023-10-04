02ee2fda84b3776faf11d1c697257097ccabbef6
Azarael-Romero <93936271+Azarael-Romero@users.noreply.github.com>
2023-10-04 14:15:52 -0600

    ERA-8921: Fix broken unit test in utils, reports, core and das_server apps (#2770)
    * fix: utils, reports, core and das_server UT

--- das/core/tests/conftest.py
+++ das/core/tests/conftest.py
@@ -1,10 +1,17 @@
 import pytest
+from django_multitenant.utils import get_current_tenant, set_current_tenant
+from factory import Faker
 
 from factories import TenantFactory
-from django_multitenant.utils import set_current_tenant, get_current_tenant
+
 
 @pytest.fixture
 def five_tenants():
     previous_tenant = get_current_tenant()
-    yield TenantFactory.create_batch(5)
+
+    yield TenantFactory.create_batch(
+        size=5,
+        id=Faker("uuid4"),
+        domain=Faker("domain_name"),
+    )
     set_current_tenant(previous_tenant)
