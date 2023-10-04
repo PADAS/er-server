19adf7ce1457cb582e3db206790b179631d86d02
Mariano Martinez Grasso <marianom@earthranger.com>
2023-10-04 10:25:10 -0300

    feat: add a single function to set both Tentant and DASTenant in thread

--- das/utils/tenant/__init__.py
+++ das/utils/tenant/__init__.py
@@ -2,7 +2,7 @@ from utils.tenant.builder import DjangoSettingsTenantBuilder
 from utils.tenant.cache import make_cache_key
 from utils.tenant.dataclass import Tenant
 from utils.tenant.exceptions import TenantNotFoundException
-from utils.tenant.thread import get_tenant_settings, set_tenant_settings
+from utils.tenant.thread import get_tenant_settings, set_tenant, set_tenant_settings
 
 __all__ = (
     "DjangoSettingsTenantBuilder",
@@ -11,4 +11,5 @@ __all__ = (
     "get_tenant_settings",
     "make_cache_key",
     "set_tenant_settings",
+    "set_tenant",
 )
