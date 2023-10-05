154d022cdf9828f186d2b60d1f72e91d42f36ef0
Azarael-Romero <93936271+Azarael-Romero@users.noreply.github.com>
2023-10-05 10:01:16 -0600

    fix: missing decorator (#2772)

--- das/das_server/tests/test_kml.py
+++ das/das_server/tests/test_kml.py
@@ -1,3 +1,5 @@
+import pytest
+
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
@@ -6,13 +8,15 @@ from observations.kmlutils import get_kml_access_token
 User = get_user_model()
 
 
+@pytest.mark.usefixtures("tenant_settings", "das_tenant_monkeypatch")
 class UserModelTest(TestCase):
     password = User.objects.make_random_password()
     user_const = dict(last_name="last", first_name="first")
 
     def test_get_kml_key(self):
         user, _ = User.objects.get_or_create(
-            username="User", defaults=dict(email="user4@test.com", password=self.password, **self.user_const)
+            username="User",
+            defaults=dict(email="user4@test.com", password=self.password, **self.user_const),
         )
 
         token = get_kml_access_token(user)
