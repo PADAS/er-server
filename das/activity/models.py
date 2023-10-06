e9b6b3388432588373d7f0023f52577dc2c6197b
Mariano Martinez Grasso <marianom@earthranger.com>
2023-10-05 23:39:11 -0300

    feat: Add index on tenant + serial number

--- das/activity/models.py
+++ das/activity/models.py
@@ -838,6 +838,7 @@ class Event(TenantModelMixin, SerialNumberModelMixin, RevisionMixin, Timestamped
             models.Index(fields=["das_tenant", "created_at"]),
             models.Index(fields=["das_tenant", "updated_at"]),
             models.Index(fields=["das_tenant", "event_time"]),
+            models.Index(fields=["das_tenant", "serial_number"]),
         ]
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
