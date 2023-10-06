0614534f76f9c355447ea981f6c07fc8042cadf1
Mariano Martinez Grasso <marianom@earthranger.com>
2023-10-06 01:02:17 -0300

    test: coverage for serial numbers in events

--- das/activity/tests/test_events.py
+++ das/activity/tests/test_events.py
@@ -3882,3 +3882,28 @@ class TestEventView2(BaseTestToolMixin):
         assert response.status_code == 201
         assert "id" in response.data
         assert len(response.data.keys()) == 1
+
+
+@pytest.mark.django_db
+@pytest.mark.usefixtures("das_tenant_monkeypatch")
+class TestSerialNumberOnEventModel:
+    def test_serial_number_added_on_save(self):
+        event_1 = Event(title="Test Event 1")
+        event_1.save()
+        event_2 = Event(title="Test Event 2")
+        event_2.save()
+        event_3 = Event(title="Test Event 3")
+        event_3.save()
+
+        assert event_1.serial_number == 1
+        assert event_2.serial_number == 2
+        assert event_3.serial_number == 3
+
+    def test_serial_number_added_on_create(self):
+        event_1 = Event.objects.create(title="Test Event 1")
+        event_2 = Event.objects.create(title="Test Event 2")
+        event_3 = Event.objects.create(title="Test Event 3")
+
+        assert event_1.serial_number == 1
+        assert event_2.serial_number == 2
+        assert event_3.serial_number == 3
