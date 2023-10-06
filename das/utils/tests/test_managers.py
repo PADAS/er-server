a9103e14695e97747d9604d7dc0938f22eb2d5c8
Mariano Martinez Grasso <marianom@earthranger.com>
2023-10-05 22:30:37 -0300

    test: add a fixme comment in a failing test to be reviewed

--- das/utils/tests/test_managers.py
+++ das/utils/tests/test_managers.py
@@ -7,7 +7,7 @@ from utils.tenant.thread import TENANT_DEFAULT_KEY, _get_local_thread
 
 
 @pytest.mark.django_db
-@pytest.mark.skipif(features.tms.is_on() is False, reason="TMS feature flag is off")
+@pytest.mark.skipif(not features.tms.is_on(), reason="TMS feature flag is off")
 class TestTenantContextManager:
     def test_tenant_context_manager(self, memory_store_client_mock):
         main_thread = _get_local_thread()
@@ -16,6 +16,8 @@ class TestTenantContextManager:
             assert TENANT_DEFAULT_KEY in main_thread.__dict__.keys()
             assert isinstance(get_tenant_settings(), Tenant)
 
+        # FixMe?: The test assumes that there was no previous Tenant set in the thread
+        # It fails when there was a previous Tenant set
         assert TENANT_DEFAULT_KEY not in main_thread.__dict__.keys()
 
     @pytest.mark.parametrize("domain", ["", None])
