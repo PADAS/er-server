f09df6a5f916a6faa35950f9083f4052678a6d4c
Diego Guzmán <diegog@earthranger.com>
2023-10-11 16:28:46 -0600

    ERA-8557: update disconnect test (#2784)

--- das/rt_api/tests/test_rt_server.py
+++ das/rt_api/tests/test_rt_server.py
@@ -25,18 +25,19 @@ class TestRTServer(TestCase):
     @mock.patch("redis.Redis", MockRedis)
     def test_cleanup_disconnected_clients(self):
         manager_mock = mock.MagicMock()
+        manager_mock.sid_from_eio_sid.return_value = "e8ef807c2bbe4418b32de45786d82a52"
+        socket_mock = mock.MagicMock()
         sios = server.Server(client_manager=manager_mock)
+        sios.eio.sockets = {"e8ef807c2bbe4418b32de45786d82a52": socket_mock}
         handler = mock.MagicMock()
         sios.on("connect", handler)
         sios._handle_eio_connect("e8ef807c2bbe4418b32de45786d82a52", "sid")
         assert sios.handlers["/"]["connect"] is handler
-        assert sios.environ == {"e8ef807c2bbe4418b32de45786d82a52": "sid"}
         self.add_client()
         client.redis_client.hset(client.EXPIRED_CLIENT_TRACES_LIST, "e8ef807c2bbe4418b32de45786d82a52", "message")
 
         cleanup_disconnected_clients(sios)
 
-        assert sios.environ == {}
         manager_mock.disconnect.assert_called_once_with(
             "e8ef807c2bbe4418b32de45786d82a52", namespace="/", ignore_queue=True
         )
