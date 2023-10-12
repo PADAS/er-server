2eaae65109aef52831a81b2506babee7c8bcffa9
Chris Jones <41801350+chrisj-er@users.noreply.github.com>
2023-10-11 20:56:01 -0700

    Era 8941 CI Pipeline fails on terraform_apply_feature_branch (#2786)
    * ERA-8941 CI Pipeline fails on terraform_apply_feature_branch
    fix: test to see if database has data in it, if it does, exit out of the script used by the feature branch.
    fix: update command create_tenant to take a tenant domain name and update start_load_initial_data.sh script to use it.
    
    * fix: tenant_domain in GNUmakefile
    
    * fix: set a startupProbe for the API container. Specifically tuned to wait for all migrations to complete. It will poll for 10 minutes.
    
    * fix: update how the tenant name is formed in database_init
    
    * fix: re-enable commented out code
    
    ---------
    
    Co-authored-by: “Chris <“chrisj@earthranger.com”>

--- das/start_scripts/start_load_initial_data.sh
+++ das/start_scripts/start_load_initial_data.sh
@@ -12,5 +12,14 @@ wait_for $DB_HOST $DB_PORT
 
 . $(dirname "$0")/django_common_startup.sh
 
-python3 manage.py create_tenant
+
+
+python3 manage.py create_tenant $TENANT_DOMAIN
+
+if python3 manage.py runscript tenant_has_initial_data --script-args "$TENANT_DOMAIN" ; then
+  echo "There is data in the db already, exiting out"
+  exit 0
+fi
+echo "Load $TENANT_DOMAIN initial data"
+
 python3 manage.py loaddata_with_tenant --tenant_domain "$TENANT_DOMAIN" initial_admin initial_groups initial_eventdata initial_dev_map initial_features initial_tilelayers event_data_model
