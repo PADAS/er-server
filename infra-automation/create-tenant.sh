cce81f565a9acb1c9fd852cfe266696fe7eba126
Azarael-Romero <93936271+Azarael-Romero@users.noreply.github.com>
2023-10-20 11:34:53 -0600

    ERA-8813: ER CLI create tenant document with cluster_namespace and cluster_name values (#2789)
    * feat: send cluster namespace and cluster name to er create-tenant-document command
    
    * feat: send cluster namespace and cluster name to er create-tenant-document command
    
    * fix: wrong variable name
    
    * fix: param name
    
    * fix: makefile
    
    * fix: makefile
    
    * fix
    
    * fix: remove fixed version for tms-services
    
    * fix
    
    * add echo
    
    * add tfvars_environment to install_er_cli
    
    * fix
    
    * fix
    
    * fix
    
    * fix: move er-cli step
    
    * fix: move er-cli step
    
    * fix: move er-cli step
    
    * fix: move er-cli step
    
    * fix
    
    * feat: simplify where install_er_cli is called
    
    * fix
    
    * remove echoes
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * remove echoes
    
    ---------
    
    Co-authored-by: “Chris <“chrisj@earthranger.com”>

--- infra-automation/create-tenant.sh
+++ infra-automation/create-tenant.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 declare current_branch=$1
+declare cluster_namespace=$2
+declare cluster_name=$3
 
-er create-tenant-document $current_branch --subdomain $(echo $current_branch | tr "[:upper:]." "[:lower:]_") > tenant_data.json
+er create-tenant-document $current_branch --cluster-namespace $cluster_namespace --cluster-name $cluster_name \
+    --subdomain $(echo $current_branch | tr "[:upper:]." "[:lower:]_") > tenant_data.json
+
 test -f tenant_data.json && er create-tenants < tenant_data.json
