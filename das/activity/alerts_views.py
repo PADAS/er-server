9943fcdcbb086588efae0d769ddd1f565f2fbefd
“Chris <chrisj@earthranger.com>
2023-11-17 13:22:34 +0000

    ERA-8901 MT - Endpoint activity/events/categories/{eventCategory} is throwing 404 fix: apply our DRF view fix moving queryset initialization from a class variabled into a function to be called on each request.

--- das/activity/alerts_views.py
+++ das/activity/alerts_views.py
@@ -1,32 +1,32 @@
 import logging
-from rest_framework import generics, status, response, permissions
 
-from activity.models import AlertRule, NotificationMethod, EventType
+from rest_framework import generics, permissions, response, status
 
-from activity.serializers import EventTypeSerializer, AlertRuleSerializer, NotificationMethodSerializer
-
-from activity.permissions import EventCategoryPermissions, IsOwner
 from activity.alerting.businessrules import render_aggregate_event_variables
-
+from activity.models import AlertRule, EventType, NotificationMethod
+from activity.permissions import EventCategoryPermissions, IsOwner
+from activity.serializers import (
+    AlertRuleSerializer,
+    EventTypeSerializer,
+    NotificationMethodSerializer,
+)
 from utils.drf import StandardResultsSetPagination
 from utils.json import parse_bool
 from utils.schema_utils import get_schema_renderer_method
+
 logger = logging.getLogger(__name__)
 
 # Views for Advanced Alert Functionality.
 
 
 class EventAlertConditionsListView(generics.ListAPIView):
-
     permission_classes = (EventCategoryPermissions,)
     serializer_class = EventTypeSerializer
 
-    queryset = EventType.objects.all()
-
     def get_queryset(self):
-        qs = super().get_queryset()
+        qs = EventType.objects.all()
 
-        event_types = self.request.query_params.get('event_type', '')
+        event_types = self.request.query_params.get("event_type", "")
         if event_types:
             qs = qs.by_event_type(event_types)
 
@@ -44,17 +44,16 @@ class EventAlertConditionsListView(generics.ListAPIView):
         return qs
 
     def get(self, *args, **kwargs):
-        only_common_factors = parse_bool(
-            self.request.query_params.get('only_common_factors', False))
+        only_common_factors = parse_bool(self.request.query_params.get("only_common_factors", False))
 
         rules = render_aggregate_event_variables(
-            self.get_queryset(), only_common_factors=only_common_factors, user=self.request.user)
+            self.get_queryset(), only_common_factors=only_common_factors, user=self.request.user
+        )
 
         return response.Response(rules, status=status.HTTP_200_OK)
 
 
 class AlertRuleListView(generics.ListCreateAPIView):
-
     permission_classes = [permissions.DjangoModelPermissions & IsOwner]
 
     serializer_class = AlertRuleSerializer
@@ -62,14 +61,13 @@ class AlertRuleListView(generics.ListCreateAPIView):
     queryset = AlertRule.objects.none()  # Required for DjangoModelPermission
 
     def get_queryset(self):
-        return AlertRule.objects.filter(owner=self.request.user).order_by('ordernum', 'title')
+        return AlertRule.objects.filter(owner=self.request.user).order_by("ordernum", "title")
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 
 class AlertRuleView(generics.RetrieveUpdateDestroyAPIView):
-
     permission_classes = [permissions.DjangoModelPermissions & IsOwner]
 
     serializer_class = AlertRuleSerializer
@@ -77,7 +75,7 @@ class AlertRuleView(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = AlertRule.objects.all()
 
-    lookup_field = 'id'
+    lookup_field = "id"
 
     def get_queryset(self):
         return AlertRule.objects.filter(owner=self.request.user)
@@ -90,26 +88,24 @@ class AlertRuleView(generics.RetrieveUpdateDestroyAPIView):
 
 
 class NotificationMethodListView(generics.ListCreateAPIView):
-
     permission_classes = (IsOwner,)
     serializer_class = NotificationMethodSerializer
     pagination_class = StandardResultsSetPagination
 
     def get_queryset(self):
-        return NotificationMethod.objects.filter(owner=self.request.user).order_by('method')
+        return NotificationMethod.objects.filter(owner=self.request.user).order_by("method")
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 
 class NotificationMethodView(generics.RetrieveUpdateDestroyAPIView):
-
     permission_class = (IsOwner,)
     serializer_class = NotificationMethodSerializer
 
     queryset = NotificationMethod.objects.all()
 
-    lookup_field = 'id'
+    lookup_field = "id"
 
     def get_queryset(self):
         return NotificationMethod.objects.filter(owner=self.request.user)
