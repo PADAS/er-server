c09f2ce7eff75ff15b02ff8aba1d5d431b989bc0
Diego Guzmán <diegog@earthranger.com>
2023-11-17 07:38:19 -0500

    fix: update tenant domains on  core app config (#2830)

--- das/core/apps.py
+++ das/core/apps.py
@@ -7,3 +7,6 @@ class CoreConfig(AppConfig):
 
     def ready(self):
         import core.signals
+        from utils.tenant.domains import add_new_tenant_domains_to_settings
+
+        add_new_tenant_domains_to_settings()
