236939200d392f6fc4a2c5acbdd246f9f7941c29
Francisco Suárez <tarkin88@users.noreply.github.com>
2023-11-20 12:00:14 -0600

    fix: add CommonTenantManager (#2856)

--- das/tracking/models/er_track.py
+++ das/tracking/models/er_track.py
@@ -13,6 +13,7 @@ from core.models import TimestampedModel
 from core.models.core import DASTenant
 from observations.models import SourceProvider, SubjectType
 from utils.migrations.columns import default_tenant_id
+from utils.models import CommonTenantManager
 
 logger = logging.getLogger(__name__)
 
@@ -74,6 +75,7 @@ class SourceProviderConfiguration(TenantModelMixin, TimestampedModel):
     name_change_match_case = models.BooleanField(default=False, verbose_name="Match case")
     das_tenant = models.ForeignKey(DASTenant, on_delete=models.CASCADE, default=default_tenant_id)
     tenant_id = "das_tenant_id"
+    objects = CommonTenantManager()
 
     class Meta:
         verbose_name = "EarthRanger Track Configuration"
