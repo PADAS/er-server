ab75257c9e17390a3f87c7a1e1533f5cce63b20c
“Chris <chrisj@earthranger.com>
2024-05-17 08:15:29 -0700

    fix: don't 500 when validating a user pin and the username field is empty

--- das/accounts/mixins.py
+++ das/accounts/mixins.py
@@ -4,7 +4,6 @@ from itertools import chain
 from django_multitenant.fields import TenantForeignKey
 
 import django.db.models as models
-from django.apps import apps
 from django.contrib import auth
 from django.core.exceptions import PermissionDenied, ValidationError
 from django.db.models import UniqueConstraint
@@ -280,9 +279,10 @@ class UserFormValidatorMixin:
         return email
 
     def clean_pin(self):
-        User = apps.get_model(app_label="accounts", model_name="User")
-        pin = self.cleaned_data["pin"]
-        username = self.cleaned_data["username"]
+        User = auth.get_user_model()
+
+        pin = self.cleaned_data.get("pin")
+        username = self.cleaned_data.get("username")
 
         if not pin:
             return None
