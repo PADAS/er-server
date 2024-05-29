ce0ff66167c4cd06c848712d3f619f4f9e5d244d
“Chris <chrisj@earthranger.com>
2024-05-29 21:39:52 +0000

    ERA-9759 Firms plugin not running ERA-9241 Failed Parsing Firms Line fix: update the firms command to support a tenant. fix: if we don't have history, bring in yesterday's firms events too as Today's firms file might not have been made yet. fix: look to see if the data returned has HTML, if so it's because our token has expired fix: recover if we run into a parsing error and we have fetched data using the stored headers, which we have seen have occasional offset issues. We try again without the offset.

--- das/tracking/management/commands/firms.py
+++ das/tracking/management/commands/firms.py
@@ -1,9 +1,11 @@
 from django.core.management.base import BaseCommand
+
 from tracking.models import FirmsPlugin
+from utils.tenant.commands import TenantCommandMixin
 
 
-class Command(BaseCommand):
-    help = 'Run FIRMS plugins'
+class Command(TenantCommandMixin, BaseCommand):
+    help = "Run FIRMS plugins"
 
     def handle(self, *args, **options):
 
