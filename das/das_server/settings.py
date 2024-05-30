49325bcc254b4ea0555260bce369114d748c43a2
“Chris <chrisj@earthranger.com>
2024-05-30 19:17:02 +0000

    fix: adding terraform configuration of the memory store host and api key so it selects the correct prod or dev key and host.

--- das/das_server/settings.py
+++ das/das_server/settings.py
@@ -645,7 +645,7 @@ TMS_API = {
 
 MEMORY_STORE = {
     "CLIENT": env.str("MEMORY_STORE_CLIENT", "utils.persistent.RedisStorageReadOnly"),
-    "HOST": env.str("MEMORY_STORE_HOST", "redis-proxy.dev.pamdas.org"),
+    "HOST": env.str("MEMORY_STORE_HOST", ""),
     "PORT": env.int("MEMORY_STORE_PORT", 6379),
     "DATABASE": env.int("MEMORY_STORE_DATABASE", 0),
     "API_KEY": env.str("MEMORY_STORE_API_KEY", ""),
