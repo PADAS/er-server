092e5d1914e99202b05199335b37d27992b33aab
Azarael Itai Romero Ramirez <azar@earthranger.com>
2024-06-05 10:46:15 -0600

    fix: UT

--- das/observations/tests/test_repositories.py
+++ das/observations/tests/test_repositories.py
@@ -2,64 +2,22 @@ import uuid
 
 import pytest
 
-from observations.models import Observation
-from observations.services.repositories.observation import ReadObservationRepository
+from observations.domain.observation import ObservationData
+from observations.services.exceptions import ObservationDoesNotExist
+from observations.services.repositories import ReadObservationRepository
 
-repository = ReadObservationRepository()
-
-
-@pytest.mark.django_db
-def test_get_observations_queryset_get_all(five_observations) -> None:
-    qs = repository.get()
-
-    assert qs.count() == 5
-
-
-@pytest.mark.django_db
-def test_get_observations_queryset_with_simple_filters(five_observations) -> None:
-    observation = five_observations[1]
-
-    qs = repository.get(filter_fields={"id": observation.pk})
-
-    assert qs.count() == 1
-    assert observation.id == qs[0].id
-
-
-@pytest.mark.django_db
-def test_get_observations_queryset_with_q_filters(subject_source_with_observations) -> None:
-    subject_source, observation = subject_source_with_observations
-    since = subject_source.safe_assigned_range.lower
-    until = subject_source.safe_assigned_range.upper
-
-    filter_q = {"recorded_at__range": [since, until]}
-
-    qs = repository.get(filter_q_fields=filter_q)
-
-    assert qs[0].id == observation.id
-
-
-@pytest.mark.django_db
-def test_get_observations_queryset_with_exclude(five_observations) -> None:
-    excluded_observation = five_observations[0]
-
-    qs = repository.get(
-        exclude_fields={"id": excluded_observation.id},
-    )
-
-    assert qs.count() == 4
-    assert qs.filter(id=excluded_observation.id).count() == 0
+repository = ReadObservationRepository("database")
 
 
 @pytest.mark.django_db
 def test_get_observations_instance(observation) -> None:
-    obj = repository.get_by_id(id=observation.id)
+    observation_data = repository.get_by_id(id=observation.id)
 
-    assert isinstance(obj, Observation)
-    assert obj.id == observation.id
+    assert isinstance(observation_data, ObservationData)
+    assert observation_data.id == observation.id
 
 
 @pytest.mark.django_db
 def test_get_observations_instance_does_not_exists() -> None:
-    obj = repository.get_by_id(id=uuid.uuid4())
-
-    assert obj is None
+    with pytest.raises(ObservationDoesNotExist):
+        repository.get_by_id(id=uuid.uuid4())
