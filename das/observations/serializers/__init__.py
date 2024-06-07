fb090a592fb9fd1b9580b531217ce98f47eda84d
Azarael Itai Romero Ramirez <azar@earthranger.com>
2024-06-07 11:42:53 -0600

    fix

--- das/observations/serializers/__init__.py
+++ das/observations/serializers/__init__.py
@@ -690,8 +690,8 @@ class SubjectTrackSerializer(rest_framework.serializers.BaseSerializer):
             # on a Source level
             for source in subject_linked_sources:
                 data = get_observation_coordinates_and_times_by_subject_id_and_source_id(subject.id, source.id)
-                coordinates = data["coordinates"]
-                times = data["times"]
+                coordinates.append(data["coordinates"])
+                times.append(data["times"])
         else:
             coordinates, times = subject.get_track(user, tracks_since, tracks_until, tracks_limit)
 
