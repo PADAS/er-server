448719c917b4e03f3aebe9b33564f75fa1db70e3
Francisco Suarez <franksg88@gmail.com>
2024-06-07 16:02:37 -0600

    fix: use User model directly

--- das/observations/management/commands/remove_users_from_wrong_permission_set_view_tracks_last_16_days_delayed.py
+++ das/observations/management/commands/remove_users_from_wrong_permission_set_view_tracks_last_16_days_delayed.py
@@ -1,15 +1,12 @@
-from django.conf import settings
 from django.core.management import BaseCommand
 
-from accounts.models import PermissionSet
+from accounts.models import PermissionSet, User
 
 
 class Command(BaseCommand):
     help = "remove users from_wrong permission set view tracks last 16 days delayed"
 
     def handle(self, *args, **options):
-        User = settings.AUTH_USER_MODEL
-
         try:
             wrong_permission = PermissionSet.objects.get(name="View Tracks Last 16 Days Delayed")
         except PermissionSet.DoesNotExist:
