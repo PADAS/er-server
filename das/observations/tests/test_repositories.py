e46c2c4f1d1ac8117935d98a37e20bb4f7b12dfd
Azarael Itai Romero Ramirez <azar@earthranger.com>
2024-06-11 15:59:59 -0600

    fix: move services tests to a class and parametrize data source

--- das/observations/tests/test_repositories.py
+++ das/observations/tests/test_repositories.py
@@ -8,32 +8,27 @@ from observations.services.repositories import ReadObservationRepository
 
 
 @pytest.mark.django_db
-@pytest.mark.parametrize("source", ["database"])
-def test_get_observations_instance(observation, source) -> None:
-    repository = ReadObservationRepository(source)
-    observation_data = repository.get_by_id(id=observation.id)
+@pytest.mark.parametrize("data_source", ["database"])
+class TestRepositories:
+    def test_get_observations_instance(self, observation, data_source) -> None:
+        repository = ReadObservationRepository(data_source)
+        observation_data = repository.get_by_id(id=observation.id)
 
-    assert isinstance(observation_data, ObservationData)
-    assert observation_data.id == observation.id
+        assert isinstance(observation_data, ObservationData)
+        assert observation_data.id == observation.id
 
+    def test_get_observations_instance_does_not_exists(self, data_source) -> None:
+        repository = ReadObservationRepository(data_source)
 
-@pytest.mark.django_db
-@pytest.mark.parametrize("source", ["database"])
-def test_get_observations_instance_does_not_exists(source) -> None:
-    repository = ReadObservationRepository(source)
+        with pytest.raises(ObservationDoesNotExist):
+            repository.get_by_id(id=uuid.uuid4())
 
-    with pytest.raises(ObservationDoesNotExist):
-        repository.get_by_id(id=uuid.uuid4())
+    def test_get_observations_by_subject_id_and_source_id(self, subject_source_with_observations, data_source):
+        subject = subject_source_with_observations[0].subject
+        source = subject_source_with_observations[0].source
+        observation = subject_source_with_observations[1]
 
+        repository = ReadObservationRepository(data_source)
+        data = repository.get_observations_by_subject_id_and_source_id(subject_id=subject.id, source_id=source.id)
 
-@pytest.mark.django_db
-@pytest.mark.parametrize("repo_source", ["database"])
-def test_get_observations_by_subject_id_and_source_id(subject_source_with_observations, repo_source):
-    subject = subject_source_with_observations[0].subject
-    source = subject_source_with_observations[0].source
-    observation = subject_source_with_observations[1]
-
-    repository = ReadObservationRepository(repo_source)
-    data = repository.get_observations_by_subject_id_and_source_id(subject_id=subject.id, source_id=source.id)
-
-    assert data[0].id == observation.id
+        assert data[0].id == observation.id
