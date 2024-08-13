1fcc868e8428e0cba3ca530ec3c9907116bce5da
chrisj-er <chrisj@earthranger.com>
2024-08-13 18:55:46 +0000

    ERA-9715 Disallow the creation of geofences with non-linestring-typed geometric features feat: query to check if the selected feature group has geometry types outside the one specified. When we go to save a Geofence analyzer configuration in the admin, we ensure the critical and the warning feature groups specified only contain multi-linestrings.

--- das/analyzers/forms.py
+++ das/analyzers/forms.py
@@ -10,12 +10,13 @@ from analyzers.gfw_alert_schema import GFWLayerSlugs
 from analyzers.gfw_outbound import create_subscription, update_subscription
 from analyzers.models.gfw import GlobalForestWatchSubscription
 from core.forms_utils import FixedWidthFontTextArea, JSONFieldFormMixin
+from mapping.models import SpatialFeatureGroupStatic
 
 logger = logging.getLogger(__name__)
 
 
 class TimeFrameWidget(forms.MultiWidget):
-    template_name = 'widgets/analyzer_time.html'
+    template_name = "widgets/analyzer_time.html"
 
     def __init__(self, attrs=None):
         widgets = [forms.NumberInput, forms.NumberInput]
@@ -23,29 +24,30 @@ class TimeFrameWidget(forms.MultiWidget):
 
     def get_context(self, name, value, attrs):
         context = super().get_context(name, value, attrs)
-        context['hour_label'] = _('Hours:')
-        context['min_label'] = _('Minutes:')
+        context["hour_label"] = _("Hours:")
+        context["min_label"] = _("Minutes:")
         return context
 
     def decompress(self, value):
         if value:
             minutes, hours = math.modf(value)
-            return [int(hours), round(minutes*60)]
+            return [int(hours), round(minutes * 60)]
         return [24, 0]
 
 
 class TimeFrameField(forms.fields.MultiValueField):
     widget = TimeFrameWidget
-    error_message_hours = {
-        'min_value': "Ensure 'value for hours' is greater than or equal to 0"}
-    error_message_minutes = {'max_value': "Ensure 'value for minutes' is less than or equal to 59",
-                             'min_value': "Ensure 'value for minutes' is greater than or equal to 0"}
+    error_message_hours = {"min_value": "Ensure 'value for hours' is greater than or equal to 0"}
+    error_message_minutes = {
+        "max_value": "Ensure 'value for minutes' is less than or equal to 59",
+        "min_value": "Ensure 'value for minutes' is greater than or equal to 0",
+    }
 
     def __init__(self, *args, **kwargs):
         _fields = [
-            forms.fields.IntegerField(
-                min_value=0, max_value=2147483647, error_messages=self.error_message_hours),
-            forms.fields.IntegerField(min_value=0, max_value=59, error_messages=self.error_message_minutes)]
+            forms.fields.IntegerField(min_value=0, max_value=2147483647, error_messages=self.error_message_hours),
+            forms.fields.IntegerField(min_value=0, max_value=59, error_messages=self.error_message_minutes),
+        ]
         super().__init__(_fields, *args, **kwargs)
 
     def compress(self, values):
@@ -55,45 +57,46 @@ class TimeFrameField(forms.fields.MultiValueField):
 
 class EnvironmentalAnalyzerAdminForm(JSONFieldFormMixin, forms.ModelForm):
 
-    earth_engine_json_key = forms.CharField(label='Earth Engine JSON Key',
-                                            widget=FixedWidthFontTextArea(
-                                                attrs={'cols': '100', 'rows': '30'}),
-                                            required=False,
-                                            help_text=_(
-                                                'Paste the contents of your Earth Engine JSON key here.'))
-    search_time_hours = TimeFrameField(label='Analysis time frame')
+    earth_engine_json_key = forms.CharField(
+        label="Earth Engine JSON Key",
+        widget=FixedWidthFontTextArea(attrs={"cols": "100", "rows": "30"}),
+        required=False,
+        help_text=_("Paste the contents of your Earth Engine JSON key here."),
+    )
+    search_time_hours = TimeFrameField(label="Analysis time frame")
 
     class Meta:
         model = EnvironmentalSubjectAnalyzerConfig
-        json_fields = ('earth_engine_json_key',)
-        fields = ('additional',) + json_fields
+        json_fields = ("earth_engine_json_key",)
+        fields = ("additional",) + json_fields
 
 
 class GlobalForestWatchSubscriptionForm(JSONFieldFormMixin, forms.ModelForm):
 
     class Meta:
         model = GlobalForestWatchSubscription
-        widgets = {'Fire_confidence': forms.RadioSelect,
-                   'Deforestation_confidence': forms.RadioSelect}
+        widgets = {"Fire_confidence": forms.RadioSelect, "Deforestation_confidence": forms.RadioSelect}
         labels = {
-            'Fire_confidence': _('Fire Alerts (VIIRS) Confidence Level'),
-            'Deforestation_confidence': _('Deforestation Alerts (GLAD) Confidence Level')
+            "Fire_confidence": _("Fire Alerts (VIIRS) Confidence Level"),
+            "Deforestation_confidence": _("Deforestation Alerts (GLAD) Confidence Level"),
         }
-        fields = '__all__'
-        json_fields = ('alert_types',)
+        fields = "__all__"
+        json_fields = ("alert_types",)
 
-    alert_types = forms.MultipleChoiceField(choices=(
-        (GFWLayerSlugs.GLAD_ALERTS.value, _(
-            'Deforestation alerts (GLAD) / weekly / 30m')),
-        (GFWLayerSlugs.VIIRS_ACTIVE_FIRES.value, _(
-            'Fire Alerts (VIIRS) / daily / 375m')),
-    ), help_text='Click to select one, SHIFT+click to select both')
-    glad_confirmed_backfill_days = forms.IntegerField(initial=180,
-                                                      max_value=365,
-                                                      min_value=10,
-                                                      label=_(
-                                                          'Number of days to backfill for confirmed GLAD alerts'),
-                                                      help_text=_('Valid range: 10-365 days.'))
+    alert_types = forms.MultipleChoiceField(
+        choices=(
+            (GFWLayerSlugs.GLAD_ALERTS.value, _("Deforestation alerts (GLAD) / weekly / 30m")),
+            (GFWLayerSlugs.VIIRS_ACTIVE_FIRES.value, _("Fire Alerts (VIIRS) / daily / 375m")),
+        ),
+        help_text="Click to select one, SHIFT+click to select both",
+    )
+    glad_confirmed_backfill_days = forms.IntegerField(
+        initial=180,
+        max_value=365,
+        min_value=10,
+        label=_("Number of days to backfill for confirmed GLAD alerts"),
+        help_text=_("Valid range: 10-365 days."),
+    )
 
     def clean(self):
         res = super().clean()
@@ -102,45 +105,55 @@ class GlobalForestWatchSubscriptionForm(JSONFieldFormMixin, forms.ModelForm):
             # form data is good, do gfw operations
             model_info = self.get_gfw_info(self.cleaned_data)
             if GlobalForestWatchSubscription.objects.filter(pk=self.instance.pk).exists():
-                model = GlobalForestWatchSubscription.objects.get(
-                    pk=self.instance.pk)
-                model_info['geostore_id'] = model.geostore_id
-                model_info['subscription_id'] = model.subscription_id
-                geometry_changed = 'subscription_geometry' in self.changed_data
-                service_response = update_subscription(
-                    model_info, geometry_changed)
+                model = GlobalForestWatchSubscription.objects.get(pk=self.instance.pk)
+                model_info["geostore_id"] = model.geostore_id
+                model_info["subscription_id"] = model.subscription_id
+                geometry_changed = "subscription_geometry" in self.changed_data
+                service_response = update_subscription(model_info, geometry_changed)
             else:
                 service_response = create_subscription(model_info)
 
-            status_code = service_response.get('status_code')
+            status_code = service_response.get("status_code")
             if status_code == 200:
-                data = service_response.get('data')
-                self.instance.subscription_id = data['subscription_id']
-                self.instance.geostore_id = data['geostore_id']
+                data = service_response.get("data")
+                self.instance.subscription_id = data["subscription_id"]
+                self.instance.geostore_id = data["geostore_id"]
             else:
-                raise forms.ValidationError(service_response.get('text'))
+                raise forms.ValidationError(service_response.get("text"))
 
         return res
 
     def get_gfw_info(self, cleaned_data):
         return {
-            'name': cleaned_data.get('name'),
-            'subscription_id': cleaned_data.get('subscription_id'),
-            'geostore_id': cleaned_data.get('geostore_id'),
-            'alert_types': cleaned_data['alert_types'],
-            'subscription_geometry': cleaned_data['subscription_geometry'],
+            "name": cleaned_data.get("name"),
+            "subscription_id": cleaned_data.get("subscription_id"),
+            "geostore_id": cleaned_data.get("geostore_id"),
+            "alert_types": cleaned_data["alert_types"],
+            "subscription_geometry": cleaned_data["subscription_geometry"],
         }
 
 
 class BaseAnalyzerForm(forms.ModelForm):
-    search_time_hours = TimeFrameField(label='Analysis time frame')
+    search_time_hours = TimeFrameField(label="Analysis time frame")
 
     class Meta:
-        fields = '__all__'
+        fields = "__all__"
 
 
 class GeofenceSubjectAnalyzerForm(BaseAnalyzerForm):
     BaseAnalyzerForm.Meta.model = models.GeofenceAnalyzerConfig
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["critical_geofence_group"].queryset = SpatialFeatureGroupStatic.objects.by_spatial_type(
+            models.GeofenceAnalyzerConfig.GEOFENCE_SPATIAL_TYPE
+        )
+        self.fields["warning_geofence_group"].queryset = SpatialFeatureGroupStatic.objects.by_spatial_type(
+            models.GeofenceAnalyzerConfig.GEOFENCE_SPATIAL_TYPE
+        )
+        self.fields["containment_regions"].queryset = SpatialFeatureGroupStatic.objects.by_spatial_type(
+            models.GeofenceAnalyzerConfig.CONTAINMENT_REGIONS_SPATIAL_TYPE
+        )
 
 
 class ImmobilityAnalyzerForm(BaseAnalyzerForm):
@@ -150,19 +163,27 @@ class ImmobilityAnalyzerForm(BaseAnalyzerForm):
 class FeatureProximityAnalyzerForm(BaseAnalyzerForm):
     BaseAnalyzerForm.Meta.model = models.FeatureProximityAnalyzerConfig
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["critical_geofence_group"].queryset = SpatialFeatureGroupStatic.objects.by_spatial_type(
+            "MULTIPOINT"
+        )
+
 
 class SubjectProximityAnalyzerForm(forms.ModelForm):
     analysis_search_time_hours = TimeFrameField(
-        label='Analysis time frame', initial=1,
-        help_text=_(
-            'Analysis will be performed on recent data within this time frame.')
+        label="Analysis time frame",
+        initial=1,
+        help_text=_("Analysis will be performed on recent data within this time frame."),
     )
     proximity_time = TimeFrameField(
-        initial=1, label='Proximity Time',
-        help_text=_("A proximity event will only occur when the two subject's position points occur within this time."))
+        initial=1,
+        label="Proximity Time",
+        help_text=_("A proximity event will only occur when the two subject's position points occur within this time."),
+    )
 
     class Meta:
-        fields = '__all__'
+        fields = "__all__"
         model = models.SubjectProximityAnalyzerConfig
 
 
