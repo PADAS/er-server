5280d277553cf94ce5e0ab50a7ec79fe0477ce61
Halley Weitzman <halleyw@earthranger.com>
2024-08-21 00:38:00 +0000

    Begin working on Gear views. Remove models.py

--- das/buoy/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-
-
-def GearView(request, id):
-    return HttpResponse("GearView: " + str(id))
-
-
-def GearsView(request):
-    return HttpResponse("GearsView")
