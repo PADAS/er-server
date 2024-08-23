0b25904d26f2d16d43dbf124a80f8001e04ad610
Halley Weitzman <halleyw@earthranger.com>
2024-08-23 05:27:48 +0000

    test_gear straight forward case is passing

--- das/buoy/urls.py
+++ das/buoy/urls.py
@@ -5,10 +5,10 @@ from utils.constants import regex
 from . import views
 
 urlpatterns = [
-    re_path(r"^gear/?$", views.GearView.as_view(), name="gear-list-view"),
+    re_path(r"^gear/?$", views.GearsView.as_view(), name="gear-list-view"),
     re_path(
         rf"^gear/(?P<id>{regex.UUID})/?$",
-        views.SingleGearView.as_view(),
+        views.GearView.as_view(),
         name="gear-view",
     ),
 ]
