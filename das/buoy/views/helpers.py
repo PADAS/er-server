13536c039d642e8cfc96fa4b3cfb1fc8f97b7f69
Halley Weitzman <halleyw@earthranger.com>
2024-09-28 00:02:40 +0000

    Fix tests.

--- das/buoy/views/helpers.py
+++ das/buoy/views/helpers.py
@@ -4,11 +4,24 @@ from django.contrib.gis.geos import Point, Polygon
 from geopy import Point
 from psycopg2.extras import DateTimeTZRange
 from django.db.models import Q
+from datetime import datetime
+from typing import Optional
 
+from observations.utils import dateparse
 from observations.models import LatestObservationSource, SubjectSource, Subject
 
 STATIONARY_SUBJECT_VALUE = "stationary-object"
 NAUTICAL_MILE_RADIUS = 5
+
+
+def check_valid_date_string(date_str: Optional[str], parameter_name: str) -> (bool, Optional[datetime]):
+    if not date_str:
+        return False, None
+
+    try:
+        return True, dateparse(date_str)
+    except ValueError:
+        raise ValueError("Invalid value for %s: '%s'" % (parameter_name, date_str))
 
 
 def check_valid_state_string(state_str):
