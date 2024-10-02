fd1c9dee35cb25d6593a54c7859ad2f0a8ff58dc
Chouffe <arthurc@earthranger.com>
2024-10-02 10:49:06 +0200

    fix(celery): add feedback from PR
    - Move the partition maintenance task to observations.task for
      maintainability
    - Use QueueOnce instead of TenantTask

--- das/utils/db/postgresql.py
+++ das/utils/db/postgresql.py
@@ -12,28 +12,31 @@ def execute_sql_query(query: str, logger: Logger, fetch: bool = True) -> Optiona
     """
     Execute the SQL `query`. Return one row if `fetch` is set to True.
     The process/thread is exited on execution error and an exception is written
-    with the `logger`.
+    with the `logger`. It can raise a ProgrammingError exception.
 
     Args:
         query (str): SQL query string to be executed. Should contain a `;` at the end.
         logger (logging.Logger): The logger to use to write potential errors.
         fetch (bool): Should we return one row of the results? Defaults to
         `True`.
+
+    Output:
+        result (Optional[str]) | ProgrammingError:  returns the sql result as a
+        string or raise an exception.
     """
     result = None
     with connection.cursor() as cursor:
         try:
             cursor.execute(query)
         except ProgrammingError as e:
-            logger.error(e)
-            logger.exception("Exiting due to error")
-            exit(1)
+            logger.exception(f"programming error: {e}")
+            raise e
         if fetch:
             result = cursor.fetchone()
         return result
 
 
-def is_posgresql_extension_installed(extension_name: str, logger: Logger) -> bool:
+def is_postgresql_extension_installed(extension_name: str, logger: Logger) -> bool:
     """
     Check whether the postgresql extension `extension_name` is installed.
 
@@ -41,24 +44,31 @@ def is_posgresql_extension_installed(extension_name: str, logger: Logger) -> boo
         extension_name (str): Name of the postgresql extension.
         logger (logging.Logger): logger to use to write potential execution
         errors.
+
+    Output:
+        bool: Is the extension `extension_name` installed?
     """
-    sql_result = execute_sql_query(
-        query=f"SELECT COUNT(*) FROM pg_extension WHERE extname = '{extension_name}';",
-        logger=logger,
-        fetch=True,
-    )
-    return bool(sql_result and sql_result[0] == 1)
+    try:
+        sql_result = execute_sql_query(
+            query=f"SELECT COUNT(*) FROM pg_extension WHERE extname = '{extension_name}';",
+            logger=logger,
+            fetch=True,
+        )
+        return bool(sql_result and sql_result[0] == 1)
+    except Exception as e:
+        logger.exception(f"cannot execute sql query: {e}")
+        return False
 
 
 def partman_partition_maintenance_query(table_name: str) -> str:
     """
-    Create the SQL query for running the partman partition maintenance.
+    Create the SQL query string for running the partman partition maintenance.
 
     Args:
         table_name (str): Name of the postgresql table to run partman
         maintenance on.
 
     Note: This does not check for SQL injection. Make sure to know what you are
-    doing. with `table_name`.
+    doing with `table_name`.
     """
     return f"SELECT partman.run_maintenance('{table_name}');"
--- das/utils/db/tasks.py
+++ das/utils/db/tasks.py
@@ -1,35 +1,37 @@
 """
-Celery tasks for the DB in general, not related to a particular Model.
+Helper to create Celery tasks for the DB in general, not related to a
+particular Model.
 """
 
-import logging
-
-from das_server import celery
-from utils.tenant.celery import OverAllTenantTask
+from logging import Logger
 
 from .postgresql import (
     execute_sql_query,
-    is_posgresql_extension_installed,
+    is_postgresql_extension_installed,
     partman_partition_maintenance_query,
 )
 
-logger = logging.getLogger(__name__)
 
-
-@celery.app.task(base=OverAllTenantTask)
-def run_partition_maintenance(table_name: str):
+def run_partition_maintenance(table_name: str, logger: Logger) -> None:
     """
     Run the partition maintenance on `table_name`.
     It uses pg_partman behind the scene so make sure to make this postgresql
     extension available before running it. It logs a warning if the extension
     is not available.
+
+    Args:
+        table_name (str): postgresql table name to run the maintenance on.
+        logger (logging.Logger): logger to write to.
     """
     logger.info(f"Running partition maintenance for {table_name}")
-    is_pg_partman_extension_available = is_posgresql_extension_installed(extension_name="pg_partman", logger=logger)
+    is_pg_partman_extension_available = is_postgresql_extension_installed(extension_name="pg_partman", logger=logger)
     if not is_pg_partman_extension_available:
         logger.warning("pg_partman is not installed, cannot run the maintenance")
     else:
-        maintenance_query = partman_partition_maintenance_query(table_name=table_name)
-        logger.info(f"maintenance_query to run: {maintenance_query}")
-        execute_sql_query(maintenance_query, logger=logger, fetch=True)
-        logger.info(f"partman partition maintenance done on table '{table_name}'")
+        try:
+            maintenance_query = partman_partition_maintenance_query(table_name=table_name)
+            logger.info(f"maintenance_query to run: {maintenance_query}")
+            execute_sql_query(maintenance_query, logger=logger, fetch=True)
+            logger.info(f"partman partition maintenance done on table '{table_name}'")
+        except Exception as e:
+            logger.exception(f"cannot run partition maintenance on table {table_name}: {e}")
