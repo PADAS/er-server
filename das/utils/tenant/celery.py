20a5d76f1c839f0b86d8103e40e36ea94d6c9ef2
Chris Jones <41801350+chrisj-er@users.noreply.github.com>
2024-11-22 10:58:10 -0800

    ERA-10560 Analyzer job start failure does not retry (#3289)
    * ERA-10560 Analyzer job start failure does not retry
    fix: handle OperationalError when setting up tenant settings in our celery task mixin. If failure, retry the task.
    
    * feat: remove fix for OverAllTenantTask as that will get fixed in ticket ERA-10564
    feat: document the retry parameters
    
    ---------
    
    Co-authored-by: chrisj-er <“chrisj@earthranger.com”>

--- das/utils/tenant/celery.py
+++ das/utils/tenant/celery.py
@@ -3,8 +3,9 @@ import logging
 from celery import Task
 from celery_once import QueueOnce
 
+from django.db.utils import OperationalError
+
 from core.models import DASTenant
-from utils.features import features
 from utils.tenant import get_tenant_settings
 from utils.tenant.exceptions import TenantNotFoundException
 from utils.tenant.managers import TenantContextManager
@@ -15,16 +16,18 @@ logger = logging.getLogger(__name__)
 
 class TenantTaskMixin:
     def __call__(self, *args, **kwargs):
-        if features.tms.is_on():
-            logger.debug("TenantTaskMixin", extra={"kwargs": kwargs})
-            domain = kwargs.get("domain")
-            if not domain:
-                raise ValueError("domain needs to be defined via kwargs for a celery TenantTask")
+        logger.debug("TenantTaskMixin", extra={"kwargs": kwargs})
+        domain = kwargs.get("domain")
+        if not domain:
+            raise ValueError("domain needs to be defined via kwargs for a celery TenantTask")
 
+        try:
             with TenantContextManager(domain):
                 return super().__call__(*args, **kwargs)
-        else:
-            return super().__call__(*args, **kwargs)
+        except OperationalError as ex:
+            logger.warning("OperationalError occurred while running: %s for Tenant domain: %s", self.name, domain)
+            # default_retry_delay == 180 seconds, max_retries == 3, retry_backoff == exponential backoff, with jitter, max of ten minutes
+            self.retry(exc=ex, retry_backoff=True)
 
     def apply(self, args=None, kwargs=None, *arg, **kw):
         kwargs = kwargs or {}
