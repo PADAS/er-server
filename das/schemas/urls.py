802b2b794fcaa48e9414637a05d2ed10479d6bdf
Ernesto Mancilla <emancillao@gmail.com>
2025-01-10 13:19:51 -0500

    ERA-10454 removed FeatureCategoriesDynamicSchemaView and prunned code from previous implementations

--- das/schemas/urls.py
+++ das/schemas/urls.py
@@ -5,7 +5,6 @@ from schemas import views
 app_name = "schemas"
 
 urlpatterns = [
-    path("featurecategories.json", views.FeatureCategoriesDynamicSchemaView.as_view(), name="feature-categories"),
     path("users.json", views.UsersDynamicSchemaView.as_view(), name="users"),
     path("subjects.json", views.SubjectsDynamicSchemaView.as_view(), name="subjects"),
 ]
--- das/schemas/views.py
+++ das/schemas/views.py
@@ -1,20 +1,6 @@
-from typing import List
-
-from rest_framework.request import Request
-
 from accounts.views import UsersView
-from mapping.views import FeatureSetListJsonView
 from observations.views import SubjectsView
 from schemas.view_mixins import DynamicSchemaFromSourceView
-
-
-class FeatureCategoriesDynamicSchemaView(DynamicSchemaFromSourceView):
-    source_view = FeatureSetListJsonView
-    data_path = "features"
-
-    schema_title = "FeatureCategories"
-    schema_description = "Feature categories list"
-    default_title_field = "name"
 
 
 class UsersDynamicSchemaView(DynamicSchemaFromSourceView):
@@ -25,19 +11,6 @@ class UsersDynamicSchemaView(DynamicSchemaFromSourceView):
 
     def get_display_name_from_item(self, item: dict) -> str:
         return f"{item.get('first_name')} {item.get('last_name')}"
-
-    def get_requested_fields(self, request: Request) -> List[str]:
-        fields_base = super().get_requested_fields(request)
-        fields_map = {
-            "display_name": ["first_name", "last_name"],
-        }
-        fields = []
-        for field in fields_base:
-            if field in fields_map:
-                fields.extend(fields_map[field])
-            else:
-                fields.append(field)
-        return fields
 
 
 class SubjectsDynamicSchemaView(DynamicSchemaFromSourceView):
