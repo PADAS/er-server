66fd340f4029c769a156277b33760d7d62538c2b
Chris Jones <41801350+chrisj-er@users.noreply.github.com>
2025-02-07 13:26:34 -0800

    ERA-11157 Add Camel Subject Icon (#3354)
    * ERA-11157 Add Camel Subject Icon
    feat: camel subject icon
    fix: tenant logging feature. Delay init of this feature until django subsystem has been initialized.
    
    * feat: update initial_data with camel icon
    
    ---------
    
    Co-authored-by: “chrisj-er” <“chrisj@earthranger.com”>

--- das/das_server/apps.py
+++ das/das_server/apps.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.apps import AppConfig
 from django.conf import settings
 
@@ -11,3 +13,14 @@ class DasServerConfig(AppConfig):
     def ready(self):
         if settings.DISABLE_STATSD:
             utils.stats.statsd.disable_telemetry()
+        add_log_filters()
+
+
+def add_log_filters():
+    """Add any filters now that require the django subsystem to be up and running"""
+    from utils.tenant.log import TenantFilter
+
+    tenant_filter = TenantFilter()
+    root_logger = logging.getLogger()
+    for handler in root_logger.handlers:
+        handler.addFilter(tenant_filter)
