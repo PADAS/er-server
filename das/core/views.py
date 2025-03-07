8c31ceada3499982d7eb6e2b3bb8457d7ca20533
“chrisj-er” <chrisj@earthranger.com>
2025-03-07 19:15:22 +0000

    ERA-9269 Server Response Issue during Easter Island Script Source Deletion feat: add an async method to delete a source. This can be added to other single views as well if async delete is needed.

--- das/core/views.py
+++ das/core/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from core.tasks import delete_object_status
+
+
+class DeleteObjectStatusView(APIView):
+    def get(self, request, task_id, *args, **kwargs):
+        result = delete_object_status(task_id)
+        return Response(result)
