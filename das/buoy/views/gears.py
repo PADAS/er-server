80efa25f09a2139b2537a25eb7451d5d51d0a2b2
Halley Weitzman <halleyw@earthranger.com>
2025-03-10 17:54:54 +0000

    Add order by additional field

--- das/buoy/views/gears.py
+++ das/buoy/views/gears.py
@@ -80,7 +80,7 @@ class GearsView(generics.ListAPIView):
         )
 
         # Filter queryset by removing subjects where the additional field is the same
-        queryset = queryset.order_by("subject__name").distinct("additional")
+        queryset = queryset.order_by("subject__name", "additional").distinct("additional")
 
         page = self.paginate_queryset(queryset)
         if page is not None:
