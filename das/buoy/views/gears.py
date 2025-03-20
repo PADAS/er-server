bac83bf44cbed6c90c85f4bad058c00ef1cca40a
Halley Weitzman <halleyw@earthranger.com>
2025-03-20 22:07:12 +0000

    - Fix duplication bug by ordering query results by subject name in 1 step and then performing distinct on the additional property in a separate step.

--- das/buoy/views/gears.py
+++ das/buoy/views/gears.py
@@ -80,7 +80,8 @@ class GearsView(generics.ListAPIView):
         )
 
         # Filter queryset by removing subjects where the additional field is the same
-        queryset = queryset.order_by("subject__name", "additional").distinct("subject__name", "additional")
+        queryset = queryset.order_by("subject__name")
+        queryset = queryset.order_by("additional").distinct("additional")
 
         page = self.paginate_queryset(queryset)
         if page is not None:
