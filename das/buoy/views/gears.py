e91bba4a6ac760afa74002abc4bba398ed72ae1b
Halley W <halleyw@earthranger.com>
2025-03-21 18:32:02 -0700

    RF-850 hot fix: Fix duplication bug by correcting order by steps (#3401)
    * - Fix duplication bug by ordering query results by subject name in 1 step and then performing distinct on the additional property in a separate step.
    
    * - Add unit tests for deterministic order_by subject name when additional property matches (trawl case and non-trawl case)
    - Fix datetime missing timezone info warnings
    
    * - Remove skip for Buoy unit tests
    - Compare additional.display_id instead of entire additional dictionary to prevent flakiness of dictionary comparison
    
    * - Fix flaky tests
    
    * - Remove flaky test. Deterministic ordering does not apply because display_ids would not match for gears that are not part of the same trawl. This would affect ordering of gears in the output.

--- das/buoy/views/gears.py
+++ das/buoy/views/gears.py
@@ -80,7 +80,7 @@ class GearsView(generics.ListAPIView):
         )
 
         # Filter queryset by removing subjects where the additional field is the same
-        queryset = queryset.order_by("subject__name", "additional").distinct("subject__name", "additional")
+        queryset = queryset.order_by("additional__display_id", "subject__name").distinct("additional__display_id")
 
         page = self.paginate_queryset(queryset)
         if page is not None:
