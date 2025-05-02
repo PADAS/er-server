04ef613dcaf14570cf840f4dd767ca039120270f
Gabriel Borges <gabriel.viviani@nimblegravity.com>
2025-05-02 15:01:08 +0000

    feat: Added middleware to generate an EFB token based on admin login to set it on Session Cookie

--- das/utils/authentication.py
+++ das/utils/authentication.py
@@ -5,14 +5,24 @@ Allow a superuser to browse the DRF api.
 
 import json
 import logging
+from datetime import timedelta
 
 from oauth2_provider.models import get_access_token_model
+from oauthlib.common import generate_token
 
+from django.conf import settings
+from django.utils import timezone
+from django.utils.deprecation import MiddlewareMixin
 from rest_framework.authentication import SessionAuthentication
+
+from core.models.oauth import DASAccessToken, DASApplication
 
 logger = logging.getLogger("django.request")
 
 AccessToken = get_access_token_model()
+
+
+EFB_APPLICATION_ID = "EFB_APPLICATION_ID"
 
 
 class SuperUserSessionAuthentication(SessionAuthentication):
@@ -63,3 +73,50 @@ class SkylinePostAuthentication(SessionAuthentication):
                 access_token = AccessToken.objects.get(token=token)
                 return access_token.user, None
             return None
+
+
+class AdminEFBTokenAuthentication(MiddlewareMixin):
+    def process_response(self, request, response):
+        if (
+            "/admin" in request.path
+            and request.user.is_authenticated
+            and request.user.is_staff
+            and not response.has_header("Set-Cookie")
+        ):
+            # Check if the user has is logged in within scope of this request
+            if "_auth_user_id" in request.session and request.session["_auth_user_id"] == str(request.user.pk):
+                try:
+                    efb_app, _ = DASApplication.objects.get_or_create(
+                        client_id=EFB_APPLICATION_ID,
+                        defaults={
+                            "client_type": "Confidential",
+                            "authorization_grant_type": "password",
+                            "client_secret": "",
+                            "name": "Event Form Builder Das App",
+                            "skip_authorization": True,
+                        },
+                    )
+
+                    oauth2_settings = getattr(settings, "OAUTH2_PROVIDER", {})
+                    expire_in_secs = oauth2_settings.get("ACCESS_TOKEN_EXPIRE_SECONDS")
+                    expires = timezone.now() + timedelta(seconds=expire_in_secs)
+
+                    access_token = DASAccessToken.objects.create(
+                        user=request.user,
+                        token=generate_token(),
+                        application=efb_app,
+                        expires=expires,
+                        scope="read write",
+                        das_tenant=request.user.das_tenant,
+                    )
+                    logger.info(
+                        "Middleware: Created access token for user %s at %s",
+                        request.user.username,
+                        EFB_APPLICATION_ID,
+                    )
+
+                    response.set_cookie("efb_access_token", access_token.token)
+
+                except Exception as e:
+                    logger.error(f"Middleware: Error creating token: {e}")
+        return response
