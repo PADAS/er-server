30a55fcdb6e947166dd85b1982b9dea72fac1615
Ernesto Mancilla <emancillao@gmail.com>
2025-05-22 09:37:14 -0500

    docs: update the OpenAPI page description and title

--- das/das_server/settings.py
+++ das/das_server/settings.py
@@ -286,14 +286,18 @@ CSRF_COOKIE_SECURE = True
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 10000
 
 SPECTACULAR_SETTINGS = {
-    "TITLE": "Your Project API",
-    "DESCRIPTION": "Your project description",
+    "TITLE": "EarthRanger API Documentation",
+    "DESCRIPTION": (
+        "Welcome to the EarthRanger API! </br>"
+        "You're a conservationist, ranger, or technologist? </br>"
+        "Explore our endpoints to discover how EarthRanger can help protected areas and "
+        "conservation organizations safeguard wildlife and their habitats."
+    ),
     "VERSION": "1.0.0",
     "SERVE_INCLUDE_SCHEMA": False,
     "SWAGGER_UI_DIST": "SIDECAR",  # shorthand to use the sidecar instead
     "SWAGGER_UI_FAVICON_HREF": "SIDECAR",
     "REDOC_DIST": "SIDECAR",
-    # OTHER SETTINGS
 }
 
 SWAGGER_SETTINGS = {
