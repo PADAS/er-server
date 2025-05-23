3f15e9cc5129ea1274a09b4dadfc80a173bcb4a1
Ernesto Mancilla <emancillao@gmail.com>
2025-05-23 10:24:47 -0500

    feat: spatial features endpoint with feature_type and feature_set filters with validations and support for multiple values using csv format

--- das/mapping/filters.py
+++ das/mapping/filters.py
@@ -1,37 +1,24 @@
-from django.db.models import QuerySet
-from rest_framework.exceptions import ValidationError
-from rest_framework.filters import BaseFilterBackend
-from rest_framework.views import APIView, Request
+from django_filters import rest_framework as filters
+from django_filters.widgets import CSVWidget
+
+from mapping.models import DisplayCategory, SpatialFeature, SpatialFeatureType
 
 
-class SpatialFeatureFilter(BaseFilterBackend):
-    """
-    Filter the list of spatial features based on:
+class SpatialFeatureFilterSet(filters.FilterSet):
 
-    - feature_type_id (`feature_type` in query params, multiple values allowed)
-    - display_category_id (`feature_set` in query params, multiple values allowed)
+    feature_type = filters.ModelMultipleChoiceFilter(
+        queryset=SpatialFeatureType.objects.all(),
+        widget=CSVWidget(),
+        label="Feature Type",
+    )
+    feature_set = filters.ModelMultipleChoiceFilter(
+        field_name="feature_type__display_category__id",
+        to_field_name="id",
+        queryset=DisplayCategory.objects.all(),
+        widget=CSVWidget(),
+        label="Feature Set (Display Category)",
+    )
 
-    Because of the way the data is structured, we can filter by either feature_type_id or display_category_id,
-    but not both at the same time.
-
-    If both are provided, a ValidationError is raised.
-    """
-
-    def filter_queryset(self, request: Request, queryset: QuerySet, view: APIView) -> QuerySet:
-
-        query_params = request.query_params
-
-        # Filter by feature_type_id xor by display_category_id (`feature_set` in query params)
-        feature_sets = query_params.getlist("feature_set")
-        feature_types = query_params.getlist("feature_type")
-
-        if feature_sets and feature_types:
-            raise ValidationError(detail={"detail": "You can't filter by both feature_set and feature_type."})
-
-        if feature_types:
-            queryset = queryset.filter(feature_type_id__in=feature_types)
-
-        if feature_sets:
-            queryset = queryset.filter(feature_type__display_category_id__in=feature_sets)
-
-        return queryset
+    class Meta:
+        model = SpatialFeature
+        fields = ["feature_type", "feature_set"]
