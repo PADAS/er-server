60ac8f0051d442058e72043363b1b902205c4e9d
chrisj-er <chrisj@earthranger.com>
2025-06-06 00:23:34 +0000

    ERA-11617 Optimize er mobile sensor POST feat: cache the sources created/retrieved by ensure_source for reuse with identically attributed observations in the batch being uploaded.

--- das/sensors/tests/test_er_track.py
+++ das/sensors/tests/test_er_track.py
@@ -3,7 +3,6 @@ import datetime
 import json
 from unittest import mock
 
-import pytest
 import pytz
 
 from django.db import transaction
@@ -133,7 +132,6 @@ class ErTrackHandlerTest(BaseAPITest):
         assert db_observation.exclusion_flags.EXCLUDED_AUTOMATICALLY.is_set
 
     @override_settings(CELERY_TASK_ALWAYS_EAGER=True)
-    @pytest.mark.skip
     def test_post_a_duplicate_observation(self):
         response = self._post_data(json.dumps(self.one_observation), user=self.super_user)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
