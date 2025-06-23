3b4b7144e7a1ce28da174d5c7f186dd69a4e24fe
Ernesto Mancilla <emancillao@gmail.com>
2025-06-23 09:54:43 -0500

    refactor: reorganize event type tests with better fixtures and validation coverage

--- das/activity/models.py
+++ das/activity/models.py
@@ -326,8 +326,10 @@ class EventType(TenantModelMixin, RankModelMixin, TimestampedModel):
         validators=[
             RegexValidator(
                 regex="^[A-Za-z0-9-_]*$",
-                message="""An invalid character was detected in the Event type Value field.
-        Supported characters are: Letters a-z (lowercase), Numbers 0-9 and Underscore""",
+                message=(
+                    "An invalid character was detected in the Event type Value field. "
+                    "Supported characters are: Letters a-z (lowercase), Numbers 0-9 and Underscore"
+                ),
             )
         ],
     )
