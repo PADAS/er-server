2b9c5bc9d39166fa54271d141c1ece8170c76f5d
Ernesto Mancilla <emancillao@gmail.com>
2025-07-01 17:45:03 -0500

    feat: redirect to drf-authentication for Redoc UI

--- das/das_server/spectacular_views.py
+++ das/das_server/spectacular_views.py
@@ -2,7 +2,7 @@
 Custom DRF Spectacular views with authentication handling.
 """
 
-from drf_spectacular.views import SpectacularSwaggerView
+from drf_spectacular.views import SpectacularRedocView, SpectacularSwaggerView
 
 from django.http import HttpResponseRedirect
 from django.urls import reverse
@@ -11,6 +11,20 @@ from django.urls import reverse
 class AuthenticatedSpectacularSwaggerView(SpectacularSwaggerView):
     """
     Swagger UI view that redirects unauthenticated users to login page.
+    """
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            # Redirect to DRF's login page with next parameter
+            login_url = reverse("rest_framework:login")
+            next_url = request.get_full_path()
+            return HttpResponseRedirect(f"{login_url}?next={next_url}")
+        return super().dispatch(request, *args, **kwargs)
+
+
+class AuthenticatedSpectacularRedocView(SpectacularRedocView):
+    """
+    Redoc view that redirects unauthenticated users to login page.
     """
 
     def dispatch(self, request, *args, **kwargs):
