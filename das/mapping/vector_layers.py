4fbd824a7870c1f0ef69944ec3e415eea883977c
JoshuaVulcan <38018017+JoshuaVulcan@users.noreply.github.com>
2025-07-14 21:16:15 +0000

    Fix vector tile geometry field with queryset annotation
    - Use cleaner approach: annotate 'geom' field in get_vector_tile_queryset()
    - Remove complex get_tile override in favor of simple F() annotation
    - This ensures django-vectortiles can find the expected 'geom' field for spatial filtering

--- das/mapping/vector_layers.py
+++ das/mapping/vector_layers.py
@@ -29,17 +29,6 @@ class SpatialFeatureLayer(VectorLayer):
         return self.model.objects.select_related("feature_type", "feature_type__display_category").annotate(
             feature_type_name=F("feature_type__name"),
             display_category_name=F("feature_type__display_category__name"),
+            # Add geom annotation for django-vectortiles compatibility
+            geom=F(self.geometry_field),
         )
-
-    def get_tile(self, x, y, z):
-        """Override to ensure proper geometry field handling"""
-        # The parent class may not be using our geometry_field correctly
-        # Let's check if we need to alias the geometry field
-        queryset = self.get_vector_tile_queryset(z, x, y)
-
-        # If the library expects 'geom' but we have 'feature_geometry',
-        # we need to alias it
-        if self.geometry_field != "geom":
-            queryset = queryset.annotate(geom=F(self.geometry_field))
-
-        return super().get_tile(x, y, z)
