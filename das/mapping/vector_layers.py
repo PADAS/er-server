c5c7005be11cc92e068316625082b3d908a1b42c
JoshuaVulcan <38018017+JoshuaVulcan@users.noreply.github.com>
2025-07-15 04:22:24 +0000

    fix:forgot to set the custom filterset for the spatialfeaturelayer.

--- das/mapping/vector_layers.py
+++ das/mapping/vector_layers.py
@@ -2,13 +2,14 @@ from vectortiles import VectorLayer
 
 from django.db.models import F
 
+from mapping.filters import SpatialFeatureFilterSet
 from mapping.models import SpatialFeature
 
 
 class SpatialFeatureLayer(VectorLayer):
     model = SpatialFeature
     id = "spatial_features"
-    geometry_field = "feature_geometry"
+    geometry_field = "geom"  # Use the transformed geometry field
     tile_fields = (
         "id",
         "name",
@@ -24,6 +25,7 @@ class SpatialFeatureLayer(VectorLayer):
     )
     min_zoom = 3
     max_zoom = 24
+    filterset_class = SpatialFeatureFilterSet
 
     def get_vector_tile_queryset(self, zoom, x, y):
         from django.contrib.gis.db import models as gis_models
@@ -34,5 +36,5 @@ class SpatialFeatureLayer(VectorLayer):
             feature_type_name=F("feature_type__name"),
             display_category_name=F("feature_type__display_category__name"),
             # Cast geography to geometry, then transform to Web Mercator for django-vectortiles compatibility
-            geom=Transform(Cast(self.geometry_field, gis_models.GeometryField()), 3857),
+            geom=Transform(Cast(F("feature_geometry"), gis_models.GeometryField()), 3857),
         )
