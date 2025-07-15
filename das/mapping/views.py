6cdcb5226a136d084e5ee648598b8e5e6ce8710e
JoshuaVulcan <38018017+JoshuaVulcan@users.noreply.github.com>
2025-07-15 04:14:21 +0000

    feat:auth

--- das/mapping/views.py
+++ das/mapping/views.py
@@ -236,7 +236,7 @@ class SpatialFeatureTileView(MVTView):
     """
 
     layer_classes = [SpatialFeatureLayer]
-    permission_classes = ()  # No authentication required for tile endpoint
+    permission_classes = (LayerObjectPermissions,)
 
     def get(self, request, z, x, y):
         # Create simple cache key from view name and tile coordinates
