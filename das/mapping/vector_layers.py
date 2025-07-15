9f007d20077e4a11e07c184f00aaddd2aff54acf
JoshuaVulcan <38018017+JoshuaVulcan@users.noreply.github.com>
2025-07-15 23:29:21 +0000

    feat:adding image to encoded tile data

--- das/mapping/vector_layers.py
+++ das/mapping/vector_layers.py
@@ -21,6 +21,7 @@ class SpatialFeatureLayer(VectorLayer):
         "display_category_name",
         "presentation",
         "attributes",
+        "image",
     )
     min_zoom = 3
     max_zoom = 24
@@ -29,6 +30,7 @@ class SpatialFeatureLayer(VectorLayer):
     def get_vector_tile_queryset(self, zoom, x, y):
         from django.contrib.gis.db import models as gis_models
         from django.contrib.gis.db.models.functions import Transform
+        from django.db.models import Case, CharField, Value, When
         from django.db.models.functions import Cast
 
         return self.model.objects.select_related("feature_type", "feature_type__display_category").annotate(
@@ -36,4 +38,11 @@ class SpatialFeatureLayer(VectorLayer):
             display_category_name=F("feature_type__display_category__name"),
             # Cast geography to geometry, then transform to Web Mercator for django-vectortiles compatibility
             geom=Transform(Cast(F("feature_geometry"), gis_models.GeometryField()), 3857),
+            # Extract image from presentation data - prioritize feature-level image, then fall back to feature_type
+            image=Case(
+                When(presentation__has_key="image", then=F("presentation__image")),
+                When(feature_type__presentation__has_key="image", then=F("feature_type__presentation__image")),
+                default=Value(None),
+                output_field=CharField(),
+            ),
         )
