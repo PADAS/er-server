efa6beba2fc38140c3d056e5b417cc4db42e8f3a
JoshuaVulcan <38018017+JoshuaVulcan@users.noreply.github.com>
2025-07-17 19:45:34 +0000

    fix:undo bad change to tile URL pattern

--- das/mapping/urls.py
+++ das/mapping/urls.py
@@ -59,8 +59,8 @@ urlpatterns = (
         name="spatialfeature-detail",
     ),
     # Vector tile endpoint for spatial features
-    path(
-        "spatialfeatures/tiles/<int:z>/<int:x>/<int:y>.pbf",
+    re_path(
+        r"^spatialfeatures/tiles/(?P<z>\d+)/(?P<x>\d+)/(?P<y>\d+)\.pbf$",
         SpatialFeatureTileView.as_view(),
         name="spatialfeature-tiles",
     ),
