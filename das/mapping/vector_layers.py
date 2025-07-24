fbf441d3a5cfc4cd42c07a5afeea616a31e42c24
JoshuaVulcan <38018017+JoshuaVulcan@users.noreply.github.com>
2025-07-24 21:10:38 +0000

    feat:fixes to rendered tile data props

--- das/mapping/vector_layers.py
+++ das/mapping/vector_layers.py
@@ -3,6 +3,7 @@ from vectortiles import VectorLayer
 from django.contrib.gis.db import models as gis_models
 from django.contrib.gis.db.models.functions import Transform
 from django.db.models import Case, CharField, F, Value, When
+from django.db.models.expressions import RawSQL
 from django.db.models.functions import Cast
 
 from mapping.filters import SpatialFeatureFilterSet
@@ -10,12 +11,24 @@ from mapping.models import SpatialFeature
 
 
 class SpatialFeatureLayer(VectorLayer):
-    model = SpatialFeature
+    queryset = SpatialFeature.objects.select_related("feature_type", "feature_type__display_category").annotate(
+        int_id=RawSQL("hashtext(CAST(mapping_spatialfeature.id AS TEXT))", []),
+        feature_type_name=F("feature_type__name"),
+        display_category_name=F("feature_type__display_category__name"),
+        geom=Transform(Cast(F("feature_geometry"), gis_models.GeometryField()), 3857),
+        # Extract image from presentation data - prioritize feature-level image, then fall back to feature_type
+        image=Case(
+            When(presentation__has_key="image", then=F("presentation__image")),
+            When(feature_type__presentation__has_key="image", then=F("feature_type__presentation__image")),
+            default=Value(None),
+            output_field=CharField(),
+        ),
+    )
+
     id = "spatial_features"
-    geometry_field = "feature_geometry"  # Use the transformed geometry field
     tile_fields = (
-        "id",
         "name",
+        "int_id",
         "short_name",
         "external_id",
         "description",
@@ -29,36 +42,3 @@ class SpatialFeatureLayer(VectorLayer):
     min_zoom = 3
     max_zoom = 24
     filterset_class = SpatialFeatureFilterSet
-
-    def get_vector_tile_queryset(self, zoom, x, y):
-        return self.model.objects.select_related("feature_type", "feature_type__display_category").annotate(
-            feature_type_name=F("feature_type__name"),
-            display_category_name=F("feature_type__display_category__name"),
-            # Cast geography to geometry, then transform to Web Mercator for django-vectortiles compatibility
-            geom=Transform(Cast(F("feature_geometry"), gis_models.GeometryField()), 3857),
-            # Extract image from presentation data - prioritize feature-level image, then fall back to feature_type
-            image=Case(
-                When(presentation__has_key="image", then=F("presentation__image")),
-                When(feature_type__presentation__has_key="image", then=F("feature_type__presentation__image")),
-                default=Value(None),
-                output_field=CharField(),
-            ),
-        )
-
-    def get_tile(self, x, y, z):
-        """Override to use our custom annotated queryset for vector tiles"""
-        # Store the current queryset and temporarily replace it with our annotated version
-        original_queryset = getattr(self, "queryset", None)
-
-        # Set our annotated queryset for tile generation
-        self.queryset = self.get_vector_tile_queryset(z, x, y)
-
-        try:
-            # Call parent method which will use our custom queryset
-            return super().get_tile(x, y, z)
-        finally:
-            # Restore original queryset
-            if original_queryset is not None:
-                self.queryset = original_queryset
-            elif hasattr(self, "queryset"):
-                delattr(self, "queryset")
