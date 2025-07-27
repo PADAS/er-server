0f2a5554812bd3a7a278fb6ff6ece8e47d71749a
chrisj-er <chrisj@earthranger.com>
2025-07-27 23:21:49 +0000

    fix: added target = _blank

--- das/activity/static/admin/js/form-builder-admin.js
+++ das/activity/static/admin/js/form-builder-admin.js
@@ -3,5 +3,6 @@ window.addEventListener('DOMContentLoaded', function() {
     var link = document.querySelector('a.viewlink[href="/admin/activity/formbuilderproxy/"]');
     if (link && link.textContent.trim() === 'View') {
         link.textContent = 'View and Edit';
+        link.target = '_blank';
     }
 });
