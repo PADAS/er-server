03ed3387ea1401f60a3538f2b36deb367f198aec
Ernesto Mancilla <emancillao@gmail.com>
2025-08-05 14:21:47 -0500

    feat: add DRF Spectacular extension for JSONSchema field

--- das/activity/apps.py
+++ das/activity/apps.py
@@ -2,8 +2,9 @@ from django.apps import AppConfig
 
 
 class ActivityConfig(AppConfig):
-    name = 'activity'
-    verbose_name = 'Activity'
+    name = "activity"
+    verbose_name = "Activity"
 
     def ready(self):
         import activity.signals
+        import activity.spectacular_extensions  # fmt: skip
