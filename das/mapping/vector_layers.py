14f459c6ff806626c491388f429a3f36e205d3fb
JoshuaVulcan <38018017+JoshuaVulcan@users.noreply.github.com>
2025-08-05 18:17:06 +0000

    tweak:removing filter while troubleshooting permissions flow for spatial features

--- das/mapping/vector_layers.py
+++ das/mapping/vector_layers.py
@@ -11,24 +11,20 @@ from mapping.models import SpatialFeature
 
 
 class SpatialFeatureLayer(VectorLayer):
-    queryset = (
-        SpatialFeature.objects.select_related("feature_type", "feature_type__display_category")
-        .filter(feature_type__display_category__isnull=False)
-        .annotate(
-            int_id=RawSQL("hashtext(CAST(mapping_spatialfeature.id AS TEXT))", []),
-            feature_type_name=F("feature_type__name"),
-            display_category_name=F("feature_type__display_category__name"),
-            geom=Transform(Cast(F("feature_geometry"), gis_models.GeometryField()), 3857),
-            image=Case(
-                When(presentation__has_key="image", then=F("presentation__image")),
-                When(
-                    feature_type__presentation__has_key="image",
-                    then=F("feature_type__presentation__image"),
-                ),
-                default=Value(None),
-                output_field=CharField(),
+    queryset = SpatialFeature.objects.select_related("feature_type", "feature_type__display_category").annotate(
+        int_id=RawSQL("hashtext(CAST(mapping_spatialfeature.id AS TEXT))", []),
+        feature_type_name=F("feature_type__name"),
+        display_category_name=F("feature_type__display_category__name"),
+        geom=Transform(Cast(F("feature_geometry"), gis_models.GeometryField()), 3857),
+        image=Case(
+            When(presentation__has_key="image", then=F("presentation__image")),
+            When(
+                feature_type__presentation__has_key="image",
+                then=F("feature_type__presentation__image"),
             ),
-        )
+            default=Value(None),
+            output_field=CharField(),
+        ),
     )
 
     id = "spatial_features"
