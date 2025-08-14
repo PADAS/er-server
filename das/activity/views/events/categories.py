580e28a5c1a8e101dfc33cf705f663463a9c86c9
Ernesto Mancilla <emancillao@gmail.com>
2025-08-14 11:59:36 -0500

    fix: fix category move handling

--- das/activity/views/events/categories.py
+++ das/activity/views/events/categories.py
@@ -144,8 +144,6 @@ class EventCategoryRankView(GenericAPIView):
     def post(self, request, *args, **kwargs) -> Response:
         instance = self.get_object()
         before_key = request.data.get("before_key")
-        if not before_key:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
         ranked_tool = RankedTool(instance=instance, before_key=before_key)
         ranked_tool.rank()
 
