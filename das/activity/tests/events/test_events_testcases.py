44ab56360b100327ed44d2f355fae552ce8dfda5
Ernesto Mancilla <emancillao@gmail.com>
2025-09-04 10:37:50 -0500

    feat: add delete_event_relationship permission

--- das/activity/tests/events/test_events_testcases.py
+++ das/activity/tests/events/test_events_testcases.py
@@ -104,7 +104,6 @@ class TestEventView(BaseTestToolMixin, BaseAPITest):
 
     def setUp(self):
         super().setUp()
-        call_command("loaddata_with_tenant", "initial_eventdata")
         call_command("loaddata_with_tenant", "event_data_model")
         call_command("loaddata_with_tenant", "test_events_schema")
 
