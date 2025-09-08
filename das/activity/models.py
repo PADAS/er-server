f8443a5c34b14a7221f448b1124845768bb967ee
Ernesto Mancilla <emancillao@gmail.com>
2025-09-08 16:44:42 -0500

    chore: update permission description for delete_event_related_segments

--- das/activity/models.py
+++ das/activity/models.py
@@ -1219,7 +1219,7 @@ class EventRelatedSegments(TenantModelMixin, UUIDModel):
         base_manager_name = "objects"
         default_manager_name = "objects"
         permissions = [
-            ("delete_event_related_segments", "Can delete events from patrol segments"),
+            ("delete_event_related_segments", "Can remove an event from a patrol segment"),
         ]
 
 
