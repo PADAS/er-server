2aa46cdbc85c9fde5bb71d0b89dfe2790c36a1b2
Chris Doehring <chrisdoehring@gmail.com>
2025-09-29 08:54:40 -0700

    fix: short-circuit /obseravtions when no relevant source assignments exist.

--- das/observations/models.py
+++ das/observations/models.py
@@ -616,6 +616,10 @@ class ObservationQuerySet(models.QuerySet, FilterMixin):
             # Get all source assignments that overlap with our time range
             source_assignments = SubjectSource.objects.filter(subject=subject, assigned_range__overlap=time_range)
 
+            if not source_assignments:
+                # Short-circuit if there are no source assignments for the given time range.
+                return self.none()
+
             # Build a single query using Q objects to combine conditions
             from django.db.models import Q
 
