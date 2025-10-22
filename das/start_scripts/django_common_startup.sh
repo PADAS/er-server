29c7dabb8378bf49018488c9a30be8619623f3b4
Gabriel Borges <gabriel.viviani@nimblegravity.com>
2025-10-22 19:01:01 -0300

    fix: swapping python3 to python and upgrading sphinx docs build

--- das/start_scripts/django_common_startup.sh
+++ das/start_scripts/django_common_startup.sh
@@ -4,5 +4,5 @@
 # are already in place from build.
 if [ "$(echo $DEV | tr [[:upper:]] [[:lower:]])" = "true" ]; then
    echo "DEV=${DEV} so running collectstatic now"
-   uv run python3 manage.py collectstatic --no-input
+   uv run python manage.py collectstatic --no-input
 fi
