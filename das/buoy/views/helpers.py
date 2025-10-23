d4978892e2cc7459526d9fa02766e99734c4a8f9
Cácio Lucas <cclucas060901@gmail.com>
2025-10-23 16:45:04 +0000

    feat: improve and optimize filter_by_bbox method

--- das/buoy/views/helpers.py
+++ das/buoy/views/helpers.py
@@ -1,8 +1,6 @@
 from psycopg2.extras import DateTimeTZRange
 
-from django.contrib.gis.db import models
 from django.contrib.gis.geos import Polygon
-from django.db.models import Q
 
 from observations.models import LatestObservationSource, Subject, SubjectSource
 from utils.gis import calculate_bbox
@@ -32,39 +30,36 @@ def filter_by_bbox(
     :return: queryset of Subjects.
     """
     geom = Polygon.from_bbox(calculate_bbox(latitude, longitude, nautical_miles))
-    sources = LatestObservationSource.objects.filter(observation__location__within=geom)
 
-    sources = sources.exclude(
+    sources_qs = LatestObservationSource.objects.filter(observation__location__within=geom).exclude(
         source__subjectsource__subject__subject_subtype__subject_type__value=STATIONARY_SUBJECT_VALUE
     )
 
-    date_range = None
     if updated_since:
-        gt = updated_since
-        sources = sources.filter(recorded_at__gte=gt)
+        sources_qs = sources_qs.filter(recorded_at__gte=updated_since)
+
+    source_ids = list(sources_qs.values_list("source_id", flat=True))
+
+    if not source_ids:
+        return queryset.none()
+
+    subject_sources_qs = SubjectSource.objects.filter(source_id__in=source_ids)
+
+    if updated_since:
         date_range = DateTimeTZRange(lower=updated_since)
+        subject_sources_qs = subject_sources_qs.filter(assigned_range__overlap=date_range)
 
-    sources = sources.values("source").annotate(models.Count("source")).values("source")
-
-    if date_range:
-        subject_sources = SubjectSource.objects.filter(
-            source__in=sources,
-            assigned_range__overlap=date_range,
-        )
-    else:
-        subject_sources = SubjectSource.objects.filter(
-            source__in=sources,
-        )
-
-    subjects = subject_sources.values("subject")
+    subject_ids = list(subject_sources_qs.values_list("subject_id", flat=True))
 
     if include_stationary_subjects:
-        stationary_subjects = Subject.objects.filter(
-            subjectstatus__delay_hours=0,
-            is_active=True,
-            subjectsource__location__within=geom,
-            subject_subtype__subject_type__value=STATIONARY_SUBJECT_VALUE,
+        stationary_subject_ids = list(
+            Subject.objects.filter(
+                subjectstatus__delay_hours=0,
+                is_active=True,
+                subjectsource__location__within=geom,
+                subject_subtype__subject_type__value=STATIONARY_SUBJECT_VALUE,
+            ).values_list("id", flat=True)
         )
-        return queryset.values("subject").filter(Q(pk__in=subjects) | Q(pk__in=stationary_subjects))
-    else:
-        return queryset.filter(subject__in=subjects)
+        subject_ids.extend(stationary_subject_ids)
+
+    return queryset.filter(subject_id__in=subject_ids)
