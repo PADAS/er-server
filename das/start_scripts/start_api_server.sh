0836a94e1c004666a61eb3622e884f9ac389eb87
J <38018017+JoshuaVulcan@users.noreply.github.com>
2025-11-05 16:48:28 +0000

    hotfix: increased max req length + longer request timeout for API calls

--- das/start_scripts/start_api_server.sh
+++ das/start_scripts/start_api_server.sh
@@ -62,14 +62,14 @@ fi
 # Override GUNICORN_CMD_ARGS at deployment if desired.
 # Keep in mind that the flags specified below, when running gunicorn, take
 # precedence.
-GUNICORN_CMD_ARGS=${GUNICORN_CMD_ARGS:-"--workers 3 --threads 2 --worker-class gthread --max-requests 20000 --max-requests-jitter 500 --timeout 60 --graceful-timeout 40 --keep-alive 10"}
+GUNICORN_CMD_ARGS=${GUNICORN_CMD_ARGS:-"--workers 3 --threads 2 --worker-class gthread --max-requests 20000 --max-requests-jitter 500 --timeout 120 --graceful-timeout 40 --keep-alive 10"}
 export GUNICORN_CMD_ARGS
 
 echo "Notice GUNICORN_CMD_ARGS: ${GUNICORN_CMD_ARGS}"
 
 gunicorn das_server.wsgi --name das \
     --bind 0.0.0.0:8000 \
-    --limit-request-line 8190 \
+    --limit-request-line 65536 \
     --worker-tmp-dir /dev/shm \
     -c das_server/gunicorn.conf.py \
     --log-level info \
