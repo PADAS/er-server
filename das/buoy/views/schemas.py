8d14df2c44bcce1bead08f2ce81111840c0e3158
Cácio Lucas <cclucas060901@gmail.com>
2025-11-19 18:32:13 +0000

    Revert "Merge pull request #3639 from PADAS/RF-735"
    This reverts commit 1dae0be172c68aedc2142942db959c51bcedc0cb, reversing
    changes made to 832344dcc74e5abf470457ce31247d564bd56264.

--- das/buoy/views/schemas.py
+++ das/buoy/views/schemas.py
@@ -1,6 +1,3 @@
-from drf_spectacular.utils import inline_serializer
-
-from rest_framework import serializers as drf_serializers
 from rest_framework.exceptions import ValidationError
 
 from observations.views import CustomSchema
