3e8d2c9c0d20c39c10c189ea608a6a18d3e21fc3
StephenWithPH <stephenwithph@users.noreply.github.com>
2025-12-03 14:04:04 -0800

    feat[utils]: JWT validator for Auth0 tokens via Authlib and joserfc

--- das/test_scripts/unittest_settings.py
+++ das/test_scripts/unittest_settings.py
@@ -28,6 +28,9 @@ TENANT_DOCUMENT_CACHE = {
     "API_KEY": "",
 }
 
+AUTH0_DOMAIN = "auth0.com"
+AUTH0_RESOURCE_SERVER = "example.com"
+
 """
 We put test fixtures in a non-conventional place, so build a list of directories here to let Django
 know where to find them.
