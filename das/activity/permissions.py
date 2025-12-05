303d3c243020dc9e93f651d7bb58afbcc9d62770
chrisj-er <chrisj@earthranger.com>
2025-12-05 05:45:04 +0000

    ERA-12340 403 calling the V2 eventtype api when user only has create permission on that eventtype fix: the EventCategoryPermissions class didn't have support for the eventtype_value used by the v2 eventtype api, so the get fell through to the has_object permissions which fails because the user does not have read permission.

--- das/activity/permissions.py
+++ das/activity/permissions.py
@@ -65,20 +65,29 @@ class EventCategoryPermissions(IsAuthenticated):
         perms = {"POST": "create", "PATCH": "update", "PUT": "update", "GET": "read", "DELETE": "delete"}
         for k, v in perms.items():
             if request.method == k and (
-                "event_type" in request.data or "id" in view.kwargs or "eventtype_id" in view.kwargs
+                "event_type" in request.data
+                or "id" in view.kwargs
+                or "eventtype_id" in view.kwargs
+                or "eventtype_value" in view.kwargs
             ):
                 try:
                     if "event_type" in request.data:
                         event_type = EventType.objects.get_by_natural_key(request.data["event_type"])
                     elif "eventtype_id" in view.kwargs:
                         event_type = get_object_or_404(EventType, id=view.kwargs["eventtype_id"])
+                    elif "eventtype_value" in view.kwargs:
+                        event_type = EventType.objects.get_by_natural_key(view.kwargs["eventtype_value"])
                     else:
                         event_type = get_object_or_404(Event, id=view.kwargs["id"]).event_type
 
                     permission_name = "activity.{0}_{1}".format(event_type.category.value, v)
 
                     permitted = user.has_perm(permission_name)
+                    # For GET requests on EventType, also allow users with "create" permission
                     if k == "GET" and not permitted and user.is_authenticated:
+                        create_permission_name = "activity.{0}_create".format(event_type.category.value)
+                        if user.has_perm(create_permission_name):
+                            return True
                         return False
                     return permitted
                 except EventType.DoesNotExist:
@@ -107,7 +116,15 @@ class EventCategoryPermissions(IsAuthenticated):
                     is_subject = set(event_subjects) <= set(user_subjects)
 
         permission_name = permission_fmt.format(value, EventCategoryPermissions.http_method_map[request.method])
-        return request.user.has_perm(permission_name) and is_subject
+        has_perm = request.user.has_perm(permission_name)
+
+        # For GET requests on EventType objects, also allow users with "create" permission
+        # (users who can create events should be able to view the event type definition)
+        if not has_perm and request.method == "GET" and isinstance(obj, EventType):
+            create_permission_name = permission_fmt.format(value, "create")
+            has_perm = request.user.has_perm(create_permission_name)
+
+        return has_perm and is_subject
 
 
 class EventCategoryObjectPermissions(DjangoObjectPermissions):
--- das/activity/tests/test_types_v2.py
+++ das/activity/tests/test_types_v2.py
@@ -4,10 +4,12 @@ from pathlib import Path
 
 import pytest
 
+from django.contrib.auth.models import Permission
 from django.urls import reverse
 from django.utils import timezone
 from rest_framework import status
 
+from accounts.models import PermissionSet
 from activity.constants import PRI_IMPORTANT, PRI_URGENT
 from activity.models import AlertRule, Event, EventType
 from activity.serializers.event_types_v2 import EventTypeV2Serializer
@@ -15,6 +17,7 @@ from activity.tests.helpers.schema_test_utils import (
     V2SchemaBuilder,
     minimal_event_type_schema,
 )
+from utils.categories import make_eventcategory_permission_codename_with_tenant
 
 
 @pytest.mark.django_db
@@ -215,6 +218,23 @@ class TestEventTypesV2:
 
         for field in self.expected_fields:
             assert field in response.data
+
+    def test_get_event_type_detail_create_only_permission(self, user_client, cat1_cat2_event_types, tenant_settings):
+        target = cat1_cat2_event_types[0]
+        target_category = target.category
+        permission_set = PermissionSet.objects.create(name=f"test_perm_set_{target_category.value}")
+        add_permission = Permission.objects.get(
+            codename=make_eventcategory_permission_codename_with_tenant(
+                target_category.value, "create", tenant_settings.id
+            )
+        )
+        permission_set.permissions.add(add_permission)
+        user_client.user.permission_sets.add(permission_set)
+
+        url = reverse("v2-eventtype-detail", kwargs={"eventtype_value": target.value})
+        response = user_client.get(url)
+
+        assert response.status_code == status.HTTP_200_OK
 
     def test_event_type_detail_not_found(self, superuser_client):
         url = reverse("v2-eventtype-detail", kwargs={"eventtype_value": "nonexistent"})
