433a4c69ab623f0903e9b99e10097b20b2795016
StephenWithPH <stephenwithph@users.noreply.github.com>
2025-12-16 20:57:59 +0000

    fix[accounts]: fallback to AnonymousUser when Auth0 is required but not Authorization header is found

--- das/das_server/tests/test_views.py
+++ das/das_server/tests/test_views.py
@@ -4,6 +4,7 @@ import pytest
 
 from django.urls import reverse
 from rest_framework import status
+from rest_framework.test import APIClient
 
 from utils.tenant import Tenant
 
@@ -95,6 +96,20 @@ class TestStatusView:
 
         assert response.status_code == status.HTTP_200_OK
         assert response.data["services"] == []
+
+    @pytest.mark.usefixtures("tenant_settings", "das_tenant_monkeypatch")
+    def test_get_status_with_idp_required(self, monkeypatch, tenant_response, tenant_document_cache_client_mock):
+        tenant_settings = Tenant.from_dict(tenant_response)
+        tenant_settings.feature_flags.require_idp = True
+        tenant_settings.feature_flags.idp_org_id = "some-org"
+        monkeypatch.setattr("das_server.views.get_tenant_settings", MagicMock(return_value=tenant_settings))
+        monkeypatch.setattr("accounts.backends.get_tenant_settings", MagicMock(return_value=tenant_settings))
+        url = reverse("api-status")
+
+        client = APIClient()
+        response = client.get(url)
+
+        assert response.status_code == status.HTTP_200_OK
 
     def _assert_feature_flags_response_match(self, response):
         assert response.status_code == status.HTTP_200_OK
