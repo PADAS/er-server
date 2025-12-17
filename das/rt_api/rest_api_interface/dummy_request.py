0a976ee206a341acf6147bd68bac5c0412f12da9
chrisj-er <chrisj@earthranger.com>
2025-12-17 22:07:48 +0000

    fix: DummyRequest class, we weren't allowing HttpRequest populate the headers property from the META property.

--- das/rt_api/rest_api_interface/dummy_request.py
+++ das/rt_api/rest_api_interface/dummy_request.py
@@ -1,5 +1,5 @@
 # from oauthlib.common import Request
-from oauthlib.common import CaseInsensitiveDict, extract_params, to_unicode
+from oauthlib.common import extract_params, to_unicode
 
 from django.http.request import HttpRequest
 from rest_framework.request import Request
@@ -24,11 +24,20 @@ class DummyRequest(HttpRequest):
     def __init__(
         self, uri="/dummy", http_method="POST", body={}, headers={}, encoding="utf-8", user=None, query_parameters=None
     ):
+        """Initialize a DummyRequest.
+        Args:
+            uri: The URI of the request.
+            http_method: The HTTP method of the request.
+            body: The body of the request.
+            headers: The headers of the request. Should be WSGI format, e.g. {"HTTP_AUTHORIZATION": "Bearer <token>"}
+            encoding: The encoding of the request.
+            user: The user of the request.
+            query_parameters: The query parameters of the request.
+        """
         super().__init__()
         self.host = get_tenant_settings().domain
         self.uri = self.encode(uri)
         self.http_method = self.encode(http_method)
-        self.headers = CaseInsensitiveDict(self.encode(headers or {}))
         self._body = self.encode(body)
         self.decoded_body = extract_params(self.body)
 
