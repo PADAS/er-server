c43c0bc3bb7c220e610d6dc45cb717151fd70594
Chris Jones <41801350+chrisj-er@users.noreply.github.com>
2025-12-22 10:25:49 -0800

    Update version to 2.131.1 in __init__.py

--- das/das_server/__init__.py
+++ das/das_server/__init__.py
@@ -4,7 +4,7 @@ try:
 except ImportError:
     BUILD_NUMBER = 1
 
-VERSION = (2, 130, 1, "dev", BUILD_NUMBER)
+VERSION = (2, 131, 1, "dev", BUILD_NUMBER)
 
 if VERSION[3]:  # pragma: no cover
     __version__ = "{0}.{1}.{2}-{3}.{4}".format(*VERSION)
