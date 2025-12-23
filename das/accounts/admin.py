490aa75d4f986c46abfeb1e1758689bcdc3eb1f8
Chris Doehring <chrisdoehring@gmail.com>
2025-12-23 09:13:04 -0800

    Add auth0-id as readonly field in User change form.

--- das/accounts/admin.py
+++ das/accounts/admin.py
@@ -117,7 +117,7 @@ class PermissionSetAdmin(ModelAdminDisplayingManyToManyFieldMixin, DjangoGroupAd
 
 class UserAdmin(ModelAdminDisplayingManyToManyFieldMixin, DefaultFilterMixin, FieldSetElementMixin, DjangoUserAdmin):
     checks_class = CustomM2MChecks
-    readonly_fields = ("_last_login", "_profiles", "_linked_subject_warning")
+    readonly_fields = ("_last_login", "_profiles", "_linked_subject_warning", "auth0_id")
     ordering = (
         "username",
         "last_name",
@@ -136,6 +136,7 @@ class UserAdmin(ModelAdminDisplayingManyToManyFieldMixin, DefaultFilterMixin, Fi
                     "username",
                     "pin",
                     "password",
+                    "auth0_id",
                     "_last_login",
                 )
             },
