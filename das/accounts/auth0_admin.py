2ef2a13329f3a4166edb92deca4432bc443801f8
Chris Doehring <chrisdoehring@gmail.com>
2025-12-22 18:52:36 -0800

    Add conditional logout handler, for Auth0 integration

--- das/accounts/auth0_admin.py
+++ das/accounts/auth0_admin.py
@@ -8,6 +8,7 @@ is used.
 """
 
 import logging
+import urllib.parse
 
 # https://github.com/python/typeshed/issues/15145
 # noinspection PyUnresolvedReferences
@@ -16,6 +17,7 @@ from authlib.integrations.django_client import OAuth
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth import login
+from django.contrib.auth import logout as django_logout
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.urls import reverse
@@ -64,6 +66,38 @@ def admin_login_entrypoint(request):
         return redirect(f"{reverse('auth0_admin_login')}?next={next_param}&org_id={org_id}")
     else:
         return _use_default_django_admin_login(request)
+
+
+def admin_logout(request):
+    """
+    Conditional admin logout that checks the tenant's require_idp flag.
+
+    If require_idp=True, redirects to our Auth0 logout endpoint with a return to URL of the admin index page.
+    If require_idp=False, uses Django's default admin login.
+
+    This function replaces the default admin logout URL handler.
+    """
+
+    django_logout(request)
+
+    try:
+        tenant_settings = get_tenant_settings()
+        require_idp = tenant_settings.feature_flags.require_idp
+
+        if require_idp:
+            return_to_url = request.build_absolute_uri(reverse("admin:index"))
+            # Construct the Auth0 logout URL
+            auth0_logout_url = (
+                f"https://{settings.AUTH0_CUSTOM_DOMAIN}/v2/logout?"
+                f"client_id={settings.AUTH0_CLIENT_ID_FOR_DJANGO_ADMIN}&"
+                f"returnTo={urllib.parse.quote_plus(return_to_url)}"
+            )
+            return redirect(auth0_logout_url)
+
+    except Exception as e:
+        logger.error("Failed to get tenant settings in admin logout: %s", e)
+
+    return redirect(reverse("admin:index"))
 
 
 def _use_default_django_admin_login(request):
