62b60ad2c0fd32559cfd3de97acd1ceb8489a55b
chrisj-er <chrisj@earthranger.com>
2026-01-30 01:04:35 +0000

    feat: parse subject subtype of Buoy gear looking for edgetech serialnumber and using that as the subject name in the subjects API call

--- das/observations/models.py
+++ das/observations/models.py
@@ -45,6 +45,7 @@ from django.db import connections, transaction
 from django.db.models import (
     BooleanField,
     Case,
+    Exists,
     ExpressionWrapper,
     F,
     FilteredRelation,
@@ -973,7 +974,7 @@ class Observation(TenantModelMixin, models.Model):
     EXCLUDED_AUTOMATICALLY = 2
 
     EXCLUDED_AUTOMATICALLY_TIME_DELTA = timedelta(days=7)
-    
+
     # Bit masks for partitioning exclusion flags (64-bit BigIntegerField)
     # Lower 48 bits reserved for system use (bits 0-47)
     SYSTEM_FLAGS_MASK = 0x0000FFFFFFFFFFFF  # Lower 48 bits: 0-47
@@ -1575,16 +1576,26 @@ class SubjectQuerySet(models.QuerySet, FilterMixin):
         return self.none()
 
     def annotate_with_subjectsource_transforms(self):
-        latest_subjectsource = SubjectSource.objects.filter(subject=OuterRef("pk")).order_by("-assigned_range")
+        """
+        Annotates the queryset with the location and transforms from the most current subjectsource.
+        Uses a subquery to get the latest subjectsource record for each subject.
 
-        return self.annotate(
-            source_transforms=Subquery(latest_subjectsource.values("source__provider__transforms")[:1])
+        Returns:
+            QuerySet: Annotated with latest_subjectsource_location, latest_subjectsource_transforms,
+                      latest_subjectsource_exists, and latest_source_manufacturer_id
+        """
+        # Get the latest subjectsource for each subject with both location and transforms
+        latest_subjectsource = (
+            SubjectSource.objects.filter(subject=OuterRef("pk"))
+            .order_by("-assigned_range")
+            .values("location", "source__provider__transforms", "source__manufacturer_id")[:1]
         )
 
-    def annotate_with_subjectsource(self):
         return self.annotate(
-            s2=FilteredRelation("subjectsource", condition=Q(subjectsource__isnull=False)),
-            subjectsource_location=F("s2__location"),
+            latest_subjectsource_location=Subquery(latest_subjectsource.values("location")),
+            latest_subjectsource_transforms=Subquery(latest_subjectsource.values("source__provider__transforms")),
+            latest_subjectsource_exists=Exists(latest_subjectsource),
+            latest_source_manufacturer_id=Subquery(latest_subjectsource.values("source__manufacturer_id")),
         )
 
     def annotate_with_subjectsource(self, use_lkl=False, use_bbox=False):
