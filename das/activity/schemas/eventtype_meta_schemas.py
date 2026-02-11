4d78b706ef022eb3f8f97232b13b9fb09197d8e4
Luis Alfonso Chávez Abbadie <luixlive@gmail.com>
2026-02-11 09:42:13 -0600

    ERA-12580: Update eventtype meta-schema with the new condition operator names.

--- das/activity/schemas/eventtype_meta_schemas.py
+++ das/activity/schemas/eventtype_meta_schemas.py
@@ -521,12 +521,12 @@ ui_sections_schema = {
                     "id": {"type": "string", "pattern": "^condition-.*"},
                     "operator": {
                         "enum": [
-                            "INPUT_IS_EXACTLY",
-                            "HAS_INPUT",
-                            "DOES_NOT_HAVE_INPUT",
                             "CONTAINS",
                             "IS_CONTAINED_BY",
+                            "IS_EMPTY",
+                            "IS_EXACTLY",
                             "IS_NOT_CONTAINED_BY"
+                            "IS_NOT_EMPTY",
                         ]
                     },
                     "value": {
@@ -633,9 +633,9 @@ contains_condition_schema = {
     "additionalProperties": False,
 }
 
-does_not_have_input_condition_schema = {
+is_empty_condition_schema = {
     "type": "object",
-    "title": "Does Not Have Input condition schema for EventType Builder",
+    "title": "Is Empty condition schema for EventType Builder",
     "properties": {
         "anyOf": {
             "type": "array",
@@ -751,9 +751,9 @@ does_not_have_input_condition_schema = {
     "additionalProperties": False,
 }
 
-has_input_condition_schema = {
+is_not_empty_condition_schema = {
     "type": "object",
-    "title": "Has Input condition schema for EventType Builder",
+    "title": "Is Not Empty condition schema for EventType Builder",
     "properties": {
         "properties": {
             "type": "object",
@@ -851,9 +851,9 @@ has_input_condition_schema = {
     "additionalProperties": False,
 }
 
-input_is_exactly_condition_schema = {
+is_exactly_condition_schema = {
     "type": "object",
-    "title": "Input Is Exactly condition schema for EventType Builder",
+    "title": "Is Exactly condition schema for EventType Builder",
     "properties": {
         "properties": {
             "type": "object",
@@ -1236,11 +1236,11 @@ json_field_schema = {
                                 "items": {
                                     "anyOf": [
                                         {"$ref": "#/$defs/containsCondition"},
-                                        {"$ref": "#/$defs/doesNotHaveInputCondition"},
-                                        {"$ref": "#/$defs/hasInputCondition"},
-                                        {"$ref": "#/$defs/inputIsExactlyCondition"},
                                         {"$ref": "#/$defs/isContainedByCondition"},
+                                        {"$ref": "#/$defs/isEmptyCondition"},
+                                        {"$ref": "#/$defs/isExactlyCondition"},
                                         {"$ref": "#/$defs/isNotContainedByCondition"},
+                                        {"$ref": "#/$defs/isNotEmptyCondition"},
                                     ]
                                 },
                             }
@@ -1354,10 +1354,10 @@ main_event_type_schema = {
         "uiHeadersSchema": ui_headers_schema,
         "uiSectionsSchema": ui_sections_schema,
         "containsCondition": contains_condition_schema,
-        "doesNotHaveInputCondition": does_not_have_input_condition_schema,
-        "hasInputCondition": has_input_condition_schema,
-        "inputIsExactlyCondition": input_is_exactly_condition_schema,
         "isContainedByCondition": is_contained_by_condition_schema,
+        "isEmptyCondition": is_empty_condition_schema,
+        "isExactlyCondition": is_exactly_condition_schema,
         "isNotContainedByCondition": is_not_contained_by_condition_schema,
+        "isNotEmptyCondition": is_not_empty_condition_schema,
     },
 }
