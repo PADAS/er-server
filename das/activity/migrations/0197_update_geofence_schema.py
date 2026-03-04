4ea7e2dd6c7e92fba33df22f3a9141aa158918cc
jeslefcourt <32883811+jeslefcourt@users.noreply.github.com>
2026-03-04 18:40:57 +0000

    Updated descriptive comments

--- das/activity/migrations/0197_update_geofence_schema.py
+++ das/activity/migrations/0197_update_geofence_schema.py
@@ -1,3 +1,7 @@
+# Empty migration to replace a migration that updated some analyzer event type schemas, but was causing problems.
+# Decided instead to have new event types include the updates but not change existing ones.  Leaving this empty
+# migration here for compatability with any Django databases that already applied this update.
+
 from django.db import migrations
 
 logger = logging.getLogger(__name__)
